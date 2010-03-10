9833b562e212c8f6cdfe98747a42e2c757288d26
Nick Shectman <shectman@voltdb.com>
2010-03-10 20:46:39 +0000

    Check all 12 cluster machines before running tests.
    This change is already in the places it needs to be, but eventually we'll auto update those places...

--- tools/check-cluster.sh
+++ tools/check-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `echo 3a 3b 3c 3d 3e 4a 4b 4c`; do
+for i in `echo 3a 3b 3c 3d 3e 3f 3g 3h 3i 3j 3k 3l 4a 4b 4c`; do
     echo $i
     ssh volt$i ps -ef | grep java && exit
 done
