d6c0ad1c6f8a30e3e2e0272ea318f67d841a2abc
Nick Shectman <shectman@voltdb.com>
2010-03-11 15:08:27 +0000

    Allow cluster machines to be running a Hudson slave process.
    Hudson slaves don't interfere with a server from a ports point of view, and
    practically speaking anything they're doing that interferes from a CPU point
    of view will probably still turn up in the check-cluster script.

--- tools/check-cluster.sh
+++ tools/check-cluster.sh
@@ -2,6 +2,6 @@
 
 for i in `echo 3a 3b 3c 3d 3e 3f 3g 3h 3i 3j 3k 3l 4a 4b 4c`; do
     echo $i
-    ssh volt$i ps -ef | grep java && exit
+    ssh volt$i ps -ef | grep java | grep -v slave.jar && exit
 done
 
