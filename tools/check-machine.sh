42d017169f5522380ecf710d7c5812745f9870bb
Nick Shectman <shectman@voltdb.com>
2010-04-21 17:19:20 +0000

    Tool to check for proper cluster machine configuration. Checks for puppet process running. Checks OS against labels in Hudson config. Must be run as test user for access to Hudson info.

---
+++ tools/check-machine.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+EXIT=0
+if ! ps -ef | grep pup*et; then
+  echo "puppet is not running on $HOSTNAME"
+  EXIT=1
+fi
+
+# what kind of machine does hudson think we are?
+LABEL=`ssh newbobbi grep -A7 host.$HOSTNAME .hudson/config.xml | grep label`
+if echo $LABEL | grep ubuntu; then OS=Ubuntu; fi
+if echo $LABEL | grep CentOS; then OS=el5; fi
+
+if ! uname -a | grep $OS; then
+  echo "wrong operating system running on $HOSTNAME"
+  EXIT="1$EXIT" # there might not be a calculator installed
+fi
+
+exit $EXIT
