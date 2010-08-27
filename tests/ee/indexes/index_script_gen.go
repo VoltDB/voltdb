f8cbd95c0d949990733475dc4f48d31e46d5fbd5
Ryan Betts <rbetts@voltdb.com>
2010-08-27 00:25:59 +0000

    Make sure all unique index test inputs are unique. Also test for insert failures on intentional duplicate inserts.

--- tests/ee/indexes/index_script_gen.go
+++ tests/ee/indexes/index_script_gen.go
@@ -1,120 +1,218 @@
 /*
- * Generate input test scripts to verify index behaviour.
- *
- * Unique / Non-unique
- * Ints-only / Not Ints-only
- * Array, Tree, Hash implementations
- * Key sizes
- */
+ Generate input test scripts to verify index behaviour.
+
+ Current functionality:
+ - random width (1 to maxIdxColumn), random type schemas
+ - prng values for all columns
+ - "is", "ls", "if" commands on unique indexes
+
+ todo: The abbrev (int, bint, sint...) handling is weird and
+ repeated in the voltIdxTypes table as well as the Abbrev()
+ functions. Oddly, NValues don't store any value, they only
+ exist as types. Probably they should store their string
+ representations.
+*/
 package main
 
 import (
-    "fmt"
-    "rand"
-    "container/list"
-    "strings"
+	"fmt"
+	"rand"
+	"strings"
 )
 
-// Types known to test tool that are valid index column types
-var voltIdxTypes = [...]string{
-    "int",
-    "bint",
-    "sint",
-    "tint",
-    "dec",
-    "str4",
-    "str128"}
+var (
+	// Types known to test tool that are valid index column types
+	voltIdxTypes = [...]string{
+		"int",
+		"bint",
+		"sint",
+		"tint",
+		"dec",
+		"str4",
+		"str128"}
 
-// Maximum number of indexed columns
-var maxIdxColumns = 3
+	// Maximum number of indexed columns
+	maxIdxColumns = 50
+)
 
-func createSchema() []string {
-    schema := make([]string, (rand.Int() % maxIdxColumns) + 1)
-    for ii := 0; ii < len(schema); ii++ {
-        schema[ii] = voltIdxTypes[rand.Int() % len(voltIdxTypes)]
-    }
-    return schema
+
+// Interface all values implement
+type NValue interface {
+	Create() string
+	Abbrev() string
 }
 
-func createTuple(schema []string) []string {
-    tuple := make([]string, len(schema))
-    for ii := 0; ii < len(schema); ii++ {
-        switch schema[ii] {
-            case "int": tuple[ii] = createIntVal()
-            case "bint" : tuple[ii] = createBintVal()
-            case "sint" : tuple[ii] = createSintVal()
-            case "tint" : tuple[ii] = createTintVal()
-            case "dec" : tuple[ii] = createDecimalVal()
-            case "str4" : tuple[ii] = createStringVal(4)
-            case "str128" : tuple[ii] = createStringVal(128)
-            default: panic(schema[ii])
-        }
-    }
-    return tuple
+// INTEGER
+type IntValue struct{}
+
+func (nv IntValue) Create() string {
+	val := rand.Int() * sign()
+	return fmt.Sprint(val)
 }
 
-func sign() int {
-    sign := rand.Int() % 2
-    if sign == 0 {
-        return 1
-    }
-    return -1
+func (nv IntValue) Abbrev() string {
+	return "int"
 }
 
-func createIntVal() string {
-    val := rand.Int() * sign()
-    return fmt.Sprint(val)
+// BIGINT
+type BintValue struct{}
+
+func (nv BintValue) Create() string {
+	val := rand.Int63()
+	val = val * int64(sign())
+	return fmt.Sprint(val)
 }
 
-func createBintVal() string {
-    val := rand.Int63()
-    val = val * int64(sign())
-    return fmt.Sprint(val)
+func (nv BintValue) Abbrev() string {
+	return "bint"
 }
 
-func createSintVal() string {
-    val := rand.Intn(0xFFFF) * sign()
-    return fmt.Sprint(val)
+// SMALLINT
+type SintValue struct{}
+
+func (nv SintValue) Create() string {
+	val := rand.Intn(0xFFFF) * sign()
+	return fmt.Sprint(val)
 }
 
-func createTintVal() string {
-    val := rand.Intn(0xFF) * sign()
-    return fmt.Sprint(val)
+func (nv SintValue) Abbrev() string {
+	return "sint"
 }
 
-func createDecimalVal() string {
-    vals := make([]string, 3)
-    vals[0] = createBintVal()
-    vals[1] = "."
-    vals[2] = fmt.Sprint(rand.Int())
-    return strings.Join(vals,"")
+// TINYINT
+type TintValue struct{}
+
+func (nv TintValue) Create() string {
+	val := rand.Intn(0xFF) * sign()
+	return fmt.Sprint(val)
 }
 
-func createStringVal(size int) string {
-	if size == 128 {
-		substrs := [...]string{"ning","izzy","ariel","nick",
-			"mazur","ryan","hugg","yankeesfan","volt","runs",
-			"with","scissors","blue","awesome","weak","sauce",
-			"chicken","strength","vikram","bobbi","jarr","bungee",
-			"banjo","arrow","trinity","coffee","pvc"}
+func (nv TintValue) Abbrev() string {
+	return "tint"
+}
+
+// DECIMAL
+type DecValue struct{}
+
+func (nv DecValue) Create() string {
+	vals := make([]string, 3)
+	lhs := rand.Int63()
+	lhs = lhs * int64(sign())
+	vals[0] = fmt.Sprint(lhs)
+	vals[1] = "."
+	vals[2] = fmt.Sprint(rand.Int())
+	return strings.Join(vals, "")
+}
+
+func (nv DecValue) Abbrev() string {
+	return "dec"
+}
+
+// VARCHAR(4|128)
+type StrValue struct {
+	size int
+}
+
+func (nv StrValue) Create() string {
+	if nv.size == 128 {
+		substrs := [...]string{"ning", "izzy", "ariel", "nick",
+			"mazur", "ryan", "hugg", "yankeesfan", "volt", "runs",
+			"with", "scissors", "blue", "awesome", "weak", "sauce",
+			"chicken", "strength", "vikram", "bobbi", "jarr", "bungee",
+			"banjo", "arrow", "trinity", "coffee", "pvc"}
 		cnt := (rand.Int() % 10) + 1
 		vals := make([]string, cnt)
 
 		for i := 0; i < cnt; i++ {
-			vals[i] = substrs[rand.Int() % len(substrs)]
+			vals[i] = substrs[rand.Int()%len(substrs)]
 		}
-		return strings.Join(vals,"")
-	} else if size == 4 {
-		substrs := [...]string{"a","b","c","d"}
+		return strings.Join(vals, "")
+	} else if nv.size == 4 {
+		substrs := [...]string{"a", "b", "c", "d"}
 		cnt := 4
 		vals := make([]string, cnt)
 
 		for i := 0; i < cnt; i++ {
-			vals[i] = substrs[rand.Int() % len(substrs)]
+			vals[i] = substrs[rand.Int()%len(substrs)]
 		}
-		return strings.Join(vals,"")
+		return strings.Join(vals, "")
 	}
 	panic("Invalid string size.")
+}
+
+func (nv StrValue) Abbrev() string {
+	return fmt.Sprintf("%s%d", "str", nv.size)
+}
+
+// Utility Functions
+
+func nvalueFactory(abbrev string) NValue {
+	switch abbrev {
+	case "int":
+		return &IntValue{}
+	case "bint":
+		return &BintValue{}
+	case "sint":
+		return &SintValue{}
+	case "tint":
+		return &TintValue{}
+	case "dec":
+		return &DecValue{}
+	case "str4":
+		return &StrValue{4}
+	case "str128":
+		return &StrValue{128}
+	}
+	panic(abbrev)
+}
+
+func printSliceAsList(slice []string) {
+	for i := 0; i < len(slice); i++ {
+		fmt.Printf(slice[i])
+		if i == len(slice)-1 {
+			fmt.Printf("\n")
+		} else {
+			fmt.Printf(",")
+		}
+	}
+}
+
+func createSchema() ([]NValue, []string) {
+	schema := make([]NValue, (rand.Int()%maxIdxColumns)+1)
+	abbrev := make([]string, len(schema))
+	for ii := 0; ii < len(schema); ii++ {
+		schemaType := voltIdxTypes[rand.Int()%len(voltIdxTypes)]
+		schema[ii] = nvalueFactory(schemaType)
+		abbrev[ii] = schemaType
+	}
+	return schema, abbrev
+}
+
+func createTuple(schema []NValue) []string {
+	tuple := make([]string, len(schema))
+	for ii := 0; ii < len(schema); ii++ {
+		tuple[ii] = schema[ii].Create()
+	}
+	return tuple
+}
+
+// Create a reasonable serialization given a schema a tuple
+func tupleKey(schema []NValue, tuple []string) string {
+	parts := make([]string, len(schema) * 2)
+	pi := 0
+	for si := 0; si < len(schema); si++ {
+		parts[pi], parts[pi+1] = schema[si].Abbrev(), tuple[si]
+		pi = pi + 2
+	}
+	return strings.Join(parts, ":")
+}
+
+func sign() int {
+	sign := rand.Int() % 2
+	if sign == 0 {
+		return 1
+	}
+	return -1
 }
 
 /*
@@ -130,47 +228,51 @@ func createStringVal(size int) string {
 
 
 func generateUniqueGenericTree(testrun int) {
-    schema := createSchema()
-    tuples := list.New()
+	// map where the keys are strings and the values are slices of strings
+	tuples := make(map[string][]string)
+	schema, abbrev := createSchema()
 
-    // print the test introduction
-    fmt.Printf("begin TestUniqueGenericTree_%d UniqueGenericTree ", testrun)
-    printSliceAsList(schema)
+	// print the test introduction
+	fmt.Printf("begin TestUniqueGenericTree_%d UniqueGenericTree ", testrun)
+	printSliceAsList(abbrev)
 
-    // print the test commands
-    for cmd := 0; cmd < 10; cmd++ {
-        tuple := createTuple(schema)
-        tuples.PushBack(tuple)
-        fmt.Printf("is ")
-        printSliceAsList(tuple)
-    }
+	// create tuples. push them into a map to uniqify them.
+	// (this is a unique index test)
+	for cmd := 0; cmd < 10; cmd++ {
+		tuple := createTuple(schema)
+		tuplekey := tupleKey(schema, tuple)
+		tuples[tuplekey] = tuple
+	}
 
-    // print the verification commands
-    for e := range tuples.Iter() {
-        fmt.Printf("ls ")
-        printSliceAsList(e.([]string))
-    }
+	// is commands
+	for _, v := range tuples {
+		fmt.Printf("is ")
+		printSliceAsList(v)
+	}
 
-    fmt.Println("exec")
+	// ls commands
+	for _, v := range tuples {
+		fmt.Printf("ls ")
+		printSliceAsList(v)
+	}
+
+	// if commands (reinserting existing keys should fail)
+	for _, v := range tuples {
+		fmt.Printf("if ")
+		printSliceAsList(v)
+	}
+
+	// print the test conclusion
+	fmt.Println("exec")
 }
 
-func printSliceAsList(slice []string) {
-    for i := 0; i < len(slice); i++ {
-        fmt.Printf(slice[i])
-        if (i == len(slice) - 1) {
-            fmt.Printf("\n")
-        } else {
-            fmt.Printf(",")
-        }
-    }
-}
 
 func main() {
-    fmt.Printf("# File generated by index_script_gen.go\n")
+	fmt.Printf("# File generated by index_script_gen.go\n")
 	var i int = 0
-    for {
-        generateUniqueGenericTree(i)
+	for {
+		generateUniqueGenericTree(i)
 		i++
-    }
-    fmt.Println("done")
+	}
+	fmt.Println("done")
 }
