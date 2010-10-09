c5b646d3f0ee482dab3d43f349ce7c7b1265d954
Nick Shectman <shectman@voltdb.com>
2010-10-09 00:17:19 +0000

    Dumb typo.

--- tools/twitter.sh
+++ tools/twitter.sh
@@ -1,7 +1,7 @@
 TOOLSDIR=`readlink -f $(dirname "${BASH_SOURCE[0]}")`
 TRUNKDIR=`dirname ${TOOLSDIR}`
 cd $TRUNKDIR
-ln -sf obj/release/prod/voltdb .
+ln -sf obj/release/prod voltdb
 cd examples/twitter
 ant &
 sleep 60
