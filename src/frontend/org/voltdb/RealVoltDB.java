325555153546ba347e9f39530d989ca55ab2c633
John Hugg <jhugg@voltdb.com>
2010-10-20 15:49:17 +0000

    Bump the revision number on the trunk to 1.3.trunk. Tested "ant distcheck". Cool that this works.

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -244,7 +244,7 @@ public class RealVoltDB implements VoltDBInterface
     public VoltDB.Configuration m_config = new VoltDB.Configuration();
     private CatalogContext m_catalogContext;
     private String m_buildString;
-    private static final String m_defaultVersionString = "1.2.01";
+    private static final String m_defaultVersionString = "1.3.trunk";
     private String m_versionString = m_defaultVersionString;
     // fields accessed via the singleton
     private HostMessenger m_messenger = null;
