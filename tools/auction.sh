41c0cadbf538c7a9efd78f0462adad1382c05d8d
Nick Shectman <shectman@voltdb.com>
2010-10-22 19:05:24 +0000

    First pass example expect helper scripts. Allows running all examples in a single shell. Performs basic sanity-checking on output. Expected version number is hardcoded for now.

---
+++ tools/auction.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# run this after running auction.exp, to check the export output files.
+! grep -v '^[0-9]*,[0-9]*,[0-9]*,0,1,1,[0-9]*,[0-9],-*[0-9]*,[0-9]*\.[0-9]*\.[0-9]* [012][0-9]:[0-5][0-9]:[0-5][0-9]:[0-9]*:0,[0-9]*.[0-9]*$' EXPORTDEMO-BID.csv
