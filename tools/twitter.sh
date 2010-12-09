35823f0396066bd3d44dcfd62b1d913352ad564c
Ryan Betts <rbetts@voltdb.com>
2010-12-09 20:22:02 +0000

    Change success factor of twitter test script.
    Exit 0 if at least one hashtag-count exists in
    the running web page; otherwise, return -1.
    
    Script previously returned result of a kill cleanup
    command, which was sometimes not 0.
    
    Will this satisfy hudson's whimsical tyranny?

--- tools/twitter.sh
+++ tools/twitter.sh
@@ -10,7 +10,9 @@ ant client -DTwitterUsername=$TUNAME -DTwitterPassword=$TUPASS &
 sleep 20
 ant cull &
 sleep 180
-links -source http://localhost:12345
+OUTPUT=$(links -source http://localhost:12345 | grep hashtag-count | wc -l)
 # links leaves a line unterminated
 echo
 ps -ef | grep ant.jar | awk " { print \$2 } " | xargs kill -9
+test $OUTPUT -gt 0 && exit 0 || exit 1
+
