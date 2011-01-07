7aed4c356c7fce842d8b30f4b30fc94f86c61f53
Nick Shectman <shectman@voltdb.com>
2011-01-07 17:30:27 +0000

    Let Hudson keep track of OS types itself instead of doing it manually and needing a check that it's right. Clean up after temp files.

--- tools/check-machine.sh
+++ tools/check-machine.sh
@@ -11,16 +11,6 @@ if ! ps -ef | grep pup*et; then
   fi
 fi
 
-# check whether hudson has the right idea what kind of machine we are
-LABEL=`ssh newbobbi grep -A7 host.$HOSTNAME .hudson/config.xml | grep label`
-if echo $LABEL | grep ubuntu; then OS=Ubuntu; fi
-if echo $LABEL | grep CentOS; then OS=el5; fi
-if echo $LABEL | grep RedHat; then OS=el6; fi
-if echo $LABEL | grep fedora; then OS=fc10; fi
-
-if ! uname -a | grep $OS; then
-  echo "wrong operating system running on $HOSTNAME"
-  EXIT="1$EXIT" # there might not be a calculator installed
-fi
+cd /tmp && ls -tr | grep volt_snapshot | head -n-10 | xargs rm
 
 exit $EXIT
