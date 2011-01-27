0fb178ffcf3f2741f32263bbd9e6e1c5750d1fa9
Nick Shectman <shectman@voltdb.com>
2011-01-27 22:03:44 +0000

    Don't upload the symbols file.

--- tools/build-kit.sh
+++ tools/build-kit.sh
@@ -1,8 +1,7 @@
-mkdir -p ~/releases/`cat eng/version.txt`/other
+mkdir -p ~/releases/`cat eng/version.txt`
 cd eng
 svn status
 ant clean default dist voltbin
-cp obj/release/voltdb-`cat version.txt`.sym ~/releases/`cat version.txt`/other/
 cp obj/release/voltdb-`cat version.txt`.tar.gz ~/releases/`cat version.txt`/LINUX-voltdb-`cat version.txt`.tar.gz
 cd ../pro
 svn status
@@ -13,5 +12,9 @@ cd ../doc
 mv pdfs/enterprise_releasenotes.pdf ~/releases/`cat ../eng/version.txt`/`cat ../eng/version.txt`-enterprise_releasenotes.pdf
 zip ~/releases/`cat ../eng/version.txt`/`cat ../eng/version.txt`-docs_pdf.zip pdfs/*.pdf
 cp ~/releases/`cat ../eng/version.txt`/`cat ../eng/version.txt`-enterprise_releasenotes.pdf pdfs/enterprise_releasenotes.pdf
-cksum ~/releases/`cat ../eng/version.txt`/*.* > ~/releases/`cat ../eng/version.txt`/checksums.txt
-scp -r ~/releases/`cat ../eng/version.txt` root@community.voltdb.com:/var/www/drupal/sites/default/files/archive
+cd ../eng
+cksum ~/releases/`cat version.txt`/*.* > ~/releases/`cat version.txt`/checksums.txt
+scp -r ~/releases/`cat version.txt` root@community.voltdb.com:/var/www/drupal/sites/default/files/archive
+mkdir -p ~/releases/`cat version.txt`/other
+cp obj/release/voltdb-`cat version.txt`.sym ~/releases/`cat version.txt`/other/
+cd ..
