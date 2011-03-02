9201ed5f0ac0d704ea49ad799c902ffcc48d8628
John Hugg <jhugg@voltdb.com>
2011-03-02 22:23:28 +0000

    Adding a small reproducer for ENG-1016. This may not live in here forever.

---
+++ tests/frontend/org/voltdb/eng1016/Runner.java
@@ -0,0 +1,83 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2011 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.voltdb.eng1016;
+
+import junit.framework.TestCase;
+
+import org.voltdb.BackendTarget;
+import org.voltdb.ServerThread;
+import org.voltdb.VoltDB.Configuration;
+import org.voltdb.VoltTable;
+import org.voltdb.client.ClientFactory;
+import org.voltdb.compiler.VoltProjectBuilder;
+
+public class Runner extends TestCase {
+
+    public static void main(String[] args) throws Exception {
+        // compile the catalog
+        VoltProjectBuilder project = new VoltProjectBuilder();
+        project.addLiteralSchema("create table items (id bigint not null, created bigint not null, primary key (id));");
+        project.addLiteralSchema("create index idx_item_tree on items (created, id);");
+        project.addProcedures(GetItems.class);
+        project.addStmtProcedure("CreateItem", "insert into items (id, created) values (?,?);");
+        project.addPartitionInfo("items", "id");
+        boolean success = project.compile(Configuration.getPathToCatalogForTest("poc.jar"));
+        if (!success) {
+            System.err.println("Failure to compile catalog.");
+            System.exit(-1);
+        }
+        String pathToDeployment = project.getPathToDeployment();
+
+        // start up voltdb
+        ServerThread server = new ServerThread(Configuration.getPathToCatalogForTest("poc.jar"), pathToDeployment, BackendTarget.NATIVE_EE_JNI);
+        server.start();
+        server.waitForInitialization();
+
+        final org.voltdb.client.Client voltclient = ClientFactory.createClient();
+        voltclient.createConnection("localhost");
+
+        // create initial items
+        long numItems = 20;
+        for (long questionid = 0; questionid < numItems; questionid++) {
+            voltclient.callProcedure("CreateItem", questionid, System.currentTimeMillis());
+        }
+        System.out.printf("Created %,d items\n",numItems);
+
+        // check that the query does the right thing
+        VoltTable result;
+        result = voltclient.callProcedure("GetItems",0,System.currentTimeMillis(),20,1).getResults()[0];
+        if (result.asScalarLong() != 20) {
+            System.err.printf("First call failed with %d rows\n", result.asScalarLong());
+        }
+        result = voltclient.callProcedure("GetItems",0,System.currentTimeMillis(),10,2).getResults()[0];
+        if (result.asScalarLong() != 20) {
+            System.err.printf("REPRODUCED with %d rows\n", result.asScalarLong());
+        }
+
+        // clean up / shutdown
+        voltclient.close();
+        server.shutdown();
+        server.join();
+    }
+}
