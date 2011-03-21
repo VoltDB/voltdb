b00136b6cf124cc7c16047a580612903fb153b8c
Seb Coursol <sebc@voltdb.com>
2011-03-21 21:42:57 +0000

    Generic QA validation client providing support for various types of workload simulations.

---
+++ tests/test_apps/genqa/daemon.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+usage="Usage: daemon.sh (start|stop) [rejoinhostname]"
+if [ $# = 0 ]; then
+ echo $usage
+ exit 1
+fi
+
+if [ "$VOLTDB_PID_DIR" = "" ]; then
+  VOLTDB_PID_DIR=/tmp
+fi
+
+if [ "$VOLTDB_IDENT_STRING" = "" ]; then
+  export VOLTDB_IDENT_STRING="$USER"
+fi
+
+# Set default scheduling priority
+if [ "$VOLTDB_NICENESS" = "" ]; then
+    export VOLTDB_NICENESS=5
+fi
+
+startStop=$1
+pid=$VOLTDB_PID_DIR/voltdb-$VOLTDB_IDENT_STRING.pid
+
+log=/home/sebc/svndev/eng/tests/test_apps/genqa/daemon.log
+command="java -server -Xmn1536m -Xmx2048m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -XX:-ReduceInitialCardMarks -Dvolt.rmi.agent.port=9090 -Dvolt.rmi.server.hostname=$HOSTNAME -Djava.library.path=../../../obj/release/dist/voltdb -Dlog4j.configuration=/home/sebc/svndev/eng/obj/release/dist/voltdb/log4j.properties -cp .:../../../obj/release/dist/voltdb/* org.voltdb.VoltDB port 21212 internalport 3021 catalog genqa.jar deployment /home/sebc/svndev/eng/tests/test_apps/genqa/deployment.xml"
+case $startStop in
+
+  (start)
+
+    mkdir -p "/tmp/genqa"
+
+    if [ -f $pid ]; then
+      if kill -0 `cat $pid` > /dev/null 2>&1; then
+        echo Already running as process `cat $pid`.  Stop it first.
+        exit 1
+      fi
+    fi
+    
+    if [ "$2" == "" ]; then
+        nohup nice -n $VOLTDB_NICENESS $command &> "$log" < /dev/null &
+    else
+        nohup nice -n $VOLTDB_NICENESS $command rejoinhost $2 &> "$log" < /dev/null &
+    fi
+    echo $! > $pid
+    sleep 1; head -n 100 "$log"
+    ;;
+
+  (stop)
+    
+    if [ -f $pid ]; then
+      if kill -0 `cat $pid` > /dev/null 2>&1; then
+        echo stopping
+        kill `cat $pid`
+      else
+        echo no process to stop
+      fi
+    else
+      echo no process to stop
+    fi
+    ;;
+
+  (*)
+    echo $usage
+    exit 1
+    ;;
+
+esac
---
+++ tests/test_apps/genqa/src/com/procedures/JiggleExportSinglePartition.java
@@ -0,0 +1,79 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2011 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.procedures;
+
+import java.util.Random;
+
+import org.voltdb.ProcInfo;
+import org.voltdb.SQLStmt;
+import org.voltdb.VoltProcedure;
+import org.voltdb.VoltTable;
+
+@ProcInfo(
+    partitionInfo = "export_partitioned_table.rowid:0",
+    singlePartition = true
+)
+
+public class JiggleExportSinglePartition extends VoltProcedure {
+    public final SQLStmt insert = new SQLStmt("INSERT INTO export_partitioned_table (rowid, rowid_group, type_null_tinyint, type_not_null_tinyint, type_null_smallint, type_not_null_smallint, type_null_integer, type_not_null_integer, type_null_bigint, type_not_null_bigint, type_null_timestamp, type_not_null_timestamp, type_null_float, type_not_null_float, type_null_decimal, type_not_null_decimal, type_null_varchar25, type_not_null_varchar25, type_null_varchar128, type_not_null_varchar128, type_null_varchar1024, type_not_null_varchar1024) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+    public VoltTable[] run(long rowid)
+    {
+        // Critical for proper determinism: get a cluster-wide consistent Random instance
+        Random rand = getSeededRandomNumberGenerator();
+
+        // Insert a new record
+        SampleRecord record = new SampleRecord(rowid, rand);
+        voltQueueSQL(
+                      insert
+                    , rowid
+                    , record.rowid_group
+                    , record.type_null_tinyint
+                    , record.type_not_null_tinyint
+                    , record.type_null_smallint
+                    , record.type_not_null_smallint
+                    , record.type_null_integer
+                    , record.type_not_null_integer
+                    , record.type_null_bigint
+                    , record.type_not_null_bigint
+                    , record.type_null_timestamp
+                    , record.type_not_null_timestamp
+                    , record.type_null_float
+                    , record.type_not_null_float
+                    , record.type_null_decimal
+                    , record.type_not_null_decimal
+                    , record.type_null_varchar25
+                    , record.type_not_null_varchar25
+                    , record.type_null_varchar128
+                    , record.type_not_null_varchar128
+                    , record.type_null_varchar1024
+                    , record.type_not_null_varchar1024
+                    );
+
+        // Execute last statement batch
+        voltExecuteSQL(true);
+
+        // Retun to caller
+        return null;
+    }
+}
