74a040f4918418c084645bf79dbc62e2bdd3cd40
Nick Shectman <shectman@voltdb.com>
2011-03-29 00:25:24 +0000

    More c++ client library tests.

--- tools/cppclients.sh
+++ tools/cppclients.sh
@@ -3,12 +3,17 @@
 # get the directory containing this script
 TOOLSDIR=`readlink -f $(dirname "${BASH_SOURCE[0]}")`
 
+# there's embedded code in the readme.
+# try to compile it; readmes should be correct.
 grep -A1000 \/\\\* README > clientvoter.cpp
+# there's no makefile, but there is a c++ command in the readme.  Run it.
 `grep -A5 g++ README | sed 'N;N;s/[\n\\]//g' | sed 's/voltdb.[ul][si][rb].//'`
-${TOOLSDIR}/cppclients.exp
 
 cd AsyncHelloWorld/
 make clean all
 
 cd ../HelloWorld/
 make clean all
+
+cd ..
+${TOOLSDIR}/cppclients.exp
