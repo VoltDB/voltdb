9b430737907a2dba00e542fcb61e56673fe04eee
Lisa Geisler <lgeisler@voltdb.com>
2011-04-21 15:23:27 +0000

    New tools for daily QA build on volt3's. Seb and Nicks scripts

---
+++ tools/build.pro.cos.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -e
+
+# Cleanup previous log
+rm -f ~/.svncos.build.log
+
+
+./build.pro.sh $1; &> ~/.svncos.build.log
+
+
+# Validate no-single-failure during the build process (or we will not replace the kit!)
+if [ "`cat ~/.svncos.build.log | grep 'BUILD FAILED' | awk 'END {print NR}'`" != "0" ]; then
+ echo "Build failed - see ~/.svncos.build.log"
+ exit -1
+fi
+
+# Find active version to prepare tar kit file
+
+VERSION=`cat $1/eng/version.txt`
+SSH_PACK_COMMAND="rm -rf /tmp/voltdb-ent*; cp $1/pro/obj/pro/voltdb-ent-"$VERSION".tar.gz /tmp"
+eval $SSH_PACK_COMMAND
+
+
+# Prepare mail message for anyone interested
+echo "To: volt-dev@voltdb.com
+Subject: QA PRO Kit Update
+Body:
+----- This is an automated message - Do not reply -----
+@ "$(date)"
+-------------------------------------------------------
+The latest PRO Kit (CentOS build) is available at:
+
+ ${hostname}:/tmp/voltdb-ent-$VERSION.tar.gz
+
+SVN Revisions for this build:
+ - "`cat ~/.svncos.build.log | grep ' revision ' | awk 'NR == 1 {print "voltdb/eng/trunk :: "$0};'`"
+ - "`cat ~/.svncos.build.log | grep ' revision ' | awk 'NR == 2 {print "voltdb/pro/rest  :: "$0};'`"
+
+A valid  license is  pre-installed, so you  can run VEM
+out-of-the-box,  and all  sample  application  catalogs
+have been built.
+
+The following  script will  retrieve and setup this kit
+on your machine at: ~/Downloads/voltdb-ent-$VERSION
+Note: previously installed version will be removed.
+
+-------------------------------------------------------
+
+rm -rf ~/Downloads/voltdb-ent-$VERSION*
+cd ~/Downloads
+scp ${hostname}:/tmp/voltdb-ent-$VERSION.tar.gz 
+tar -xzf voltdb-ent-$VERSION.tar.gz
+rm voltdb-ent-$VERSION.tar.gz
+
+-------------------------------------------------------
+" > ~/.svncos.mail
+
+# Send mail
+msmtp -t < ~/.svncos.mail
+
+# Cleanup
+rm ~/.svncos.mail
+
---
+++ tools/build.pro.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+BUILD_BRANCH=$1
+BUILD_TARGET=$BUILD_BRANCH/pro/obj/pro
+DIST_NAME=voltdb-ent-`cat $BUILD_BRANCH/eng/version.txt`
+
+rm -rf $BUILD_TARGET
+cd $BUILD_BRANCH/eng
+svn update
+ant clean
+cd $BUILD_BRANCH/pro
+svn update
+ant -f mmt.xml clean
+VOLTCORE=$BUILD_BRANCH/eng ant -f mmt.xml dist.pro
+cd $BUILD_TARGET/$DIST_NAME/examples/auction
+ant build
+cd $BUILD_TARGET/$DIST_NAME/examples/game_of_life
+ant build
+cd $BUILD_TARGET/$DIST_NAME/examples/helloworld
+ant build
+cd $BUILD_TARGET/$DIST_NAME/examples/key_value
+ant build
+cd $BUILD_TARGET/$DIST_NAME/examples/satellite
+ant build
+cd $BUILD_TARGET/$DIST_NAME/examples/twitter
+ant build
+cd $BUILD_TARGET/$DIST_NAME/examples/voter
+ant build
+cp $BUILD_ROOT/license.xml $BUILD_TARGET/$DIST_NAME/management
+cd $BUILD_TARGET
+rm -rf $DIST_NAME.tar.gz
+tar -czf $DIST_NAME.tar.gz $DIST_NAME 
