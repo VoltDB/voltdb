95e2c7dea182306bccca9fd294346a945908340e
Nick Shectman <shectman@voltdb.com>
2011-04-22 14:43:11 +0000

    Don't try to capture the (incredibly verbose) output from Hudson into a logfile now that it has its own log file mechanism. Also don't rotate and compress logfiles on restart since they don't exist any more.

--- tools/start-hudson.sh
+++ tools/start-hudson.sh
@@ -1,5 +1,5 @@
 # run this script from a directory containing hudson.war
 # might I suggest /home/test/Downloads
 
-cat /var/log/hudson >> /var/log/hudson.0
-java -Duser.timezone="America/New_York" -jar hudson.war &> /var/log/hudson &
+# cat /var/log/hudson >> /var/log/hudson.0
+java -Duser.timezone="America/New_York" -jar hudson.war # &> /var/log/hudson &
