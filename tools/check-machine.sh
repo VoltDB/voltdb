125674ad40ac8b783fdb8c20c4d5547627d4aad1
John Hugg <jhugg@voltdb.com>
2011-05-06 20:50:43 +0000

    Disable automatic puppet restarting on our machines. We should work toward getting these internal scripts out of the public repo. No ticket for this as it's pure IT work.

--- tools/check-machine.sh
+++ tools/check-machine.sh
@@ -2,15 +2,15 @@
 # check that puppet is running and start it if it's not
 hostname
 EXIT=0
-if ! ps -ef | grep pup*et; then
-  echo "puppet is not running on $HOSTNAME -- restarting"
-  # try to restart
-  /usr/bin/ruby /usr/sbin/puppetd --server=volt1 || EXIT=1
-  if ! ps -ef | grep pup*et; then
-    echo "puppet is still not running on $HOSTNAME"
-    EXIT=1
-  fi
-fi
+#if ! ps -ef | grep pup*et; then
+#  echo "puppet is not running on $HOSTNAME -- restarting"
+#  # try to restart
+#  /usr/bin/ruby /usr/sbin/puppetd --server=volt1 || EXIT=1
+#  if ! ps -ef | grep pup*et; then
+#    echo "puppet is still not running on $HOSTNAME"
+#    EXIT=1
+#  fi
+#fi
 
 cd /tmp && ls -tr | grep volt_snapshot | head -n-10 | xargs rm -f
 
