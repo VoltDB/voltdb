5e7965654aba03e3d3ddc2d4aca6be887bb35fd4
Tim Callaghan <tcallaghan@voltdb.com>
2011-05-27 19:08:59 +0000

    Further cleanup on the kit license repacker.

--- tools/embed-kit-license.sh
+++ tools/embed-kit-license.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-# Parameters to pass/auto-discover
-LICENSE_SOURCE_DIRECTORY="/home/test/workspace/pro-trunk/licensedata"
-ACTIVE_VERSION="1.3.1"
+# Directory containing trial license files
+LICENSE_SOURCE_DIRECTORY="/home/test/workspace/pro-branch-rest/licensedata"
 
-# Working variables
+# VoltDB Version of the kit we are repacking
+ACTIVE_VERSION="1.3.1"
 
 # Create a license that lasts 45 days (30 days from 2 weeks out)
 LICENSE_SOURCE=$LICENSE_SOURCE_DIRECTORY/`date --date='14 days' +"trial_%Y-%m-%d.xml"`
@@ -15,8 +15,7 @@ mkdir -p /tmp/trialkitbuilder
 cd /tmp/trialkitbuilder
 
 # Grab the source & extract - assuming here that we grab the kit from the community site and push it right back
-#scp root@community.voltdb.com:/var/www/drupal/sites/default/files/archive/$ACTIVE_VERSION/voltdb-ent-$ACTIVE_VERSION.tar.gz .
-cp /tmp/$ACTIVE_VERSION-candidate/voltdb-ent-$ACTIVE_VERSION.tar.gz .
+scp root@community.voltdb.com:/var/www/drupal/sites/default/files/archive/$ACTIVE_VERSION/voltdb-ent-$ACTIVE_VERSION.tar.gz .
 tar -xzf voltdb-ent-$ACTIVE_VERSION.tar.gz
 rm voltdb-ent-$ACTIVE_VERSION.tar.gz
 
