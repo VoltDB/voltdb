6013ffe439675e6e9038e51f5ff1264362bc9556
John Hugg <jhugg@voltdb.com>
2011-08-02 18:17:51 +0000

    Add a single line of code to, seemingly, fix a race condition where fsync to disk could happen before the jar is finished writing itself.

--- src/frontend/org/voltdb/utils/InMemoryJarfile.java
+++ src/frontend/org/voltdb/utils/InMemoryJarfile.java
@@ -152,6 +152,8 @@ public class InMemoryJarfile extends TreeMap<String, byte[]> {
             jarOut.flush();
             jarOut.closeEntry();
         }
+
+        jarOut.finish();
     }
 
     ///////////////////////////////////////////////////////
