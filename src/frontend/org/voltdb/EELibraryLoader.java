01890adecdb9d1739ea3680ae74acdc013bd1a31
John Hugg <jhugg@voltdb.com>
2011-08-29 14:33:30 +0000

    Fixing ENG-1714: Improve the can't load lib error message
    It now logs:
    FATAL HOST - Library VOLTDB JNI shared library loading failed. Library path /Users/jhugg/Documents/svnscratch/trunk/obj/release/nativelibs
    The library may have failed to load because it can't be found in your load library path, or because it is not compatibile with the current platform.
    VoltDB provides builds on our website for 64-bit OSX systems >= 10.6, and 64-bit Linux systems with kernels >= 2.6.18.

--- src/frontend/org/voltdb/EELibraryLoader.java
+++ src/frontend/org/voltdb/EELibraryLoader.java
@@ -55,9 +55,14 @@ public class EELibraryLoader {
                     hostLog.info("Successfully loaded native VoltDB library " + libname + ".");
                 } catch (Throwable e) {
                     if (mustSuccede) {
-                        hostLog.fatal("Library VOLTDB JNI shared library loading failed. Library path "
-                                + System.getProperty("java.library.path"));
-                        VoltDB.crashVoltDB();
+                        String msg = "Library VOLTDB JNI shared library loading failed. Library path " +
+                                System.getProperty("java.library.path") + "\n";
+                        msg += "The library may have failed to load because it can't be found in your " +
+                                "load library path, or because it is not compatibile with the current " +
+                                "platform.\n";
+                        msg +=  "VoltDB provides builds on our website for 64-bit OSX systems >= 10.6, " +
+                                "and 64-bit Linux systems with kernels >= 2.6.18.";
+                        VoltDB.crashLocalVoltDB(msg, false, null);
                     } else {
                         hostLog.info("Library VOLTDB JNI shared library loading failed. Library path "
                                 + System.getProperty("java.library.path"));
