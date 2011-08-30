f11a1cccda2cb024ddd19dba53160b9bf6f4e76a
Seb Coursol <sebc@voltdb.com>
2011-08-30 17:50:15 +0000

    Namespace re-org: clientutils moved to client.exampleutils (references updated)  ENG-1674 & ENG-1620

---
+++ tests/test_apps/genqa/src/com/ClientExtensions.java
@@ -0,0 +1,44 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2011 VoltDB Inc.
+ *
+ * VoltDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VoltDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com;
+
+import org.voltdb.client.Client;
+import org.voltdb.client.ClientConfig;
+import org.voltdb.client.ClientFactory;
+
+public class ClientExtensions
+{
+
+    public static Client GetClient(String[] servers, int port) throws Exception
+    {
+        final ClientConfig config = new ClientConfig("", "");
+        final Client client = ClientFactory.createClient(config);
+
+        for (String server : servers)
+            client.createConnection(server.trim(), port);
+        return client;
+    }
+
+    public static Client GetClient(ClientConfig config, String[] servers, int port) throws Exception
+    {
+        final Client client = ClientFactory.createClient(config);
+
+        for (String server : servers)
+            client.createConnection(server.trim(), port);
+        return client;
+    }
+}
