dc3df803b5433ec9f034ef6d3d602c94fc12a5fb
Seb Coursol <sebc@voltdb.com>
2011-08-30 20:07:55 +0000

    Cleaned up Voter sample code: Async, Multi-Threaded Sync and Multi-threaded JDBC samples now available  ENG-1674 & ENG-1620

---
+++ examples/voter/src/com/AsyncVoterBenchmark.java
@@ -0,0 +1,257 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2011 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+/*
+ * This samples uses the native asynchronous request processing protocol
+ * to post requests to the VoltDB server, thus leveraging to the maximum
+ * VoltDB's ability to run requests in parallel on multiple database
+ * partitions, and multiple servers.
+ *
+ * While asynchronous processing is (marginally) more convoluted to work
+ * with and not adapted to all workloads, it is the preferred interaction
+ * model to VoltDB as it guarantees blazing performance.
+ *
+ * Because there is a risk of 'firehosing' a database cluster (if the
+ * cluster is too slow (slow or too few CPUs), this sample performs
+ * self-tuning to target a specific latency (10ms by default).
+ * This tuning process, as demonstrated here, is important and should be
+ * part of your pre-launch evalution so you can adequately provision your
+ * VoltDB cluster with the number of servers required for your needs.
+ */
+package com;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicLongArray;
+
+import org.voltdb.VoltTable;
+import org.voltdb.client.ClientResponse;
+import org.voltdb.client.ProcedureCallback;
+
+import org.voltdb.client.exampleutils.AppHelper;
+import org.voltdb.client.exampleutils.ClientConnection;
+import org.voltdb.client.exampleutils.ClientConnectionPool;
+import org.voltdb.client.exampleutils.IRateLimiter;
+import org.voltdb.client.exampleutils.LatencyLimiter;
+import org.voltdb.client.exampleutils.RateLimiter;
+
+public class AsyncVoterBenchmark
+{
+    // Initialize some commong constants and variables
+    private static final String ContestantNamesCSV = "Edwina Burnam,Tabatha Gehling,Kelly Clauss,Jessie Alloway,Alana Bregman,Jessie Eichman,Allie Rogalski,Nita Coster,Kurt Walser,Ericka Dieter,Loraine NygrenTania Mattioli";
+    private static final AtomicLongArray VotingBoardResults = new AtomicLongArray(4);
+
+    // Reference to the database connection we will use
+    private static ClientConnection Con;
+
+    // Application entry point
+    public static void main(String[] args)
+    {
+        try
+        {
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // Use the AppHelper utility class to retrieve command line application parameters
+
+            // Define parameters and pull from command line
+            AppHelper apph = new AppHelper(AsyncVoterBenchmark.class.getCanonicalName())
+                .add("display-interval", "display_interval_in_seconds", "Interval for performance feedback, in seconds.", 10)
+                .add("duration", "run_duration_in_seconds", "Benchmark duration, in seconds.", 120)
+                .add("servers", "comma_separated_server_list", "List of VoltDB servers to connect to.", "localhost")
+                .add("port", "port_number", "Client port to connect to on cluster nodes.", 21212)
+                .add("contestants", "contestant_count", "Number of contestants in the voting contest (from 1 to 10).", 6)
+                .add("max-votes", "max_votes_per_phone_number", "Maximum number of votes accepted for a given voter (phone number).", 2)
+                .add("rate-limit", "rate_limit", "Rate limit to start from (number of transactions per second).", 100000)
+                .add("auto-tune", "auto_tune", "Flag indicating whether the benchmark should self-tune the transaction rate for a target execution latency (true|false).", "true")
+                .add("latency-target", "latency_target", "Execution latency to target to tune transaction rate (in milliseconds).", 10.0d)
+                .setArguments(args)
+            ;
+
+            // Retrieve parameters
+            long displayInterval = apph.longValue("display-interval");
+            long duration        = apph.longValue("duration");
+            String servers       = apph.stringValue("servers");
+            int port             = apph.intValue("port");
+            int contestantCount  = apph.intValue("contestants");
+            int maxVoteCount     = apph.intValue("max-votes");
+            long rateLimit       = apph.longValue("rate-limit");
+            boolean autoTune     = apph.booleanValue("auto-tune");
+            double latencyTarget = apph.doubleValue("latency-target");
+
+
+            // Validate parameters
+            apph.validate("contestants", (contestantCount > 0))
+                .validate("max-votes", (maxVoteCount > 0))
+                .validate("rate-limit", (rateLimit > 0))
+                .validate("latency-target", (latencyTarget > 0))
+            ;
+
+            // Display actual parameters, for reference
+            apph.printActualUsage();
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // Get a client connection - we retry for a while in case the server hasn't started yet
+            System.out.printf("Connecting to servers: %s at port: %d\n", servers, port);
+            int sleep = 1000;
+            while(true)
+            {
+                try
+                {
+                    Con = ClientConnectionPool.get(servers, port);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    System.err.printf("Connection failed - retrying in %d second(s).\n", sleep/1000);
+                    try {Thread.sleep(sleep);} catch(Exception tie){}
+                    if (sleep < 8000)
+                        sleep += sleep;
+                }
+            }
+            System.out.println("Connected.  Starting benchmark.");
+
+            // Initialize the application
+            final int maxContestants = (int)Con.execute("Initialize", contestantCount, ContestantNamesCSV).getResults()[0].fetchRow(0).getLong(0);
+
+            // Get a Phone Call Generator that will simulate voter entries from the call center
+            PhoneCallGenerator switchboard = new PhoneCallGenerator(maxContestants);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // Create a Timer task to display performance data on the Vote procedure
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask()
+            {
+                @Override
+                public void run()
+                {
+                    System.out.print(Con.getStatistics("Vote"));
+                }
+            }
+            , displayInterval*1000l
+            , displayInterval*1000l
+            );
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // Pick the transaction rate limiter helping object to use based on user request (rate limiting or latency targeting)
+            IRateLimiter limiter = null;
+            if (autoTune)
+                limiter = (IRateLimiter)new LatencyLimiter(Con, "Vote", latencyTarget, rateLimit);
+            else
+                limiter = (IRateLimiter)new RateLimiter(rateLimit);
+
+            // Run the benchmark loop for the requested duration
+            long endTime = System.currentTimeMillis() + (1000l * duration);
+            while (endTime > System.currentTimeMillis())
+            {
+                // Get the next phone call
+                PhoneCallGenerator.PhoneCall call = switchboard.receive();
+
+                // Post the vote, asynchronously
+                Con.executeAsync(new ProcedureCallback()
+                {
+                    @Override
+                    public void clientCallback(ClientResponse response) throws Exception
+                    {
+                        // Track the result of the vote (Accepted, Rejected, Failure...)
+                        if (response.getStatus() == ClientResponse.SUCCESS)
+                            VotingBoardResults.incrementAndGet((int)response.getResults()[0].fetchRow(0).getLong(0));
+                        else
+                            VotingBoardResults.incrementAndGet(3);
+                    }
+                }
+                , "Vote"
+                , call.PhoneNumber
+                , call.ContestantNumber
+                , maxVoteCount
+                );
+
+                // Use the limiter to throttle client activity
+                limiter.throttle();
+            }
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // We're done - stop the performance statistics display task
+            timer.cancel();
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // Now print application results:
+
+            // 1. Voting Board statistics, Voting results and performance statistics
+            System.out.printf(
+              "-------------------------------------------------------------------------------------\n"
+            + " Voting Results\n"
+            + "-------------------------------------------------------------------------------------\n\n"
+            + "A total of %d votes was received...\n"
+            + " - %,9d Accepted\n"
+            + " - %,9d Rejected (Invalid Contestant)\n"
+            + " - %,9d Rejected (Maximum Vote Count Reached)\n"
+            + " - %,9d Failed (Transaction Error)\n"
+            + "\n\n"
+            + "-------------------------------------------------------------------------------------\n"
+            + "Contestant Name\t\tVotes Received\n"
+            , Con.getStatistics("Vote").getExecutionCount()
+            , VotingBoardResults.get(0)
+            , VotingBoardResults.get(1)
+            , VotingBoardResults.get(2)
+            , VotingBoardResults.get(3)
+            );
+
+            // 2. Voting results
+            VoltTable result = Con.execute("Results").getResults()[0];
+            String winner = "";
+            long winnerVoteCount = 0;
+            while(result.advanceRow())
+            {
+                if (result.getLong(2) > winnerVoteCount)
+                {
+                    winnerVoteCount = result.getLong(2);
+                    winner = result.getString(0);
+                }
+                System.out.printf("%s\t\t%,14d\n", result.getString(0), result.getLong(2));
+            }
+            System.out.printf("\n\nThe Winner is: %s\n-------------------------------------------------------------------------------------\n", winner);
+
+            // 3. Performance statistics (we only care about the Vote procedure that we're benchmarking)
+            System.out.println(
+              "\n\n-------------------------------------------------------------------------------------\n"
+            + " System Statistics\n"
+            + "-------------------------------------------------------------------------------------\n\n");
+            System.out.print(Con.getStatistics("Vote").toString(false));
+
+            Con.close();
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+        }
+        catch(Exception x)
+        {
+            System.out.println("Exception: " + x);
+            x.printStackTrace();
+        }
+    }
+}
---
+++ examples/voter/src/com/PhoneCallGenerator.java
@@ -0,0 +1,76 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2011 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com;
+
+import java.util.Random;
+
+public class PhoneCallGenerator
+{
+    // Initialize some common constants and variables
+    private static final String[] AreaCodes = "907,205,256,334,251,870,501,479,480,602,623,928,520,341,764,628,831,925,909,562,661,510,650,949,760,415,951,209,669,408,559,626,442,530,916,627,714,707,310,323,213,424,747,818,858,935,619,805,369,720,303,970,719,860,203,959,475,202,302,689,407,239,850,727,321,754,954,927,352,863,386,904,561,772,786,305,941,813,478,770,470,404,762,706,678,912,229,808,515,319,563,641,712,208,217,872,312,773,464,708,224,847,779,815,618,309,331,630,317,765,574,260,219,812,913,785,316,620,606,859,502,270,504,985,225,318,337,774,508,339,781,857,617,978,351,413,443,410,301,240,207,517,810,278,679,313,586,947,248,734,269,989,906,616,231,612,320,651,763,952,218,507,636,660,975,816,573,314,557,417,769,601,662,228,406,336,252,984,919,980,910,828,704,701,402,308,603,908,848,732,551,201,862,973,609,856,575,957,505,775,702,315,518,646,347,212,718,516,917,845,631,716,585,607,914,216,330,234,567,419,440,380,740,614,283,513,937,918,580,405,503,541,971,814,717,570,878,835,484,610,267,215,724,412,401,843,864,803,605,423,865,931,615,901,731,254,325,713,940,817,430,903,806,737,512,361,210,979,936,409,972,469,214,682,832,281,830,956,432,915,435,801,385,434,804,757,703,571,276,236,540,802,509,360,564,206,425,253,715,920,262,414,608,304,307".split(",");
+
+    public static class PhoneCall
+    {
+        public final int ContestantNumber;
+        public final long PhoneNumber;
+        protected PhoneCall(int contestantNumber, long phoneNumber)
+        {
+            this.ContestantNumber = contestantNumber;
+            this.PhoneNumber = phoneNumber;
+        }
+    }
+
+    private final int ContestantCount;
+    private final Random Rand = new Random();
+    private final int[] VotingMap = new int[AreaCodes.length];
+    public PhoneCallGenerator(int contestantCount)
+    {
+        this.ContestantCount = contestantCount;
+
+        // This is a just a small fudge to make the geographical voting map more interesting for the benchmark!
+        for(int i=0;i<VotingMap.length;i++)
+            VotingMap[i] = this.Rand.nextInt(100) < 30 ? 1 : (int)(Math.abs(Math.sin(i)*this.ContestantCount) % this.ContestantCount) + 1;
+
+    }
+
+    /**
+     * Receives/generates a simulated voting call
+     * @return Call details (calling number and contestant to whom the vote is given)
+     */
+    public PhoneCall receive()
+    {
+        // For the purpose of a benchmark, issue random voting activity (including invalid votes to demonstrate transaction validationg in the database)
+
+        // Pick a random area code for the originating phone call
+        final int areaCodeIndex = (this.Rand.nextInt(AreaCodes.length) * this.Rand.nextInt(AreaCodes.length)) % AreaCodes.length;
+
+        // Pick a contestant number - introduce an invalid contestant every 100 call or so to simulate fraud / invalid entries (something the transaction validates against)
+        final int contestantNumber =   ((this.Rand.nextInt(100) % 100) == 0) ? 999 : (this.Rand.nextInt(100) > 50 ? VotingMap[areaCodeIndex] : ((this.Rand.nextInt(this.ContestantCount) % this.ContestantCount) + 1));
+
+        // Build the phone number
+        final long phoneNumber = Long.parseLong(AreaCodes[areaCodeIndex])*10000000l + (Math.abs(this.Rand.nextLong()) % 9999999l);
+
+        // Return the generated phone number
+        return new PhoneCall(contestantNumber, phoneNumber);
+    }
+}
