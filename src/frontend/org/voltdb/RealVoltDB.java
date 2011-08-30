5442f479579a31771279ed0152eda7aca07c75b3
John Hugg <jhugg@voltdb.com>
2011-08-30 21:14:50 +0000

    Bumping the version to 2.0. Like a boss.

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -127,7 +127,7 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback
     public VoltDB.Configuration m_config = new VoltDB.Configuration();
     CatalogContext m_catalogContext;
     private String m_buildString;
-    private static final String m_defaultVersionString = "1.3.6";
+    private static final String m_defaultVersionString = "2.0";
     private String m_versionString = m_defaultVersionString;
     // fields accessed via the singleton
     HostMessenger m_messenger = null;
