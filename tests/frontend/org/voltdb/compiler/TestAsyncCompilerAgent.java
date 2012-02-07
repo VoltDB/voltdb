696118a98b9c5bc6dec9526a92cfbde220920492
Mike Ismert <izzy@voltdb.com>
2012-02-07 13:50:07 -0500

    TestAsyncCompilerAgent compiles

--- tests/frontend/org/voltdb/compiler/TestAsyncCompilerAgent.java
+++ tests/frontend/org/voltdb/compiler/TestAsyncCompilerAgent.java
@@ -32,9 +32,9 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.mockito.verification.VerificationMode;
-import org.voltdb.messaging.HostMessenger;
-import org.voltdb.messaging.LocalObjectMessage;
-import org.voltdb.messaging.MessagingException;
+import org.voltcore.messaging.HostMessenger;
+import org.voltcore.messaging.LocalObjectMessage;
+import org.voltcore.messaging.MessagingException;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -99,7 +99,7 @@ public class TestAsyncCompilerAgent {
 
         // check for one rejected request
         ArgumentCaptor<LocalObjectMessage> captor = ArgumentCaptor.forClass(LocalObjectMessage.class);
-        verify(m_agent.m_mailbox).send(eq(100), eq(0), captor.capture());
+        verify(m_agent.m_mailbox).send(eq(100), captor.capture());
         assertNotNull(((AsyncCompilerResult) captor.getValue().payload).errorMsg);
         // let all requests return
         blockingAnswer.flag.release(AsyncCompilerAgent.MAX_QUEUE_DEPTH + 1);
@@ -107,6 +107,6 @@ public class TestAsyncCompilerAgent {
         // check if all previous requests finish
         m_agent.shutdown();
         VerificationMode expected = times(AsyncCompilerAgent.MAX_QUEUE_DEPTH + 2);
-        verify(m_agent.m_mailbox, expected).send(eq(100), eq(0), any(LocalObjectMessage.class));
+        verify(m_agent.m_mailbox, expected).send(eq(100), any(LocalObjectMessage.class));
     }
 }
