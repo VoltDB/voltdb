af7d1869aa0f62b317a7b1cadec4ef1e6e718fa3
Ruth Morgenstein <rmorgenstein@voltdb.com>
2012-02-09 15:14:59 -0500

    ENG-2393 - added voter-selfcheck to test_apps for use in replication testing.

---
+++ tests/test_apps/voter-selfcheck/ddl.sql
@@ -0,0 +1,72 @@
+-- contestants table holds the contestants numbers (for voting) and names
+CREATE TABLE contestants
+(
+  contestant_number integer     NOT NULL
+, contestant_name   varchar(50) NOT NULL
+, CONSTRAINT PK_contestants PRIMARY KEY
+  (
+    contestant_number
+  )
+);
+
+-- votes table holds every valid vote.
+--   voters are not allowed to submit more than <x> votes, x is passed to client application
+CREATE TABLE votes
+(
+  phone_number       bigint     NOT NULL
+, state              varchar(2) NOT NULL
+, contestant_number  integer    NOT NULL
+-- PARTITION BY ( phone_number )
+);
+
+-- track how many rejected votes each phone number has done
+CREATE TABLE rejected_votes_by_phone_number
+(
+  phone_number   bigint NOT NULL
+, rejected_votes bigint NOT NULL
+, CONSTRAINT PK_rejected_votes_by_phone_number PRIMARY KEY
+  (
+    phone_number
+  )
+);
+
+-- Map of Area Codes and States for geolocation classification of incoming calls
+CREATE TABLE area_code_state
+(
+  area_code smallint   NOT NULL
+, state     varchar(2) NOT NULL
+, CONSTRAINT PK_area_code_state PRIMARY KEY
+  (
+    area_code
+  )
+);
+
+-- rollup of votes by phone number, used to reject excessive voting
+CREATE VIEW v_votes_by_phone_number
+(
+  phone_number
+, num_votes
+)
+AS
+   SELECT phone_number
+        , COUNT(*)
+     FROM votes
+ GROUP BY phone_number
+;
+
+-- rollup of votes by contestant and state for the heat map and results
+CREATE VIEW v_votes_by_contestant_number_state
+(
+  contestant_number
+, state
+, num_votes
+)
+AS
+   SELECT contestant_number
+        , state
+        , COUNT(*)
+     FROM votes
+ GROUP BY contestant_number
+        , state
+;
+
---
+++ tests/test_apps/voter-selfcheck/run.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+APPNAME="voter"
+CLASSPATH="`ls -x ../../../voltdb/voltdb-*.jar | tr '[:space:]' ':'``ls -x ../../../lib/*.jar | tr '[:space:]' ':'`"
+VOLTDB="../../../bin/voltdb"
+VOLTCOMPILER="../../../bin/voltcompiler"
+LICENSE="../../../voltdb/license.xml"
+LOG4J="`pwd`/../../../voltdb/log4j.xml"
+LEADER="localhost"
+
+# remove build artifacts
+function clean() {
+    rm -rf obj debugoutput $APPNAME.jar voltdbroot voltdbroot
+}
+
+# compile the source code for procedures and the client
+function srccompile() {
+    mkdir -p obj
+    javac -classpath $CLASSPATH -d obj \
+        src/voter/*.java \
+        src/voter/procedures/*.java
+    # stop if compilation fails
+    if [ $? != 0 ]; then exit; fi
+}
+
+# build an application catalog
+function catalog() {
+    srccompile
+    $VOLTCOMPILER obj project.xml $APPNAME.jar
+    # stop if compilation fails
+    if [ $? != 0 ]; then exit; fi
+}
+
+# run the voltdb server locally
+function server() {
+    # if a catalog doesn't exist, build one
+    if [ ! -f $APPNAME.jar ]; then catalog; fi
+    # run the server
+    $VOLTDB create catalog $APPNAME.jar deployment deployment.xml \
+        license $LICENSE leader $LEADER
+}
+
+# run the client that drives the example
+function client() {
+    async-benchmark
+}
+
+# Asynchronous benchmark sample
+# Use this target for argument help
+function async-benchmark-help() {
+    srccompile
+    java -classpath obj:$CLASSPATH:obj voter.AsyncBenchmark --help
+}
+
+function async-benchmark() {
+    srccompile
+    java -classpath obj:$CLASSPATH:obj -Dlog4j.configuration=file://$LOG4J \
+        voter.AsyncBenchmark \
+        --display-interval=5 \
+        --duration=120 \
+        --servers=localhost \
+        --port=21212 \
+        --contestants=6 \
+        --voter=905000000 \
+        --max-votes=2 \
+        --rate-limit=100000 \
+        --auto-tune=false \
+        --latency-target=10.0
+}
+
+# Multi-threaded synchronous benchmark sample
+# Use this target for argument help
+function sync-benchmark-help() {
+    srccompile
+    java -classpath obj:$CLASSPATH:obj voter.SyncBenchmark --help
+}
+
+function sync-benchmark() {
+    srccompile
+    java -classpath obj:$CLASSPATH:obj -Dlog4j.configuration=file://$LOG4J \
+        voter.SyncBenchmark \
+        --threads=40 \
+        --display-interval=5 \
+        --duration=120 \
+        --servers=localhost \
+        --port=21212 \
+        --contestants=6 \
+        --max-votes=2
+}
+
+# JDBC benchmark sample
+# Use this target for argument help
+function jdbc-benchmark-help() {
+    srccompile
+    java -classpath obj:$CLASSPATH:obj voter.JDBCBenchmark --help
+}
+
+function jdbc-benchmark() {
+    srccompile
+    java -classpath obj:$CLASSPATH:obj -Dlog4j.configuration=file://$LOG4J \
+        voter.JDBCBenchmark \
+        --threads=40 \
+        --display-interval=5 \
+        --duration=120 \
+        --servers=localhost \
+        --port=21212 \
+        --contestants=6 \
+        --max-votes=2
+}
+
+function help() {
+    echo "Usage: ./run.sh {clean|catalog|server|async-benchmark|aysnc-benchmark-help|...}"
+    echo "       {...|sync-benchmark|sync-benchmark-help|jdbc-benchmark|jdbc-benchmark-help}"
+}
+
+# Run the target passed as the first arg on the command line
+# If no first arg, run server
+if [ $# -gt 1 ]; then help; exit; fi
+if [ $# = 1 ]; then $1; else server; fi
