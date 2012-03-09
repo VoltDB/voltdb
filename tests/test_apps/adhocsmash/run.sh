9b690a131e612998bdfa167bd5ea5481613d4f6b
Mike Ismert <izzy@voltdb.com>
2012-03-09 11:37:20 -0500

    ENG-2509: Add a new app to demonstrate the defect expected when replicating adhoc writes to the replica cluster.

---
+++ tests/test_apps/adhocsmash/run.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+APPNAME="adhocsmash"
+CLASSPATH="`ls -x ../../../voltdb/voltdb-*.jar | tr '[:space:]' ':'``ls -x ../../../lib/*.jar | tr '[:space:]' ':'`"
+VOLTDB="../../../bin/voltdb"
+VOLTCOMPILER="../../../bin/voltcompiler"
+LOG4J="`pwd`/../../../voltdb/log4j.xml"
+LICENSE="../../../voltdb/license.xml"
+LEADER="localhost"
+
+# remove build artifacts
+function clean() {
+    rm -rf obj debugoutput $APPNAME.jar voltdbroot voltdbroot
+}
+
+# compile the source code for procedures and the client
+function srccompile() {
+    mkdir -p obj
+    javac -classpath $CLASSPATH -d obj \
+        src/adhocsmash/*.java \
+    # stop if compilation fails
+    if [ $? != 0 ]; then exit; fi
+}
+
+# build an application catalog
+function catalog() {
+    srccompile
+    $VOLTCOMPILER obj project.xml $APPNAME.jar
+    # stop if compilation fails
+    if [ $? != 0 ]; then exit; fi
+}
+
+# run the voltdb server locally
+function server() {
+    # if a catalog doesn't exist, build one
+    if [ ! -f $APPNAME.jar ]; then catalog; fi
+    # run the server
+    $VOLTDB create catalog $APPNAME.jar deployment deployment.xml \
+        license $LICENSE leader $LEADER
+}
+
+# run the client that drives the example
+function client() {
+    adhoc-smash
+}
+
+function adhoc-smash() {
+    srccompile
+    java -classpath obj:$CLASSPATH:obj -Dlog4j.configuration=file://$LOG4J \
+        adhocsmash.AdhocSmash \
+        --display-interval=5 \
+        --duration=120000 \
+        --servers=10.10.180.144 \
+        --port=21212 \
+        --rate-limit=4000
+}
+
+function help() {
+    echo "Usage: ./run.sh {clean|catalog|server|adhoc-smash}"
+}
+
+# Run the target passed as the first arg on the command line
+# If no first arg, run server
+if [ $# -gt 1 ]; then help; exit; fi
+if [ $# = 1 ]; then $1; else server; fi
