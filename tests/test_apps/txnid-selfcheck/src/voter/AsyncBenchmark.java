680a33b94b8e23290bee5f3a3b0b386b48fa5cf3
Ariel Weisberg <aweisberg@voltdb.com>
2012-03-23 13:22:44 -0400

    Add a simple self checking tnxid app that can be used to determine whether transactions out of order or missing.

---
+++ tests/test_apps/txnid-selfcheck/src/voter/AsyncBenchmark.java
@@ -0,0 +1,203 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2012 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+/*
+ * This samples uses the native asynchronous request processing protocol
+ * to post requests to the VoltDB server, thus leveraging to the maximum
+ * VoltDB's ability to run requests in parallel on multiple database
+ * partitions, and multiple servers.
+ *
+ * While asynchronous processing is (marginally) more convoluted to work
+ * with and not adapted to all workloads, it is the preferred interaction
+ * model to VoltDB as it guarantees blazing performance.
+ *
+ * Because there is a risk of 'firehosing' a database cluster (if the
+ * cluster is too slow (slow or too few CPUs), this sample performs
+ * self-tuning to target a specific latency (10ms by default).
+ * This tuning process, as demonstrated here, is important and should be
+ * part of your pre-launch evalution so you can adequately provision your
+ * VoltDB cluster with the number of servers required for your needs.
+ */
+
+package voter;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicLongArray;
+
+import org.voltdb.VoltTable;
+import org.voltdb.client.ClientResponse;
+import org.voltdb.client.ProcedureCallback;
+import org.voltdb.client.exampleutils.AppHelper;
+import org.voltdb.client.exampleutils.ClientConnection;
+import org.voltdb.client.exampleutils.ClientConnectionPool;
+import org.voltdb.client.exampleutils.IRateLimiter;
+import org.voltdb.client.exampleutils.LatencyLimiter;
+import org.voltdb.client.exampleutils.RateLimiter;
+
+public class AsyncBenchmark
+{
+    private static final AtomicLongArray VotingBoardResults = new AtomicLongArray(4);
+
+    // Reference to the database connection we will use
+    private static ClientConnection Con;
+
+    // Application entry point
+    public static void main(String[] args)
+    {
+        try
+        {
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // Use the AppHelper utility class to retrieve command line application parameters
+
+            // Define parameters and pull from command line
+            AppHelper apph = new AppHelper(AsyncBenchmark.class.getCanonicalName())
+                .add("display-interval", "display_interval_in_seconds", "Interval for performance feedback, in seconds.", 10)
+                .add("duration", "run_duration_in_seconds", "Benchmark duration, in seconds.", 120)
+                .add("servers", "comma_separated_server_list", "List of VoltDB servers to connect to.", "localhost")
+                .add("port", "port_number", "Client port to connect to on cluster nodes.", 21212)
+                .add("rate-limit", "rate_limit", "Rate limit to start from (number of transactions per second).", 100000)
+                .add("auto-tune", "auto_tune", "Flag indicating whether the benchmark should self-tune the transaction rate for a target execution latency (true|false).", "true")
+                .add("latency-target", "latency_target", "Execution latency to target to tune transaction rate (in milliseconds).", 10.0d)
+                .setArguments(args)
+            ;
+
+            // Retrieve parameters
+            long displayInterval = apph.longValue("display-interval");
+            long duration        = apph.longValue("duration");
+            String servers       = apph.stringValue("servers");
+            int port             = apph.intValue("port");
+            long rateLimit       = apph.longValue("rate-limit");
+            boolean autoTune     = apph.booleanValue("auto-tune");
+            double latencyTarget = apph.doubleValue("latency-target");
+            final String csv     = apph.stringValue("stats");
+
+
+            // Validate parameters
+            apph.validate("duration", (duration > 0))
+                .validate("display-interval", (displayInterval > 0))
+                .validate("rate-limit", (rateLimit > 0))
+                .validate("latency-target", (latencyTarget > 0))
+            ;
+
+            // Display actual parameters, for reference
+            apph.printActualUsage();
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // Get a client connection - we retry for a while in case the server hasn't started yet
+            Con = ClientConnectionPool.getWithRetry(servers, port);
+
+            // Create a Timer task to display performance data on the Vote procedure
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask()
+            {
+                @Override
+                public void run()
+                {
+                    System.out.print(Con.getStatistics("doTxn"));
+                }
+            }
+            , displayInterval*1000l
+            , displayInterval*1000l
+            );
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+            java.util.Random r = new java.util.Random(2);
+            // Pick the transaction rate limiter helping object to use based on user request (rate limiting or latency targeting)
+            IRateLimiter limiter = null;
+            if (autoTune)
+                limiter = new LatencyLimiter(Con, "doTxn", latencyTarget, rateLimit);
+            else
+                limiter = new RateLimiter(rateLimit);
+
+            // Run the benchmark loop for the requested duration
+            final long endTime = System.currentTimeMillis() + (1000l * duration);
+            while (endTime > System.currentTimeMillis())
+            {
+	        Con.executeAsync( new ProcedureCallback() {
+                    @Override
+                    public void clientCallback(ClientResponse response) throws Exception {
+                        if (response.getStatus() != ClientResponse.SUCCESS) {
+                            System.out.println(response.getStatusString()); System.exit(-1);}
+                    }
+                },
+                 "doTxn",
+                (byte)r.nextInt(127));
+                
+                // Use the limiter to throttle client activity
+                limiter.throttle();
+            }
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // We're done - stop the performance statistics display task
+            timer.cancel();
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+            // Now print application results:
+
+            // 1. Voting Board statistics, Voting results and performance statistics
+            System.out.printf(
+              "-------------------------------------------------------------------------------------\n"
+            + " Voting Results\n"
+            + "-------------------------------------------------------------------------------------\n\n"
+            + "A total of %d votes was received...\n"
+            + " - %,9d Accepted\n"
+            + " - %,9d Rejected (Invalid Contestant)\n"
+            + " - %,9d Rejected (Maximum Vote Count Reached)\n"
+            + " - %,9d Failed (Transaction Error)\n"
+            + "\n\n"
+            + "-------------------------------------------------------------------------------------\n"
+            + "Contestant Name\t\tVotes Received\n"
+            , Con.getStatistics("doTxn").getExecutionCount()
+            , VotingBoardResults.get(0)
+            , VotingBoardResults.get(1)
+            , VotingBoardResults.get(2)
+            , VotingBoardResults.get(3)
+            );
+
+            // 3. Performance statistics (we only care about the Vote procedure that we're benchmarking)
+            System.out.println(
+              "\n\n-------------------------------------------------------------------------------------\n"
+            + " System Statistics\n"
+            + "-------------------------------------------------------------------------------------\n\n");
+            System.out.print(Con.getStatistics("doTxn").toString(false));
+
+            // Dump statistics to a CSV file
+            Con.saveStatistics(csv);
+
+            Con.close();
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+        }
+        catch(Exception x)
+        {
+            System.out.println("Exception: " + x);
+            x.printStackTrace();
+        }
+    }
+}
