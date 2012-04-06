857eb9f57410f97003ddfa797b4aaf721e208315
Ryan Betts <rbetts@voltdb.com>
2012-04-06 17:18:00 -0400

    Findbugs: invalid use of reference equality.

--- src/frontend/org/voltdb/client/AuthenticatedConnectionCache.java
+++ src/frontend/org/voltdb/client/AuthenticatedConnectionCache.java
@@ -119,7 +119,7 @@ public class AuthenticatedConnectionCache {
             try
             {
                 adminClient = (ClientImpl) ClientFactory.createClient();
-                if ((userName == null) || (userName == "")) {
+                if ((userName == null) || (userName.equals(""))) {
                     if ((hashedPassword != null) && (hashedPassword.length > 0)) {
                         throw new IOException("Username was null but password was not.");
                     }
