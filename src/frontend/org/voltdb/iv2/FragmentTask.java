5519e91868c17237a6ddc4da70f8c25446e17524
Mike Ismert <izzy@voltdb.com>
2012-05-02 13:07:19 -0400

    Checkpoint a semi-complete FragmentTask handling implementation.

--- src/frontend/org/voltdb/iv2/FragmentTask.java
+++ src/frontend/org/voltdb/iv2/FragmentTask.java
@@ -1,8 +1,20 @@
 package org.voltdb.iv2;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.voltcore.logging.Level;
 import org.voltcore.logging.VoltLogger;
+import org.voltdb.ParameterSet;
 import org.voltdb.SiteProcedureConnection;
+import org.voltdb.VoltDB;
+import org.voltdb.VoltTable;
+import org.voltdb.exceptions.EEException;
+import org.voltdb.exceptions.SQLException;
+import org.voltdb.messaging.FastDeserializer;
+import org.voltdb.messaging.FragmentResponseMessage;
 import org.voltdb.messaging.FragmentTaskMessage;
+import org.voltdb.utils.LogKeys;
 
 public class FragmentTask extends SiteTasker
 {
@@ -11,17 +23,25 @@ public class FragmentTask extends SiteTasker
 
     final Iv2MPTransactionState m_txn;
     final InitiatorMailbox m_initiator;
+    final FragmentTaskMessage m_task;
 
     FragmentTask(InitiatorMailbox initiator, FragmentTaskMessage message)
     {
         m_initiator = initiator;
         // IZZY: work out right transaction ID?
         m_txn = new Iv2MPTransactionState(message.getTxnId(), message);
+        m_task = message;
     }
 
     @Override
     public void run(SiteProcedureConnection siteConnection)
     {
+        if (!m_txn.isReadOnly()) {
+            m_txn.setBeginUndoToken(siteConnection.getLatestUndoToken());
+        }
+        final FragmentResponseMessage response = processFragmentTask(siteConnection);
+        // completion?
+        m_initiator.deliver(response);
     }
 
     @Override
@@ -30,4 +50,77 @@ public class FragmentTask extends SiteTasker
         return 0;
     }
 
+    // Cut and pasted from ExecutionSite processFragmentTask(), then
+    // modifed to work in the new world
+    public FragmentResponseMessage processFragmentTask(SiteProcedureConnection siteConnection)
+    {
+        // IZZY: actually need the "executor" HSId these days?
+        final FragmentResponseMessage currentFragResponse =
+            new FragmentResponseMessage(m_task, m_initiator.getHSId());
+        currentFragResponse.setStatus(FragmentResponseMessage.SUCCESS, null);
+
+        if (m_task.isSysProcTask())
+        {
+            throw new RuntimeException("IV2 unable to handle system procedures yet");
+        }
+
+        for (int frag = 0; frag < m_task.getFragmentCount(); frag++)
+        {
+            final long fragmentId = m_task.getFragmentId(frag);
+            final int outputDepId = m_task.getOutputDepId(frag);
+
+            // this is a horrible performance hack, and can be removed with small changes
+            // to the ee interface layer.. (rtb: not sure what 'this' encompasses...)
+            // (izzy: still not sure what 'this' encompasses...)
+            ParameterSet params = null;
+            final ByteBuffer paramData = m_task.getParameterDataForFragment(frag);
+            if (paramData != null) {
+                final FastDeserializer fds = new FastDeserializer(paramData);
+                try {
+                    params = fds.readObject(ParameterSet.class);
+                }
+                catch (final IOException e) {
+                    hostLog.l7dlog(Level.FATAL,
+                                   LogKeys.host_ExecutionSite_FailedDeserializingParamsForFragmentTask.name(), e);
+                    VoltDB.crashLocalVoltDB(e.getMessage(), true, e);
+                }
+            }
+            else {
+                params = new ParameterSet();
+            }
+
+            final int inputDepId = m_task.getOnlyInputDepId(frag);
+
+            /*
+             * Currently the error path when executing plan fragments
+             * does not adequately distinguish between fatal errors and
+             * abort type errors that should result in a roll back.
+             * Assume that it is ninja: succeeds or doesn't return.
+             * No roll back support.
+             */
+            try {
+                final VoltTable dependency =
+                    siteConnection.executePlanFragment(fragmentId,
+                                                       inputDepId,
+                                                       params,
+                                                       m_txn.txnId,
+                                                       m_txn.isReadOnly());
+                if (hostLog.isTraceEnabled()) {
+                    hostLog.l7dlog(Level.TRACE,
+                       LogKeys.org_voltdb_ExecutionSite_SendingDependency.name(),
+                       new Object[] { outputDepId }, null);
+                }
+                currentFragResponse.addDependency(outputDepId, dependency);
+            } catch (final EEException e) {
+                hostLog.l7dlog( Level.TRACE, LogKeys.host_ExecutionSite_ExceptionExecutingPF.name(), new Object[] { fragmentId }, e);
+                currentFragResponse.setStatus(FragmentResponseMessage.UNEXPECTED_ERROR, e);
+                break;
+            } catch (final SQLException e) {
+                hostLog.l7dlog( Level.TRACE, LogKeys.host_ExecutionSite_ExceptionExecutingPF.name(), new Object[] { fragmentId }, e);
+                currentFragResponse.setStatus(FragmentResponseMessage.UNEXPECTED_ERROR, e);
+                break;
+            }
+        }
+        return currentFragResponse;
+    }
 }
