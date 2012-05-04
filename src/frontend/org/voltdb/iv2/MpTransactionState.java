3f94766cc5a0f22b4c2b7b58420fe5e91e7c4943
Mike Ismert <izzy@voltdb.com>
2012-05-04 16:38:26 -0400

    WIP - Hacky, skeletal MpTransactionState.

--- src/frontend/org/voltdb/iv2/MpTransactionState.java
+++ src/frontend/org/voltdb/iv2/MpTransactionState.java
@@ -17,15 +17,21 @@
 
 package org.voltdb.iv2;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingDeque;
 
 import org.voltcore.messaging.Mailbox;
 import org.voltcore.messaging.TransactionInfoBaseMessage;
 import org.voltcore.utils.Pair;
+import org.voltdb.SiteProcedureConnection;
 import org.voltdb.StoredProcedureInvocation;
+import org.voltdb.VoltTable;
 import org.voltdb.dtxn.TransactionState;
 import org.voltdb.messaging.FragmentResponseMessage;
 import org.voltdb.messaging.FragmentTaskMessage;
@@ -36,15 +42,21 @@ public class MpTransactionState extends TransactionState
     final Iv2InitiateTaskMessage m_task;
     final Mailbox m_mailbox;
     LinkedBlockingDeque<FragmentResponseMessage> m_newDeps;
-    HashMap<Integer, List<Pair<Integer, FragmentResponseMessage>>> m_trackedDeps;
+    Map<Integer, List<Pair<Integer, FragmentResponseMessage>>> m_remoteDeps;
+    Map<Integer, List<Pair<Integer, FragmentResponseMessage>>> m_localDeps;
+    Set<Integer> m_finalDeps;
+    int[] m_usePartitions;
+    FragmentTaskMessage m_remoteWork = null;
+    FragmentTaskMessage m_localWork = null;
 
-    MpTransactionState(Mailbox mailbox,long txnId,
+    MpTransactionState(Mailbox mailbox, long txnId,
                        TransactionInfoBaseMessage notice,
-                       int[] partitions)
+                       int[] usePartitions)
     {
         super(txnId, notice);
         m_mailbox = mailbox;
         m_task = (Iv2InitiateTaskMessage)notice;
+        m_usePartitions = usePartitions;
     }
 
     @Override
@@ -63,6 +75,7 @@ public class MpTransactionState extends TransactionState
     public boolean isBlocked()
     {
         // TODO Auto-generated method stub
+        // Not clear this method is useful in the new world?
         return false;
     }
 
@@ -99,29 +112,110 @@ public class MpTransactionState extends TransactionState
     public void setupProcedureResume(boolean isFinal, int[] dependencies)
     {
         // Create some record of expected dependencies for tracking
+        m_finalDeps = new HashSet<Integer>();
+        for (int dep : dependencies) {
+            m_finalDeps.add(dep);
+        }
     }
 
     @Override
     public void createLocalFragmentWork(FragmentTaskMessage task, boolean nonTransactional)
     {
+        m_localWork = task;
+        // Create some record of expected dependencies for tracking
+        m_localDeps = createTrackedDependenciesFromTask(task, new int[] {0});
     }
 
     @Override
     public void createAllParticipatingFragmentWork(FragmentTaskMessage task)
     {
-        // Create some record of expected dependencies for tracking
-        // Distribute fragments
+        m_remoteWork = task;
+//        // Distribute fragments
+//        try {
+//            // send to all non-coordinating sites
+//            m_mbox.send(m_nonCoordinatingSites, task);
+//            // send to this site
+//            // IZZY: DO THE RIGHT THING HERE
+//        }
+//        catch (MessagingException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+
+    private Map<Integer, List<Pair<Integer, FragmentResponseMessage>>>
+    createTrackedDependenciesFromTask(FragmentTaskMessage task,
+                                      int[] expectedPartitions)
+    {
+        Map<Integer, List<Pair<Integer, FragmentResponseMessage>>> depMap =
+            new HashMap<Integer, List<Pair<Integer, FragmentResponseMessage>>>();
+        for (int i = 0; i < task.getFragmentCount(); i++) {
+            int dep = task.getOutputDepId(i);
+            ArrayList<Pair<Integer, FragmentResponseMessage>> scoreboard =
+                new ArrayList<Pair<Integer, FragmentResponseMessage>>();
+            depMap.put(dep, scoreboard);
+            for (int partition : expectedPartitions) {
+                scoreboard.add(new Pair<Integer, FragmentResponseMessage>(partition, null));
+            }
+        }
+        return depMap;
     }
 
     @Override
-    public Map<Integer, List<VoltTable>> recursableRun()
+    public Map<Integer, List<VoltTable>> recursableRun(SiteProcedureConnection siteConnection)
     {
-        boolean doneWithCurrentDeps = false;
-        while (!doneWithCurrentDeps)
+        // Do distributed fragments, if any
+        if (m_remoteWork != null)
         {
-            FragmentResponseMessage msg = m_newDeps.poll();
-            doneWithCurrentDeps = handleReceivedFragResponse(msg);
+            // Create some record of expected dependencies for tracking
+            m_remoteDeps = createTrackedDependenciesFromTask(m_remoteWork,
+                                                             m_usePartitions);
+            boolean doneWithRemoteDeps = false;
+            while (!doneWithRemoteDeps)
+            {
+                FragmentResponseMessage msg = m_newDeps.poll();
+                doneWithRemoteDeps = handleReceivedFragResponse(msg);
+            }
         }
+
+        // Next do local fragment stuff
+        // Inject input deps for the local frags into the EE
+        // use siteConnection.stashWorkUnitDependencies()
+        Map<Integer, List<VoltTable>> deps = extractDepTablesFromResponses();
+        siteConnection.stashWorkUnitDependencies(deps);
+
+        // Then execute the fragment task.  Looks like ExecutionSite.processFragmentTask(),
+        // kinda, at least for now while we're executing stuff locally.
+        // Probably don't need to generate a FragmentResponse
+        Map<Integer, List<VoltTable>> results = processLocalFragmentTask();
+        return results;
+    }
+
+    private boolean handleReceivedFragResponse(FragmentResponseMessage msg)
+    {
+        // WRITE ME
+        return false;
+    }
+
+    private Map<Integer, List<VoltTable>> extractDepTablesFromResponses()
+    {
+        Map<Integer, List<VoltTable>> depTables =
+            new HashMap<Integer, List<VoltTable>>();
+
+        // WRITE ME
+
+
+        return depTables;
+    }
+
+    private Map<Integer, List<VoltTable>> processLocalFragmentTask()
+    {
+        Map<Integer, List<VoltTable>> depResults =
+            new HashMap<Integer, List<VoltTable>>();
+
+        // WRITE ME
+
+
+        return depResults;
     }
 
     // Runs from Mailbox's network thread
