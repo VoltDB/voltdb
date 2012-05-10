10db396bf92f00e7c3bc7f60af67dd8821e713e5
Mike Ismert <izzy@voltdb.com>
2012-05-10 11:57:50 -0400

    Add TransactionTaskQueue and test

---
+++ src/frontend/org/voltdb/iv2/TransactionTaskQueue.java
@@ -0,0 +1,91 @@
+package org.voltdb.iv2;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TransactionTaskQueue
+{
+    final private Deque<TransactionTask> m_backlog =
+        new ArrayDeque<TransactionTask>();
+    final private SiteTaskerQueue m_taskQueue;
+
+    TransactionTaskQueue(SiteTaskerQueue queue)
+    {
+        m_taskQueue = queue;
+    }
+
+    /**
+     * If necessary, stick this task in the backlog.
+     * Many network threads may be racing to reach here, synchronize to
+     * serialize queue order
+     * @param task
+     * @return true if this task was stored, false if not
+     */
+    synchronized boolean offer(TransactionTask task)
+    {
+        boolean retval = false;
+        // Single partitions never queue if empty
+        // Multipartitions always queue
+        // Fragments queue if they're not part of the queue head TXN ID
+        // offer to SiteTaskerQueue if:
+        // the queue was empty
+        // the queue wasn't empty but the txn IDs matched
+        if (!m_backlog.isEmpty()) {
+            if (task.getMpTxnId() != m_backlog.getFirst().getMpTxnId())
+            {
+                m_backlog.addLast(task);
+                retval = true;
+            }
+            else {
+                m_taskQueue.offer(task);
+            }
+        }
+        else {
+            if (!task.getTransactionState().isSinglePartition()) {
+                m_backlog.addLast(task);
+                retval = true;
+            }
+            m_taskQueue.offer(task);
+        }
+        return retval;
+    }
+
+    /**
+     * Try to offer as many runnable Tasks to the SiteTaskerQueue as possible.
+     * Currently just blocks on the next uncompleted multipartition transaction
+     * @return
+     */
+    synchronized int flush()
+    {
+        int offered = 0;
+        // check to see if head is done
+        // then offer until the next MP or FragTask
+        if (!m_backlog.isEmpty()) {
+            if (m_backlog.getFirst().getTransactionState().isDone()) {
+                // remove the completed MP txn
+                m_backlog.removeFirst();
+                while (!m_backlog.isEmpty()) {
+                    TransactionTask next = m_backlog.getFirst();
+                    m_taskQueue.offer(next);
+                    ++offered;
+                    if (next.getTransactionState().isSinglePartition()) {
+                        m_backlog.removeFirst();
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+        }
+        return offered;
+    }
+
+    /**
+     * How many Tasks are un-runnable?
+     * @return
+     */
+    synchronized int size()
+    {
+        return m_backlog.size();
+    }
+}
---
+++ tests/frontend/org/voltdb/iv2/Iv2TestTransactionTaskQueue.java
@@ -0,0 +1,162 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2012 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.voltdb.iv2;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.voltcore.messaging.Mailbox;
+import org.voltdb.dtxn.TransactionState;
+import org.voltdb.messaging.CompleteTransactionMessage;
+import org.voltdb.messaging.FragmentTaskMessage;
+import org.voltdb.messaging.Iv2InitiateTaskMessage;
+
+import static org.mockito.Mockito.*;
+
+public class Iv2TestTransactionTaskQueue extends TestCase
+{
+
+    // Cases to test:
+    // several single part txns
+
+    private SpProcedureTask createSpProc(long localTxnId)
+    {
+        Iv2InitiateTaskMessage init = mock(Iv2InitiateTaskMessage.class);
+        SpProcedureTask task =
+            new SpProcedureTask(null, null, localTxnId, init);
+        return task;
+    }
+
+    // Create the first fragment of a MP txn
+    private FragmentTask createFrag(long localTxnId, long mpTxnId)
+    {
+        FragmentTaskMessage msg = mock(FragmentTaskMessage.class);
+        when(msg.getTxnId()).thenReturn(mpTxnId);
+        ParticipantTransactionState pft =
+            new ParticipantTransactionState(localTxnId, msg);
+        FragmentTask task =
+            new FragmentTask(null, pft, msg);
+        return task;
+    }
+
+    // Create follow-on fragments of an MP txn
+    private FragmentTask createFrag(TransactionState txn, long mpTxnId)
+    {
+        FragmentTaskMessage msg = mock(FragmentTaskMessage.class);
+        when(msg.getTxnId()).thenReturn(mpTxnId);
+        FragmentTask task =
+            new FragmentTask(null, (ParticipantTransactionState)txn, msg);
+        return task;
+    }
+
+    private CompleteTransactionTask createComplete(TransactionState txn,
+                                                   long mpTxnId)
+    {
+        CompleteTransactionMessage msg = mock(CompleteTransactionMessage.class);
+        when(msg.getTxnId()).thenReturn(mpTxnId);
+        CompleteTransactionTask task =
+            new CompleteTransactionTask(txn, msg);
+        return task;
+    }
+
+    private void addTask(TransactionTask task, TransactionTaskQueue dut,
+                         Deque<TransactionTask> teststorage)
+    {
+        teststorage.addLast(task);
+        dut.offer(task);
+        dut.flush();
+    }
+
+    @Test
+    public void testBasicParticipantOps() throws InterruptedException
+    {
+        long localTxnId = 0;
+        long mpTxnId = 0;
+        SiteTaskerQueue task_queue = new SiteTaskerQueue();
+        TransactionTaskQueue dut = new TransactionTaskQueue(task_queue);
+        Deque<TransactionTask> expected_order =
+            new ArrayDeque<TransactionTask>();
+
+        // add a few SP procs
+        TransactionTask next = createSpProc(localTxnId++);
+        addTask(next, dut, expected_order);
+        next = createSpProc(localTxnId++);
+        addTask(next, dut, expected_order);
+        next = createSpProc(localTxnId++);
+        addTask(next, dut, expected_order);
+        // Should squirt on through the queue
+        assertEquals(0, dut.size());
+
+        // Now a fragment task to block things
+        long blocking_mp_txnid = mpTxnId;
+        next = createFrag(localTxnId++, mpTxnId++);
+        TransactionTask block = next;
+        addTask(next, dut, expected_order);
+        assertEquals(1, dut.size());
+
+        // Add some tasks that are going to be blocked
+        ArrayDeque<TransactionTask> blocked = new ArrayDeque<TransactionTask>();
+        next = createSpProc(localTxnId++);
+        addTask(next, dut, blocked);
+        next = createSpProc(localTxnId++);
+        addTask(next, dut, blocked);
+
+        // here's our next blocker
+        long next_blocking_local_txnid = localTxnId;
+        long next_blocking_mp_txnid = mpTxnId;
+        next = createFrag(localTxnId++, mpTxnId++);
+        TransactionTask next_block = next;
+        addTask(next, dut, blocked);
+        assertEquals(blocked.size() + 1, dut.size());
+
+        // now, do more work on the blocked task
+        next = createFrag(block.getTransactionState(), blocking_mp_txnid);
+        addTask(next, dut, expected_order);
+        // Should have passed through and not be in the queue
+        assertEquals(blocked.size() + 1, dut.size());
+
+        // now, complete the blocked task
+        next = createComplete(block.getTransactionState(), blocking_mp_txnid);
+        addTask(next, dut, expected_order);
+        // Should have passed through and not be in the queue
+        assertEquals(blocked.size() + 1, dut.size());
+        // DONE!  Should flush everything to the next blocker
+        block.getTransactionState().setDone();
+        int offered = dut.flush();
+        assertEquals(blocked.size(), offered);
+        assertEquals(1, dut.size());
+        expected_order.addAll(blocked);
+
+        while (!expected_order.isEmpty())
+        {
+            TransactionTask next_poll = (TransactionTask)task_queue.poll();
+            TransactionTask expected = expected_order.removeFirst();
+            assertEquals(expected.getLocalTxnId(), next_poll.getLocalTxnId());
+            assertEquals(expected.getMpTxnId(), next_poll.getMpTxnId());
+        }
+    }
+}
