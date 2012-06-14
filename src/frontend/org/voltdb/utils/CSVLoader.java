e778559581a1b84da369fdf06aa6918d2a8123c0
Xin Jia <Xin_Jia@voltdb.com>
2012-06-14 13:54:38 -0400

    Name mistake

--- src/frontend/org/voltdb/utils/CSVLoader.java
+++ src/frontend/org/voltdb/utils/CSVLoader.java
@@ -86,7 +86,6 @@ public class CSVLoader {
 		public void clientCallback(ClientResponse response) throws Exception {
 			if (response.getStatus() != ClientResponse.SUCCESS) {
 				System.err.println(response.getStatusString());
-				System.err.println("<xin>Stop at line " + m_lineNum);
 				synchronized (errorInfo) {
 					if (!errorInfo.containsKey(m_lineNum)) {
 						String[] info = {m_rowdata, response.getStatusString()};
@@ -307,11 +306,9 @@ public class CSVLoader {
 
 	private static String checkparams_trimspace(String[] linefragement, int columnCnt ) {
 		if( linefragement.length == 1 && linefragement[0].equals( "" ) ) {
-			System.err.println("<zheng>Stop at line " + (outCount.get()) );
 			return "Error: blank line";
 		}
 		if( linefragement.length != columnCnt ){
-			System.err.println("<zheng>Stop at line " + (outCount.get()) );
 			return "Error: # of attributes do not match, # of attributes needed: " + columnCnt + "# of attributes inputed: " + linefragement.length;
 		}
 
@@ -349,8 +346,8 @@ public class CSVLoader {
 		String myinsert = insertProcedure;
 		myinsert = myinsert.replaceAll("\\.", "_");
 		pathInvalidrowfile = config.reportdir + myinsert + "_" + "csvloaderinvalidrows.csv";
-		pathLogfile =  config.reportdir + myinsert + "_"+ "csvloaderReport.log";
-		pathReportfile = config.reportdir  + myinsert + "_"+ "csvloaderLog.log";
+		pathLogfile =  config.reportdir + myinsert + "_" + "csvloaderLog.log";
+		pathReportfile = config.reportdir  + myinsert + "_" + "csvloaderReport.log";
 
 		try {
 			out_invaliderowfile = new BufferedWriter(new FileWriter(pathInvalidrowfile));
