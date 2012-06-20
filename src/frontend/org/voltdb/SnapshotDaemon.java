4d1c75dbd63e824ae9b4d84827d32fb6d1a2f458
Mike Ismert <izzy@voltdb.com>
2012-06-20 10:17:44 -0400

    Use the for-snapshot SiteTracker call so IV2 snapshots pick up the IV2 site info.

--- src/frontend/org/voltdb/SnapshotDaemon.java
+++ src/frontend/org/voltdb/SnapshotDaemon.java
@@ -519,7 +519,7 @@ public class SnapshotDaemon implements SnapshotCompletionInterest {
                      */
                     JSONObject obj = new JSONObject(clientResponse.getAppStatusString());
                     final long snapshotTxnId = Long.valueOf(obj.getLong("txnId"));
-                    int hosts = VoltDB.instance().getSiteTracker()
+                    int hosts = VoltDB.instance().getSiteTrackerForSnapshot()
                                       .getAllHosts().size();
                     try {
                         m_zk.delete(VoltZK.request_truncation_snapshot, -1);
