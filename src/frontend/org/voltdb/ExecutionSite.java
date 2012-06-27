473cfe89e0f7a9e19c8097bdd6157a51b226266d
John Hugg <jhugg@voltdb.com>
2012-06-27 17:36:22 -0400

    ENG-2858: Additional unit test for rejoin/replay transaction states. It's kinda horrible, but should go away when IV2 arrives.

--- src/frontend/org/voltdb/ExecutionSite.java
+++ src/frontend/org/voltdb/ExecutionSite.java
@@ -150,7 +150,7 @@ implements Runnable, SiteTransactionConnection, SiteProcedureConnection, SiteSna
     protected SiteTracker m_tracker;
 
     final long m_siteId;
-    public final long getSiteId() {
+    public long getSiteId() {
         return m_siteId;
     }
 
@@ -2486,7 +2486,7 @@ implements Runnable, SiteTransactionConnection, SiteProcedureConnection, SiteSna
      * when the transaction ID changes.
      */
     @Override
-    public final void beginNewTxn(TransactionState txnState)
+    public void beginNewTxn(TransactionState txnState)
     {
         if (m_txnlog.isTraceEnabled())
         {
@@ -2502,7 +2502,7 @@ implements Runnable, SiteTransactionConnection, SiteProcedureConnection, SiteSna
         }
     }
 
-    public final void rollbackTransaction(TransactionState txnState)
+    public void rollbackTransaction(TransactionState txnState)
     {
         if (m_txnlog.isTraceEnabled())
         {
