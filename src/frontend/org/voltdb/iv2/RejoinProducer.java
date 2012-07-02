050c2b8160f05dc7878d917bc8a24896167604e6
Mike Ismert <izzy@voltdb.com>
2012-07-02 14:00:01 -0400

    Changes to use the blocking take() call on StreamSnapshotSink

--- src/frontend/org/voltdb/iv2/RejoinProducer.java
+++ src/frontend/org/voltdb/iv2/RejoinProducer.java
@@ -29,6 +29,8 @@ import org.json_voltpatches.JSONException;
 import org.json_voltpatches.JSONObject;
 import org.json_voltpatches.JSONStringer;
 
+import org.voltcore.logging.VoltLogger;
+
 import org.voltcore.utils.Pair;
 
 import org.voltdb.client.ClientResponse;
@@ -59,6 +61,8 @@ import org.voltdb.VoltTable;
  */
 public class RejoinProducer extends SiteTasker
 {
+    private static final VoltLogger hostLog = new VoltLogger("HOST");
+
     private final SiteTaskerQueue m_taskQueue;
     private final int m_partitionId;
     private InitiatorMailbox m_mailbox;
@@ -185,18 +189,14 @@ public class RejoinProducer extends SiteTasker
             @Override
             public void run()
             {
+                Pair<Integer, ByteBuffer> rejoinWork = null;
                 try {
-                Thread.sleep(30000);
-                } catch (Exception e) { }
-                Pair<Integer, ByteBuffer> rejoinWork;
-                do {
-                    rejoinWork = m_rejoinSiteProcessor.poll();
-                    if (rejoinWork != null) {
-                        firstWork.set(rejoinWork);
-                    }
+                    rejoinWork = m_rejoinSiteProcessor.take();
+                } catch (InterruptedException e) {
+                    VoltDB.crashLocalVoltDB("Interrupted in take()ing first snapshot block for rejoin",
+                                            true, e);
                 }
-                while (rejoinWork == null);
-
+                firstWork.set(rejoinWork);
                 // run the rejoin task (ourself) in the Site
                 m_taskQueue.offer(RejoinProducer.this);
             }
@@ -254,12 +254,14 @@ public class RejoinProducer extends SiteTasker
      */
     @Override
     public void run(SiteProcedureConnection siteConnection) {
-        restoreBlock(firstWork.get(), siteConnection);
-
-        while (!m_rejoinSiteProcessor.isEOF()) {
-            Pair<Integer, ByteBuffer> rejoinWork = m_rejoinSiteProcessor.poll();
-            if (rejoinWork != null) {
-                restoreBlock(rejoinWork, siteConnection);
+        Pair<Integer, ByteBuffer> rejoinWork = firstWork.get();
+        while (rejoinWork != null) {
+            restoreBlock(rejoinWork, siteConnection);
+            try {
+                rejoinWork = m_rejoinSiteProcessor.take();
+            } catch (InterruptedException e) {
+                hostLog.warn("RejoinProducer interrupted at take()");
+                rejoinWork = null;
             }
         }
         // m_recoveryLog.debug("Rejoin snapshot transfer is finished");
