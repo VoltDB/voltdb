efabf4c97bcf0bfa9a58508dbe9b8f38c6c6cfd4
Ariel Weisberg <aweisberg@voltdb.com>
2012-07-26 15:23:14 -0400

    First stab at validating that only the expected number of hosts are present at startup. For ENG-3366

--- src/frontend/org/voltcore/messaging/HostMessenger.java
+++ src/frontend/org/voltcore/messaging/HostMessenger.java
@@ -622,8 +622,14 @@ public class HostMessenger implements SocketJoiner.JoinHandler, InterfaceToMesse
         try {
             while (true) {
                 ZKUtil.FutureWatcher fw = new ZKUtil.FutureWatcher();
-                if (m_zk.getChildren(CoreZK.hosts, fw).size() == expectedHosts) {
+                final int numChildren = m_zk.getChildren(CoreZK.hosts, fw).size();
+                if ( numChildren == expectedHosts) {
                     break;
+                }
+                if ( numChildren > expectedHosts) {
+                    org.voltdb.VoltDB.crashLocalVoltDB("Expected to find " + expectedHosts +
+                            " hosts in cluster at startup but found " + numChildren +
+                            ".  Terminating this host.", false, null);
                 }
                 fw.get();
             }
