6980f0103df2062ae0cf1cc7dfb2954eb44a9a77
Xin Jia <jiaxincs@gmail.com>
2012-07-28 17:23:10 -0400

    Fix the failed sqlcoverage test case, Add the test case for the bug of two table join on two column index

--- tests/frontend/org/voltdb/regressionsuites/TestIndexColumnLessThanSuite.java
+++ tests/frontend/org/voltdb/regressionsuites/TestIndexColumnLessThanSuite.java
@@ -28,7 +28,6 @@ import junit.framework.Test;
 import org.voltdb.BackendTarget;
 import org.voltdb.VoltTable;
 import org.voltdb.client.Client;
-import org.voltdb.client.ClientResponse;
 import org.voltdb.compiler.VoltProjectBuilder;
 import org.voltdb_testprocs.regressionsuites.sqlfeatureprocs.BatchedMultiPartitionTest;
 public class TestIndexColumnLessThanSuite extends RegressionSuite {
@@ -45,44 +44,55 @@ public class TestIndexColumnLessThanSuite extends RegressionSuite {
         super(name);
     }
 
-    public void testOneColumnIndexLessThan() throws Exception {
-        Client client = getClient();
-
-//        client.callProcedure("P1.insert", 1, "1", 1, 1);
-//        client.callProcedure("P1.insert", 2, "1", 1, 2);
-//        client.callProcedure("P1.insert", 3, "1", 1, 3);
+//    public void testFailedSqlcoverageCase() throws Exception {
+//        Client client = getClient();
 //
-//        client.callProcedure("P2.insert", 1, "1", 1, 1);
-//        client.callProcedure("P2.insert", 2, "1", 1, 2);
-//        client.callProcedure("P2.insert", 3, "1", 1, 3);
-
 //        client.callProcedure("@AdHoc","INSERT INTO P1 VALUES(0, 'JOUmtGnsKFCGOaWvg', -27392, 7.30754864900605705103e-01);");
-        client.callProcedure("@AdHoc","INSERT INTO P1 VALUES(1, 'aXwwPrkDwabomDdAZ', 30036, 1.09425040281800223241e-01);");
+//        client.callProcedure("@AdHoc","INSERT INTO P1 VALUES(1, 'aXwwPrkDwabomDdAZ', 30036, 1.09425040281800223241e-01);");
 //        client.callProcedure("@AdHoc","INSERT INTO P2 VALUES(4, 'xJAmmYpKGJJyflOMw', -7060, 14977);");
 //        client.callProcedure("@AdHoc","INSERT INTO P2 VALUES(5, 'xybOXumtYDvBNhbUs', 13529, -2025);");
+//
+//        VoltTable table;
+//
+//        table = client.callProcedure("@AdHoc","SELECT P1.ID, P2.P2_ID from P1, P2 where P1.ID >= P2.P2_ID order by P1.ID, P2.P2_ID limit 10;").getResults()[0];
+//        assertTrue(table.advanceRow() == false);
+//        
+//        table = client.callProcedure("@AdHoc","SELECT P1.ID, P2.P2_ID from P1, P2 where P2.P2_ID <= P1.ID order by P1.ID, P2.P2_ID limit 10;").getResults()[0];
+//        assertTrue(table.advanceRow() == false);
+//        
+//        table = client.callProcedure("@AdHoc","SELECT P1.ID, P2.P2_ID from P1, P2 where P1.ID <= P2.P2_ID order by P1.ID, P2.P2_ID limit 10;").getResults()[0];
+//        assertTrue(table.getRowCount() == 4);
+//        assertTrue(table.advanceRow());
+//        //System.err.println("RESULT01:\n" + table);
+//        
+//        table = client.callProcedure("@AdHoc","SELECT P1.ID, P2.P2_ID from P1, P2 where P2.P2_ID >= P1.ID order by P1.ID, P2.P2_ID limit 10;").getResults()[0];
+//        assertTrue(table.getRowCount() == 4);
+//        assertTrue(table.advanceRow());
+//        //System.err.println("RESULT02:\n" + table);
+//    }
+    
+    public void testTwoTableJoinBug() throws Exception {
+        Client client = getClient();
 
+        client.callProcedure("P2.insert", 1, "1", 1, 1);
+        client.callProcedure("P2.insert", 2, "1", 1, 2);
+        client.callProcedure("P2.insert", 3, "1", 1, 3);
+        
+        client.callProcedure("P3.insert", 1, "1", 1, 1);
+        client.callProcedure("P3.insert", 2, "1", 1, 2);
+        client.callProcedure("P3.insert", 3, "1", 1, 3);
 
         VoltTable table;
 
-        ClientResponse cr = client.callProcedure("@AdHoc","SELECT P1.ID, P2.P2_ID from P1, P2 where P1.ID >= P2.P2_ID order by P1.ID, P2.P2_ID limit 10;");
-        //assertTrue(table.advanceRow());
-        System.err.println(cr.getStatus() + " " + cr.getStatusString());
-        if (cr.getException() != null)
-            cr.getException().printStackTrace();
-        //System.err.println("RESULT0:\n" + table);
-        assertTrue(true);
+        table = client.callProcedure("@AdHoc","SELECT * from P2, P3 where P2_NUM1 = P3_NUM1 AND P2_NUM2 > P3_NUM2 order by P2.P2_ID, P3.P3_ID limit 10").getResults()[0];
+        assertTrue(table.getRowCount() == 3);
+        assertTrue(table.advanceRow());
+        System.err.println("RESULT11:\n" + table);
 
-//        table = client.callProcedure("@AdHoc","SELECT * from P1, P2 where P1_NUM1 = P2_NUM1 AND P1_NUM2 >= P2_NUM2 order by P1.ID, P2.P2_ID limit 10").getResults()[0];
-//        assertTrue(table.advanceRow());
-//        System.err.println("RESULT1:\n" + table);
-//        assertTrue(true);
-//
-//        table = client.callProcedure("@AdHoc","SELECT * from P1, P2 where P1_NUM1 = P2_NUM1 AND P2_NUM2 <= P1_NUM2 order by P1.ID, P2.P2_ID limit 10").getResults()[0];
-//        assertTrue(table.advanceRow());
-//        System.err.println("RESULT2:\n" + table);
-//        assertTrue(true);
-
-
+        table = client.callProcedure("@AdHoc","SELECT * from P2, P3 where P2_NUM1 = P3_NUM1 AND P3_NUM2 < P2_NUM2 order by P2.P2_ID, P3.P3_ID limit 10").getResults()[0];
+        assertTrue(table.getRowCount() == 3);
+        assertTrue(table.advanceRow());
+        System.err.println("RESULT12:\n" + table);
     }
 
     /**
@@ -118,7 +128,7 @@ public class TestIndexColumnLessThanSuite extends RegressionSuite {
         /////////////////////////////////////////////////////////////
 
         // get a server config for the native backend with one sites/partitions
-        config = new LocalCluster("sql-onesite.jar", 1, 1, 0, BackendTarget.NATIVE_EE_IPC);
+        config = new LocalCluster("sql-onesite.jar", 1, 1, 0, BackendTarget.NATIVE_EE_JNI);
 
         // build the jarfile
         success = config.compile(project);
