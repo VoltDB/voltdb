d22308a1b7498c78877951ca6e2ac990a3888e5b
John Hugg <jhugg@voltdb.com>
2012-08-01 14:05:31 -0400

    Allow parsing of decimals with too many zeros at the end.

--- src/frontend/org/voltdb/types/VoltDecimalHelper.java
+++ src/frontend/org/voltdb/types/VoltDecimalHelper.java
@@ -221,9 +221,12 @@ public class VoltDecimalHelper {
         if (decimal == null) {
             return null;
         }
-        final BigDecimal bd = new BigDecimal(decimal);
+        BigDecimal bd = new BigDecimal(decimal);
         if (bd.scale() > 12) {
-            throw new IOException("Decimal " + bd + " has more then 12 digits of scale");
+            bd = bd.stripTrailingZeros();
+            if (bd.scale() > 12) {
+                throw new IOException("Decimal " + bd + " has more then 12 digits of scale");
+            }
         }
         if (bd.precision() > 38) {
             throw new RuntimeException("Decimal " + bd + " has more than  38 digits of precision.");
