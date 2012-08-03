05a2c7a56cf96837273484e0fb12eee42c7d83a3
Steve Cooper <github@wijjo.com>
2012-08-03 17:53:17 -0400

    Fix run.sh scripts after Debian changes to hide error message from which command on CentOS.

--- tests/test_apps/genqa/run.sh
+++ tests/test_apps/genqa/run.sh
@@ -4,7 +4,7 @@ APPNAME="genqa"
 APPNAME2="genqa2"
 
 # find voltdb binaries in either installation or distribution directory.
-if [ -n "$(which voltdb)" ]; then
+if [ -n "$(which voltdb 2> /dev/null)" ]; then
     VOLTDB_BIN=$(dirname "$(which voltdb)")
 else
     VOLTDB_BIN="$(pwd)/../../../bin"
