287e275f3b7bae7b2ecc7d81649e194ca6eb2705
cyan <cyan@voltdb.com>
2012-09-07 17:30:21 -0400

    This version covers all 4 examples (inc. helloworld). But with lots of debug lines.

--- tests/scripts/exp_test.py
+++ tests/scripts/exp_test.py
@@ -23,9 +23,6 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 import sys
-# add the path to the volt python client, just based on knowing
-# where we are now
-sys.path.append('../../src/py_client')
 import os.path
 import shutil
 import fnmatch
@@ -33,7 +30,14 @@ import subprocess
 import time
 from optparse import OptionParser
 from subprocess import call # invoke unix/linux cmds
-from voltdbclient import *
+# add the path to the volt python client, just based on knowing
+# where we are now
+sys.path.append('../../src/py_client')
+try:
+    from voltdbclient import *
+except ImportError:
+    sys.path.append('./src/py_client')
+    from voltdbclient import *
 from Query import VoltQueryClient
 #
 #import random
@@ -52,11 +56,24 @@ tail = "tar.gz"
 # http://volt0/kits/candidate/LINUX-voltdb-ent-2.8.1.tar.gz
 root = 'http://volt0/kits/candidate/'
 destDir = "/tmp/"
-#workDir = destDir + "exp_test/"
-workDir = destDir + sys.argv[0].replace(".py", "")
 elem2Test = {'helloworld':'run.sh', 'voltcache':'run.sh', 'voltkv':'run.sh', 'voter':'run.sh'}
 defaultHost = "localhost"
 defaultPort = 21212
+
+def findSectionInFile(srce, start, end):
+    flag = 0
+    status = False
+    ins = open(srce, "r" )
+    array = []
+    for line in ins:
+        if(line.find(start) > -1):
+            flag = 1
+        if(flag == 1):
+            array.append( line )
+        if(line.find(end) > -1):
+            flag = 0
+            status = True
+    return (status, array)
 
 def readFirstLine(srce):
     firstline = None
@@ -124,8 +141,8 @@ def installVoltDB(pkg, release):
                     info['pkgname'] = pkgname
                     info['workDir'] = workDir
                     info['ok'] = True
-#    info['ok'] = False
     return info
+# end of installVoltDB(pkg, release):
 
 def setExecutables(dname, suite):
     testSuiteList = {}
@@ -173,17 +190,83 @@ def getClient():
 
     return client
 
+def runVoter(service, logS, logC):
+    print "In runVoter(), service = '%s'" % service
+    (ps, serviceHandle) = startService(service, logS, logC)
+
+    print "serviceHandle's type: '%s'" % type(serviceHandle)
+    serviceHandle.onecmd("adhoc select count(*) from votes")
+    time.sleep(41)
+    stopService(ps, serviceHandle)
+
 def runHelloWorld(service, logS, logC):
     print "In runHelloWorld(), service = '%s'" % service
+    (ps, serviceHandle) = startService(service, logS, logC)
+    stopService(ps, serviceHandle)
+
+def startService(service, logS, logC):
     service_ps = subprocess.Popen(service + " > " + logS + " 2>&1", shell=True)
     time.sleep(2)
     client = getClient()
 #    client_ps = subprocess.Popen(service + " client", shell = True)
     ret = call(service + " client > " + logC + " 2>&1", shell=True)
-    print "returning results from helloworld: '%s'" % ret
+    print "returning results from service execution: '%s'" % ret
     time.sleep(1)
+    return (service_ps, client)
+
+def stopService(ps, serviceHandle):
+    serviceHandle.onecmd("shutdown")
+    ps.communicate()
+
+def execThisService(service, logS, logC):
+    service_ps = subprocess.Popen(service + " > " + logS + " 2>&1", shell=True)
+    time.sleep(2)
+    client = getClient()
+    ret = call(service + " client > " + logC + " 2>&1", shell=True)
+    print "returning results from service execution: '%s'" % ret
     client.onecmd("shutdown")
     service_ps.communicate()
+
+#def assertVotecache(logC):
+# Command Line Configuration
+# Setup & Initialization
+#Starting Benchmark
+# KV Store Results
+# Client Workload Statistics
+# System Server Statistics
+
+def assertVotekv_Votecache(logC):
+    keyStr = {
+"Command Line Configuration":1,
+"Setup & Initialization":1,
+"Starting Benchmark":1,
+"KV Store Results":1,
+"Client Workload Statistics":1,
+"System Server Statistics":1,
+    }
+    with open(logC) as f:
+        content = f.readlines()
+    print "content length: '%d'" % len(content)
+    cnt = 0
+    for line in content:
+        x = line.strip()
+        if x in keyStr.keys():
+#            print "==-->>x = '%s'" % x
+            cnt += 1
+
+#    print "keyStr length: '%d', cnt = '%d'" % (len(keyStr), cnt)
+    result = False
+    if(cnt == len(keyStr)):
+        result = True
+    return result
+
+def assertVoter(logC):
+    result = False
+    aStr = "Voting Results"
+    expected = "The Winner is: Edwina Burnam"
+    (result, section) = findSectionInFile(logC, aStr, expected)
+    print "In assertVoter: expected = '%s', section length: '%s'" % (expected, len(section))
+    return result
 
 def assertHelloWorld(logC):
     result = False
@@ -204,46 +287,55 @@ def startTest(testSuiteList):
         logFileS = "/tmp/" + e + "_server"
         logFileC = "/tmp/" + e + "_client"
         print "logFileS = '%s', logFileC = '%s'" % (logFileS, logFileC)
+#        execThisService(service, logFileS, logFileC)
         if(e == "helloworld"):
-            runHelloWorld(service, logFileS, logFileC)
-            print "calling assertThis() from startTest()"
+#            runHelloWorld(service, logFileS, logFileC)
             result = assertHelloWorld(logFileC)
+            retCode[e] = result
+        elif(e == "voter"):
+#            runVoter(service, logFileS, logFileC)
+            result = assertVoter(logFileC)
+            retCode[e] = result
+        elif(e == "voltkv" or e == "voltcache"):
+#            execThisService(service, logFileS, logFileC)
+            result = assertVotekv_Votecache(logFileC)
             retCode[e] = result
         else:
             print "e = '%s' ==-->> to be implemented..." % e
-#        print "elem: '%s', work dir: '%s'" % (e, testSuiteList[e])
-#        print "service_ps = '%s', PID = '%d'" % (service_ps, service_ps.pid)
-#        stopPS(service_ps)
-        print "origDir = '%s'" % origDir
-        os.chdir(origDir)
-        currDir = os.getcwd()
-        print "should be back to the original dir: '%s'" % (currDir)
+            retCode[e] = False
 
+#        print "origDir = '%s'" % origDir
+        os.chdir(origDir)
+#        currDir = os.getcwd()
+#        print "should be back to the original dir: '%s'" % (currDir)
+    # end of for e in testSuiteList:
     return retCode
 
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-r", "--release", dest="release",
-                      help="VoltDB release no. ")
+                      help="VoltDB release no. If ommitted, it will find it from version.txt.")
     parser.add_option("-p", "--pkg", dest="pkg",
-                      help="VoltDB package type: Community or Pro")
+                      help="VoltDB package type: Community or Pro. Defalut is Community.")
     parser.add_option("-s", "--suite", dest="suite",
                       help="Test suite name, if not set, then take all suites")
 
+    parser.set_defaults(pkg="comm")
+    parser.set_defaults(suite="all")
     (options, args) = parser.parse_args()
+    workDir = destDir + os.path.basename(os.path.abspath(__file__)).replace(".py", "")
 
+    print "============================================================"
+#    print "argv[0] = '%s'" % (sys.argv[0])
     print "options = '%s'" % options
-    print "args = '%s'" % args
-    print "options.release = '%s' options.pkg = '%s'" % (options.release, options.pkg)
     print "workDir = '%s'" % workDir
+    print "options.release = '%s' options.pkg = '%s'" % (options.release, options.pkg)
     suite = options.suite
-    if suite not in elem2Test.keys():
+    if suite not in elem2Test.keys() and suite != "all":
         print "Warning: unknown suite name - '%s'" % suite
         print "Info: So we're going to cover all test suites in this run"
-        suite = "all"
 
     origDir = os.getcwd()
-#    print "cwd = '%s'" % cwd
 
     ret = installVoltDB(options.pkg, options.release)
 
