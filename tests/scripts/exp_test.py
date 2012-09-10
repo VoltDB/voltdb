47dd98a093d3e2f5c0896ce6ab2a0712953c6d30
cyan <cyan@voltdb.com>
2012-09-10 13:58:05 -0400

    Still a draft but close to a complete stand alone version.

--- tests/scripts/exp_test.py
+++ tests/scripts/exp_test.py
@@ -28,6 +28,7 @@ import shutil
 import fnmatch
 import subprocess
 import time
+import filecmp
 from optparse import OptionParser
 from subprocess import call # invoke unix/linux cmds
 # add the path to the volt python client, just based on knowing
@@ -39,19 +40,11 @@ except ImportError:
     sys.path.append('./src/py_client')
     from voltdbclient import *
 from Query import VoltQueryClient
-#
-#import random
-#import cPickle
-#import imp
-#import re
-#from SQLCoverageReport import generate_summary
-#from SQLGenerator import SQLGenerator
-#from xml.etree import ElementTree
-#from xml.etree.ElementTree import Element, SubElement
-#from XMLUtils import prettify # To create a human readable xml file
 
 pkgName = {'comm': 'LINUX-voltdb', 'pro': 'LINUX-voltdb-ent'}
 tail = "tar.gz"
+sepLineD = "========================================================"
+sepLineS = "--------------------------------------------"
 # http://volt0/kits/candidate/LINUX-voltdb-2.8.1.tar.gz
 # http://volt0/kits/candidate/LINUX-voltdb-ent-2.8.1.tar.gz
 root = 'http://volt0/kits/candidate/'
@@ -60,42 +53,68 @@ elem2Test = {'helloworld':'run.sh', 'voltcache':'run.sh', 'voltkv':'run.sh', 'vo
 defaultHost = "localhost"
 defaultPort = 21212
 
+# To parse the output of './examples/voter/run.sh client' and get a specific portion
+# of the output. A sample value would be like the one below:
+'''
+ Voting Results
+ --------------------------------------------------------------------------------
+
+ A total of 8166781 votes were received...
+  - 7,816,923 Accepted
+   -    79,031 Rejected (Invalid Contestant)
+     -        12 Rejected (Maximum Vote Count Reached)
+     -         0 Failed (Transaction Error)
+
+    Contestant Name     Votes Received
+    Edwina Burnam            2,156,993
+    Jessie Eichman           1,652,654
+    Alana Bregman            1,189,909
+    Kelly Clauss             1,084,995
+    Jessie Alloway           1,060,892
+    Tabatha Gehling            939,604
+
+    The Winner is: Edwina Burnam
+'''
 def findSectionInFile(srce, start, end):
     flag = 0
     status = False
     ins = open(srce, "r" )
-    array = []
+    str = None
     for line in ins:
         if(line.find(start) > -1):
             flag = 1
         if(flag == 1):
-            array.append( line )
+            if(str == None):
+                str = line
+            else:
+                str += line
         if(line.find(end) > -1):
             flag = 0
             status = True
-    return (status, array)
+    return (status, str)
 
+# To read the first line of a srouce file 'srce'
 def readFirstLine(srce):
     firstline = None
     if(os.path.getsize(srce) > 0):
         with open(srce) as f:
             content = f.readlines()
         firstline = content[0].rstrip()
-        print "firstline = '%s'" % firstline
     return firstline
 
+# The release number can be optionally passed in from cmdline with -r switch
+# If it's ommitted at cmdline, then this function is called to get the release
+# number from 'version.txt'
 def getReleaseNum():
     cwd = os.path.realpath(__file__)
     path = os.path.dirname(os.path.abspath(__file__))
     root = path.replace("tests/scripts", "")
-#    print "In getReleaseNum path = '%s'" % path
-#    print "In getReleaseNum root = '%s'" % root
     verFile = root + "version.txt"
-#    print "In getReleaseNum verFile = '%s'" % verFile
     ver = readFirstLine(verFile)
     return ver
 
-def createDir(dir):
+# Always create a fresh new subdir
+def createAFreshDir(dir):
     ret = 0
     if os.path.exists(dir):
         shutil.rmtree(dir)
@@ -105,35 +124,27 @@ def createDir(dir):
         ret = -1
     return ret
 
+# To get a VoltDB tar ball file and untar it in a designated place
 def installVoltDB(pkg, release):
     if(pkg not in pkgName):
         print "Invalid pkg name!"
         exit(1)
-
-    if(release == None):
-        release = getReleaseNum()
 
     pkgname = pkgName[pkg] + '-' + release + "." + tail
     srce = root + pkgname
     dest = destDir + pkgname
     cmd = "wget " + srce + " -O " + dest + " 2>/dev/null"
 
-#    print "In installVoltDB, root = '%s', release = '%s'" % (root, release)
-#    print "In installVoltDB, pkgname = '%s'" % (pkgname)
-#    print "In installVoltDB, srce = '%s'" % (srce)
     ret = call(cmd, shell=True)
-#    print "In installVoltDB, cmd = '%s'" % (cmd)
-#    print "In installVoltDB, ret = '%s'" % (ret)
     info = {}
     info['ok'] = False
     if ret == 0 and os.path.exists(dest):
         fsize = os.path.getsize(dest)
         if fsize > 0:
-#           print "File '%s' exists!! Filesize = '%d'" % (dest, fsize)
-            ret = createDir(workDir)
+            ret = createAFreshDir(workDir)
             if ret == 0:
                 cmd = "tar zxf " + dest + " -C " + workDir + " 2>/dev/null"
-                print "cmd = '%s'" % cmd
+#                print "cmd = '%s'" % cmd
                 ret = call(cmd, shell=True)
                 if ret == 0:
                     info['dest'] = dest
@@ -144,7 +155,13 @@ def installVoltDB(pkg, release):
     return info
 # end of installVoltDB(pkg, release):
 
-def setExecutables(dname, suite):
+# Test Suite can be optionally passed in from cmdline with the swtich '-s'
+# Sample key/val pairs for testSuiteList are:
+# key: voltcache,   val: /tmp/exp_test/voltdb-2.8.1/examples/voltcache
+# key: voter,       val: /tmp/exp_test/voltdb-2.8.1/examples/voter
+# key: voltkv,      val: /tmp/exp_test/voltdb-2.8.1/examples/voltkv
+# key: helloworld', val: /tmp/exp_test/voltdb-2.8.1/doc/tutorials/helloworld
+def setTestSuite(dname, suite):
     testSuiteList = {}
     for dirname, dirnames, filenames in os.walk(dname):
         for subdirname in dirnames:
@@ -154,14 +171,14 @@ def setExecutables(dname, suite):
                 if(os.access(run_sh, os.X_OK)):
                     if(suite != "all"):
                         if(path.find(suite) > -1):
-#                            testSuiteList[suite] = run_sh
                             testSuiteList[suite] = path
                     else:
                         if(path.find(subdirname) > -1):
-#                            testSuiteList[subdirname] = run_sh
                             testSuiteList[subdirname] = path
     return testSuiteList
 
+# Not used yet.
+# It would be necessary if we wanted to run certain queries before 
 def stopPS(ps):
     print "Going to kill this process: '%d'" % ps.id
     killer = subprocess.Popen("kill -9 %d" % (ps.pid), shell = True)
@@ -170,6 +187,7 @@ def stopPS(ps):
 #        print >> sys.stderr, "Failed to kill the server process %d" % (server.pid)
         print "Failed to kill the server process %d" % (ps.pid)
 
+# To return a voltDB client
 def getClient():
     host = defaultHost
     port = defaultPort
@@ -190,34 +208,27 @@ def getClient():
 
     return client
 
-def runVoter(service, logS, logC):
-    print "In runVoter(), service = '%s'" % service
-    (ps, serviceHandle) = startService(service, logS, logC)
-
-    print "serviceHandle's type: '%s'" % type(serviceHandle)
-    serviceHandle.onecmd("adhoc select count(*) from votes")
-    time.sleep(41)
-    stopService(ps, serviceHandle)
-
-def runHelloWorld(service, logS, logC):
-    print "In runHelloWorld(), service = '%s'" % service
-    (ps, serviceHandle) = startService(service, logS, logC)
-    stopService(ps, serviceHandle)
-
+# Not used yet. 
+# Currently, both startService() and stopService() are implemented in 
+# execThisService(). However, if we wanted to run certain queries before 
+# shutdown VoltDB, we have to separate startService() and stopService(),
+# so that we can add more implementations in between.
 def startService(service, logS, logC):
     service_ps = subprocess.Popen(service + " > " + logS + " 2>&1", shell=True)
     time.sleep(2)
     client = getClient()
-#    client_ps = subprocess.Popen(service + " client", shell = True)
     ret = call(service + " client > " + logC + " 2>&1", shell=True)
     print "returning results from service execution: '%s'" % ret
     time.sleep(1)
     return (service_ps, client)
 
+# Not used yet. Refer to the comments for startService()
 def stopService(ps, serviceHandle):
     serviceHandle.onecmd("shutdown")
     ps.communicate()
 
+# To execute 'run.sh' and save the output in logS
+# To execute 'run.sh client' and save the output in logC
 def execThisService(service, logS, logC):
     service_ps = subprocess.Popen(service + " > " + logS + " 2>&1", shell=True)
     time.sleep(2)
@@ -227,16 +238,11 @@ def execThisService(service, logS, logC):
     client.onecmd("shutdown")
     service_ps.communicate()
 
-#def assertVotecache(logC):
-# Command Line Configuration
-# Setup & Initialization
-#Starting Benchmark
-# KV Store Results
-# Client Workload Statistics
-# System Server Statistics
-
-def assertVotekv_Votecache(logC):
-    keyStr = {
+# Further assertion is required
+# We want to make sure that logFileC contains several key strings
+# which is defined in 'staticKeyStr'
+def assertVotekv_Votecache(mod, logC):
+    staticKeyStr = {
 "Command Line Configuration":1,
 "Setup & Initialization":1,
 "Starting Benchmark":1,
@@ -244,72 +250,127 @@ def assertVotekv_Votecache(logC):
 "Client Workload Statistics":1,
 "System Server Statistics":1,
     }
+
+    dynamicKeyStr = {}
     with open(logC) as f:
         content = f.readlines()
-    print "content length: '%d'" % len(content)
+
     cnt = 0
     for line in content:
         x = line.strip()
-        if x in keyStr.keys():
-#            print "==-->>x = '%s'" % x
+        dynamicKeyStr[x] = 1
+        if x in staticKeyStr.keys():
             cnt += 1
 
-#    print "keyStr length: '%d', cnt = '%d'" % (len(keyStr), cnt)
     result = False
-    if(cnt == len(keyStr)):
+    msg = None
+    if(cnt == len(staticKeyStr)):
+        msg = "The client output has all the expected key words:\n" + sepLineS + "\n"
+        for key in staticKeyStr:
+            msg += key + "\n"
         result = True
-    return result
+    else:
+        msg = "The client output does not have all the expected key words:\n"
+        msg += "The missing keys are:\n" + sepLineS + "\n"
+        for key in staticKeyStr:
+            if key not in dynamicKeyStr.keys():
+                msg += key + "\n"
 
-def assertVoter(logC):
+    return (result, msg)
+
+# We want to make sure that logFileC contains this KEY string:
+# The Winner is: Edwina Burnam
+def assertVoter(mod, logC):
     result = False
     aStr = "Voting Results"
     expected = "The Winner is: Edwina Burnam"
     (result, section) = findSectionInFile(logC, aStr, expected)
-    print "In assertVoter: expected = '%s', section length: '%s'" % (expected, len(section))
-    return result
+    return (result, section)
 
-def assertHelloWorld(logC):
-    result = False
+# To make sure that we see the key string 'Hola, Mundo!'
+def assertHelloWorld(modulename, logC):
     expected = "Hola, Mundo!"
     actual = readFirstLine(logC)
-    print "In assertThis: expected = '%s', actual = '%s'" % (expected, actual)
     if(expected == actual):
+        msg = expected
         result = True
-    return result
+    else:
+        msg = "Expected '%s' for module '%s'. Actually returned: '%s'" % (expected, modulename, actual)
+        result = False
+    return (result, msg)
+
+# To make sure the content of logC which is the output of 'run.sh client'
+# is identical to the static baseline file.
+# If True, the test is PASSED
+# If False, then we need to parse the LogC more carefully before we declare 
+# this test is FAILED
+def assertClient(e, logC):
+    baselineD = origDir + "/plannertester/baseline/"
+    baselineF = baselineD + e + "/client_output.txt"
+#    print "baselineD = '%s', baselineF = '%s'" % (baselineD, baselineF)
+    ret = False
+    msg = None
+    if(os.path.exists(baselineF)):
+        ret = filecmp.cmp(baselineF, logC)
+        if(ret == True):
+            msg = "The client output matches the baseline:"
+        else:
+            msg = "Warning!! The client output does NOT match the baseline:"
+        msg += "\nBaseline: %s" % baselineF
+        msg += "\nThe client output: %s" % logC
+    else:
+        msg = "Warning!! Cannot find the baseline file:\n%s" % baselineF
+    return (ret, msg)
 
 def startTest(testSuiteList):
     retCode = {}
     for e in testSuiteList:
+        retCode[e] = {}
         os.chdir(testSuiteList[e])
         currDir = os.getcwd()
         service = elem2Test[e]
-        print "currDir: '%s', service: '%s'" % (currDir, service)
+        msg = "===--->>> Start to test this suite: %s" % e
         logFileS = "/tmp/" + e + "_server"
         logFileC = "/tmp/" + e + "_client"
-        print "logFileS = '%s', logFileC = '%s'" % (logFileS, logFileC)
-#        execThisService(service, logFileS, logFileC)
+        msg1 = msg2 = None
+#        print "logFileS = '%s', logFileC = '%s'" % (logFileS, logFileC)
+        execThisService(service, logFileS, logFileC)
         if(e == "helloworld"):
-#            runHelloWorld(service, logFileS, logFileC)
-            result = assertHelloWorld(logFileC)
-            retCode[e] = result
+            # To make sure that we see the key string 'Hola, Mundo!'
+            (result, msg1) = assertHelloWorld(e, logFileC)
+            retCode[e]["status"] = result
+            retCode[e]["msg"] = msg + "\n" + sepLineS + "\n" + msg1
         elif(e == "voter"):
-#            runVoter(service, logFileS, logFileC)
-            result = assertVoter(logFileC)
-            retCode[e] = result
+            (result, msg1) = assertClient(e, logFileC)
+            if(result == False):
+                # Further assertion is required
+                # We want to make sure that logFileC contains this KEY string:
+                # The Winner is: Edwina Burnam
+                (result, msg2) = assertVoter(e, logFileC)
+                retCode[e]["msg"] = msg + "\n" + sepLineS + "\n" + msg1 + "\n" + sepLineS + "\n" + msg2
+            else:
+                retCode[e]["msg"] = msg + "\n" + sepLineS + "\n" + msg1
+            retCode[e]["status"] = result
         elif(e == "voltkv" or e == "voltcache"):
-#            execThisService(service, logFileS, logFileC)
-            result = assertVotekv_Votecache(logFileC)
-            retCode[e] = result
+            (result, msg1) = assertClient(e, logFileC)
+            if(result == False):
+                # Further assertion is required
+                # We want to make sure that logFileC contains several key strings
+                # which is defined inside assertVotekv_Votecache:
+                (result, msg2) = assertVotekv_Votecache(e, logFileC)
+                retCode[e]["msg"] = msg + "\n" + sepLineS + "\n" + msg1 + "\n" + sepLineS + "\n" + msg2
+            else:
+                retCode[e]["msg"] = msg + "\n" + sepLineS + "\n" + msg1
+            retCode[e]["status"] = result
         else:
             print "e = '%s' ==-->> to be implemented..." % e
-            retCode[e] = False
+            retCode[e]["status"] = False
+            retCode[e]["msg"] = "e = '%s' ==-->> to be implemented..." % e
 
-#        print "origDir = '%s'" % origDir
         os.chdir(origDir)
-#        currDir = os.getcwd()
-#        print "should be back to the original dir: '%s'" % (currDir)
     # end of for e in testSuiteList:
     return retCode
+# end of def startTest(testSuiteList):
 
 if __name__ == "__main__":
     parser = OptionParser()
@@ -324,12 +385,8 @@ if __name__ == "__main__":
     parser.set_defaults(suite="all")
     (options, args) = parser.parse_args()
     workDir = destDir + os.path.basename(os.path.abspath(__file__)).replace(".py", "")
+    # e.g. workDir = '/tmp/exp_test'
 
-    print "============================================================"
-#    print "argv[0] = '%s'" % (sys.argv[0])
-    print "options = '%s'" % options
-    print "workDir = '%s'" % workDir
-    print "options.release = '%s' options.pkg = '%s'" % (options.release, options.pkg)
     suite = options.suite
     if suite not in elem2Test.keys() and suite != "all":
         print "Warning: unknown suite name - '%s'" % suite
@@ -337,7 +394,15 @@ if __name__ == "__main__":
 
     origDir = os.getcwd()
 
-    ret = installVoltDB(options.pkg, options.release)
+    releaseNum = options.release
+    if(releaseNum == None):
+        releaseNum = getReleaseNum()
+
+    print "############################################"
+    print "Tested Version in this RUN: %s" % releaseNum
+    print "############################################"
+
+    ret = installVoltDB(options.pkg, releaseNum)
 
     success = ret['ok']
     if not success:
@@ -345,16 +410,16 @@ if __name__ == "__main__":
 #        print >> sys.stderr, "Test Failed!!"
         exit(1)
 
-    testSuiteList = setExecutables(ret['workDir'], suite)
-    for e in testSuiteList:
-        executable = testSuiteList[e]
-        print "==-->>elem: '%s', executable: '%s'" % (e, executable)
+    testSuiteList = setTestSuite(ret['workDir'], suite)
+#    for i in testSuiteList:
+#        print "i = '%s', exec = '%s'" % (i, testSuiteList[i])
 
     success = startTest(testSuiteList)
-    for k in success:
-        if not success[k]:
+    for module in success:
+        if not success[module]["status"]:
 #            print >> sys.stderr, "Test '%s' Failed!!" % k
-            print "Test '%s' Failed!!" % k
-#        exit(1)
+            print "\n%s\n%s\nTest '%s' FAILED!!\n%s" \
+                % (success[module]["msg"], sepLineS, module, sepLineD)
         else:
-            print "Test '%s' passed!!" % k
+            print "\n%s\n%s\nTest '%s' PASSED!!\n%s" \
+                % (success[module]["msg"], sepLineS, module, sepLineD)
