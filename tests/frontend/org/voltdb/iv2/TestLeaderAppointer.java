5a1ba7bdf2cc840dad72de04aff85ccb38f062aa
Mike Ismert <izzy@voltdb.com>
2012-10-11 13:45:07 -0400

    Add some crude PPD tests for LeaderAppointer.

--- tests/frontend/org/voltdb/iv2/TestLeaderAppointer.java
+++ tests/frontend/org/voltdb/iv2/TestLeaderAppointer.java
@@ -39,6 +39,8 @@ import org.junit.Test;
 
 import org.voltcore.messaging.HostMessenger;
 
+import org.voltcore.utils.CoreUtils;
+
 import org.voltcore.zk.LeaderElector;
 import org.voltcore.zk.ZKTestBase;
 import org.voltcore.zk.ZKUtil;
@@ -75,6 +77,7 @@ public class TestLeaderAppointer extends ZKTestBase {
     public void setUp() throws Exception
     {
         VoltDB.ignoreCrash = false;
+        VoltDB.wasCrashCalled = false;
         setUpZK(NUM_AGREEMENT_SITES);
     }
 
@@ -87,7 +90,8 @@ public class TestLeaderAppointer extends ZKTestBase {
         tearDownZK();
     }
 
-    void configure(int hostCount, int sitesPerHost, int replicationFactor) throws JSONException, Exception
+    void configure(int hostCount, int sitesPerHost, int replicationFactor,
+            boolean enablePPD) throws JSONException, Exception
     {
         m_hm = mock(HostMessenger.class);
         m_zk = getClient(0);
@@ -99,17 +103,18 @@ public class TestLeaderAppointer extends ZKTestBase {
         for (int i = 0; i < hostCount; i++) {
             m_hostIds.add(i);
         }
+        when(m_hm.getLiveHostIds()).thenReturn(m_hostIds);
         m_mpi = mock(MpInitiator.class);
-        createAppointer();
+        createAppointer(enablePPD);
 
         m_cache = new LeaderCache(m_zk, VoltZK.iv2appointees, m_changeCallback);
         m_cache.start(true);
     }
 
-    void createAppointer() throws JSONException
+    void createAppointer(boolean enablePPD) throws JSONException
     {
         m_dut = new LeaderAppointer(m_hm, m_config.getPartitionCount(),
-                m_config.getReplicationFactor(), m_config.getTopology(m_hostIds), m_mpi);
+                m_config.getReplicationFactor(), enablePPD, m_config.getTopology(m_hostIds), m_mpi);
     }
 
     void addReplica(int partitionId, long HSId) throws KeeperException, InterruptedException, Exception
@@ -147,7 +152,7 @@ public class TestLeaderAppointer extends ZKTestBase {
     @Test
     public void testBasicStartup() throws Exception
     {
-        configure(2, 2, 0);
+        configure(2, 2, 0, false);
 
         Thread dutthread = new Thread() {
             @Override
@@ -185,7 +190,7 @@ public class TestLeaderAppointer extends ZKTestBase {
     @Test
     public void testStartupFailure() throws Exception
     {
-        configure(2, 2, 1);
+        configure(2, 2, 1, false);
         // Write an appointee before we start to simulate a failure during startup
         m_cache.put(0, 0L);
         VoltDB.ignoreCrash = true;
@@ -203,7 +208,7 @@ public class TestLeaderAppointer extends ZKTestBase {
     @Test
     public void testStartupFailure2() throws Exception
     {
-        configure(2, 2, 1);
+        configure(2, 2, 1, false);
         // Write the appointees and one master before we start to simulate a failure during startup
         m_cache.put(0, 0L);
         m_cache.put(1, 1L);
@@ -224,7 +229,7 @@ public class TestLeaderAppointer extends ZKTestBase {
     @Test
     public void testWaitsForAllReplicasAndLeaders() throws Exception
     {
-        configure(2, 2, 1);
+        configure(2, 2, 1, false);
         Thread dutthread = new Thread() {
             @Override
             public void run() {
@@ -265,7 +270,7 @@ public class TestLeaderAppointer extends ZKTestBase {
     public void testPromotesOnReplicaDeathAndDiesOnKSafety() throws Exception
     {
         // run once to get to a startup state
-        configure(2, 2, 1);
+        configure(2, 2, 1, false);
         VoltDB.ignoreCrash = true;
         Thread dutthread = new Thread() {
             @Override
@@ -305,7 +310,7 @@ public class TestLeaderAppointer extends ZKTestBase {
     public void testAppointerPromotion() throws Exception
     {
         // run once to get to a startup state
-        configure(2, 2, 1);
+        configure(2, 2, 1, false);
         VoltDB.ignoreCrash = true;
         Thread dutthread = new Thread() {
             @Override
@@ -331,12 +336,120 @@ public class TestLeaderAppointer extends ZKTestBase {
         m_dut.shutdown();
         deleteReplica(0, m_cache.pointInTimeCache().get(0));
         // create a new appointer and start it up
-        createAppointer();
+        createAppointer(false);
         m_newAppointee.set(false);
         m_dut.acceptPromotion();
         while (!m_newAppointee.get()) {
             Thread.sleep(0);
         }
         assertEquals(1L, (long)m_cache.pointInTimeCache().get(0));
+    }
+
+    // These can only test that there was a crash, but since there's only one
+    // test instance of VoltDB, there's not a good way to determine what
+    // "survived".
+    @Test
+    public void testPartitionDetectionMinoritySet() throws Exception
+    {
+        configure(3, 1, 2, true);
+        VoltDB.ignoreCrash = true;
+        Thread dutthread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    m_dut.acceptPromotion();
+                } catch (Exception e) {
+                }
+            }
+        };
+        dutthread.start();
+        // Need to sleep so we don't write to ZK before the LeaderAppointer appears or we'll crash
+        Thread.sleep(1000);
+
+        // Add replicas for partitions 0 and 1 at each host.
+        addReplica(0, CoreUtils.getHSIdFromHostAndSite(0, 0));
+        addReplica(0, CoreUtils.getHSIdFromHostAndSite(1, 0));
+        addReplica(0, CoreUtils.getHSIdFromHostAndSite(2, 0));
+        waitForAppointee(0);
+        registerLeader(0, m_cache.pointInTimeCache().get(0));
+        dutthread.join();
+        // Get rid of host 0 and 1 from what the host messenger will report.
+        m_hostIds.clear();
+        m_hostIds.add(2);
+        // Then kill host 0 and 1's replicas
+        deleteReplica(0, CoreUtils.getHSIdFromHostAndSite(0, 0));
+        deleteReplica(0, CoreUtils.getHSIdFromHostAndSite(1, 0));
+        while (!VoltDB.wasCrashCalled) {
+            Thread.sleep(1);
+        }
+    }
+
+    @Test
+    public void testPartitionDetection5050KillBlessed() throws Exception
+    {
+        configure(2, 1, 1, true);
+        VoltDB.ignoreCrash = true;
+        Thread dutthread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    m_dut.acceptPromotion();
+                } catch (Exception e) {
+                }
+            }
+        };
+        dutthread.start();
+        // Need to sleep so we don't write to ZK before the LeaderAppointer appears or we'll crash
+        Thread.sleep(1000);
+
+        // Add replicas for partitions 0 and 1 at each host.
+        addReplica(0, CoreUtils.getHSIdFromHostAndSite(0, 0));
+        addReplica(0, CoreUtils.getHSIdFromHostAndSite(1, 0));
+        waitForAppointee(0);
+        registerLeader(0, m_cache.pointInTimeCache().get(0));
+        dutthread.join();
+        // Get rid of host 0 from what the host messenger will report.
+        m_hostIds.clear();
+        m_hostIds.add(1);
+        // Then kill host 0's replicas, host 1 should crash
+        deleteReplica(0, CoreUtils.getHSIdFromHostAndSite(0, 0));
+        while (!VoltDB.wasCrashCalled) {
+            Thread.sleep(1);
+        }
+    }
+
+    @Test
+    public void testPartitionDetection5050KillNonBlessed() throws Exception
+    {
+        configure(2, 1, 1, true);
+        VoltDB.ignoreCrash = true;
+        Thread dutthread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    m_dut.acceptPromotion();
+                } catch (Exception e) {
+                }
+            }
+        };
+        dutthread.start();
+        // Need to sleep so we don't write to ZK before the LeaderAppointer appears or we'll crash
+        Thread.sleep(1000);
+
+        // Add replicas for partitions 0 and 1 at each host.
+        addReplica(0, CoreUtils.getHSIdFromHostAndSite(0, 0));
+        addReplica(0, CoreUtils.getHSIdFromHostAndSite(1, 0));
+        waitForAppointee(0);
+        registerLeader(0, m_cache.pointInTimeCache().get(0));
+        dutthread.join();
+        // Get rid of host 1 from what the host messenger will report.
+        m_hostIds.clear();
+        m_hostIds.add(0);
+        // Then kill host 1's replicas, host 0 should not crash
+        deleteReplica(0, CoreUtils.getHSIdFromHostAndSite(1, 0));
+        // Ugly, but there's not a condition we can really sleep on to see if partition detection is done
+        // Sleep for a couple of seconds, then check that crash wasn't called on the global instance
+        Thread.sleep(2000);
+        assertFalse(VoltDB.wasCrashCalled);
     }
 }
