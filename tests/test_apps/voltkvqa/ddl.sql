567c4642e39e7c7ab002c81d7f75329ab457cb51
Phil Rosegay <prosegay@gmail.com>
2012-11-14 17:36:32 -0500

    Merge branch notask_voltkvqa to Master
    Voltkvqa is a rewrite of the old voltkvqa app test which brings it up
    to date and adds multi-part operations to the mix. The coding was done
    by Cheng and Ruth. Ruth asked me to test it and merge it to master.
    It appears to work in the legacy tests but not with iv2 due to a known
    issue. I checked that Ariel's recent changes for new Export were not
    disrupted, but I did not run it with export.

--- tests/test_apps/voltkvqa/ddl.sql
+++ tests/test_apps/voltkvqa/ddl.sql
@@ -4,6 +4,7 @@ CREATE TABLE store
 , value    varbinary(1048576) not null
 , PRIMARY KEY (key)
 );
+PARTITION TABLE store ON COLUMN key;
 
 CREATE TABLE store_export (
   key      varchar(250) not null
