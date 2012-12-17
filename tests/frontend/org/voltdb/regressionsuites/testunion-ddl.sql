ae82830c64c11f2e8fbc0b908562c9d3516e64f2
Paul Martel <pmartel@voltdb.com>
2012-12-17 13:34:12 -0500

    Add/clean-up UNION tests.

--- tests/frontend/org/voltdb/regressionsuites/testunion-ddl.sql
+++ tests/frontend/org/voltdb/regressionsuites/testunion-ddl.sql
@@ -1,25 +1,22 @@
--- partitioned in test on pkey
 CREATE TABLE A (
  PKEY          INTEGER NOT NULL,
  I             INTEGER,
  PRIMARY KEY (PKEY)
 );
+PARTITION TABLE A ON COLUMN PKEY;
 
--- replicated in test
 CREATE TABLE B (
  PKEY          INTEGER,
  I             INTEGER,
  PRIMARY KEY (PKEY)
 );
 
--- replicated in test
 CREATE TABLE C (
  PKEY          INTEGER,
  I             INTEGER,
  PRIMARY KEY (PKEY)
 );
 
--- replicated in test
 CREATE TABLE D (
  PKEY          INTEGER,
  I             INTEGER,
