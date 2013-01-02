bebf0a3a79b5f92e6ed77a15b29264f596f1a031
Ning Shi <nshi@voltdb.com>
2013-01-02 11:51:45 -0500

    Update most recently generated UID for DR txns.

--- src/frontend/org/voltdb/iv2/SpScheduler.java
+++ src/frontend/org/voltdb/iv2/SpScheduler.java
@@ -356,7 +356,7 @@ public class SpScheduler extends Scheduler implements SnapshotCompletionInterest
         final String procedureName = message.getStoredProcedureName();
         if (message.isSinglePartition()) {
             long newSpHandle;
-            long uniqueId;
+            long uniqueId = Long.MIN_VALUE;
             Iv2InitiateTaskMessage msg = message;
             if (m_isLeader || message.isReadOnly()) {
                 /*
@@ -372,14 +372,20 @@ public class SpScheduler extends Scheduler implements SnapshotCompletionInterest
                 }
 
                 /*
+                 * If this is for CL replay or DR, update the unique ID generator
+                 */
+                if (message.isForReplay() || message.isForDR()) {
+                    uniqueId = message.getUniqueId();
+                    m_uniqueIdGenerator.updateMostRecentlyGeneratedUniqueId(uniqueId);
+                }
+
+                /*
                  * If this is CL replay use the txnid from the CL and also
                  * update the txnid to match the one from the CL
                  */
                 if (message.isForReplay()) {
                     newSpHandle = message.getTxnId();
-                    uniqueId = message.getUniqueId();
                     setMaxSeenTxnId(newSpHandle);
-                    m_uniqueIdGenerator.updateMostRecentlyGeneratedUniqueId(uniqueId);
                 } else if (m_isLeader) {
                     TxnEgo ego = advanceTxnEgo();
                     newSpHandle = ego.getTxnId();
