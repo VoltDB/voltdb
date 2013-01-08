352a6f17e822dec8e6570e87840e716acd743437
Ariel Weisberg <aweisberg@voltdb.com>
2013-01-08 15:08:01 -0500

    For ENG-3982, fix test case and modify to cover another type of restore bug

--- tests/frontend/org/voltdb/regressionsuites/TestSaveRestoreSysprocSuite.java
+++ tests/frontend/org/voltdb/regressionsuites/TestSaveRestoreSysprocSuite.java
@@ -467,7 +467,7 @@ public class TestSaveRestoreSysprocSuite extends SaveRestoreBase {
         int num_replicated_items = 1000;
         int num_partitioned_items = 126;
 
-        LocalCluster lc = new LocalCluster( JAR_NAME, 2, 2, 0, BackendTarget.NATIVE_EE_JNI);
+        LocalCluster lc = new LocalCluster( JAR_NAME, 2, 3, 0, BackendTarget.NATIVE_EE_JNI);
         lc.setHasLocalServer(false);
         SaveRestoreTestProjectBuilder project =
             new SaveRestoreTestProjectBuilder();
@@ -488,7 +488,7 @@ public class TestSaveRestoreSysprocSuite extends SaveRestoreBase {
                 saveTablesWithDefaultOptions(client);
 
                 boolean skipFirst = true;
-                for (File f : lc.listFiles(new File("/tmp"))) {
+                for (File f : lc.listFiles(new File(TMPDIR))) {
                     if (f.getName().startsWith(TESTNONCE + "-REPLICATED")) {
                         if (skipFirst) {
                             skipFirst = false;
