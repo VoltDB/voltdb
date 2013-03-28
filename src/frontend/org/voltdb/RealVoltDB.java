107a5a24290178f5ac1eca26a6801c39f9b0df58
Ruth Morgenstein <rmorgenstein@voltdb.com>
2013-03-28 14:19:05 -0400

    Version 2.8.4.4.

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -156,7 +156,7 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, Mailb
     MailboxPublisher m_mailboxPublisher;
     MailboxTracker m_mailboxTracker;
     private String m_buildString;
-    private static final String m_defaultVersionString = "2.8.4.3";
+    private static final String m_defaultVersionString = "2.8.4.4";
     private String m_versionString = m_defaultVersionString;
     HostMessenger m_messenger = null;
     final ArrayList<ClientInterface> m_clientInterfaces = new ArrayList<ClientInterface>();
