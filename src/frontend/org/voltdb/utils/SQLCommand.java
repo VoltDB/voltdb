df9ea421b0e9c9c75a17eebab4f5e14bf1837646
John Piekos <jpiekos@voltdb.com>
2013-04-08 08:47:59 -0400

    Phone home URL invocation added to sqlcmd utility

--- src/frontend/org/voltdb/utils/SQLCommand.java
+++ src/frontend/org/voltdb/utils/SQLCommand.java
@@ -27,8 +27,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -1138,6 +1142,9 @@ public class SQLCommand
             // Split server list
             String[] servers = serverList.split(",");
 
+            // Phone home to see if there is a newer version of VoltDB
+            openURLAsync();
+
             // Load system procedures
             loadSystemProcedures();
 
@@ -1256,4 +1263,43 @@ public class SQLCommand
         }
     }
 
+    // The following two methods implement a "phone home" version check for VoltDB.
+    // Asynchronously ping VoltDB to see what the current released version is.
+    // If it is newer than the one running here, then notify the user in some manner TBD.
+    // Note that this processing should not impact utility use in any way.  Ignore all
+    // errors.
+    private static void openURLAsync()
+    {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                 openURL();
+            }
+        }).start();
+    }
+
+    private static void openURL()
+    {
+        URL url;
+
+        try {
+            // Read the response from VoltDB
+            String a="http://community.voltdb.com/versioncheck?app=sqlcmd&ver=3.2";
+            url = new URL(a);
+            URLConnection conn = url.openConnection();
+
+            // open the stream and put it into BufferedReader
+            BufferedReader br = new BufferedReader(
+                               new InputStreamReader(conn.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                // At this time do nothing, just drain the stream.
+                // In the future we'll notify the user that a new version of VoltDB is available.
+            }
+            br.close();
+        } catch (Throwable e) {
+            // ignore any error
+        }
+    }
 }
