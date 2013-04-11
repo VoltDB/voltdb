2076e4d7f3d3ea7cd5ca9cae09301e6c18c757c1
John Hugg <jhugg@voltdb.com>
2013-04-11 16:46:07 -0400

    Fix whitespace / licensecheck failure.

--- src/frontend/org/voltdb/TableHelper.java
+++ src/frontend/org/voltdb/TableHelper.java
@@ -130,7 +130,7 @@ public class TableHelper {
 
         public String ddlForView() {
             return String.format("CREATE VIEW %s (col1,col2,col3) AS " +
-            		"SELECT %s, COUNT(*), SUM(%s) FROM %s GROUP BY %s;",
+                    "SELECT %s, COUNT(*), SUM(%s) FROM %s GROUP BY %s;",
                     viewName, groupColName, sumColName, srcTableName, groupColName);
         }
 
