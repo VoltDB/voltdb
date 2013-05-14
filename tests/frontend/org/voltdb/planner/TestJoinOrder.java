35fa0c96df18804f492e24c75771299db49563e4
John Hugg <jhugg@voltdb.com>
2013-05-14 13:13:55 -0400

    ENG-4671: Add join stmts to the planner tester. Remove a join from junit.

--- tests/frontend/org/voltdb/planner/TestJoinOrder.java
+++ tests/frontend/org/voltdb/planner/TestJoinOrder.java
@@ -27,7 +27,6 @@ import org.voltdb.plannodes.AbstractPlanNode;
 import org.voltdb.plannodes.NestLoopPlanNode;
 import org.voltdb.plannodes.SeqScanPlanNode;
 
-
 public class TestJoinOrder extends PlannerTestCase {
     public void testBasicJoinOrder() {
         AbstractPlanNode pn = compileWithJoinOrder("select * FROM T1, T2, T3, T4, T5, T6, T7", "T7,T6,T5,T4,T3,T2,T1");
@@ -61,35 +60,6 @@ public class TestJoinOrder extends PlannerTestCase {
                 n = node.getChild(1);
             }
         }
-    }
-
-    public void testENG4671() {
-        String sql = "SELECT " +
-                "  c1.name2," +
-                "  c1.name1," +
-                "  c2.ts," +
-                "  count(*)," +
-                "  sum(c2.value * c3.factor) " +
-                "FROM " +
-                "  c2, c4, c5, c1, c3 " +
-                "WHERE " +
-                "  c3.b = ? AND" +
-                "  c2.ts BETWEEN ? AND ? AND" +
-                "  c1.y = ? AND" +
-                "  c4.f = ? AND" +
-                "  c4.partitionkey = c5.partitionkey AND " +
-                "  c2.partitionkey = c5.partitionkey AND " +
-                "  c2.e = c1.e AND " +
-                "  c2.a = c3.a AND" +
-                "  c3.ts = c2.ts AND" +
-                "  c3.g = c5.g " +
-                "GROUP BY " +
-                "  c1.name2," +
-                "  c1.name1," +
-                "  c2.ts;";
-
-        AbstractPlanNode root = compile(sql, 5);
-        System.out.println(root.toExplainPlanString());
     }
 
     @Override
