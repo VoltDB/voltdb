576d77d2b24e3ad5ef943090fafb72363af8d256
jmandile <johnjmandile@gmail.com>
2013-07-09 14:48:41 -0400

    removed unnecessary checks

--- tests/geb/studioWeb/src/test/resources/GebConfig.groovy
+++ tests/geb/studioWeb/src/test/resources/GebConfig.groovy
@@ -3,24 +3,24 @@ import org.openqa.selenium.firefox.FirefoxDriver
 
 
 waiting {
-	timeout = 2
+    timeout = 2
 }
 
 environments {
-	
-	// run via “./gradlew firefoxTest”
-	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-	
-	firefox {
-		driver = { new FirefoxDriver() }
-	}
+    
+    // run via “./gradlew firefoxTest”
+    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+    
+    firefox {
+        driver = { new FirefoxDriver() }
+    }
 
-	// run via “./gradlew chromeTest”
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chrome {
-		driver = { new ChromeDriver() }
-	}
-	
+    // run via “./gradlew chromeTest”
+    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+    chrome {
+        driver = { new ChromeDriver() }
+    }
+    
 }
 
 // To run the tests with all browsers run “./gradlew test”
