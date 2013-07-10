e59fd5b51dae9040cbbf1dcfa610ad333b584802
Xin Jia <xinjiacs@gmail.com>
2013-07-10 14:05:47 -0400

    wired merge make duplicated codes, remove them

--- src/ee/expressions/datefunctions.h
+++ src/ee/expressions/datefunctions.h
@@ -72,46 +72,6 @@ static inline int64_t epoch_microseconds_from_components(unsigned short int year
     return epoch_seconds * 1000000;
 }
 
-static const boost::posix_time::ptime EPOCH(boost::gregorian::date(1970,1,1));
-static const int64_t GREGORIAN_EPOCH = -12212553600000000;  // 1583-01-01 00:00:00
-static const int8_t QUARTER_START_MONTH_BY_MONTH[] = {
-        /*[0] not used*/-1,  1, 1, 1,  4, 4, 4,  7, 7, 7,  10, 10, 10 };
-
-/** Convert from epoch_micros to date **/
-static void micros_to_date(int64_t epoch_micros_in, boost::gregorian::date& date_out) {
-    if (epoch_micros_in < GREGORIAN_EPOCH) {
-        throw voltdb::SQLException(voltdb::SQLException::data_exception_numeric_value_out_of_range,
-                "Value out of range. Cannot convert dates prior to the year 1583");
-    }
-    boost::posix_time::ptime input_ptime = EPOCH + boost::posix_time::microseconds(epoch_micros_in);
-    date_out = input_ptime.date();
-
-}
-
-/** Convert from epoch_micros to date and time **/
-static void micros_to_date_and_time(int64_t epoch_micros_in, boost::gregorian::date& date_out,
-        boost::posix_time::time_duration& time_out) {
-    if (epoch_micros_in < GREGORIAN_EPOCH) {
-        throw voltdb::SQLException(voltdb::SQLException::data_exception_numeric_value_out_of_range,
-                "Value out of range. Cannot convert dates prior to the year 1583");
-    }
-    boost::posix_time::ptime input_ptime = EPOCH + boost::posix_time::microseconds(epoch_micros_in);
-    date_out = input_ptime.date();
-    time_out = input_ptime.time_of_day();
-}
-
-/** Convert from timestamp to micros since epoch **/
-static int64_t epoch_microseconds_from_components(unsigned short int year, unsigned short int month = 1,
-        unsigned short int day = 1, int hour = 0, int minute = 0, int second = 0) {
-    boost::gregorian::date goal_date = boost::gregorian::date(year, month, day);
-    boost::posix_time::ptime goal_ptime =
-            boost::posix_time::ptime(goal_date,boost::posix_time::time_duration(hour,minute,second));
-    boost::posix_time::time_period goal_period (EPOCH, goal_ptime);
-    boost::posix_time::time_duration goal_duration = goal_period.length();
-    int64_t epoch_seconds = goal_duration.ticks() / goal_duration.ticks_per_second();
-    return epoch_seconds * 1000000;
-}
-
 namespace voltdb {
 
 /** implement the timestamp YEAR extract function **/
