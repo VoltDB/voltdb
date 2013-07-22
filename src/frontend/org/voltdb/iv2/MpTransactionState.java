b2f3661e297a66121f92dc6151c7a9071f2359cd
Anish Khanzode <akhanzode@gmail.com>
2013-07-22 16:26:56 -0400

    combine in one check

--- src/frontend/org/voltdb/iv2/MpTransactionState.java
+++ src/frontend/org/voltdb/iv2/MpTransactionState.java
@@ -282,22 +282,20 @@ public class MpTransactionState extends TransactionState
             final String snapShotRestoreProcName = "@SnapshotRestore";
             while (msg == null) {
                 msg = m_newDeps.poll(60L * 5, TimeUnit.SECONDS);
-                if (msg == null) {
-                    if (!snapShotRestoreProcName.equals(m_initiationMsg.getStoredProcedureName()) ) {
-                        tmLog.warn("Possible multipartition transaction deadlock detected for: " + m_initiationMsg);
-                        if (m_remoteWork == null) {
-                            tmLog.warn("Waiting on local BorrowTask response from site: " +
-                                    CoreUtils.hsIdToString(m_buddyHSId));
-                        }
-                        else {
-                            tmLog.warn("Waiting on remote dependencies: ");
-                            for (Entry<Integer, Set<Long>> e : m_remoteDeps.entrySet()) {
-                                tmLog.warn("Dep ID: " + e.getKey() + " waiting on: " +
-                                        CoreUtils.hsIdCollectionToString(e.getValue()));
-                            }
-                        }
-                        m_mbox.send(com.google.common.primitives.Longs.toArray(m_useHSIds), new DumpMessage());
+                if (msg == null && !snapShotRestoreProcName.equals(m_initiationMsg.getStoredProcedureName())) {
+                    tmLog.warn("Possible multipartition transaction deadlock detected for: " + m_initiationMsg);
+                    if (m_remoteWork == null) {
+                        tmLog.warn("Waiting on local BorrowTask response from site: " +
+                                CoreUtils.hsIdToString(m_buddyHSId));
                     }
+                    else {
+                        tmLog.warn("Waiting on remote dependencies: ");
+                        for (Entry<Integer, Set<Long>> e : m_remoteDeps.entrySet()) {
+                            tmLog.warn("Dep ID: " + e.getKey() + " waiting on: " +
+                                    CoreUtils.hsIdCollectionToString(e.getValue()));
+                        }
+                    }
+                    m_mbox.send(com.google.common.primitives.Longs.toArray(m_useHSIds), new DumpMessage());
                 }
             }
         }
