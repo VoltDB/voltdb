f8260a84f6ef3790e4ae51b51d1e1433d2427740
John Hugg <jhugg@voltdb.com>
2013-07-24 16:11:18 -0400

    ENG-4880: Make vectors return false from NValue::isNull()

--- src/ee/common/NValue.hpp
+++ src/ee/common/NValue.hpp
@@ -2816,35 +2816,36 @@ inline void NValue::allocatePersistentObjectFromInlineValue()
 
 inline bool NValue::isNull() const {
     switch (getValueType()) {
-      case VALUE_TYPE_NULL:
-      case VALUE_TYPE_INVALID:
-        return true;
-      case VALUE_TYPE_TINYINT:
-        return getTinyInt() == INT8_NULL;
-      case VALUE_TYPE_SMALLINT:
-        return getSmallInt() == INT16_NULL;
-      case VALUE_TYPE_INTEGER:
-        return getInteger() == INT32_NULL;
-      case VALUE_TYPE_TIMESTAMP:
-      case VALUE_TYPE_BIGINT:
-        return getBigInt() == INT64_NULL;
-      case VALUE_TYPE_ADDRESS:
-        return *reinterpret_cast<void* const*>(m_data) == NULL;
-      case VALUE_TYPE_DOUBLE:
-        return getDouble() <= DOUBLE_NULL;
-      case VALUE_TYPE_VARCHAR:
-      case VALUE_TYPE_VARBINARY:
-        return *reinterpret_cast<void* const*>(m_data) == NULL ||
-          *reinterpret_cast<const int32_t*>(&m_data[8]) == OBJECTLENGTH_NULL;
-      case VALUE_TYPE_DECIMAL: {
-          TTInt min;
-          min.SetMin();
-          return getDecimal() == min;
-      }
-      default:
-          throwDynamicSQLException(
-                  "NValue::isNull() called with unknown ValueType '%s'",
-                  getValueTypeString().c_str());
+        case VALUE_TYPE_NULL:
+        case VALUE_TYPE_INVALID:
+            return true;
+        case VALUE_TYPE_TINYINT:
+            return getTinyInt() == INT8_NULL;
+        case VALUE_TYPE_SMALLINT:
+            return getSmallInt() == INT16_NULL;
+        case VALUE_TYPE_INTEGER:
+            return getInteger() == INT32_NULL;
+        case VALUE_TYPE_TIMESTAMP:
+        case VALUE_TYPE_BIGINT:
+            return getBigInt() == INT64_NULL;
+        case VALUE_TYPE_ADDRESS:
+            return *reinterpret_cast<void* const*>(m_data) == NULL;
+        case VALUE_TYPE_DOUBLE:
+            return getDouble() <= DOUBLE_NULL;
+        case VALUE_TYPE_VARCHAR:
+        case VALUE_TYPE_VARBINARY:
+            return *reinterpret_cast<void* const*>(m_data) == NULL ||
+            *reinterpret_cast<const int32_t*>(&m_data[8]) == OBJECTLENGTH_NULL;
+        case VALUE_TYPE_DECIMAL: {
+            TTInt min;
+            min.SetMin();
+            return getDecimal() == min;
+        }
+        case VALUE_TYPE_ARRAY:
+            return false;
+        default:
+            throwDynamicSQLException("NValue::isNull() called with unknown ValueType '%s'",
+                                     getValueTypeString().c_str());
     }
     return false;
 }
