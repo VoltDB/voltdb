126202c46ae5d8c863b2b61d01490c5eb3061f5d
Paul Martel <pmartel@voltdb.com>
2013-08-15 16:57:52 -0400

    Yet another version of sqlcoverage join-template.

--- tests/scripts/examples/sql_coverage/join-template.sql
+++ tests/scripts/examples/sql_coverage/join-template.sql
@@ -9,14 +9,15 @@ SELECT * FROM _table LHS13 ,              _table RHS WHERE LHS13.@id_col = RHS.@
 SELECT * FROM _table LHS14 ,              _table RHS WHERE LHS14.@id_col = RHS.@id_col AND   LHS14.@num_col = 2
 SELECT * FROM _table LHS15 ,              _table RHS WHERE LHS15.@id_col = RHS.@id_col AND   LHS15._variable[@col_type] < 45 AND LHS15._variable[@col_type] = RHS._variable
 
-SELECT * FROM P1 LHS23 LEFT  JOIN R1 RHS ON    LHS23.ID = RHS.ID AND     RHS.ID = 2
-SELECT * FROM R1 LHS24 RIGHT JOIN P1 RHS ON    LHS24.ID = RHS.ID AND     LHS24.ID = 2
-SELECT * FROM P1 LHS24 LEFT  JOIN R1 RHS ON    LHS24.ID = RHS.ID AND     LHS24.ID = 2
-SELECT * FROM R1 LHS23 RIGHT JOIN P1 RHS ON    LHS23.ID = RHS.ID AND     RHS.ID = 2
-SELECT * FROM P1 LHS23 RIGHT JOIN R1 RHS ON    LHS23.ID = RHS.ID AND     RHS.ID = 2
-SELECT * FROM R1 LHS24 LEFT  JOIN P1 RHS ON    LHS24.ID = RHS.ID AND     LHS24.ID = 2
-SELECT * FROM P1 LHS24 RIGHT JOIN R1 RHS ON    LHS24.ID = RHS.ID AND     LHS24.ID = 2
-SELECT * FROM R1 LHS23 LEFT  JOIN P1 RHS ON    LHS23.ID = RHS.ID AND     RHS.ID = 2
+-- Still triggers wrong answer from mis-partitioning.
+-- SELECT * FROM _table[@lhs] @join_type JOIN _table[@rhs] ON __[@lhs].@id_col = __[@rhs].@id_col AND __[@rhs].@id_col = 2
+-- SELECT * FROM _table[@lhs] @join_type JOIN _table[@rhs] ON __[@lhs].@id_col = __[@rhs].@id_col AND __[@lhs].@id_col = 2
+
+SELECT * FROM _table[@lhs] @join_type JOIN _table[@rhs] ON __[@lhs]._variable[@col_type] = __[@rhs]._variable
+SELECT * FROM _table[@lhs] @join_type JOIN _table[@rhs] ON __[@lhs].@id_col = __[@rhs].@id_col
+SELECT * FROM _table[@lhs] @join_type JOIN _table[@rhs] ON __[@lhs].@id_col = __[@rhs].@id_col AND __[@rhs].@num_col = 2
+SELECT * FROM _table[@lhs] @join_type JOIN _table[@rhs] ON __[@lhs].@id_col = __[@rhs].@id_col AND __[@lhs].@num_col = 2
+SELECT * FROM _table[@lhs] @join_type JOIN _table[@rhs] ON __[@lhs].@id_col = __[@rhs].@id_col WHERE __[@lhs]._variable[@col_type] < 45 AND __[@lhs]._variable[@col_type] = __[@rhs]._variable
 
 
 --TODO: ENG-4929 Investigate why these get a planner NullPointerException for a timeout.
