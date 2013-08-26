c0cac7f38961901493557c7887f55a1540c7fb35
Phil Rosegay <prosegay@voltdb.com>
2013-08-26 17:33:10 -0400

    killstragglers: change kill strategy; use unrestricted kills on the 12s and 13s 24/7

--- tools/killstragglers.sh
+++ tools/killstragglers.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
-# kill org.voltdb processes that aren't BenchmarkController
-
-# list all process pids and the name of the associated executable
-# look for java executables
-# use ps to see the command arguments for those java pids
-# find args that look like an org.voltdb class main entry point
-# that aren't benchmark controller
-# and kill them.
-
-# as one line:
-# ps cx -o pid -o command  | grep java | awk '{print $1}' | xargs ps -w -w -o pid -o args -p | \
-#  grep " org.voltdb" | grep -v BenchmarkController | awk '{print $1}'  | xargs kill -9
-
-# sudo kill if USER is 'test' and we're in the evening test hours on specific volt hosts...
-# if you need to run overnight, please choose a machine which is not hit by this. thanks.
+# Kill any/all java processes that are listening on any port(s)
+# sudo kill if USER is 'test' and we're in the evening test hours and/or on specific volt hosts...
+# If you need to run overnight, schedule with qa.
 
 HOUR=`date +%H`
-if [ $USER = "test" ] && ( [ $HOUR -ge 20 ] || [ $HOUR -le 8 ] ); then
-   for H in volt{3{a,b,c,d,e,f,g,h,i},7{b,c,d},5{b,c,d},12{a,b,c,d,e,f,i,j,k,l,m,n,o,p,q,r,s,t,u,v},13{a,b,c,d,e,f,g,h}}
-   do
-      if [ $HOSTNAME = $H ]; then
-        SUDO=sudo
-        break
-      fi
-   done
+DAY=`date +%u`
+
+if [ $USER = "test" ]; then
+    case `hostname` in
+        volt10?|volt12?|volt13?)
+            SUDO=sudo
+            ;;
+        *)
+            if [ $DAY -ge 6 ] || [ $HOUR -ge 20 ] || [ $HOUR -le 9 ]; then
+                SUDO=sudo
+            fi
+            ;;
+    esac
 fi
-for P in `$SUDO pgrep -f org.voltdb.VoltDB | xargs`
+for P in `$SUDO netstat -tnlp | egrep 'LISTEN.*/java' | tr -s \  | cut -d\  -f7 | cut -d\/ -f1 | sort | uniq`
 do
-    logger -isp user.notice -t TESTKILL "$USER Killing `$SUDO ps --no-headers -p $P -o user,command`"
+    logger -isp user.notice -t TESTKILL "$BUILD_TAG Killing `$SUDO ps --no-headers -p $P -o user,command`"
     $SUDO kill -9 $P
 done
 exit 0
