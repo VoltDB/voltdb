fd2275316ab5e5da8145bfcaba9dce28690cf838
Anish Khanzode <akhanzode@voltdb.com>
2013-09-24 12:48:14 -0400

    Update non-static hashinator to use new cooked.

--- src/frontend/org/voltdb/TheHashinator.java
+++ src/frontend/org/voltdb/TheHashinator.java
@@ -93,7 +93,7 @@ public abstract class TheHashinator {
      */
     public static TheHashinator getHashinator(Class<? extends TheHashinator> hashinatorImplementation,
             byte config[]) {
-        return constructHashinator(hashinatorImplementation, config);
+        return constructHashinator(hashinatorImplementation, config, false);
     }
 
     /**
