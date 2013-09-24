498a196d7bb01af929f436dd83dcc4c363d2f160
Stefano Santoro <ssantoro@voltdb.com>
2013-09-24 13:31:19 -0400

    updated voter client dependency to guava 15

--- examples/voter/run.sh
+++ examples/voter/run.sh
@@ -27,7 +27,7 @@ APPCLASSPATH=$CLASSPATH:$({ \
 CLIENTCLASSPATH=$CLASSPATH:$({ \
     \ls -1 "$VOLTDB_VOLTDB"/voltdbclient-*.jar; \
     \ls -1 "$VOLTDB_LIB"/commons-cli-1.2.jar; \
-    \ls -1 "$VOLTDB_LIB"/guava-12.0.jar; \
+    \ls -1 "$VOLTDB_LIB"/guava-15.0.jar; \
 } 2> /dev/null | paste -sd ':' - )
 VOLTDB="$VOLTDB_BIN/voltdb"
 LOG4J="$VOLTDB_VOLTDB/log4j.xml"
