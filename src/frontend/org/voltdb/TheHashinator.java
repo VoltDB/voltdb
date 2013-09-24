e04beb98f18ac0ddab98906cdeeb64c7c5a01cb9
Anish Khanzode <akhanzode@voltdb.com>
2013-09-24 13:29:24 -0400

    Fix for Master with cooked Hashinator.

--- src/frontend/org/voltdb/TheHashinator.java
+++ src/frontend/org/voltdb/TheHashinator.java
@@ -75,8 +75,8 @@ public abstract class TheHashinator {
      * @return
      */
     public static TheHashinator getHashinator(Class<? extends TheHashinator> hashinatorImplementation,
-            byte config[]) {
-        return constructHashinator(hashinatorImplementation, config, false);
+            byte config[], boolean cooked) {
+        return constructHashinator(hashinatorImplementation, config, cooked);
     }
 
     /**
