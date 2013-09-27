8461d3b24b43591e8522f049d6388e4078625840
Ruth Morgenstein <rmorgenstein@voltdb.com>
2013-09-27 15:59:26 -0400

    - You say scale-factor, I say scalefactor.  Let's call the whole thing off. - Turn off commandlogging - just in case you run with pro.  The loader doesn't lake it.

--- tests/test_apps/tpcc/src/com/MyTPCC.java
+++ tests/test_apps/tpcc/src/com/MyTPCC.java
@@ -230,8 +230,8 @@ public class MyTPCC
     {
         m_helpah = new AppHelper(MyTPCC.class.getCanonicalName());
         m_helpah.add("duration", "run_duration_in_seconds", "Benchmark duration, in seconds.", 180);
-        m_helpah.add("warehouses", "number_of_warehouses", "Number of warehouses", 12);
-        m_helpah.add("scalefactor", "scale_factor", "Scale factor", 1.0);
+        m_helpah.add("warehouses", "number_of_warehouses", "Number of warehouses", 256);
+        m_helpah.add("scalefactor", "scale_factor", "Reduces per-warehouse data by warehouses/scalefactor", 22.0);
         m_helpah.add("skewfactor", "skew_factor", "Skew factor", 0.0);
         m_helpah.add("loadthreads", "number_of_load_threads", "Number of load threads", 4);
         m_helpah.add("ratelimit", "rate_limit", "Rate limit to start from (tps)", 200000);
