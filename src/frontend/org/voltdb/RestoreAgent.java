114df3890d252f290d28050d51aa22fd3e5b5441
Ning Shi <nshi@voltdb.com>
2013-10-08 15:41:45 -0400

    Restore the hashinator on recovery if there is command log to replay.

--- src/frontend/org/voltdb/RestoreAgent.java
+++ src/frontend/org/voltdb/RestoreAgent.java
@@ -183,6 +183,11 @@ SnapshotCompletionInterest
                         if (m_action == StartAction.SAFE_RECOVER) {
                             jsObj.put(SnapshotUtil.JSON_DUPLICATES_PATH, m_voltdbrootPath);
                         }
+                        if (m_replayAgent.hasReplayedSegments() &&
+                            TheHashinator.getConfiguredHashinatorType() == TheHashinator.HashinatorType.ELASTIC) {
+                            // Restore the hashinator if there's command log to replay and we're running elastic
+                            jsObj.put(SnapshotUtil.JSON_HASHINATOR, true);
+                        }
                         Object[] params = new Object[] { jsObj.toString() };
                         initSnapshotWork(params);
                     }
