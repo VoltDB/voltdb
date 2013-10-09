62f74d1cb0ffcb634b2b013847d5791e4b2d2be0
Xin Jia <xinjiacs@gmail.com>
2013-10-09 10:43:22 -0400

    Fix the reverse scan method isPredicatesOptimizableForAggregate()

--- src/frontend/org/voltdb/plannodes/IndexScanPlanNode.java
+++ src/frontend/org/voltdb/plannodes/IndexScanPlanNode.java
@@ -711,57 +711,22 @@ public class IndexScanPlanNode extends AbstractScanPlanNode {
     // except those (post-)predicates are artifact predicates we
     // added for reverse scan purpose only
     public boolean isPredicatesOptimizableForAggregate() {
-
         // for reverse scan, need to examine "added" predicates
         List<AbstractExpression> predicates = ExpressionUtil.uncombine(m_predicate);
-        // if the size of predicates doesn't equal 2, can't be our added artifact predicates
-        // TODO: someday when index scans on non-nullable values are recognized, soften this
-        // test to make the NOT NULL predicate optional
-        if (predicates.size() != 2) {
+        // if the size of predicates doesn't equal 1, can't be our added artifact predicates
+        if (predicates.size() != 1) {
             return false;
         }
-        // examin each possible "added" predicates
-        // the 1st predicate must matches the last searchKey and the 2nd is NOT NULL expr
+        // examin the possible "added" predicates: NOT NULL expr.
         AbstractExpression expr = predicates.get(0);
-        if (expr.getExpressionType() != ExpressionType.COMPARE_LESSTHAN &&
-                expr.getExpressionType() != ExpressionType.COMPARE_LESSTHANOREQUALTO) {
-            return false;
-        }
-        int searchKeyCount = m_searchkeyExpressions.size();
-        String exprsjson = m_catalogIndex.getExpressionsjson();
-        AbstractExpression left = expr.getLeft();
-        if (exprsjson.isEmpty()) {
-            if (left.getExpressionType() != ExpressionType.VALUE_TUPLE) {
-                return false;
-            }
-            if (((TupleValueExpression)left).getColumnIndex() !=
-                    CatalogUtil.getSortedCatalogItems(m_catalogIndex.getColumns(), "index").get(searchKeyCount - 1).getColumn().getIndex()) {
-                return false;
-            }
-        } else {
-            List<AbstractExpression> indexedExprs = null;
-            try {
-                indexedExprs = AbstractExpression.fromJSONArrayString(exprsjson, null);
-            } catch (JSONException e) {
-                e.printStackTrace();
-                assert(false);
-                return false;
-            }
-            if (left.equals(indexedExprs.get(searchKeyCount - 1))) {
-                return false;
-            }
-        }
-        if (!expr.getRight().equals(m_searchkeyExpressions.get(searchKeyCount - 1))) {
-            return false;
-        }
-        expr = predicates.get(1);
         if (expr.getExpressionType() != ExpressionType.OPERATOR_NOT) {
             return false;
         }
         if (expr.getLeft().getExpressionType() != ExpressionType.OPERATOR_IS_NULL) {
             return false;
         }
-        if (!expr.getLeft().getLeft().equals(predicates.get(0).getLeft())) {
+        // Not reverse scan.
+        if (m_lookupType != IndexLookupType.LT && m_lookupType != IndexLookupType.LTE) {
             return false;
         }
 
