5e482685176f4e2ec2123b5b9c365a6c2f98de78
Ning Shi <nshi@voltdb.com>
2013-11-05 17:54:50 -0500

    Use log4j for elatic context related errors in EE.

--- src/ee/storage/ElasticContext.cpp
+++ src/ee/storage/ElasticContext.cpp
@@ -20,6 +20,7 @@
 #include "storage/persistenttable.h"
 #include "common/TupleOutputStreamProcessor.h"
 #include "common/FixUnusedAssertHack.h"
+#include "logging/LogManager.h"
 
 namespace voltdb {
 
@@ -51,14 +52,16 @@ ElasticContext::handleActivation(TableStreamType streamType)
     if (streamType == TABLE_STREAM_ELASTIC_INDEX) {
         // Can't activate an indexing stream during a snapshot.
         if (m_surgeon.hasStreamType(TABLE_STREAM_SNAPSHOT)) {
-            VOLT_ERROR("Elastic context activation is not allowed while a snapshot is in progress.");
+            LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_ERROR,
+                "Elastic context activation is not allowed while a snapshot is in progress.");
             return ACTIVATION_FAILED;
         }
 
         // Don't allow activation if there's an existing index.
         if (m_surgeon.hasIndex()) {
-            VOLT_ERROR("Elastic context activation is not allowed while an index is "
-                       "present that has not been completely consumed.");
+            LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_ERROR,
+                "Elastic context activation is not allowed while an index is "
+                "present that has not been completely consumed.");
             return ACTIVATION_FAILED;
         }
         m_surgeon.createIndex();
@@ -71,8 +74,9 @@ ElasticContext::handleActivation(TableStreamType streamType)
     if (streamType == TABLE_STREAM_ELASTIC_INDEX_CLEAR) {
         if (m_surgeon.hasIndex()) {
             if (!m_surgeon.isIndexEmpty()) {
-                VOLT_ERROR("Elastic index clear is not allowed while an index is "
-                           "present that has not been completely consumed.");
+                LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_ERROR,
+                    "Elastic index clear is not allowed while an index is "
+                    "present that has not been completely consumed.");
                 return ACTIVATION_FAILED;
             }
             m_surgeon.dropIndex();
@@ -112,11 +116,13 @@ int64_t ElasticContext::handleStreamMore(TupleOutputStreamProcessor &outputStrea
                                          std::vector<int> &retPositions)
 {
     if (!m_surgeon.hasIndex()) {
-        VOLT_ERROR("Elastic streaming was invoked without proper activation.");
+        LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_ERROR,
+            "Elastic streaming was invoked without proper activation.");
         return TABLE_STREAM_SERIALIZATION_ERROR;
     }
     if (m_surgeon.isIndexingComplete()) {
-        VOLT_ERROR("Elastic streaming was called after indexing had already completed.");
+        LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_ERROR,
+            "Elastic streaming was called after indexing had already completed.");
         return TABLE_STREAM_SERIALIZATION_ERROR;
     }
 
