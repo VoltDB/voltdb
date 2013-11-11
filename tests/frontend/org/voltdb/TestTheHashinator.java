0d95172dfa81ab28256b86c2076e8d0845abec45
John Hugg <jhugg@voltdb.com>
2013-11-11 15:35:57 -0500

    Fix broken test organization.

--- tests/frontend/org/voltdb/TestTheHashinator.java
+++ tests/frontend/org/voltdb/TestTheHashinator.java
@@ -47,15 +47,9 @@ import org.voltcore.utils.InstanceId;
 import org.voltcore.utils.Pair;
 import org.voltdb.TheHashinator.HashinatorConfig;
 import org.voltdb.TheHashinator.HashinatorType;
-import org.voltdb.VoltDB.Configuration;
-import org.voltdb.client.Client;
-import org.voltdb.client.ClientFactory;
-import org.voltdb.client.ProcCallException;
-import org.voltdb.compiler.VoltProjectBuilder;
 import org.voltdb.jni.ExecutionEngine;
 import org.voltdb.jni.ExecutionEngineJNI;
 import org.voltdb.sysprocs.saverestore.HashinatorSnapshotData;
-import org.voltdb.utils.MiscUtils;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableSortedMap;
@@ -892,68 +886,5 @@ public class TestTheHashinator {
         }
 
         return dut;
-    }
-
-    @Test
-    public void testRoutingEdgeCases() throws Exception {
-
-        ServerThread localServer = null;
-        Client client = null;
-
-        try {
-            String simpleSchema =
-                    "create table blah (" +
-                    "ival integer default 0 not null, " +
-                    "PRIMARY KEY(ival)); " +
-                    "PARTITION TABLE blah ON COLUMN ival;";
-
-            VoltProjectBuilder builder = new VoltProjectBuilder();
-            builder.addLiteralSchema(simpleSchema);
-            builder.addStmtProcedure("Insert", "insert into blah values (?);", null);
-            boolean success = builder.compile(Configuration.getPathToCatalogForTest("edgecases.jar"), 7, 1, 0);
-            assert(success);
-            MiscUtils.copyFile(builder.getPathToDeployment(), Configuration.getPathToCatalogForTest("edgecases.xml"));
-
-            VoltDB.Configuration config = new VoltDB.Configuration();
-            config.m_pathToCatalog = Configuration.getPathToCatalogForTest("edgecases.jar");
-            config.m_pathToDeployment = Configuration.getPathToCatalogForTest("edgecases.xml");
-            localServer = new ServerThread(config);
-            localServer.start();
-            localServer.waitForInitialization();
-
-            client = ClientFactory.createClient();
-            client.createConnection("localhost");
-
-            try {
-                ByteBuffer buf = ByteBuffer.allocate(4);
-                buf.order(ByteOrder.LITTLE_ENDIAN);
-                buf.putInt(7);
-                byte[] value = buf.array();
-
-                client.callProcedure("Insert", value);
-                fail();
-            }
-            catch (ProcCallException pce) {
-                assertTrue(pce.getMessage().contains("Array / Scalar parameter mismatch"));
-            }
-
-            // for now, @LoadSinglepartitionTable assumes 8 byte integers, even if type is < 8 bytes
-            // this is ok because we don't expose this functionality
-
-            //VoltTable t = new VoltTable(new VoltTable.ColumnInfo("foo", VoltType.INTEGER));
-            //t.addRow(13);
-            //
-            //ByteBuffer buf = ByteBuffer.allocate(4);
-            //buf.order(ByteOrder.LITTLE_ENDIAN);
-            //buf.putInt(13);
-            //byte[] value = buf.array();
-
-            //client.callProcedure("@LoadSinglepartitionTable", value, "blah", t);*/
-        }
-        finally {
-            if (client != null) client.close();
-            if (localServer != null) localServer.shutdown();
-        }
-
     }
 }
