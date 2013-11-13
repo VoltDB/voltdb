fc4bb15321d229c3958fbbc84179d474d6b36458
Anish Khanzode <akhanzode@voltdb.com>
2013-11-13 17:34:40 -0500

    missed passing replication port

--- lib/python/voltcli/verbs.py
+++ lib/python/voltcli/verbs.py
@@ -466,6 +466,8 @@ class ServerBundle(JavaBundle):
             final_args.extend(['internalinterface', runner.opts.internalinterface])
         if runner.opts.internalport:
             final_args.extend(['internalport', runner.opts.internalport])
+        if runner.opts.replicationport:
+            final_args.extend(['replicationport', runner.opts.internalport])
         if runner.opts.zkport:
             final_args.extend(['zkport', runner.opts.zkport])
         if runner.opts.externalinterface:
