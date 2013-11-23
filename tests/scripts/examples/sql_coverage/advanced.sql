e397784be3ccf3898ede0df37705acbb82b83805
Xin Jia <xinjiacs@gmail.com>
2013-11-22 23:18:02 -0500

    Turn on the CASE WHEN no default else clause test

--- tests/scripts/examples/sql_coverage/advanced.sql
+++ tests/scripts/examples/sql_coverage/advanced.sql
@@ -4,12 +4,12 @@
 
 -- {@columntype = "int"}
 SELECT * FROM @fromtables A WHERE CASE WHEN A._variable[@columntype] _cmp @comparableconstant THEN A._variable[@columntype] ELSE A._variable[@columntype] * 10  END _cmp @comparableconstant + 10
--- SELECT * FROM @fromtables A WHERE CASE WHEN A._variable[@columntype] _cmp @comparableconstant THEN A._variable[@columntype] END _cmp @comparableconstant + 10
+SELECT * FROM @fromtables A WHERE CASE WHEN A._variable[@columntype] _cmp @comparableconstant THEN A._variable[@columntype] END _cmp @comparableconstant + 10
 SELECT _variable[@comparabletype], CASE WHEN A._variable[@columntype] _cmp @comparableconstant THEN A._variable[@columntype] ELSE A._variable[@columntype] * 10 END FROM @fromtables WHERE @columnpredicate
 SELECT _variable[@comparabletype], CASE WHEN A._variable[@columntype] _cmp @comparableconstant THEN A._variable[@columntype] END FROM @fromtables WHERE @columnpredicate
 
 
 SELECT * FROM @fromtables A WHERE CASE A._variable[@columntype] WHEN @comparableconstant THEN A._variable[@columntype] * 2 ELSE A._variable[@columntype] * 10  END _cmp @comparableconstant + 10
--- SELECT * FROM @fromtables A WHERE CASE A._variable[@columntype] WHEN @comparableconstant THEN A._variable[@columntype] * 2 END _cmp @comparableconstant + 10
+SELECT * FROM @fromtables A WHERE CASE A._variable[@columntype] WHEN @comparableconstant THEN A._variable[@columntype] * 2 END _cmp @comparableconstant + 10
 SELECT _variable[@comparabletype], CASE A._variable[@columntype] WHEN  @comparableconstant THEN A._variable[@columntype] * 2 ELSE A._variable[@columntype] * 10  END  FROM @fromtables WHERE @columnpredicate
 SELECT _variable[@comparabletype], CASE A._variable[@columntype] WHEN  @comparableconstant THEN A._variable[@columntype] * 2 END  FROM @fromtables WHERE @columnpredicate
