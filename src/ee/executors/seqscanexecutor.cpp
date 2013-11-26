805595803f1035b6b4aa1eb082b08ba59d66ee9d
Paul Martel <pmartel@voltdb.com>
2013-11-26 18:34:09 -0500

    Normalize null NValue formatting excepting decimal

--- src/ee/executors/seqscanexecutor.cpp
+++ src/ee/executors/seqscanexecutor.cpp
@@ -184,8 +184,7 @@ bool SeqScanExecutor::p_execute(const NValueArray &params) {
             VOLT_TRACE("INPUT TUPLE: %s, %d/%d\n",
                        tuple.debug(target_table->name()).c_str(), tuple_ctr,
                        (int)target_table->activeTupleCount());
-            // Experimentally turning off accounting to check performance impact.
-            // m_engine->noteTuplesProcessedForProgressMonitoring(1);
+            m_engine->noteTuplesProcessedForProgressMonitoring(1);
             //
             // For each tuple we need to evaluate it against our predicate
             //
