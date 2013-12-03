e8e971b440a620ceed6e1edc6b7f974d63c5cdd9
Ning Shi <nshi@voltdb.com>
2013-12-03 10:42:34 -0500

    ENG-5586: Fix premature buffer discard in StreamSnapshotDataTarget.
    Discarding the buffers before they are acked causes the sender to send data too
    fast, resulting in direct byte buffer OOM.

--- src/frontend/org/voltdb/rejoin/StreamSnapshotDataTarget.java
+++ src/frontend/org/voltdb/rejoin/StreamSnapshotDataTarget.java
@@ -220,8 +220,8 @@ implements SnapshotDataTarget, StreamSnapshotAckReceiver.AckCallback {
                 bytesSent += send(mb, m_message);
                 return bytesSent;
             } finally {
-                // Always discard the buffer so that they can be reused
-                discard();
+                // Buffers are only discarded after they are acked. Discarding them here would cause the sender to
+                // generate too much work for the receiver.
                 m_future.set(true);
             }
         }
