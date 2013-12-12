561aef542e5c265d80ff7da89d0326efc1e08ce2
Steve Cooper <github@wijjo.com>
2013-12-12 11:00:44 -0500

    ENG-4681 Fix license.

--- lib/python/voltcli/daemon.py
+++ lib/python/voltcli/daemon.py
@@ -16,20 +16,38 @@
 #
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-
-# Module supporting process daemonization for Unix-like systems.
-# This is a fairly heavily modified version of the following project.
+#
+# This module supports process daemonization for Unix-like systems. This is a
+# fairly heavily modified version of the following project.
+# It is provided and licensed as follows:
+#
 #   Source: https://github.com/martinrusev/python-daemon
 #   Author: Martin Rusev
-#   License: Unspecified
+#   License: BSD
+#
+# Copyright (c) 2011-2013, Martin Rusev
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import sys
 import os
@@ -55,11 +73,11 @@ class Daemon(object):
     class AlreadyRunningException(ExceptionBase):
         def __init__(self, daemon):
             Daemon.ExceptionBase.__init__(self, daemon)
-            
+
     class NotRunningException(ExceptionBase):
         def __init__(self, daemon):
             Daemon.ExceptionBase.__init__(self, daemon)
-            
+
     class DeletePIDFileException(ExceptionBase):
         def __init__(self, daemon, cause):
             Daemon.ExceptionBase.__init__(self, daemon, cause=cause)
@@ -73,7 +91,7 @@ class Daemon(object):
             Daemon.ExceptionBase.__init__(self, daemon)
 
     ### Methods
-        
+
     def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
         """
         Constructor.
@@ -143,7 +161,7 @@ class Daemon(object):
             except (IOError, OSError), e:
                 raise Daemon.DeletePIDFileException(self, e)
             return True
-        return False            
+        return False
 
     def start(self, *args):
         """
