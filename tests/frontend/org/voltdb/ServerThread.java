4173a219352569de679a9aaab8a735fe406fab47
John Hugg <jhugg@voltdb.com>
2014-01-03 22:53:51 -0500

    ENG-5654,ENG-5527,ENG-5526,ENG-5525,ENG-5524 Fixes. Provisional fixes for 5 tickets. Needs some more tests for what can be tested.

--- tests/frontend/org/voltdb/ServerThread.java
+++ tests/frontend/org/voltdb/ServerThread.java
@@ -27,7 +27,6 @@ import java.io.File;
 import java.net.URL;
 
 import org.voltcore.utils.InstanceId;
-
 import org.voltdb.utils.MiscUtils;
 
 /**
@@ -45,7 +44,6 @@ public class ServerThread extends Thread {
         }
 
         if (!m_config.validate()) {
-            m_config.usage();
             System.exit(-1);
         }
 
@@ -84,7 +82,6 @@ public class ServerThread extends Thread {
         m_config.m_noLoadLibVOLTDB = m_config.m_backend == BackendTarget.HSQLDB_BACKEND;
 
         if (!m_config.validate()) {
-            m_config.usage();
             System.exit(-1);
         }
 
@@ -120,7 +117,6 @@ public class ServerThread extends Thread {
         m_config.m_noLoadLibVOLTDB = m_config.m_backend == BackendTarget.HSQLDB_BACKEND;
 
         if (!m_config.validate()) {
-            m_config.usage();
             System.exit(-1);
         }
 
