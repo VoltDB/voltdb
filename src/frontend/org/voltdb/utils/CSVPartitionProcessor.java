69f85fba915536e86c4ad325acbcba4e8a4dd2f8
Anish Khanzode <akhanzode@voltdb.com>
2014-01-29 15:07:56 -0500

    ENG-5685: fail fast when unexpected error in client.

--- src/frontend/org/voltdb/utils/CSVPartitionProcessor.java
+++ src/frontend/org/voltdb/utils/CSVPartitionProcessor.java
@@ -229,10 +229,18 @@ class CSVPartitionProcessor implements Runnable {
             m_csvLine = csvLine;
         }
 
+        //one insert at a time callback
         @Override
         public void clientCallback(ClientResponse response) throws Exception {
-            //one insert at a time callback
-            if (response.getStatus() != ClientResponse.SUCCESS) {
+            byte status = response.getStatus();
+            if (status > ClientResponse.OPERATIONAL_FAILURE && status < ClientResponse.UNEXPECTED_FAILURE) {
+                m_log.fatal("Fatal Response from server for: " + response.getStatusString()
+                        + " for: " + m_csvLine.rawLine.toString());
+                System.out.println("Fatal Response from server for: " + response.getStatusString()
+                        + " for: " + m_csvLine.rawLine.toString());
+                System.exit(1);
+            }
+            if (status == ClientResponse.RESPONSE_UNKNOWN) {
                 String[] info = {m_csvLine.rawLine.toString(), response.getStatusString()};
                 if (CSVFileReader.synchronizeErrorInfo(m_csvLine.lineNumber, info)) {
                     m_processor.m_errored = true;
@@ -320,7 +328,15 @@ class CSVPartitionProcessor implements Runnable {
 
         @Override
         public void clientCallback(ClientResponse response) throws Exception {
-            if (response.getStatus() != ClientResponse.SUCCESS) {
+            byte status = response.getStatus();
+            if (status > ClientResponse.OPERATIONAL_FAILURE && status < ClientResponse.UNEXPECTED_FAILURE) {
+                m_log.fatal("Fatal Response from server for batch. Please check health of the server. Status: "
+                        + response.getStatusString());
+                System.out.println("Fatal Response from server for batch. Please check health of the server. Status: "
+                        + response.getStatusString());
+                System.exit(1);
+            }
+            if (status != ClientResponse.SUCCESS) {
                 // Batch failed queue it for individual processing and find out which actually m_errored.
                 m_log.info("Unable to insert rows in a batch.  Attempting to insert them one-by-one.");
                 m_log.info("Note: this will result in reduced insertion performance.");
