98c4e8d285b62fbf3eff2480fa65b69d8a7bbba8
Paul Martel <pmartel@voltdb.com>
2014-02-10 14:40:46 -0500

    ENG-5809 whitespace to drive jenkins

--- tests/frontend/org/voltdb/regressionsuites/TestLimitOffsetSuite.java
+++ tests/frontend/org/voltdb/regressionsuites/TestLimitOffsetSuite.java
@@ -35,12 +35,14 @@ import org.voltdb.client.SyncCallback;
 import org.voltdb.compiler.VoltProjectBuilder;
 
 public class TestLimitOffsetSuite extends RegressionSuite {
-    public TestLimitOffsetSuite(String name) {
+    public TestLimitOffsetSuite(String name)
+    {
         super(name);
     }
 
     private static void load(Client client)
-    throws NoConnectionsException, IOException, InterruptedException {
+    throws NoConnectionsException, IOException, InterruptedException
+    {
         for (int i = 0; i < 10; i++) {
             SyncCallback cb = new SyncCallback();
             client.callProcedure(cb, "InsertA", i, i);
@@ -57,7 +59,8 @@ public class TestLimitOffsetSuite extends RegressionSuite {
     }
 
     private static void doLimitOffsetAndCheck(Client client, String proc)
-    throws IOException, InterruptedException, ProcCallException {
+    throws IOException, InterruptedException, ProcCallException
+    {
         ClientResponse resp = client.callProcedure(proc, 4, 0);
         assertEquals(ClientResponse.SUCCESS, resp.getStatus());
         VoltTable[] results = resp.getResults();
@@ -81,7 +84,8 @@ public class TestLimitOffsetSuite extends RegressionSuite {
         assertEquals(4, i);
     }
 
-    public void testBasicLimitOffsets() throws IOException, ProcCallException, InterruptedException {
+    public void testBasicLimitOffsets() throws IOException, ProcCallException, InterruptedException
+    {
         Client client = this.getClient();
         load(client);
         doTestMultiPartInlineLimit(client);
@@ -91,23 +95,28 @@ public class TestLimitOffsetSuite extends RegressionSuite {
         doTestJoinAndLimitOffset(client);
     }
 
-    private static void doTestMultiPartInlineLimit(Client client) throws IOException, InterruptedException, ProcCallException {
+    private static void doTestMultiPartInlineLimit(Client client) throws IOException, InterruptedException, ProcCallException
+    {
         doLimitOffsetAndCheck(client, "LimitAPKEY");
     }
 
-    private static void doTestMultiPartLimit(Client client) throws IOException, InterruptedException, ProcCallException {
+    private static void doTestMultiPartLimit(Client client) throws IOException, InterruptedException, ProcCallException
+    {
         doLimitOffsetAndCheck(client, "LimitAI");
     }
 
-    private static void doTestReplicatedInlineLimit(Client client) throws IOException, InterruptedException, ProcCallException {
+    private static void doTestReplicatedInlineLimit(Client client) throws IOException, InterruptedException, ProcCallException
+    {
         doLimitOffsetAndCheck(client, "LimitBPKEY");
     }
 
-    private static void doTestReplicatedLimit(Client client) throws IOException, InterruptedException, ProcCallException {
+    private static void doTestReplicatedLimit(Client client) throws IOException, InterruptedException, ProcCallException
+    {
         doLimitOffsetAndCheck(client, "LimitBI");
     }
 
-    public static void doTestJoinAndLimitOffset(Client client) throws IOException, ProcCallException, InterruptedException {
+    public static void doTestJoinAndLimitOffset(Client client) throws IOException, ProcCallException, InterruptedException
+    {
         int limits[] = new int[] { 1, 2, 5, 10, 12, 25 };
         int offsets[] = new int[] { 0, 1, 2, 5, 10, 12, 25 };
         String selecteds[] = new String[] { "*", "A.PKEY" };
@@ -146,7 +155,8 @@ public class TestLimitOffsetSuite extends RegressionSuite {
         }
     }
 
-    public void testDistinctLimitOffset() throws NoConnectionsException, IOException, ProcCallException {
+    public void testDistinctLimitOffset() throws NoConnectionsException, IOException, ProcCallException
+    {
         Client client = getClient();
         client.callProcedure("InsertA", 0, 1);
         client.callProcedure("InsertA", 1, 1);
@@ -160,7 +170,8 @@ public class TestLimitOffsetSuite extends RegressionSuite {
         assertEquals(0, result.getRowCount());
     }
 
-    public void testENG3487() throws IOException, ProcCallException {
+    public void testENG3487() throws IOException, ProcCallException
+    {
         Client client = this.getClient();
 
         client.callProcedure("A.insert", 1, 1);
@@ -182,7 +193,8 @@ public class TestLimitOffsetSuite extends RegressionSuite {
 
     }
 
-    public void testENG1808() throws IOException, ProcCallException {
+    public void testENG1808() throws IOException, ProcCallException
+    {
         Client client = this.getClient();
 
         client.callProcedure("A.insert", 1, 1);
@@ -223,7 +235,8 @@ public class TestLimitOffsetSuite extends RegressionSuite {
         validateTableOfLongs(result, new long[][] {{2,2}});
     }
 
-    static public junit.framework.Test suite() {
+    static public junit.framework.Test suite()
+    {
         VoltServerConfig config = null;
         MultiConfigSuiteBuilder builder = new MultiConfigSuiteBuilder(
                 TestLimitOffsetSuite.class);
