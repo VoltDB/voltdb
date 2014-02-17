700c70db11f1d444de5305a8e4c52824070e8d24
Anish Khanzode <akhanzode@voltdb.com>
2014-02-17 12:30:20 -0500

    ENG-5834: add gc options from dist ticket.

--- lib/python/voltcli/verbs.py
+++ lib/python/voltcli/verbs.py
@@ -366,12 +366,12 @@ class JavaBundle(object):
     def initialize(self, verb):
         verb.add_options(
            cli.StringOption('-l', '--license', 'license', 'specify the location of the license file'),
-           cli.StringOption(None, '--client', 'clientport', 'specify the client port number'),
-           cli.StringOption(None, '--internal', 'internalport', 'specify the internal port number used to communicate between cluster nodes'),
-           cli.StringOption(None, '--zookeeper', 'zkport', 'specify the zookeeper port number'),
-           cli.StringOption(None, '--replication', 'replicationport', 'specify the replication port number (1st of 3 sequential ports)'),
-           cli.StringOption(None, '--admin', 'adminport', 'specify the admin port number'),
-           cli.StringOption(None, '--http', 'httpport', 'specify the http port number'),
+           cli.StringOption(None, '--client', 'clientport', 'specify the client port as [ipaddress:]port-number'),
+           cli.StringOption(None, '--internal', 'internalport', 'specify the internal port as [ipaddress:]port-number used to communicate between cluster nodes'),
+           cli.StringOption(None, '--zookeeper', 'zkport', 'specify the zookeeper port as [ipaddress:]port-number'),
+           cli.StringOption(None, '--replication', 'replicationport', 'specify the replication port as [ipaddress:]port-number (1st of 3 sequential ports)'),
+           cli.StringOption(None, '--admin', 'adminport', 'specify the admin port as [ipaddress:]port-number'),
+           cli.StringOption(None, '--http', 'httpport', 'specify the http port as [ipaddress:]port-number'),
            cli.StringOption(None, '--internalinterface', 'internalinterface', 'specify the network interface to use for internal communication, such as the internal and zookeeper ports'),
            cli.StringOption(None, '--externalinterface', 'externalinterface', 'specify the network interface to use for external ports, such as the admin and client ports'))
 
@@ -448,7 +448,14 @@ class ServerBundle(JavaBundle):
                 '-server',
                 '-XX:+HeapDumpOnOutOfMemoryError',
                 '-XX:HeapDumpPath=/tmp',
-                '-XX:-ReduceInitialCardMarks')
+                '-XX:-ReduceInitialCardMarks',
+                '-XX:+UseParNewGC',
+                '-XX:+UseConcMarkSweepGC',
+                '-XX:+CMSParallelRemarkEnabled',
+                '-XX:+UseTLAB',
+                '-XX:CMSInitiatingOccupancyFraction=75',
+                '-XX:+UseCMSInitiatingOccupancyOnly',
+                '-XX:+UseCondCardMark')
 
     def go(self, verb, runner):
         if self.needs_catalog:
