a749ad19e901564709e5215c5c374d2d5ff568db
Ariel Weisberg <aweisberg@voltdb.com>
2014-03-08 10:48:51 -0500

    For ENG-5962, work around a bizarre NPE that might signal a larger problem

--- src/frontend/org/voltdb/iv2/DuplicateCounter.java
+++ src/frontend/org/voltdb/iv2/DuplicateCounter.java
@@ -105,8 +105,15 @@ public class DuplicateCounter
             /*
              * Replicas will return a response to a write with no result tables
              * always keep the local response which has the result tables
+             *
+             * This is bizarre, but you can have a non null last response
+             * but null last result tables AND non null result tables. It should
+             * be consistent since ClientResponseImpl should never return a null array
+             * for result tables yet somehow in some case it does.
+             *
+             * Is this a symptom of a larger issue?
              */
-            if (m_lastResponse != null && resultTables != null) {
+            if (m_lastResponse != null && m_lastResultTables != null && resultTables != null) {
                 if (m_lastResultTables.length < resultTables.length) {
                     m_lastResponse = message;
                     m_lastResultTables = resultTables;
