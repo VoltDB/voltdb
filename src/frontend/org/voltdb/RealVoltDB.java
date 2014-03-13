17088355ccca95c517ad2dc667948ddfc2d6a266
Ruth Morgenstein <rmorgenstein@voltdb.com>
2014-03-13 14:17:13 -0400

    ENG-5711. - Change the sed to pick up the new version string in RealVoltDB.java - Change the regex for matching yourself (the default) to be easier to sed

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -158,8 +158,8 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback
     volatile CatalogContext m_catalogContext;
     private String m_buildString;
     static final String m_defaultVersionString = "4.1";
-    // by default, 4.1 is only compatible with 4.1
-    static final String m_defaultHotfixableRegexPattern = "^4\\.1\\z";
+    // by default set the version to only be compatible with itself
+    static final String m_defaultHotfixableRegexPattern = "^\\Q4.1\\E\\z";
     // these next two are non-static because they can be overrriden on the CLI for test
     private String m_versionString = m_defaultVersionString;
     private String m_hotfixableRegexPattern = m_defaultHotfixableRegexPattern;
