e670d86b6407ec18e52a76dc4555e3f453578f4b
Xin Jia <xinjiacs@gmail.com>
2014-03-14 13:31:49 -0400

    Use upper case for stored catalogmap iterms in Java and EE. Use get without doing string cases converting work.

--- src/frontend/org/voltdb/planner/ParsedSelectStmt.java
+++ src/frontend/org/voltdb/planner/ParsedSelectStmt.java
@@ -683,7 +683,7 @@ public class ParsedSelectStmt extends AbstractParsedStmt {
             Table tb = getTableFromDB(groupbyCol.tableName);
             if (tb != null) {
                 org.voltdb.catalog.Column catalogColumn =
-                        tb.getColumns().getIgnoreCase(groupbyCol.columnName);
+                        tb.getColumns().get(groupbyCol.columnName);
                 groupbyCol.index = catalogColumn.getIndex();
             }
         }
