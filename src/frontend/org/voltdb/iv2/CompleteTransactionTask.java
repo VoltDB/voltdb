ce48941d69f5220a09e343640d4e0d2b5d78cf82
Ning Shi <nshi@voltdb.com>
2014-03-31 11:45:16 -0400

    ENG-5608: Log the fragment when it doesn't contain the InitaiteTaskMessage in logToDR().

--- src/frontend/org/voltdb/iv2/CompleteTransactionTask.java
+++ src/frontend/org/voltdb/iv2/CompleteTransactionTask.java
@@ -147,9 +147,9 @@ public class CompleteTransactionTask extends TransactionTask
             FragmentTaskMessage fragment = (FragmentTaskMessage) m_txnState.getNotice();
             Iv2InitiateTaskMessage initiateTask = fragment.getInitiateTask();
             assert(initiateTask != null);
-            if (initiateTask.getStoredProcedureInvocation() == null) {
-                hostLog.error("Unable to log MP transaction to DR because of missing invocation, " +
-                              "fragment: " + fragment.toString() + ", initiate task: " + initiateTask);
+            if (initiateTask == null) {
+                hostLog.error("Unable to log MP transaction to DR because of missing InitiateTaskMessage, " +
+                              "fragment: " + fragment.toString());
             }
             StoredProcedureInvocation invocation = initiateTask.getStoredProcedureInvocation().getShallowCopy();
             m_drGateway.onSuccessfulMPCall(m_txnState.m_spHandle,
