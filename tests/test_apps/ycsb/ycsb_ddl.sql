e638ad14c8a357c1b0e83839090ecdad307aeac1
Alex Rogers <arogers@voltdb.com>
2014-04-22 15:30:36 -0400

    Add YCSB client implementation to test_apps

---
+++ tests/test_apps/ycsb/ycsb_ddl.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Store
+(
+    keyspace VARCHAR(128)    NOT NULL
+,   key      VARCHAR(128)    NOT NULL
+,   value    VARBINARY(2056) NOT NULL
+,   PRIMARY KEY (keyspace, key)
+);
+PARTITION TABLE Store ON COLUMN key;
+
+CREATE PROCEDURE FROM CLASS com.procedures.Put;
+PARTITION PROCEDURE Put ON TABLE Store COLUMN key PARAMETER 1;
+
+CREATE PROCEDURE FROM CLASS com.procedures.Scan;
+PARTITION PROCEDURE Scan ON TABLE Store COLUMN key PARAMETER 1;
+
+CREATE PROCEDURE com.procedures.simple.Get AS
+    SELECT value FROM Store WHERE keyspace = ? AND key = ?
+;
+PARTITION PROCEDURE Get ON TABLE Store COLUMN key PARAMETER 1;
