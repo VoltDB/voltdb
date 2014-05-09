ca30c84c4b74ba52a3075184ec2fe499e385e4fc
Mike Ismert <izzy@voltdb.com>
2014-05-09 10:32:22 -0400

    ENG-5900: Add initial SQL "lexer"

---
+++ src/frontend/org/voltdb/utils/SQLLexer.java
@@ -0,0 +1,46 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2014 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.voltdb.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SQLLexer
+{
+    // Simplest possible SQL DDL token lexer
+    private static final Pattern DDL_MATCH = Pattern.compile(
+            "^\\s*" +  // start of line, 0 or more whitespace
+            "(alter|create|drop)" + // tokens we consider DDL at the moment
+            "\\s+", // one or more whitespace
+            Pattern.CASE_INSENSITIVE
+            );
+
+    /**
+     * Get the DDL token, if any, at the start of this statement.
+     * @return returns token, or null if it wasn't DDL
+     */
+    public static String extractDDLToken(String sql)
+    {
+        String ddlToken = null;
+        Matcher ddlMatcher = DDL_MATCH.matcher(sql);
+        if (ddlMatcher.find()) {
+            ddlToken = ddlMatcher.group(1).toLowerCase();
+        }
+        return ddlToken;
+    }
+}
