bba3ee300d54153fd134b930f27032b4259520b9
Yang Lu <yang@voltdb.com>
2014-05-20 10:13:19 -0400

    For ENG-6333, add logging rate limiter in latency watchdog.

--- src/frontend/org/voltcore/utils/LatencyWatchdog.java
+++ src/frontend/org/voltcore/utils/LatencyWatchdog.java
@@ -7,15 +7,15 @@ import java.util.concurrent.TimeUnit;
 
 
 public class LatencyWatchdog {
-    static Map<Thread, Long> m_latencyMap = new HashMap<Thread, Long>();
+    static volatile Map<Thread, Long> m_latencyMap = new HashMap<Thread, Long>();
     static final long WATCHDOG_DELAY = 50;
-    static final long RATE_LIMITED_INTERVAL = 5;
+    static final long MIN_LOG_INTERVAL = 5 * 1000; /* millisecond */
     static public final boolean m_enable = true;  /* Compiler will eliminate the code within its scope when turn off */
     static ScheduledThreadPoolExecutor executor = CoreUtils.getScheduledThreadPoolExecutor("Latency Watchdog Executor", 10, CoreUtils.SMALL_STACK_SIZE);
+    static volatile long m_lastLogTime = 0;
 
     static class WatchdogCallback implements Runnable {
         final Thread m_thread;
-        static int count = 0;
 
         WatchdogCallback(Thread t) {
             m_thread = t;
@@ -25,14 +25,15 @@ public class LatencyWatchdog {
         public void run() {
             Thread.currentThread().setName("Latency Watchdog - " + m_thread.getName());
             long timestamp = m_latencyMap.get(m_thread);
-            long interval = System.currentTimeMillis() - timestamp;
-            if (interval > WATCHDOG_DELAY && count++ < 10) {
-                System.out.printf("Thread [%s] has been delayed for more than %d milliseconds\n", m_thread.getName(), interval);
+            long now = System.currentTimeMillis();
+            if ((now - timestamp > WATCHDOG_DELAY) && (now - m_lastLogTime > MIN_LOG_INTERVAL)) {
+                System.out.printf("Thread [%s] has been delayed for %d milliseconds\n", m_thread.getName(), now - timestamp);
+                m_lastLogTime = now;
                 for (StackTraceElement ste : m_thread.getStackTrace()) {
                     System.out.println(ste);
                 }
             }
-            executor.scheduleWithFixedDelay(new WatchdogCallback(m_thread), WATCHDOG_DELAY, WATCHDOG_DELAY, TimeUnit.MILLISECONDS);
+            //executor.scheduleWithFixedDelay(new WatchdogCallback(m_thread), WATCHDOG_DELAY, WATCHDOG_DELAY, TimeUnit.MILLISECONDS);
         }
     }
 
