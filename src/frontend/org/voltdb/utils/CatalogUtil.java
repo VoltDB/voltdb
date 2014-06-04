34c0d5a3106798e109215da8c85692399515e073
Ning Shi <nshi@voltdb.com>
2014-06-04 11:38:35 -0400

    Add HTTP export client option to the deployment file schema.

--- src/frontend/org/voltdb/utils/CatalogUtil.java
+++ src/frontend/org/voltdb/utils/CatalogUtil.java
@@ -817,6 +817,7 @@ public abstract class CatalogUtil {
             case FILE: exportClientClassName = "org.voltdb.exportclient.ExportToFileClient"; break;
             case JDBC: exportClientClassName = "org.voltdb.exportclient.JDBCExportClient"; break;
             case KAFKA: exportClientClassName = "org.voltdb.exportclient.KafkaExportClient"; break;
+            case HTTP: exportClientClassName = "org.voltdb.exportclient.HttpExportClient"; break;
             //Validate that we can load the class.
             case CUSTOM:
                 try {
