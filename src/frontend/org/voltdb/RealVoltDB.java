1ee9bee0c702666e54264441b25a795e98f5e7cc
Ariel Weisberg <aweisberg@voltdb.com>
2014-06-19 14:34:16 -0400

    How am I seeing two VoltDBs running in the same unit test at the same time? One thread name main the other ServerThread

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -333,6 +333,7 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback
     @Override
     public void initialize(VoltDB.Configuration config) {
         ShutdownHooks.enableServerStopLogging();
+        hostLog.fatal("Initialize invoked from ", new Throwable());
         synchronized(m_startAndStopLock) {
             // check that this is a 64 bit VM
             if (System.getProperty("java.vm.name").contains("64") == false) {
