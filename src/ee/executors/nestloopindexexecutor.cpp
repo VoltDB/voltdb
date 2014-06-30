d582005daf3b8452c566de06f617e7405dea3861
Xin Jia <xinjiacs@gmail.com>
2014-06-29 20:50:41 -0400

    No need to handle inner index scan input table, as it is always persistent table now

--- src/ee/executors/nestloopindexexecutor.cpp
+++ src/ee/executors/nestloopindexexecutor.cpp
@@ -527,7 +527,6 @@ bool NestLoopIndexExecutor::p_execute(const NValueArray &params)
     VOLT_TRACE ("result table:\n %s", output_table->debug().c_str());
     VOLT_TRACE("Finished NestLoopIndex");
 
-    cleanupInputTempTable(inner_table);
     cleanupInputTempTable(outer_table);
 
     return (true);
