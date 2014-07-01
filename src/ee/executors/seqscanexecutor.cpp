8827cda6c1003c56122e244e0a875a0400953bc5
Xin Jia <xinjiacs@gmail.com>
2014-07-01 09:05:03 -0400

    Disable the subqueries early clean up because it can be called a couple of times for join

--- src/ee/executors/seqscanexecutor.cpp
+++ src/ee/executors/seqscanexecutor.cpp
@@ -276,8 +276,5 @@ bool SeqScanExecutor::p_execute(const NValueArray &params) {
     VOLT_TRACE("\n%s\n", output_table->debug().c_str());
     VOLT_DEBUG("Finished Seq scanning");
 
-    // input table can be sub-query temp table
-    cleanupInputTempTable(input_table);
-
     return true;
 }
