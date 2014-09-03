0a1ea9e85c0f0a521f372f267b439ebc1b072aa3
SurajChoudhary <surajc0309@gmail.com>
2014-09-03 16:08:07 -0400

    ENG-5540 Fixed Jenkins test failure

--- tests/frontend/org/voltdb/utils/TestCollector.java
+++ tests/frontend/org/voltdb/utils/TestCollector.java
@@ -93,8 +93,6 @@ public class TestCollector {
     }
 
     private File collect(String voltDbRootPath, boolean skipHeapDump, int days) throws Exception {
-        File collectionTgz = new File(voltDbRootPath, prefix + ".tgz");
-        System.out.println("Tar File Path is: " + collectionTgz.getAbsolutePath());
         if(resetCurrentTime) {
             Collector.m_currentTimeMillis = System.currentTimeMillis();
         }
@@ -108,6 +106,8 @@ public class TestCollector {
                                     "--fileInfoOnly=false",  // fileInfoOnly
                                     "--days="+String.valueOf(days)
                                     });
+
+        File collectionTgz = new File(voltDbRootPath, prefix + ".tgz");
         assertTrue(collectionTgz.exists());
 
         File collectionDecompressed = new File(voltDbRootPath, prefix);
