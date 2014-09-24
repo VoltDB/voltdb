b8c5b2ed5ff8ee69b5d960deef4832a8be12110a
SurajChoudhary <surajc0309@gmail.com>
2014-09-24 15:39:57 -0400

    ENG-5540 added error message for --days=0 value

--- lib/python/voltcli/voltdb.d/collect.py
+++ lib/python/voltcli/voltdb.d/collect.py
@@ -1,3 +1,4 @@
+import sys
 # This file is part of VoltDB.
 
 # Copyright (C) 2008-2014 VoltDB Inc.
@@ -59,6 +60,9 @@
 )
 
 def collect(runner):
+    if int(runner.opts.days) == 0:
+	print >> sys.stderr, "ERROR: '0' is invalid entry for option --days"
+        sys.exit(-1)
     runner.args.extend(['--voltdbroot='+runner.opts.voltdbroot, '--prefix='+runner.opts.prefix, '--host='+runner.opts.host, '--username='+runner.opts.username, '--password='+runner.opts.password,
     '--noprompt='+str(runner.opts.noprompt), '--dryrun='+str(runner.opts.dryrun), '--skipheapdump='+str(runner.opts.skipheapdump), '--days='+str(runner.opts.days)])
     runner.java_execute('org.voltdb.utils.Collector', None, *runner.args)
