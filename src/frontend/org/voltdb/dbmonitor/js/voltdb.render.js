b6a4cbd020b007dde86fe628eadff607da280e3e
Subash Aryal <saryal@deerwalk.com>
2014-10-21 16:22:13 +0545

    Bug #44870 : Menu list does not collapse in mobile when we move from one tab to another

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.render.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.render.js
@@ -1492,6 +1492,11 @@ function alertNodeClicked(obj) {
 $(function () {
     $('#toggleMenu').click(function () {
         $("#nav").slideToggle('slow');
+		$("#nav").css('left', '0');
+		$("#nav ul li").click(function(){
+				$("#nav").css('display', 'none');
+				$(window).resize();		
+			});
     });
 });
 
@@ -1501,6 +1506,7 @@ $(window).resize(function () {
     if (windowWidth > 699) {
         //alert(windowWidth);
         $("#nav").css('display', 'block');
+
     } else if (windowWidth < 699) {
         $("#nav").css('display', 'none');
     }
