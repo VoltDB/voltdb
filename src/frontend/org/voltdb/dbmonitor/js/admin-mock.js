092177aba4a1175bdb2f62f1999ed764517bb08d
Rajiv Manandhar <rmanandhar@deerwalk.com>
2014-11-14 11:54:35 +0545

    Limited expand/collapse functionality to the first header table column instead of header row.

--- src/frontend/org/voltdb/dbmonitor/js/admin-mock.js
+++ src/frontend/org/voltdb/dbmonitor/js/admin-mock.js
@@ -2,13 +2,14 @@
 
 $( document ).ready(function() { 
    // Make Expandable Rows.
-   $('tr.parent')
-		   .css("cursor","pointer")
-		   .attr("title","Click to expand/collapse")
-		   .click(function(){
-				   $(this).siblings('.child-'+this.id).toggle();
-				   $(this).find(".labelCollapsed").toggleClass("labelExpanded");
-		   });
+    $('tr.parent > td:first-child')
+        .css("cursor", "pointer")
+        .attr("title", "Click to expand/collapse")
+        .click(function () {
+            var parent = $(this).parent();
+            parent.siblings('.child-' + parent.attr("id")).toggle();
+            parent.find(".labelCollapsed").toggleClass("labelExpanded");
+        });
    $('tr[class^=child-]').hide().children('td');
 	
 	// btnServerConfigAdmin
