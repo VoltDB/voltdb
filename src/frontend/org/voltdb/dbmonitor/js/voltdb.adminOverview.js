9a5c1e575eb303d3c63cbd3adc33d4f94cc79d98
prmanandhar <prmanandhar@deerwalk.com>
2014-11-24 17:38:10 +0545

    Added new js , Deermine Task 47535

---
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.adminOverview.js
@@ -0,0 +1,40 @@
+﻿(function ($) {
+    $.fn.adminOverview = function (option) {
+        // set some defaults
+        var defaults = {
+
+        };
+
+        var options = $.extend({}, defaults, option);
+
+        //write blocks and function calls that are to be executed during load operation
+        $(document).ready(function () {
+            //site per host
+            $("#navAdmin").on("click", function () {
+                getAdminConfiguration();
+
+            });
+
+        });
+
+        //inner functions
+        var getAdminConfiguration = function () {
+            voltDbRenderer.getAdminconfiguration(function (connection) {
+                getSiteByHost(connection);
+
+            });
+
+
+        };
+
+        var getSiteByHost = function (connection) {
+            var siteCount = voltDbRenderer.getSiteCountByHost(connection);
+            $('td[name=site_per_host]').next('td').text(siteCount);
+
+
+        };
+
+    };
+
+
+})(jQuery);
