50a277d19aaa09e5e048b3ba2c2fbd32189b7134
alexeevm <michael.alexeev@gmail.com>
2014-11-26 14:44:36 -0500

    Added index test cases for scalar/row expressions

--- tests/frontend/org/voltdb/planner/TestPlansScalarSubQueries.java
+++ tests/frontend/org/voltdb/planner/TestPlansScalarSubQueries.java
@@ -27,14 +27,17 @@ import java.util.List;
 
 import org.voltdb.expressions.AbstractExpression;
 import org.voltdb.expressions.AbstractSubqueryExpression;
+import org.voltdb.expressions.ComparisonExpression;
 import org.voltdb.plannodes.AbstractPlanNode;
 import org.voltdb.plannodes.AbstractScanPlanNode;
 import org.voltdb.plannodes.AggregatePlanNode;
+import org.voltdb.plannodes.IndexScanPlanNode;
 import org.voltdb.plannodes.NodeSchema;
 import org.voltdb.plannodes.SchemaColumn;
 import org.voltdb.plannodes.SeqScanPlanNode;
 import org.voltdb.types.ExpressionType;
 import org.voltdb.types.PlanNodeType;
+import org.voltdb.types.QuantifierType;
 
 public class TestPlansScalarSubQueries extends PlannerTestCase {
 
@@ -136,8 +139,58 @@ public class TestPlansScalarSubQueries extends PlannerTestCase {
         assertEquals(0, pred.getRight().getArgs().size());
     }
 
+    public void testWhereIndexScalar() {
+        {
+            AbstractPlanNode pn = compile("select r5.c from r5 where r5.a = (select r1.a from r1 where r1.a = ?);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression pred = ((IndexScanPlanNode) pn).getEndExpression();
+            assertEquals(ExpressionType.COMPARE_EQUAL, pred.getExpressionType());
+            assertEquals(ExpressionType.SELECT_SUBQUERY, pred.getRight().getExpressionType());
+        }
+        {
+            AbstractPlanNode pn = compile("select r5.c from r5 where (select r1.a from r1 where r1.a = ?) < r5.a;");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            List<AbstractExpression> exprs = ((IndexScanPlanNode) pn).getSearchKeyExpressions();
+            assertEquals(1, exprs.size());
+            assertEquals(ExpressionType.VALUE_SCALAR, exprs.get(0).getExpressionType());
+        }
+        {
+            AbstractPlanNode pn = compile("select r5.c from r5 where r5.a IN (select r1.a from r1);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression pred = ((IndexScanPlanNode) pn).getPredicate();
+            assertEquals(ExpressionType.OPERATOR_EXISTS, pred.getExpressionType());
+            assertEquals(ExpressionType.SELECT_SUBQUERY, pred.getLeft().getExpressionType());
+        }
+        {
+            AbstractPlanNode pn = compile("select r5.c from r5 where  r5.a = ANY (select r1.a from r1);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression expr = ((IndexScanPlanNode) pn).getPredicate();
+            assertEquals(ExpressionType.OPERATOR_EXISTS, expr.getExpressionType());
+            assertEquals(ExpressionType.SELECT_SUBQUERY, expr.getLeft().getExpressionType());
+        }
+        {
+            AbstractPlanNode pn = compile("select r5.c from r5 where  r5.a = ANY (select r1.a from r1 limit 3 offset 4);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression expr = ((IndexScanPlanNode) pn).getPredicate();
+            assertEquals(ExpressionType.COMPARE_EQUAL, expr.getExpressionType());
+            assertEquals(ExpressionType.SELECT_SUBQUERY, expr.getRight().getExpressionType());
+        }
+        {
+            AbstractPlanNode pn = compile("select r5.c from r5 where  r5.a > ALL (select r1.a from r1);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression expr = ((IndexScanPlanNode) pn).getPredicate();
+            assertEquals(ExpressionType.SELECT_SUBQUERY, expr.getRight().getExpressionType());
+        }
+    }
+
     public void testWhereGreaterRow() {
-        AbstractPlanNode pn = compile("select r2.c from r2 where (a,c) > (select r1.a, r1.c from r1);");
+        AbstractPlanNode pn = compile("select r5.c from r5 where (a,c) > (select r1.a, r1.c from r1);");
         pn = pn.getChild(0);
         assertTrue(pn instanceof AbstractScanPlanNode);
         AbstractExpression pred = ((AbstractScanPlanNode) pn).getPredicate();
@@ -183,6 +236,54 @@ public class TestPlansScalarSubQueries extends PlannerTestCase {
     public void testHavingRowMismatch() {
         failToCompile("select max(r2.c) from r2 group by r2.c having (count(*), max(r2.c)) = (select a,c, 5 from r1);",
                 "row column count mismatch");
+    }
+
+    public void testWhereIndexRow() {
+        {
+            AbstractPlanNode pn = compile("select * from r5 where (a,c) = (select a, c from r1);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression pred = ((IndexScanPlanNode) pn).getPredicate();
+            assertEquals(ExpressionType.COMPARE_EQUAL, pred.getExpressionType());
+            assertEquals(ExpressionType.ROW_SUBQUERY, pred.getLeft().getExpressionType());
+            assertEquals(ExpressionType.SELECT_SUBQUERY, pred.getRight().getExpressionType());
+        }
+        {
+            AbstractPlanNode pn = compile("select * from r5 where (select a, c from r1) >= (a,c);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression pred = ((IndexScanPlanNode) pn).getPredicate();
+            assertEquals(ExpressionType.COMPARE_GREATERTHANOREQUALTO, pred.getExpressionType());
+            assertEquals(ExpressionType.SELECT_SUBQUERY, pred.getLeft().getExpressionType());
+            assertEquals(ExpressionType.ROW_SUBQUERY, pred.getRight().getExpressionType());
+        }
+        {
+            AbstractPlanNode pn = compile("select * from r5 where (a,c) IN (select a, c from r1);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression pred = ((IndexScanPlanNode) pn).getPredicate();
+            assertEquals(ExpressionType.OPERATOR_EXISTS, pred.getExpressionType());
+            assertEquals(ExpressionType.SELECT_SUBQUERY, pred.getLeft().getExpressionType());
+        }
+        {
+            AbstractPlanNode pn = compile("select * from r5 where (a,c) IN (select a, c from r1 limit 1 offset 4);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression pred = ((IndexScanPlanNode) pn).getPredicate();
+            assertEquals(ExpressionType.COMPARE_EQUAL, pred.getExpressionType());
+            assertEquals(QuantifierType.ANY, ((ComparisonExpression) pred).getQuantifier());
+            assertEquals(ExpressionType.ROW_SUBQUERY, pred.getLeft().getExpressionType());
+        }
+
+        {
+            AbstractPlanNode pn = compile("select * from r5 where (a,c) > ALL (select a, c from r1);");
+            pn = pn.getChild(0);
+            assertTrue(pn instanceof IndexScanPlanNode);
+            AbstractExpression pred = ((IndexScanPlanNode) pn).getPredicate();
+            assertEquals(ExpressionType.COMPARE_GREATERTHAN, pred.getExpressionType());
+            assertEquals(QuantifierType.ALL, ((ComparisonExpression) pred).getQuantifier());
+            assertEquals(ExpressionType.ROW_SUBQUERY, pred.getLeft().getExpressionType());
+        }
     }
 
     @Override
