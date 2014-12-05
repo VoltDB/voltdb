6de63e2aef599fde33e6206d7e9c0291bf1d535b
prmanandhar <prmanandhar@deerwalk.com>
2014-12-05 14:18:03 +0545

    replaced voltdb.config original code

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.config.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.config.js
@@ -1,40 +1,21 @@
 ﻿
 (function (window) {
-    
-  var iVoltDBConfig = (function () {
- 
-        //var CONFIG_MODE = "release"; //Enable this for production release.
-        var CONFIG_MODE = "debug"; //Enable this for debug mode.
 
-        //This JSON object should only used for debug purpose.
-        this.PrivateToPublicIP = {
-            '10.140.25.211': '184.73.30.156',
-            '10.110.218.156': 'ec2-54-83-134-161.compute-1.amazonaws.com'
-        };
+    var iVoltDBConfig = (function () {
 
-        this.GetDefaultServerIP = function() {
-            if (CONFIG_MODE == "debug")
-				return '10.10.1.52';
-                //return "184.73.30.156";
-
-            return "localhost";
+        this.GetDefaultServerIP = function () {
+            return window.location.hostname;
         };
 
         this.GetDefaultServerNameForKey = function () {
-            if (CONFIG_MODE == "debug")
-                return "184_73_30_156";
-
-            return "localhost";
+            return window.location.hostname;
         };
 
         this.GetPortId = function () {
-            if (CONFIG_MODE == "debug")
-                return null;
-
             return window.location.port;
-         };
-  });
+        };
+    });
 
-  window.VoltDBConfig = VoltDBConfig = new iVoltDBConfig();
+    window.VoltDBConfig = VoltDBConfig = new iVoltDBConfig();
 
 })(window);
