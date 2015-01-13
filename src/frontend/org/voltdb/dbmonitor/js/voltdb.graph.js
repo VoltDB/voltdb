c5295c66bb37190905a1b91c68805e6b3a60166f
Rajiv Manandhar <rmanandhar@deerwalk.com>
2015-01-13 16:57:04 +0545

    Added code to ensure that the plotted value of tps is not a NaN.

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.graph.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.graph.js
@@ -630,7 +630,7 @@
                 var delta = currentTimedTransactionCount - monitor.lastTimedTransactionCount;
                 var calculatedValue = parseFloat(delta * 1000.0 / (currentTimerTick - monitor.lastTimerTick)).toFixed(1) * 1;
                 
-                if (calculatedValue < 0 || (currentTimerTick - monitor.lastTimerTick == 0))
+                if (calculatedValue < 0 || isNaN(calculatedValue) || (currentTimerTick - monitor.lastTimerTick == 0))
                     calculatedValue = 0;
 
                 if (tpsSecCount >= 6 || monitor.tpsFirstData) {
