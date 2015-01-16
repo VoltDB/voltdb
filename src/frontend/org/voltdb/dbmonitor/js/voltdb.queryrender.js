fc562c919382a0b20947856a08f22c51809b8521
Chris Wolff <cwolff@voltdb.com>
2015-01-16 10:17:25 -0800

    Add regular expressions to VMC for LIMIT PARTITION ROWS
    In particular, we don't want to start new commands for keywords
    PARTITON, EXECUTE and DELETE when they appear in a limit rows
    constraint.

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.queryrender.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.queryrender.js
@@ -88,6 +88,14 @@
             MatchCreateSingleQueryProcedure =
                 /(\s*(?:create\s+procedure\s+)(?:(?!create\s+(?:view|procedure))[\s\S])*\s+as\s+)((?:(?:\s\()*select)|insert|update|upsert|delete|truncate)\s+/gim,
             //   ($1----------------------------------------------------------------------------)($2------------------------------------------------------)
+
+	    // LIMIT PARTITION ROWS <n> EXECUTE (DELETE ...)
+	    // There are three keywords that may start statements to escape:
+	    //	 partition, execute, and delete
+	    MatchLimitPartition = /(\s*limit\s+)(partition)/gim,
+	    MatchRowcountExecute = /(\s*rows\s+\d+\s+)(execute)/gim,
+	    MatchExecuteDelete = /(\s*execute\s*\(\s*)(delete)/gim,
+
             MatchCompoundKeywordDisguise = /#NON_BREAKING_SUFFIX_KEYWORD#/g,
             GenerateDisguisedCompoundKeywords = ' $1 #NON_BREAKING_SUFFIX_KEYWORD#$2 ';
 
@@ -153,6 +161,10 @@
             src = src.replace(MatchNonBreakingCompoundKeywords, GenerateDisguisedCompoundKeywords);
             src = src.replace(MatchCreateView, GenerateDisguisedCompoundKeywords);
             src = src.replace(MatchCreateSingleQueryProcedure, GenerateDisguisedCompoundKeywords);
+
+	    src = src.replace(MatchLimitPartition, GenerateDisguisedCompoundKeywords);
+	    src = src.replace(MatchRowcountExecute, GenerateDisguisedCompoundKeywords);
+	    src = src.replace(MatchExecuteDelete, GenerateDisguisedCompoundKeywords);
 
             if (!src.match("^explain")) {
                 // Start a new statement before each remaining statement keyword.
@@ -259,7 +271,7 @@
 
         $("#runBTn").attr('disabled', 'disabled');
         $("#runBTn").addClass("graphOpacity");
-        
+
         var statements = CommandParser.parseUserInput(source);
         var start = (new Date()).getTime();
         var connectionQueue = connection.getQueue();
