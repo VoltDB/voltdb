27c3663e9aa497ab72ec17f7bed5726a807d61ea
Tim Campbell <tcampbell@voltdb.com>
2015-01-19 16:56:49 -0500

    Added more functions to DummyExport to make it more compatable

--- tests/test_apps/export-benchmark/src/exportbenchmark/DummyExporter.java
+++ tests/test_apps/export-benchmark/src/exportbenchmark/DummyExporter.java
@@ -1,9 +1,14 @@
 package exportbenchmark;
 
-
+import org.voltdb.export.AdvertisedDataSource;
 
 public class DummyExporter extends ExportClientBase {
-    
+
+    @Override
+    public ExportDecoderBase constructExportDecoder(AdvertisedDataSource source) {
+        return new PrinterExportDecoder(source);
+    }
+
     @Override
     public boolean processRow(int rowSize, byte[] rowData) throws RestartBlockException {
         // We don't want to do anything yet
