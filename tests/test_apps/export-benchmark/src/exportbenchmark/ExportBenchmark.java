760c6ae96dc866a6ec2de54f77594e18def66c87
Tim Campbell <tcampbell@voltdb.com>
2015-01-19 16:37:09 -0500

    Added basic classes so the test runs

--- tests/test_apps/export-benchmark/src/exportbenchmark/ExportBenchmark.java
+++ tests/test_apps/export-benchmark/src/exportbenchmark/ExportBenchmark.java
@@ -1,2 +1,20 @@
 package exportbenchmark;
 
+public class ExportBenchmark {
+
+    private void runTest() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * Main routine creates a benchmark instance and kicks off the run method.
+     *
+     * @param args Command line arguments.
+     * @throws Exception if anything goes wrong.
+     */
+    public static void main(String[] args) throws Exception {
+        ExportBenchmark bench = new ExportBenchmark();
+        bench.runTest();
+    }
+}
