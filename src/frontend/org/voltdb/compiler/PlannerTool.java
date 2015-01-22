02de0a0adfab6b48f031b4321f7663514a5fef71
Mike Ismert <izzy@voltdb.com>
2015-01-22 15:01:18 -0500

    ENG-7573: Introduce catalog hash down into CorePlan.  Goes nowhere does nothing. - Swap out usage of CorePlan.catalogVersion to check plan matching for   usage of CorePlan.catalogHash. -- Add convenience method wasPlannedAgainstHash to CorePlan - Remove catalogVersion from CorePlan - Clean up CatalogContext after making catalog bytes   non-nullity a requirement. - Switch AdHocCompilerCache to use hash rather than version   number to track different catalogs.  Eliminate catalogVersion where   appropriate.

--- src/frontend/org/voltdb/compiler/PlannerTool.java
+++ src/frontend/org/voltdb/compiler/PlannerTool.java
@@ -50,20 +50,21 @@ public class PlannerTool {
     final Database m_database;
     final Cluster m_cluster;
     final HSQLInterface m_hsql;
-    final int m_catalogVersion;
+    final byte[] m_catalogHash;
     final AdHocCompilerCache m_cache;
     static PlannerStatsCollector m_plannerStats;
 
     public static final int AD_HOC_JOINED_TABLE_LIMIT = 5;
 
-    public PlannerTool(final Cluster cluster, final Database database, int catalogVersion) {
+    public PlannerTool(final Cluster cluster, final Database database, byte[] catalogHash)
+    {
         assert(cluster != null);
         assert(database != null);
 
         m_database = database;
         m_cluster = cluster;
-        m_catalogVersion = catalogVersion;
-        m_cache = AdHocCompilerCache.getCacheForCatalogVersion(catalogVersion);
+        m_catalogHash = catalogHash;
+        m_cache = AdHocCompilerCache.getCacheForCatalogHash(catalogHash);
 
         // LOAD HSQL
         m_hsql = HSQLInterface.loadHsqldb();
@@ -229,7 +230,7 @@ public class PlannerTool {
             //////////////////////
             // OUTPUT THE RESULT
             //////////////////////
-            CorePlan core = new CorePlan(plan, m_catalogVersion);
+            CorePlan core = new CorePlan(plan, m_catalogHash);
             AdHocPlannedStatement ahps = new AdHocPlannedStatement(plan, core);
 
             if (partitioning.isInferred()) {
