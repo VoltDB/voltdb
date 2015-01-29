702027ada26646851b0db2fa8991d34896aae6e8
Tim Campbell <tcampbell@voltdb.com>
2015-01-29 16:57:10 -0500

    Changed default table that loads with VoltDB

--- tests/test_apps/export-benchmark/exportTable.sql
+++ tests/test_apps/export-benchmark/exportTable.sql
@@ -1,6 +1,12 @@
 CREATE TABLE valuesToExport (
-    value1 INT,
-    value2 INT
+    value1 TINYINT,
+    value2 SMALLINT,
+    value3 INT,
+    value4 BIGINT,
+    value5 FLOAT,
+    value6 DECIMAL,
+    value7 VARCHAR(32),
+    value8 TIMESTAMP
 );
 
 EXPORT TABLE valuesToExport;
