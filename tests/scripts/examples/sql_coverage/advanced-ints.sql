6f4bd4150f6a482e4086faf90c4522f42a8edde8
Xin Jia <xinjiacs@gmail.com>
2015-02-13 14:00:39 -0500

    Add another suites for IN/EXISTS test only

--- tests/scripts/examples/sql_coverage/advanced-ints.sql
+++ tests/scripts/examples/sql_coverage/advanced-ints.sql
@@ -1,9 +1,2 @@
 <configure-for-ints.sql>
 <advanced-template.sql>
-
---- Test IN/EXISTS Advanced cases
-SELECT * FROM @fromtables A WHERE _variable[@columntype] _maybe IN ( SELECT MIN(_variable[@columntype]) FROM @fromtables B WHERE B._variable[@columntype] _cmp A._variable[@columntype] )
-SELECT * FROM @fromtables A WHERE EXISTS ( SELECT _variable[#GB]  FROM @fromtables B GROUP BY B.__[#GB] HAVING MAX(B._variable[@columntype]) _cmp  A._variable[@columntype] )
-
-{@jointype = "_jointype"}
-SELECT * FROM @fromtables LHS @jointype JOIN @fromtables B25RHS ON LHS.@idcol = B25RHS.@idcol where LHS._variable[@columntype] NOT IN (SELECT _variable[@columntype] FROM @fromtables IN_TABLE)
