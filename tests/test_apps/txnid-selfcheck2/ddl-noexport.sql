7c5c1bd4e35ad3bf71809634e464b0858a42dc14
Phil Rosegay <prosegay@voltdb.com>
2015-02-13 15:22:19 -0500

    txnid2: convert app to use ddl schema init

---
+++ tests/test_apps/txnid-selfcheck2/ddl-noexport.sql
@@ -0,0 +1,206 @@
+-- partitioned table
+CREATE TABLE partitioned
+(
+  txnid      bigint             NOT NULL ASSUMEUNIQUE
+, prevtxnid  bigint             NOT NULL
+, ts         bigint             NOT NULL
+, cid        tinyint            NOT NULL
+, cidallhash bigint             NOT NULL
+, rid        bigint             NOT NULL
+, cnt        bigint             NOT NULL
+, adhocinc   bigint             NOT NULL
+, adhocjmp   bigint             NOT NULL
+, value      varbinary(1048576) NOT NULL
+, CONSTRAINT PK_id_p PRIMARY KEY
+  (
+    cid, txnid
+  )
+, UNIQUE ( cid, rid )
+);
+PARTITION TABLE partitioned ON COLUMN cid;
+CREATE INDEX P_CIDINDEX ON partitioned (cid);
+
+-- dimension table
+CREATE TABLE dimension
+(
+  cid        tinyint            NOT NULL
+, desc	     tinyint     		NOT NULL
+, CONSTRAINT PK_id_d PRIMARY KEY
+  (
+    cid
+  )
+, UNIQUE ( cid )
+);
+CREATE UNIQUE INDEX D_DESCINDEX ON dimension (desc);
+
+-- replicated table
+CREATE TABLE replicated
+(
+  txnid      bigint             NOT NULL
+, prevtxnid  bigint             NOT NULL
+, ts         bigint             NOT NULL
+, cid        tinyint            NOT NULL
+, cidallhash bigint             NOT NULL
+, rid        bigint             NOT NULL
+, cnt        bigint             NOT NULL
+, adhocinc   bigint             NOT NULL
+, adhocjmp   bigint             NOT NULL
+, value      varbinary(1048576) NOT NULL
+, CONSTRAINT PK_id_r PRIMARY KEY
+  (
+    txnid
+  )
+, UNIQUE ( cid, rid )
+);
+CREATE INDEX R_CIDINDEX ON replicated (cid);
+
+-- replicated table
+CREATE TABLE adhocr
+(
+  id         bigint             NOT NULL
+, ts         bigint             NOT NULL
+, inc        bigint             NOT NULL
+, jmp        bigint             NOT NULL
+, CONSTRAINT PK_id_ar PRIMARY KEY (id)
+);
+CREATE INDEX R_TSINDEX ON adhocr (ts DESC);
+
+-- partitioned table
+CREATE TABLE adhocp
+(
+  id         bigint             NOT NULL
+, ts         bigint             NOT NULL
+, inc        bigint             NOT NULL
+, jmp        bigint             NOT NULL
+, CONSTRAINT PK_id_ap PRIMARY KEY (id)
+);
+PARTITION TABLE adhocp ON COLUMN id;
+CREATE INDEX P_TSINDEX ON adhocp (ts DESC);
+
+-- replicated table
+CREATE TABLE bigr
+(
+  p          bigint             NOT NULL
+, id         bigint             NOT NULL
+, value      varbinary(1048576) NOT NULL
+, CONSTRAINT PK_id_br PRIMARY KEY (p,id)
+);
+
+-- partitioned table
+CREATE TABLE bigp
+(
+  p          bigint             NOT NULL
+, id         bigint             NOT NULL
+, value      varbinary(1048576) NOT NULL
+, CONSTRAINT PK_id_bp PRIMARY KEY (p,id)
+);
+PARTITION TABLE bigp ON COLUMN p;
+
+CREATE TABLE forDroppedProcedure
+(
+  p          integer             NOT NULL
+, id         bigint             NOT NULL
+, value      varbinary(1048576) NOT NULL
+, CONSTRAINT PK_id_forDroppedProcedure PRIMARY KEY (p,id)
+);
+PARTITION TABLE forDroppedProcedure ON COLUMN p;
+
+-- For loadsinglepartition
+CREATE TABLE loadp
+(
+  cid    BIGINT NOT NULL
+, txnid  BIGINT NOT NULL
+, rowid  BIGINT NOT NULL
+);
+PARTITION TABLE loadp ON COLUMN cid;
+CREATE TABLE cploadp
+(
+  cid    BIGINT NOT NULL
+, txnid  BIGINT NOT NULL
+, rowid  BIGINT NOT NULL
+);
+PARTITION TABLE cploadp ON COLUMN cid;
+
+
+-- For loadmultiplepartition
+CREATE TABLE loadmp
+(
+  cid    BIGINT NOT NULL
+, txnid  BIGINT NOT NULL
+, rowid  BIGINT NOT NULL
+);
+CREATE TABLE cploadmp
+(
+  cid    BIGINT NOT NULL
+, txnid  BIGINT NOT NULL
+, rowid  BIGINT NOT NULL
+);
+
+CREATE TABLE trur
+(
+  p          bigint             NOT NULL
+, id         bigint             NOT NULL
+, value      varbinary(1048576) NOT NULL
+, CONSTRAINT PK_id_tr PRIMARY KEY (p,id)
+);
+
+CREATE TABLE trup
+(
+  p          bigint             NOT NULL
+, id         bigint             NOT NULL
+, value      varbinary(1048576) NOT NULL
+, CONSTRAINT PK_id_tp PRIMARY KEY (p,id)
+);
+PARTITION TABLE trup ON COLUMN p;
+
+LOAD CLASSES txnid-procs.jar;
+
+-- base you shouldn't call
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.UpdateBaseProc;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.ReplicatedUpdateBaseProc;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.PoisonBaseProc;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.CopyLoadPartitionedBase;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.DeleteLoadPartitionedBase;
+
+-- real
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.SetupAdHocTables;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.UpdatePartitionedSP;
+PARTITION PROCEDURE UpdatePartitionedSP ON TABLE partitioned COLUMN cid;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.UpdatePartitionedMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.UpdateReplicatedMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.UpdateBothMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.UpdateReplicatedMPInProcAdHoc;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.ReadSP;
+PARTITION PROCEDURE ReadSP ON TABLE partitioned COLUMN cid;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.ReadMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.ReadSPInProcAdHoc;
+PARTITION PROCEDURE ReadSPInProcAdHoc ON TABLE partitioned COLUMN cid;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.ReadMPInProcAdHoc;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.Summarize;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.BIGPTableInsert;
+PARTITION PROCEDURE BIGPTableInsert ON TABLE bigp COLUMN p;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.BIGRTableInsert;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.PoisonSP;
+PARTITION PROCEDURE PoisonSP ON TABLE partitioned COLUMN cid;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.PoisonMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.PopulateDimension;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.CopyLoadPartitionedSP;
+PARTITION PROCEDURE CopyLoadPartitionedSP ON TABLE cploadp COLUMN cid;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.CopyLoadPartitionedMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.DeleteLoadPartitionedSP;
+PARTITION PROCEDURE DeleteLoadPartitionedSP ON TABLE cploadp COLUMN cid;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.DeleteLoadPartitionedMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.DeleteOnlyLoadTableSP;
+PARTITION PROCEDURE DeleteOnlyLoadTableSP ON TABLE loadp COLUMN cid;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.DeleteOnlyLoadTableMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.TRUPTableInsert;
+PARTITION PROCEDURE TRUPTableInsert ON TABLE bigp COLUMN p;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.TRURTableInsert;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.TRUPTruncateTableSP;
+PARTITION PROCEDURE TRUPTruncateTableSP ON TABLE trup COLUMN p;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.TRUPTruncateTableMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.TRURTruncateTable;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.TRUPScanAggTableSP;
+PARTITION PROCEDURE TRUPScanAggTableSP ON TABLE trup COLUMN p;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.TRUPScanAggTableMP;
+CREATE PROCEDURE FROM CLASS txnIdSelfCheck.TRURScanAggTable;
--- tests/test_apps/txnid-selfcheck2/run.sh
+++ tests/test_apps/txnid-selfcheck2/run.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-APPNAME="txnid"
+#set -o nounset #exit if an unset variable is used
+set -o errexit #exit on any single command fail
 
 # find voltdb binaries in either installation or distribution directory.
 if [ -n "$(which voltdb 2> /dev/null)" ]; then
     VOLTDB_BIN=$(dirname "$(which voltdb)")
 else
-    VOLTDB_BIN="$(pwd)/../../../bin"
+    VOLTDB_BIN="$(dirname $(dirname $(pwd)))/bin"
+    echo "The VoltDB scripts are not in your PATH."
+    echo "For ease of use, add the VoltDB bin directory: "
+    echo
+    echo $VOLTDB_BIN
+    echo
+    echo "to your PATH."
+    echo
 fi
+# move voltdb commands into path for this script
+
 # installation layout has all libraries in $VOLTDB_ROOT/lib/voltdb
 if [ -d "$VOLTDB_BIN/../lib/voltdb" ]; then
     VOLTDB_BASE=$(dirname "$VOLTDB_BIN")
@@ -15,60 +25,95 @@ if [ -d "$VOLTDB_BIN/../lib/voltdb" ]; then
     VOLTDB_VOLTDB="$VOLTDB_LIB"
 # distribution layout has libraries in separate lib and voltdb directories
 else
-    VOLTDB_LIB="`pwd`/../../../lib"
-    VOLTDB_VOLTDB="`pwd`/../../../voltdb"
+    VOLTDB_BASE=$(dirname "$VOLTDB_BIN")
+    VOLTDB_LIB="$VOLTDB_BASE/lib"
+    VOLTDB_VOLTDB="$VOLTDB_BASE/voltdb"
 fi
 
-CLASSPATH=$(ls -x "$VOLTDB_VOLTDB"/voltdb-*.jar | tr '[:space:]' ':')$(ls -x "$VOLTDB_LIB"/*.jar | egrep -v 'voltdb[a-z0-9.-]+\.jar' | tr '[:space:]' ':')
-VOLTDB="$VOLTDB_BIN/voltdb"
-LOG4J="$VOLTDB_VOLTDB/log4j.xml"
-CLIENTLOG4J="$VOLTDB_VOLTDB/../tests/log4j-allconsole.xml"
+VOLTDB_BASE=$VOLTDB_HOME
+VOLTDB_LIB=$VOLTDB_HOME/lib
+VOLTDB_BIN=$VOLTDB_HOME/bin
+VOLTDB_VOLTDB=$VOLTDB_HOME/voltdb
+
+PATH=$VOLTDB_BIN:$PATH
+
+CLIENTLOG4J="log4j.properties"
+APPCLASSPATH=$CLASSPATH:$({ \
+    \ls -1 "$VOLTDB_VOLTDB"/voltdb-*.jar; \
+    \ls -1 "$VOLTDB_LIB"/*.jar; \
+    \ls -1 "$VOLTDB_LIB"/extension/*.jar; \
+} 2> /dev/null | paste -sd ':' - )
+CLIENTCLASSPATH=txnid-client.jar:txnid-procs.jar:$CLASSPATH:$({ \
+    \ls -1 "$VOLTDB_VOLTDB"/voltdb-*.jar; \
+    \ls -1 "$VOLTDB_LIB"/commons-cli-1.2.jar; \
+} 2> /dev/null | paste -sd ':' - )
 LICENSE="$VOLTDB_VOLTDB/license.xml"
 HOST="localhost"
 
-# remove build artifacts
+# remove binaries, logs, runtime artifacts, etc... but keep the jars
 function clean() {
-    rm -rf obj debugoutput $APPNAME.jar $APPNAME-alt.jar $APPNAME-noexport.jar voltdbroot
+    rm -rf voltdbroot log procedures/txnIdSelfCheck/*.class client/txnIdSelfCheck/*.class *.jar
 }
 
-# compile the source code for procedures and the client
-function srccompile() {
-    mkdir -p obj
-    javac -target 1.7 -source 1.7 -classpath $CLASSPATH -d obj \
-        src/txnIdSelfCheck/*.java \
-        src/txnIdSelfCheck/procedures/*.java
-    # stop if compilation fails
-    if [ $? != 0 ]; then exit; fi
+# remove everything from "clean" as well as the jarfiles
+function cleanall() {
+    clean
+    rm -rf txnid-procs.jar txnid-client.jar
 }
 
-# build an application catalog
-function catalog() {
-    srccompile
+# compile the source code for procedures and the client into jarfiles
+function jars() {
+    # compile java source
+    javac -target 1.7 -source 1.7 -classpath $APPCLASSPATH procedures/txnIdSelfCheck/*.java
+    jar cf txnid-procs.jar -C procedures txnIdSelfCheck
+    javac -target 1.7 -source 1.7 -classpath txnid-procs.jar:$CLIENTCLASSPATH client/txnIdSelfCheck/*.java
+    jar cf txnid-client.jar -C client txnIdSelfCheck
+    # remove compiled .class files
+    rm -rf procedures/txnIdSelfCheck/*.class client/txnIdSelfCheck/*.class
+}
 
-    # primary catalog
-    $VOLTDB compile --classpath obj -o $APPNAME.jar src/txnIdSelfCheck/ddl.sql
-    # stop if compilation fails
-    if [ $? != 0 ]; then exit; fi
-
-    # alternate catalog that adds an index
-    $VOLTDB compile --classpath obj -o $APPNAME-alt.jar \
-        src/txnIdSelfCheck/ddl.sql src/txnIdSelfCheck/ddl-annex.sql
-    # stop if compilation fails
-    if [ $? != 0 ]; then exit; fi
-
-    # primary no-export catalog
-    $VOLTDB compile --classpath obj -o $APPNAME-noexport.jar src/txnIdSelfCheck/ddl-noexport.sql
-    # stop if compilation fails
-    if [ $? != 0 ]; then exit; fi
-
+# compile the procedure and client jarfiles if they don't exist
+function jars-ifneeded() {
+    if [ ! -e txnid-procs.jar ] || [ ! -e txnid-client.jar ]; then
+        jars;
+    fi
 }
 
 # run the voltdb server locally
 function server() {
-    # if a catalog doesn't exist, build one
-    if [ ! -f $APPNAME.jar ]; then catalog; fi
-    # run the server
-    $VOLTDB create -d deployment.xml -l $LICENSE -H $HOST $APPNAME.jar
+    echo "Starting the VoltDB server."
+    echo "To perform this action manually, use the command line: "
+    echo
+    echo "voltdb create -d deployment.xml -l $LICENSE -H $HOST"
+    echo
+    voltdb create -d deployment.xml -l $LICENSE -H $HOST
+}
+
+# load schema and procedures
+function init() {
+    jars-ifneeded
+    sqlcmd < ddl.sql
+}
+
+# wait for backgrounded server to start up
+function wait_for_startup() {
+    until sqlcmd  --query=' exec @SystemInformation, OVERVIEW;' > /dev/null 2>&1
+    do
+        sleep 2
+        echo " ... Waiting for VoltDB to start"
+        if [[ $SECONDS -gt 60 ]]
+        then
+            echo "Exiting.  VoltDB did not startup within 60 seconds" 1>&2; exit 1;
+        fi
+    done
+}
+
+# startup server in background and load schema
+function background_server_andload() {
+    # run the server in the background
+    voltdb create -B -d deployment.xml -l $LICENSE -H $HOST > nohup.log 2>&1 &
+    wait_for_startup
+    init
 }
 
 # run the client that drives the example
@@ -79,13 +124,13 @@ function client() {
 # Asynchronous benchmark sample
 # Use this target for argument help
 function benchmark-help() {
-    srccompile
-    java -classpath obj:$CLASSPATH:obj txnIdSelfCheck.Benchmark --help
+    jars-ifneeded
+    java -classpath $CLIENTCLASSPATH txnIdSelfCheck.Benchmark --help
 }
 
 function benchmark() {
-    srccompile
-    java -ea -classpath obj:$CLASSPATH:obj -Dlog4j.configuration=file://$CLIENTLOG4J \
+    jars-ifneeded
+    java -ea -classpath $CLIENTCLASSPATH -Dlog4j.configuration=$CLIENTLOG4J -Dlog4j.debug \
         txnIdSelfCheck.Benchmark \
         --displayinterval=1 \
         --duration=120 \
@@ -100,11 +145,29 @@ function benchmark() {
         --partfillerrowmb=128 \
         --progresstimeout=120 \
         --usecompression=false \
-        --allowinprocadhoc=false
+        --allowinprocadhoc=true
+}
+
+# The following two demo functions are used by the Docker package. Don't remove.
+# compile the jars for procs and client code
+function demo-compile() {
+    jars
+}
+
+function demo() {
+    echo "starting server in background..."
+    background_server_andload
+    echo "starting client..."
+    client
+
+    echo
+    echo When you are done with the demo database, \
+        remember to use \"voltadmin shutdown\" to stop \
+        the server process.
 }
 
 function help() {
-    echo "Usage: ./run.sh {clean|catalog|server|async-benchmark|aysnc-benchmark-help}"
+    echo "Usage: ./run.sh {clean|server|init|demo|client|benchmark|benchmark-help}"
 }
 
 # Run the target passed as the first arg on the command line
