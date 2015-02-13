41db4e23c2808c1827a2f26d0b929bdd55e61151
Christopher M. Wolff <cwolff@voltdb.com>
2015-02-13 07:29:49 -0800

    Fix for ENG-7633 and ENG-7665, insert...select and export
    Backported from version 5.1.
    
    Squashed commit of the following:
    
    commit 30a740f88a23a397cfcabd44f1881444ca76200a
    Author: Chris Wolff <cwolff@voltdb.com>
    Date:   Thu Feb 12 17:55:53 2015 -0800
    
        Add in table definition so test can run
    
    commit c733063aec8191e7d68ce4e80fd125586fde6f7a
    Author: Chris Wolff <cwolff@voltdb.com>
    Date:   Mon Feb 9 10:39:26 2015 -0800
    
        Move table definition into common sqltypessuite DDL file
    
        This was causing failures when trying to compile the stored
        procedures.
    
    commit 493ad6847f886aa12858ccb37f7bd202c862cb98
    Author: Christopher M. Wolff <cwolff@voltdb.com>
    Date:   Sun Feb 8 20:30:31 2015 -0800
    
        Fix dropped rows when inserting into export table
    
    commit b33499bb8fccb1bff9395d86a5399a3bd43bd237
    Author: Christopher M. Wolff <cwolff@voltdb.com>
    Date:   Sun Feb 8 16:26:56 2015 -0800
    
        Guard against non-partitioned export tables for INSERT INTO SELECT
    
    commit a5b08e182c8cdcf9d135d5de3d1eb3148d281089
    Author: Christopher M. Wolff <cwolff@voltdb.com>
    Date:   Sun Feb 8 15:07:45 2015 -0800
    
        Add more debug logging to insert executor
    
    commit 7c391f5e3263ef656195fc11558123624aedf23c
    Author: Chris Wolff <cwolff@voltdb.com>
    Date:   Fri Feb 6 16:09:51 2015 -0800
    
        Add tests for INSERT INTO SELECT and export tables
    
    (cherry picked from commit cc21be1ccb294a805aed81806fec2a5f1c84ad95)
    
    Conflicts:
    	tests/frontend/org/voltdb/regressionsuites/sqltypessuite-nonulls-ddl.sql
    	tests/frontend/org/voltdb/regressionsuites/sqltypessuite-nonulls-export-ddl.sql

--- src/frontend/org/voltdb/planner/PlanAssembler.java
+++ src/frontend/org/voltdb/planner/PlanAssembler.java
@@ -1164,8 +1164,10 @@ public class PlanAssembler {
 
             }
 
+            boolean targetIsExportTable = tableListIncludesExportOnly(m_parsedInsert.m_tableList);
             InsertSubPlanAssembler subPlanAssembler =
-                    new InsertSubPlanAssembler(m_catalogDb, m_parsedInsert, m_partitioning);
+                    new InsertSubPlanAssembler(m_catalogDb, m_parsedInsert, m_partitioning,
+                            targetIsExportTable);
             AbstractPlanNode subplan = subPlanAssembler.nextPlan();
             if (subplan == null) {
                 throw new PlanningErrorException(subPlanAssembler.m_recentErrorMsg);
@@ -1243,6 +1245,9 @@ public class PlanAssembler {
         // the root of the insert plan is always an InsertPlanNode
         InsertPlanNode insertNode = new InsertPlanNode();
         insertNode.setTargetTableName(targetTable.getTypeName());
+        if (subquery != null) {
+            insertNode.setSourceIsPartitioned(! subquery.getIsReplicated());
+        }
 
         // The field map tells the insert node
         // where to put values produced by child into the row to be inserted.
