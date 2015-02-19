1d6490be5f6dff0624830e80e2b41868c11d0794
Alex Rogers <arogers@voltdb.com>
2015-02-19 12:41:31 -0500

    Fix merge issue from master in AsyncCompilerAgent

--- src/frontend/org/voltdb/compiler/AsyncCompilerAgent.java
+++ src/frontend/org/voltdb/compiler/AsyncCompilerAgent.java
@@ -268,7 +268,7 @@ public class AsyncCompilerAgent {
         // If we weren't provided operationBytes, it's a deployment-only change and okay to take
         // master and adhoc DDL method chosen
         if (w.invocationName.equals("@UpdateApplicationCatalog") &&
-            w.operationBytes != null && !w.onReplica && w.useAdhocDDL)
+            w.operationBytes != null && w.useAdhocDDL)
         {
             AsyncCompilerResult errResult =
                 AsyncCompilerResult.makeErrorResult(w,
@@ -277,7 +277,7 @@ public class AsyncCompilerAgent {
             w.completionHandler.onCompletion(errResult);
             return;
         }
-        else if (w.invocationName.equals("@UpdateClasses") && !w.onReplica && !w.useAdhocDDL) {
+        else if (w.invocationName.equals("@UpdateClasses") && !w.useAdhocDDL) {
             AsyncCompilerResult errResult =
                 AsyncCompilerResult.makeErrorResult(w,
                         "Cluster is configured to use @UpdateApplicationCatalog " +
