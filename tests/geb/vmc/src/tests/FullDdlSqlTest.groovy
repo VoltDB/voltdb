7e25dbc5f81c551ae8d10399722f18fef5da2da4
jcrawford <jcrawford@voltdb.com>
2015-03-23 11:40:57 -0400

    FullDdlSqlTest: now inherits from SqlQueriesTestBase, so setup(), ensureOnSqlQueryPage() & runQuery(…) were deleted (moved to SqlQueriesTestBase); added, to cleanupSpec(), removal of any new Export Tables and Roles that were added by the tests; added new isValidNameChar() & getName() methods, to parse Export Table and Role names from the commands - and call these from runFullDdlSqlFile(); runFullDdlSqlFile() also now calls getFileLines() with the ignoring blank lines option; added a kludge for ENG-7869 (for named LIMIT PARTITION ROWS commands); also, now ignore CREATE PROCEDURE … FROM CLASS commands; catch any exceptions thrown while running the JUnit test, but print them, and any errors found while executing the SQL DDL commands, at the end (& fail the test); and improved comments.

--- tests/geb/vmc/src/tests/FullDdlSqlTest.groovy
+++ tests/geb/vmc/src/tests/FullDdlSqlTest.groovy
@@ -23,19 +23,21 @@
 
 package vmcTest.tests
 
-//import geb.*
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import org.voltdb.fullddlfeatures.TestDDLFeatures
 ////import org.voltdb_testprocs.fullddlfeatures.testCreateProcFromClassProc
 import spock.lang.*
-import vmcTest.pages.*
+import vmcTest.pages.SqlQueryPage
+import vmcTest.pages.VoltDBManagementCenterPage.ColumnHeaderCase
 
 /**
- * This class contains tests of the 'SQL Query' tab of the VoltDB Management
- * Center (VMC) page, which is the VoltDB (new) web UI.
+ * This class tests the 'fullDDL.sql' file in the VMC, by running its queries
+ * on the 'SQL Query' tab of the VoltDB Management Center (VMC) page, and
+ * validating the results.
  */
-class FullDdlSqlTest extends TestBase {
+class FullDdlSqlTest extends SqlQueriesTestBase {
     static final String FULL_DDL_FILE = '../../frontend/org/voltdb/fullddlfeatures/fullDDL.sql';
 
     @Shared def fullDdlFile = new File(FULL_DDL_FILE)
@@ -43,10 +45,13 @@ class FullDdlSqlTest extends TestBase {
     @Shared def existingTables = []
     @Shared def existingViews = []
     @Shared def existingStoredProcs = []
+    @Shared def newExportTables = []
+    @Shared def newRoles = []
+    @Shared def errors = [:]
 
+    @Shared def ignoreTheseTestMethods = ['testCreateProcedureFromClass']
+    
     def setupSpec() { // called once, before any tests
-        // TestBase.setup gets called first (automatically)
-
         // Make sure we're on the SQL Query page
         ensureOnSqlQueryPage()
 
@@ -60,16 +65,8 @@ class FullDdlSqlTest extends TestBase {
         debugPrint '\nExisting User Stored Procedures:\n' + existingStoredProcs
     }
 
-    def setup() { // called before each test
-        when: 'click the SQL Query link (if needed)'
-        ensureOnSqlQueryPage()
-        then: 'should be on SQL Query page'
-        at SqlQueryPage
-    }
-
     def cleanupSpec() { // called once, after all the tests
-        // First, refresh the page, to ensure we have the latest data
-        driver.navigate().refresh()
+        // Make sure we're on the SQL Query page
         ensureOnSqlQueryPage()
 
         // Next, get the lists of new tables, views & (user) Stored Procs,
@@ -80,156 +77,145 @@ class FullDdlSqlTest extends TestBase {
         newTables.removeAll(existingTables)
         newViews.removeAll(existingViews)
         newStoredProcs.removeAll(existingStoredProcs)
-        debugPrint '\nNew Tables (to be dropped??):\n' + newTables
-        debugPrint '\nNew Views (to be dropped??):\n' + newViews
-        debugPrint '\nNew User Stored Procedures (to be dropped??):\n' + newStoredProcs
+        debugPrint '\nNew Tables (to be dropped):\n' + newTables
+        debugPrint '\nNew Export Tables (to be dropped):\n' + newExportTables
+        debugPrint '\nNew Views (to be dropped):\n' + newViews
+        debugPrint '\nNew User Stored Procedures (to be dropped):\n' + newStoredProcs
+        debugPrint '\nNew Roles (to be dropped):\n' + newRoles
 
         // Drop all the new tables, views & (user) Stored Procs that were
         // created by this test
-//*
         newStoredProcs.each { runQuery(page, 'Drop procedure ' + it + ' if exists') }
         newViews.each { runQuery(page, 'Drop view ' + it + ' if exists') }
         newTables.each { runQuery(page, 'Drop table ' + it + ' if exists') }
-//*/
-    }
-    
-    def ensureOnSqlQueryPage() {
-        ensureOnVoltDBManagementCenterPage()
-        page.openSqlQueryPage()
+        newExportTables.each { runQuery(page, 'Drop table ' + it + ' if exists') }
+        newRoles.each { runQuery(page, 'Drop role ' + it + ' if exists') }
     }
 
     /**
-     * Runs, on the specified SqlQueryPage, the specified query, and returns
-     * the result. (Also, if DEBUG is true, prints: the query, the result, an
-     * error message, if any, and the query duration.)
-     * @param sqp - the SqlQueryPage on which to run the query.
-     * @param query - the query to be run.
-     * @return the query result (as a Map of Lists of Strings).
+     * TODO
      */
-    def Map<String,List<String>> runQuery(SqlQueryPage sqp, String query) {
-        sqp.runQuery(query)
-        def qResult = sqp.getQueryResult()
-        def qResText = sqp.getQueryResultText()
-
-        if (qResText.contains("Connect to a datasource first")) {
-            debugPrint "\nQuery : " + query
-            debugPrint "Result Text: " + qResText
-            debugPrint "Result: " + qResult
-            debugPrint "Duration: " + sqp.getQueryDuration()
-            debugPrint "Error : " + sqp.getQueryError()
-            debugPrint "Reloading and trying again..."
-            driver.navigate().refresh()
-            if (!sqp.verifyAtSafely()) {
-                debugPrint "  Moving to VoltDBManagementCenterPage"
-                to VoltDBManagementCenterPage
-                page.loginIfNeeded()
-                debugPrint "  Opening SqlQueryPage"
-                page.openSqlQueryPage()
-            }
-            sqp.verifyAt()
-            sqp.runQuery(query)
-            qResult = sqp.getQueryResult()
+    private boolean isValidNameChar(char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return true
+        } else if (c >= 'a' && c <= 'z') {
+            return true
+        } else if (c >= '0' && c <= '9') {
+            return true
+        } else if (c == '$' || c == '_') {
+            return true
+        } else {
+            return false
         }
-
-        // If 'sleepSeconds' property is set and greater than zero, sleep
-        int sleepSeconds = getIntSystemProperty("sleepSeconds", 0)
-        if (sleepSeconds > 0) {
-            try {
-                Thread.sleep(1000 * sleepSeconds)
-            } catch (InterruptedException e) {
-                println "\nIn FullDdlSqlTest.runQuery, caught:\n  " + e.toString() + "\nSee standard error for stack trace."
-                e.printStackTrace()
+    }
+    
+    /**
+     * TODO
+     */
+    private String getName(String command, String afterThis) {
+        println '\nFound a ' + afterThis + ' command...'
+        println '  ' + command
+        int start = command.toUpperCase().indexOf(afterThis) + afterThis.length()
+        int end = command.length();
+        for (int i=start; i < end; i++) {
+            if (isValidNameChar(command.charAt(i))) {
+                start = i
+                break
             }
         }
-
-        debugPrint "\nQuery : " + query
-        debugPrint "Result: " + qResult
-        debugPrint "Duration: " + sqp.getQueryDuration()
-        def error = sqp.getQueryError()
-        if (error != null) {
-            debugPrint "Error : " + error
+        for (int i=start; i < command.length(); i++) {
+            if (!isValidNameChar(command.charAt(i))) {
+                end = i
+                break
+            }
         }
-
-        return qResult
+        println '  start, end: ' + start + ', ' + end
+        println '  substring : ' + command.substring(start, end)
+        return command.substring(start, end)
     }
 
     /**
      * TODO
      */
     def runFullDdlSqlFile() {
-        // Get the lines and commands of the fullDDL.sql file
-        def lines = getFileLines(fullDdlFile, '--')
-//*
+        // Get the lines of the fullDDL.sql file (ignoring comment lines
+        // starting with '--', and blank lines)
+        def lines = getFileLines(fullDdlFile, '--', false)
+
+        // Break the lines into commands, based on ending with ';'
         def commands = []
         int startCommandAtLine = 0
-        lines.eachWithIndex { line, i -> if (line.trim().endsWith(';')) {
-            def command = lines[startCommandAtLine]
-            for (int j=startCommandAtLine+1; j <= i; j++) {
+        lines.eachWithIndex { line, i ->
+            if (line.trim().endsWith(';')) {
+                def command = lines[startCommandAtLine]
+                for (int j=startCommandAtLine+1; j <= i; j++) {
                     command += '\n' + lines[j]
+                }
+                // Kludge for ENG-7869 (for named LIMIT PARTITION ROWS commands)
+                if (command.contains('DROP CONSTRAINT lpr39A')) {
+                    println '\nReplacing command:\n' + command
+                    command = command.replace('DROP CONSTRAINT lpr39A', 'DROP LIMIT PARTITION ROWS')
+                    println 'With command:\n' + command
+                }
+                commands.add(command)
+                startCommandAtLine = i + 1
             }
-            commands.add(command)
-            startCommandAtLine = i + 1
-        }}
+        }
 
         // Execute each (DDL) SQL command
         commands.each {
-            runQuery(page, it)
-            def qResults = page.getQueryResults(/*ColumnHeaderCase.AS_IS*/)
-            def error    = page.getQueryError()
-            def duration = page.getQueryDuration()
-            debugPrint '\nDDL Command:\n' + it
-            debugPrint '\nResult(s):\n' + qResults
-            if (error != null) {
-                debugPrint "Error:\n" + error
-            }
-            debugPrint 'Duration: ' + duration
-        }
-//*/
-        // Do validation that the (DDL) SQL commands worked
-        TestDDLFeatures tdf = new TestDDLFeatures();
-        tdf.init()
-        //tdf.initClient()
+            String commandUpperCase = it.toUpperCase()
 
-        // TODO
+            // TODO
+            if (commandUpperCase.contains('CREATE') && commandUpperCase.contains('PROCEDURE') &&
+                commandUpperCase.contains('FROM') && commandUpperCase.contains('CLASS')) {
+                println '\nSkipping command:\n' + it
+            } else {
+                def qResults = runQuery(page, it, ColumnHeaderCase.AS_IS)
+            }
+
+            // Keep track of CREATE ROLE commands
+            if (commandUpperCase.contains('CREATE') && commandUpperCase.contains('ROLE')) {
+                newRoles.add(getName(it, 'ROLE'))
+            // Keep track of EXPORT TABLE commands
+            } else if (commandUpperCase.contains('EXPORT') && commandUpperCase.contains('TABLE')) {
+                newExportTables.add(getName(it, 'TABLE'))
+            }
+        }
+
+        // Do validation that the (DDL) SQL commands worked, by running the
+        // JUnit tests of the TestDDLFeatures class
+        TestDDLFeatures tdf = new TestDDLFeatures();
+        tdf.startClient()
         Method[] methods = tdf.getClass().getMethods()
         for (Method method : methods) {
             Annotation[] annotations = method.getAnnotations()
             for (Annotation annotation : annotations) {
                 if (annotation.annotationType().equals(org.junit.Test.class)) {
-                    if (!['testCreateProcedureFromClass','testAlterTableDropConstraint'].contains(method.getName())) {
-                        println "\nInvoking Method: " + method.getName()
-                        method.invoke(tdf)
+                    if (ignoreTheseTestMethods.contains(method.getName())) {
+                        debugPrint '\nSkipping JUnit Method: TestDDLFeatures.' + method.getName()
                     } else {
-                        println "\nSkipping Method: " + method.getName()
+                        debugPrint '\nInvoking JUnit Method: TestDDLFeatures.' + method.getName()
+                        try {
+                            method.invoke(tdf)
+                        } catch (InvocationTargetException e) {
+                            errors.put(method.getName(), e.toString() + ':\n' + e.getCause()?.toString())
+                            println 'Caught InvocationTargetException, running TestDDLFeatures.' + method.getName() + ':'
+                            e.printStackTrace(System.out);
+                        }
                     }
                 }
             }
         }
-/*
-        tdf.testCreateUniqueIndex()
-        tdf.testCreateAssumeUniqueIndex()
-        tdf.testCreateHashIndex()
-        
-        //tdf.testCreateProcedureAsSQLStmt()
-        //tdf.testCreateProcedureFromClass()
-        //tdf.testCreateTableDataType()
-        
-        tdf.testCreateTableConstraint()
-        tdf.testCreateView()
-        tdf.testExportTable()
-        tdf.testPartitionProcedure()
-        tdf.testDropView()
-        tdf.testDropIndex()
-        tdf.testDropProcedure()
-        tdf.testDropTable()
-        //tdf.testAlterTableDropConstraint()
-        tdf.testAlterTableAddConstraint()
-        tdf.testAlterTableAddColumn()
-        tdf.testAlterTableDropColumn()
-        tdf.testAlterTableAlterColumn()
-*/
-        expect: 'TODO: just a temp placeholder'
-        lines.size() == 687
+
+        // List any errors that were found
+        for (String key : errors.keySet()) {
+            println '\nAction that caused Error:\n' + key
+            println '\nError:\n' + errors.get(key)
+        }
+
+        expect: 'There should be no errors'
+        errors.isEmpty()
     }
 
 }
