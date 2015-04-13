0d05ec2da97839bad76de149c0a2696f8b703a4e
Anish Khanzode <akhanzode@voltdb.com>
2015-04-13 10:52:57 -0400

    VMC-158 : Fixed the bug "Default & User-defined Stored Procedures don't work from VMC". VMC-155 Make changes to Starvation chart

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.render.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.render.js
@@ -451,6 +451,17 @@ function alertNodeClicked(obj) {
                 onInformationLoaded(tablesData, viewsData, proceduresData, procedureColumnsData, sysProceduresData);
             });
         };
+        
+        this.GetTableInformationClientPort = function () {
+            VoltDBService.GetTableInformationClientPort(function (connection) {
+                var tablesData = {};
+                var viewsData = {};
+                var proceduresData = {};
+                var procedureColumnsData = {};
+                var sysProceduresData = {};
+                getTableData(connection, tablesData, viewsData, proceduresData, procedureColumnsData, sysProceduresData, 'TABLE_INFORMATION_CLIENTPORT');
+            });
+        };
 
         this.GetHostNodesHtml = function (callback) {
             try {
@@ -1933,10 +1944,10 @@ function alertNodeClicked(obj) {
             connection.Metadata['@Statistics_STARVATION'].data.forEach(function (info) {
                 if (currentServer == info[colIndex["HOSTNAME"]]) {
                     if (siteId == parseInt(info[colIndex["SITE_ID"]])) {
-                        var keyMpi = info[colIndex["HOSTNAME"]] + info[colIndex["SITE_ID"]];
+                        var keyMpi = info[colIndex["HOSTNAME"]] + ': ' + info[colIndex["SITE_ID"]];
                         starvMpiData[keyMpi] = info[colIndex["PERCENT"]];
                     } else {
-                        var key = info[colIndex["HOSTNAME"]] + info[colIndex["SITE_ID"]];
+                        var key = info[colIndex["HOSTNAME"]] + ': ' + info[colIndex["SITE_ID"]];
                         keys.push(key);
                         starvStats[key] = info[colIndex["PERCENT"]];
                     }
@@ -1964,7 +1975,7 @@ function alertNodeClicked(obj) {
                                 maxPer = parseFloat(info[colIndex["PERCENT"]]);
                             }
                             previousHost = info[colIndex["HOSTNAME"]];
-                            previousHostKey = info[colIndex["HOSTNAME"]] + info[colIndex["SITE_ID"]];
+                            previousHostKey = info[colIndex["HOSTNAME"]] + ': ' + info[colIndex["SITE_ID"]];
                             previousSiteId = info[colIndex["SITE_ID"]];
                         }
                     }
@@ -2472,7 +2483,7 @@ function alertNodeClicked(obj) {
         
         function getTableData(connection, tablesData, viewsData, proceduresData, procedureColumnsData, sysProceduresData, processName) {
             var suffix = "";
-            if (processName == "TABLE_INFORMATION") {
+            if (processName == "TABLE_INFORMATION" || processName == "TABLE_INFORMATION_CLIENTPORT") {
                 suffix = "_" + processName;
             }
 
