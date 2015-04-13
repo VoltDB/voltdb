0c3d3f90065c635500b65ae2e78a63cd2f68faff
Sujesh Shakya <sujeshakya@deerwalk.com>
2015-04-13 12:06:18 +0545

    Reverting false commit on voltdb.config.js

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.config.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.config.js
@@ -3,39 +3,15 @@
 
     var iVoltDBConfig = (function () {
 
-        //var CONFIG_MODE = "release"; //Enable this for production release.
-        var CONFIG_MODE = "debug"; //Enable this for debug mode.
-
-        //This JSON object should only used for debug purpose.
-        this.PrivateToPublicIP = {
-            '10.140.25.211': '184.73.30.156',
-            '10.110.218.156': 'ec2-54-83-134-161.compute-1.amazonaws.com'
-        };
-
         this.GetDefaultServerIP = function () {
-            if (CONFIG_MODE == "debug") {
-               // return "192.168.0.213"; //LOCAL
-                 return "10.10.1.52"; //UAT
-                //return "192.168.0.239";
-                //return "10.10.1.52";
-                //return "192.168.0.212";
-                //return "184.73.30.156";
-            }
-
-            return "localhost";
+            return window.location.hostname;
         };
 
         this.GetDefaultServerNameForKey = function () {
-            if (CONFIG_MODE == "debug")
-                return "184_73_30_156";
-
-            return "localhost";
+            return window.location.hostname;
         };
 
         this.GetPortId = function () {
-            if (CONFIG_MODE == "debug")
-                return null;
-
             return window.location.port;
         };
     });
@@ -43,3 +19,4 @@
     window.VoltDBConfig = VoltDBConfig = new iVoltDBConfig();
 
 })(window);
+    
