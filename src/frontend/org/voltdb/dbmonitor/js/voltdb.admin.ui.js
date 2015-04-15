f36d0ce2fd2e56f73e01839a8621875c1b10a1aa
Anish Khanzode <akhanzode@voltdb.com>
2015-04-15 10:03:03 -0400

    Fixed design issue in error message on export configuration stream. Fixed design issue in error label of Export Configuration Stream for IE. Changed the delta value from 1 to 0 for the starvation statistics.

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.admin.ui.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.admin.ui.js
@@ -1574,8 +1574,8 @@ function loadAdminPage() {
                 '<table width="100%" cellpadding="0" cellspacing="0" class="configurTbl">' +
                 '<tr id="Tr1">' +
                 '    <td>Stream</td>' +
-                '    <td width="10%">' +
-                '       <input id="txtStream" name="txtStream" type="text" size="30">' +
+                '    <td width="15%">' +
+                '       <input id="txtStream" name="txtStream" type="text" size="38">' +
                 '       <label id="errorStream" for="txtStream" class="error" style="display: none;"></label>' +
                 '    </td>' +
                 '    <td width="8%" align="right"><input type="checkbox" checked="true" id="chkStream" class="chkStream"/></td>' +
@@ -1584,7 +1584,7 @@ function loadAdminPage() {
                 '<tr>' +
                 '    <td>Type </td>' +
                 '    <td>' +
-                '       <input id="txtType" name="txtType" type="text" size="30">' +
+                '       <input id="txtType" name="txtType" type="text" size="38">' +
                 '       <label id="errorType" for="txtType" class="error" style="display: none;"></label>' +
                 '    </td>' +
                 '    <td>&nbsp;</td>' +
@@ -1954,7 +1954,7 @@ function loadAdminPage() {
             $("#btnSaveSecUser").on("click", function () {
                 var username = $('#txtOrgUser').val();
                 var newUsername = $('#txtUser').val();
-                var password = $('#txtPassword').val();
+                var password = encodeURIComponent($('#txtPassword').val());
                 var role = $('#selectRole').val();
                 var requestType = "POST";
                 var requestUser = "";
