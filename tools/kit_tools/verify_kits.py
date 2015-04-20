6398a76f6db474fafcada8735367ccd9d5f0a412
Jenkins CI <test@voltdb.com>
2015-04-20 17:06:12 -0400

    Set Version 5.2

--- tools/kit_tools/verify_kits.py
+++ tools/kit_tools/verify_kits.py
@@ -5,7 +5,7 @@ from fabric.api import run, cd, local, get, settings, lcd
 from fabric_ssh_config import getSSHInfoForHost
 
 verifydir = "/tmp/" + os.getenv('USER') + "/verifytemp"
-version = "voltdb-5.2EA2"
+version = "5.2"
 
 ################################################
 # SETUP A DIST & TOOLS IN A TEMP DIR
