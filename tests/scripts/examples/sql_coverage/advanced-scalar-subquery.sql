738747c37cd23a790acb43f62c13c1a25fd4fd11
jcrawford <jcrawford@voltdb.com>
2015-04-30 14:24:02 -0400

    advanced-scalar-subquery.sql: fixed some query patterns to use ORDER BY (unnamed) column index(es), e.g.: ... ORDER BY 1 , 2 to fix patterns that were only producing invalid queries.

--- tests/scripts/examples/sql_coverage/advanced-scalar-subquery.sql
+++ tests/scripts/examples/sql_coverage/advanced-scalar-subquery.sql
@@ -16,6 +16,12 @@ INSERT INTO @dmltable VALUES (@insertvals)
 {_optionaloderbylimitoffset |= ""}
 {_optionaloderbylimitoffset |= "LIMIT 1000"}
 {_optionaloderbylimitoffset |= "ORDER BY C0 _optionalascdesc _optionallimitoffset"}
+{_optionaloderby1limitoffset |= ""}
+{_optionaloderby1limitoffset |= "LIMIT 1000"}
+{_optionaloderby1limitoffset |= "ORDER BY 1 _optionalascdesc _optionallimitoffset"}
+{_optionaloderby2limitoffset |= ""}
+{_optionaloderby2limitoffset |= "LIMIT 1000"}
+{_optionaloderby2limitoffset |= "ORDER BY 1 _optionalascdesc, 2 _optionalascdesc _optionallimitoffset"}
 
 
 -- TEMP, for debugging, just so I can quickly see what data was generated:
@@ -25,17 +31,17 @@ SELECT * FROM @fromtables ORDER BY @idcol
 --- Test Scalar Subquery Advanced cases
 
 --- Queries with scalar subqueries in the SELECT clause (with optional ORDER BY, LIMIT or OFFSET clauses)
-SELECT _variable AS C0, (SELECT @agg(_variable) FROM @fromtables                                                                    ) FROM @fromtables    A1 _optionaloderbylimitoffset
-SELECT _variable AS C0, (SELECT @agg(_variable) FROM @fromtables WHERE A2._variable[@comparabletype] _cmp _variable[@comparabletype]) FROM @fromtables AS A2 _optionaloderbylimitoffset
+SELECT _variable, (SELECT @agg(_variable) FROM @fromtables                                                                    ) FROM @fromtables    A1 _optionaloderby2limitoffset
+SELECT _variable, (SELECT @agg(_variable) FROM @fromtables WHERE A2._variable[@comparabletype] _cmp _variable[@comparabletype]) FROM @fromtables AS A2 _optionaloderby2limitoffset
 
 --- Queries with scalar subqueries in the FROM clause (with optional ORDER BY, LIMIT or OFFSET clauses)
-SELECT _variable AS C0 FROM (SELECT @agg(_variable) FROM @fromtables                                                                 )    A3 _optionaloderbylimitoffset
-SELECT _variable AS C0 FROM (SELECT @agg(_variable) FROM @fromtables WHERE _variable[@comparabletype] _cmp _variable[@comparabletype]) AS A4 _optionaloderbylimitoffset
+SELECT * FROM (SELECT @agg(_variable) FROM @fromtables                                                                 )    A3 _optionaloderby1limitoffset
+SELECT * FROM (SELECT @agg(_variable) FROM @fromtables WHERE _variable[@comparabletype] _cmp _variable[@comparabletype]) AS A4 _optionaloderby1limitoffset
 
 --- Queries with scalar subqueries in the WHERE clause (with optional ORDER BY, LIMIT or OFFSET clauses)
 SELECT _variable AS C0 FROM @fromtables A5 WHERE _variable _cmp (SELECT @agg(_variable) FROM @fromtables                                                                    ) _optionaloderbylimitoffset
 -- TODO: this one takes too long (when it produces anything); may try to come up with a simpler, quicker version:
---SELECT _variable AS C0 FROM @fromtables A6 WHERE _variable _cmp (SELECT @agg(_variable) FROM @fromtables WHERE _variable[@comparabletype] _cmp A6._variable[@comparabletype]) _optionaloderbylimitoffset
+SELECT _variable AS C0 FROM @fromtables A6 WHERE _variable _cmp (SELECT @agg(_variable) FROM @fromtables WHERE _variable[@comparabletype] _cmp A6._variable[@comparabletype]) _optionaloderbylimitoffset
 
 --- Queries with scalar subqueries in the ORDER BY, LIMIT or OFFSET clause (these return errors)
 SELECT (SELECT @agg(_variable) FROM @fromtables) C0 FROM @fromtables A7 ORDER BY C0 _optionalascdesc _optionallimitoffset
