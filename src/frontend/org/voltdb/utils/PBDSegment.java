a55b5286d7551c528d70fdbcb21952e051a56eab
Ning Shi <nshi@voltdb.com>
2015-05-11 17:43:44 -0400

    ENG-8237: Address Alex's review comments.

--- src/frontend/org/voltdb/utils/PBDSegment.java
+++ src/frontend/org/voltdb/utils/PBDSegment.java
@@ -128,34 +128,30 @@ class PBDSegment {
             throw new IOException("Segment is already opened");
         }
 
-        try {
-            if (!m_file.exists()) {
-                m_syncedSinceLastEdit = false;
-            }
-            if (m_ras != null) {
-                throw new IOException(m_file + " was already opened");
-            }
-            m_ras = new RandomAccessFile(m_file, "rw");
-            m_fc = m_ras.getChannel();
-
-            if (forWrite) {
-                //If this is for writing, map the chunk size RW and put the buf positions at the start
-                m_buf = DBBPool.wrapMBB(m_fc.map(MapMode.READ_WRITE, 0, m_chunkSize));
-                m_buf.b().position(SIZE_OFFSET + 4);
-                m_readBuf = m_buf.b().duplicate();
-                initNumEntries();
-            } else {
-                //If it isn't for write, map read only to the actual size and put the write buf position at the end
-                //so size is reported correctly
-                final long size = m_fc.size();
-                m_buf = DBBPool.wrapMBB(m_fc.map(MapMode.READ_ONLY, 0, size));
-                m_readBuf = m_buf.b().duplicate();
-                m_buf.b().position((int) size);
-                m_readBuf.position(SIZE_OFFSET + 4);
-            }
-        } finally {
-            m_closed = false;
+        if (!m_file.exists()) {
+            m_syncedSinceLastEdit = false;
         }
+        assert(m_ras == null);
+        m_ras = new RandomAccessFile(m_file, "rw");
+        m_fc = m_ras.getChannel();
+
+        if (forWrite) {
+            //If this is for writing, map the chunk size RW and put the buf positions at the start
+            m_buf = DBBPool.wrapMBB(m_fc.map(MapMode.READ_WRITE, 0, m_chunkSize));
+            m_buf.b().position(SIZE_OFFSET + 4);
+            m_readBuf = m_buf.b().duplicate();
+            initNumEntries();
+        } else {
+            //If it isn't for write, map read only to the actual size and put the write buf position at the end
+            //so size is reported correctly
+            final long size = m_fc.size();
+            m_buf = DBBPool.wrapMBB(m_fc.map(MapMode.READ_ONLY, 0, size));
+            m_readBuf = m_buf.b().duplicate();
+            m_buf.b().position((int) size);
+            m_readBuf.position(SIZE_OFFSET + 4);
+        }
+
+        m_closed = false;
     }
 
     public void closeAndDelete() throws IOException {
