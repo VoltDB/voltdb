59811dd173a3ed66b8ca76f7a744503e2e82e83b
Daniel Weiss <dweiss@voltdb.com>
2015-05-11 17:35:29 -0400

    ENG-8084 fixed bug with buildstring

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -1798,7 +1798,10 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback
         String buildInfo[] = extractBuildInfo();
         m_versionString = buildInfo[0];
         m_buildString = buildInfo[1];
-        consoleLog.info(String.format("Build: %s %s %s", m_versionString, m_buildString.split("_", 2)[1], editionTag));
+        String buildString = m_buildString;
+        if (m_buildString.contains("_"))
+            buildString = m_buildString.split("_", 2)[1];
+        consoleLog.info(String.format("Build: %s %s %s", m_versionString, buildString, editionTag));
     }
 
     /**
