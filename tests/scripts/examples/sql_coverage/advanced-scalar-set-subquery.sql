daa9eafeef0ebe38fb11ef6a2290d780a0ceb62c
jcrawford <jcrawford@voltdb.com>
2015-05-29 18:08:17 -0400

    advanced-scalar-set-subquery.sql: added queries suggested by Paul's review; also renamed some column variables, to be clearer and more consistent (#str for string (varchar) columns; #num for numeric columns).

--- tests/scripts/examples/sql_coverage/advanced-scalar-set-subquery.sql
+++ tests/scripts/examples/sql_coverage/advanced-scalar-set-subquery.sql
@@ -24,26 +24,36 @@ INSERT INTO R2 (SELECT * FROM R1 WHERE ID < 10)
 
 --- Queries with scalar subqueries containing a set (UNION, INTERSECT, EXCEPT [ALL]) query
 {_optionalwherestr |= ""}
-{_optionalwherestr |= "WHERE __[#agg] _cmp3 Z.__[#agg]"}
-{_optionalwherenum |= "_optionalwherestr"}
-{_optionalwherenum |= "WHERE _variable[@comparabletype] _cmp2 Z.__[#agg]"}
+{_optionalwherestr |= "WHERE __[#str] _cmp3 Z.__[#str]"}
+{_optionalwherenum |= ""}
+{_optionalwherenum |= "WHERE __[#num] _cmp3 Z.__[#num]"}
+{_optionalwherenum |= "WHERE _variable[@comparabletype] _cmp2 Z.__[#num]"}
 
-{_simplequerystr |= "SELECT __[#agg] FROM @fromtables"}
-{_simplequerynum |= "SELECT __[#agg] FROM @fromtables"}
+{_simplequerystr |= "SELECT __[#str] FROM @fromtables"}
+{_simplequerynum |= "SELECT __[#num] FROM @fromtables"}
 
-{_setopquerystr  |= "_simplequerystr _setop _simplequerystr"}
-{_setopquerynum  |= "_simplequerynum _setop _simplequerynum"}
+{_wherequerystr |= "_simplequerystr WHERE __[#str] _cmp1 Z.__[#str]"}
+{_wherequerynum |= "_simplequerynum WHERE __[#num] _cmp1 Z.__[#num]"}
 
-{_scalarquerystr |= "SELECT _genericagg(_variable[#agg string])   FROM (_setopquerystr) AS SQ _optionalwherestr"}
-{_scalarquerycount |= "SELECT     COUNT(_variable[#agg string])   FROM (_setopquerystr) AS SQ _optionalwherestr"}
-{_scalarquerynum |= "SELECT @agg(_variable[#agg @comparabletype]) FROM (_setopquerynum) AS NQ _optionalwherenum"}
+{_setopquerystr |= "_simplequerystr _setop _simplequerystr"}
+{_setopquerynum |= "_simplequerynum _setop _simplequerynum"}
+
+{_setopwherequerystr |= "_wherequerystr _setop _wherequerystr"}
+{_setopwherequerynum |= "_wherequerynum _setop _wherequerynum"}
+
+{_scalarquerystr |= "SELECT _genericagg(_variable[#str string])   FROM (_setopquerystr) AS SQ _optionalwherestr"}
+{_scalarquerycount |= "SELECT     COUNT(_variable[#str string])   FROM (_setopquerystr) AS SQ _optionalwherestr"}
+{_scalarquerynum |= "SELECT @agg(_variable[#num @comparabletype]) FROM (_setopquerynum) AS NQ _optionalwherenum"}
 
 --- Queries with scalar subqueries containing a set subquery, in the SELECT clause
 SELECT @idcol ID1, (_scalarquerystr) FROM @fromtables AS Z
 SELECT @idcol ID2, (_scalarquerynum) FROM @fromtables AS Z
 
 --- Queries with scalar subqueries containing a set subquery, in the WHERE clause
-SELECT @idcol ID3,                        __[#agg] FROM @fromtables Z WHERE __[#agg] _cmp2 (_scalarquerystr)
-SELECT @idcol ID4, _variable[#num @comparabletype] FROM @fromtables Z WHERE __[#num] _cmp1 (_scalarquerycount)
-SELECT @idcol ID5, _variable[#num @comparabletype] FROM @fromtables Z WHERE __[#num] _cmp3 (_scalarquerynum)
+SELECT @idcol ID3,                         __[#str] FROM @fromtables Z WHERE __[#str]  _cmp2 (_scalarquerystr)
+SELECT @idcol ID4, _variable[#num  @comparabletype] FROM @fromtables Z WHERE __[#num]  _cmp1 (_scalarquerycount)
+SELECT @idcol ID5, _variable[#num2 @comparabletype] FROM @fromtables Z WHERE __[#num2] _cmp3 (_scalarquerynum)
 
+--- Queries with scalar subqueries containing a set operator, in the WHERE clause (directly, without an additional subquery)
+SELECT @idcol ID6, _variable[#str string]          FROM @fromtables Z WHERE __[#str] _cmp2 (_setopwherequerystr)
+SELECT @idcol ID7, _variable[#num @comparabletype] FROM @fromtables Z WHERE __[#num] _cmp3 (_setopwherequerynum)
