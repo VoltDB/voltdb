0cf3ca67562ec2d5c52d1706d908bb08b9ba93ef
Chris Wolff <cwolff@voltdb.com>
2015-05-29 12:49:57 -0700

    Add support for approx_count_distinct aggregate function

--- src/ee/common/types.cpp
+++ src/ee/common/types.cpp
@@ -557,6 +557,15 @@ string expressionToString(ExpressionType type)
     case EXPRESSION_TYPE_AGGREGATE_COUNT_STAR: {
         return "AGGREGATE_COUNT_STAR";
     }
+    case EXPRESSION_TYPE_AGGREGATE_APPROX_COUNT_DISTINCT: {
+        return "AGGREGATE_APPROX_COUNT_DISTINCT";
+    }
+    case EXPRESSION_TYPE_AGGREGATE_VALS_TO_HYPERLOGLOG: {
+        return "AGGREGATE_VALS_TO_HYPERLOGLOG";
+    }
+    case EXPRESSION_TYPE_AGGREGATE_HYPERLOGLOGS_TO_CARD: {
+        return "AGGREGATE_HYPERLOGLOGS_TO_CARD";
+    }
     case EXPRESSION_TYPE_AGGREGATE_SUM: {
         return "AGGREGATE_SUM";
     }
@@ -654,6 +663,12 @@ ExpressionType stringToExpression(string str )
         return EXPRESSION_TYPE_AGGREGATE_COUNT;
     } else if (str == "AGGREGATE_COUNT_STAR") {
         return EXPRESSION_TYPE_AGGREGATE_COUNT_STAR;
+    } else if (str == "AGGREGATE_APPROX_COUNT_DISTINCT") {
+        return EXPRESSION_TYPE_AGGREGATE_APPROX_COUNT_DISTINCT;
+    } else if (str == "AGGREGATE_VALS_TO_HYPERLOGLOG") {
+        return EXPRESSION_TYPE_AGGREGATE_VALS_TO_HYPERLOGLOG;
+    } else if (str == "AGGREGATE_HYPERLOGLOGS_TO_CARD") {
+        return EXPRESSION_TYPE_AGGREGATE_HYPERLOGLOGS_TO_CARD;
     } else if (str == "AGGREGATE_SUM") {
         return EXPRESSION_TYPE_AGGREGATE_SUM;
     } else if (str == "AGGREGATE_MIN") {
