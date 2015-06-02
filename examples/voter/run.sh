0c994225e87d55466c8e689c01681a64fce12069
Ben Ballard <benjaminballard@gmail.com>
2015-06-02 15:58:58 -0400

    keeping the same redirect syntax

--- examples/voter/run.sh
+++ examples/voter/run.sh
@@ -93,7 +93,7 @@ function init() {
 
 # wait for backgrounded server to start up
 function wait_for_startup() {
-    until echo "exec @SystemInformation, OVERVIEW;" | sqlcmd &> /dev/null
+    until echo "exec @SystemInformation, OVERVIEW;" | sqlcmd > /dev/null 2>&1
     do
         sleep 2
         echo " ... Waiting for VoltDB to start"
