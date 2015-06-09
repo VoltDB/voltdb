b3c5b4d228225a8dfea62e51c95bd2a5ddf384ff
Bill White <bill.white@voltdb.com>
2015-06-09 11:57:40 -0400

    Added a test for overflow.

--- tests/frontend/org/voltdb/regressionsuites/TestSqlInsertSuite.java
+++ tests/frontend/org/voltdb/regressionsuites/TestSqlInsertSuite.java
@@ -99,6 +99,13 @@ public class TestSqlInsertSuite extends RegressionSuite {
         // This should round down.
         validateInsertStmt("insert into decimaltable values 0.9999999999994000;",
                            new BigDecimal("0.999999999999"));
+        try {
+            validateInsertStmt("insert into decimaltable values 999999999999999999999999999999999999999999.0;",
+                               (BigDecimal)null);
+            fail("Expected an exception here.");
+        } catch (Exception ex) {
+            assertTrue(true);
+        }
         validateInsertStmt("insert into decimaltable values null;", (BigDecimal)null);
     }
 
