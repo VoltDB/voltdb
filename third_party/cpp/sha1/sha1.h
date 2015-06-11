d664a51c9acd881822632a22921e558e8a95fb80
Anish Khanzode <akhanzode@voltdb.com>
2015-06-11 15:55:57 -0400

    Fix distcheck.

--- third_party/cpp/sha1/sha1.h
+++ third_party/cpp/sha1/sha1.h
@@ -13,5 +13,5 @@ typedef struct {
 
 void SHA1Transform(uint32_t state[5], const unsigned char buffer[64]);
 void SHA1Init(SHA1_CTX* context);
-void SHA1Update(SHA1_CTX* context, const unsigned char* data, uint32_t len);
+void SHA1Update(SHA1_CTX* context, const unsigned char* data, const size_t len);
 void SHA1Final(unsigned char digest[20], SHA1_CTX* context);
