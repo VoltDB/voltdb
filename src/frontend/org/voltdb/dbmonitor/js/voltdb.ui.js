432af25830e168bb62265648f28fc542d00642b3
Prabim Manandhar <pmanandhar@deerwalk.com>
2015-06-23 18:23:53 +0545

    VMC-196 Modified code to dispaly overlay.

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.ui.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.ui.js
@@ -1216,7 +1216,12 @@ var loadPage = function (serverName, portid) {
         voltDbRenderer.GetCommandLogInformation(function (cmdLogDetails) {
             var response = cmdLogDetails;
             var htmlcontent = "";
-            MonitorGraphUI.RefreshCommandLog(cmdLogDetails, getCurrentServer(), graphView, currentTab);
+
+            voltDbRenderer.GetSnapshotStatus(function (snapshotDetails) {
+                cmdLogDetails[getCurrentServer()].START_TIME = snapshotDetails[getCurrentServer()].START_TIME;
+                cmdLogDetails[getCurrentServer()].END_TIME = snapshotDetails[getCurrentServer()].END_TIME;
+                MonitorGraphUI.RefreshCommandLog(cmdLogDetails, getCurrentServer(), graphView, currentTab);
+            });
 
             for (var key in response) {
                 htmlcontent = htmlcontent + "<tr>";
