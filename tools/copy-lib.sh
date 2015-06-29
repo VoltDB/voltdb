f00d92d55b7c0e8a6f01a31fbc117573a8f11db0
Peter Shaw <pshaw@voltdb.com>
2015-06-29 18:26:50 -0400

    ENG-8472 Initial working version

--- tools/copy-lib.sh
+++ tools/copy-lib.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+OS=`uname`
 echo $1 | ssh volt3f "cat > libs/arg"
-if [ -e "obj/release/nativelibs/libvoltdb-5.4.jnilib" ]; then
+if [ "$OS" = "Darwin" -a -e "obj/release/nativelibs/libvoltdb-5.4.jnilib" ]
+then
     tar cf - obj/release/nativelibs/ | ssh volt3e "cd libs; tar xf -"
+else
+   mkdir -p $1
+   cp ~/libs/obj/release/nativelibs/* $1
 fi
