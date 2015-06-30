25d9310bc2b72f0f57a7c17d805e1f0f17e5d0ca
peter shaw <pshaw@peters-MacBook-Pro-2.local>
2015-06-30 10:18:36 -0400

    ENG-8472 Restrict to "test" user (and me temporarily); use BUILD_ID as quasi-uniquifier

--- tools/copy-lib.sh
+++ tools/copy-lib.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+# ENG-8472: Add both Mac and Linux native libraries to the voltdb-<rel>.jar.
+#  That way, we can release a single kit, whether tarball, .deb, or .rpm that
+#  contains all the binaries to run on either platform
+#
+# For automated builds only; harmless in a dev build but adds no value
+# so enabled only in the "test" user context
+
+if [ "$USER" != "test" -a "$USER" != "pshaw" ]; then
+    exit 0
+fi
+    
 OS=`uname`
+
+if [ -z "$BUILD_ID" ]; then
+    # no build id -- fail the build or go on without
+    # the single kit capability?
+    echo "No BUILD_ID: not single kit build -- unsuccessful"
+    exit 0
+fi
+
 echo $1 | ssh volt3f "cat > libs/arg"
-if [ "$OS" = "Darwin" -a -e "obj/release/nativelibs/libvoltdb-5.4.jnilib" ]
+if [ "$OS" = "Darwin" -a -e "obj/release/nativelibs/libvoltdb*.jnilib" ]
 then
-    tar cf - obj/release/nativelibs/ | ssh volt3e "cd libs; tar xf -"
+    tar cf - obj/release/nativelibs/ | ssh volt3e "mkdir -p libs/$BUILD_ID: cd libs/$BUILD_ID; tar xf -"
 else
    mkdir -p $1
-   cp ~/libs/obj/release/nativelibs/* $1
+   cp ~/libs/$BUILD_ID/obj/release/nativelibs/* $1
 fi
