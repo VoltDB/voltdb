d9cc34caa205bd92953df4441c786740eccac886
Yiqun Zhang <zhangyiqun9164@gmail.com>
2015-07-06 12:12:46 -0400

    ENG-4980 Add planner test cases and regression suite test

--- tests/frontend/org/voltdb/planner/TestPushDownAggregates.java
+++ tests/frontend/org/voltdb/planner/TestPushDownAggregates.java
@@ -31,6 +31,7 @@ import org.voltdb.plannodes.AggregatePlanNode;
 import org.voltdb.plannodes.PlanNodeList;
 import org.voltdb.plannodes.ProjectionPlanNode;
 import org.voltdb.plannodes.TableCountPlanNode;
+import org.voltdb.plannodes.ReceivePlanNode;
 import org.voltdb.types.ExpressionType;
 import org.voltdb.types.PlanNodeType;
 
@@ -146,6 +147,32 @@ public class TestPushDownAggregates extends PlannerTestCase {
                                               ExpressionType.AGGREGATE_SUM,
                                               ExpressionType.AGGREGATE_SUM},
                                               true);
+
+        // Test count(distinct pkey) with other compatible aggs like non-distinct aggs of other columns.
+        pn = compileToFragments("SELECT count(distinct PKEY), count(A1), min(distinct PKEY), max(distinct A1), sum(A1)" +
+                    " FROM T1;");
+        for (AbstractPlanNode apn: pn) {
+            System.out.println(apn.toExplainPlanString());
+        }
+        checkPushedDown(pn, true,
+                        new ExpressionType[] {ExpressionType.AGGREGATE_COUNT,
+                                              ExpressionType.AGGREGATE_COUNT,
+                                              ExpressionType.AGGREGATE_MIN,
+                                              ExpressionType.AGGREGATE_MAX,
+                                              ExpressionType.AGGREGATE_SUM},
+                        new ExpressionType[] {ExpressionType.AGGREGATE_SUM,
+                                              ExpressionType.AGGREGATE_SUM,
+                                              ExpressionType.AGGREGATE_MIN,
+                                              ExpressionType.AGGREGATE_MAX,
+                                              ExpressionType.AGGREGATE_SUM});
+
+        // Negative test case: count(distinct pkey) and count(distinct other) to show that
+        // it only takes one other non-trivial distinct on another column to disable the pushdown.
+        pn = compileToFragments("SELECT count(distinct PKEY), count(distinct A1) FROM T1;");
+        assertTrue(pn.size() == 2);
+        assertTrue(pn.get(1).getChild(0) instanceof AbstractScanPlanNode);
+        AbstractScanPlanNode asp = (AbstractScanPlanNode)pn.get(1).getChild(0);
+        assertTrue(asp.getInlinePlanNode(PlanNodeType.AGGREGATE) == null);
     }
 
     public void testAllAggregates() {
