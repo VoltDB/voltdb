3f1cab9bfc6afce550a51e7dc3a1a5b5df30b8d4
jcrawford <jcrawford@voltdb.com>
2015-07-13 21:35:40 -0400

    advanced-template.sql: modified two queries ('SELECT 23...' & 'SELECT 52...') to use @comparabletype rather than @columntype, since these queries were using 3 different variables of this type; but very often, only two different columns of the same type (e.g. int) are available, with one other of a comparable type (e.g. float). This should significantly reduce the number of patterns that produce no queries, across several different test suites (including in the extended sqlcoverage tests).

--- tests/scripts/examples/sql_coverage/advanced-template.sql
+++ tests/scripts/examples/sql_coverage/advanced-template.sql
@@ -83,7 +83,7 @@ SELECT 20,                                               A._variable[#GB @column
 SELECT 21,                           @optionalfn(        A._variable[#GB @columntype] ) , @agg(            A._variable[@columntype] )        FROM @fromtables A GROUP BY         A.__[#GB]
 SELECT 22,                           @optionalfn(@onefun(A._variable[#GB @columntype])) , @agg(            A._variable[@columntype] )        FROM @fromtables A GROUP BY @onefun(A.__[#GB])
 -- multiple column group by
-SELECT 23,               A._variable[#GB1 @columntype],  A._variable[#GB2 @columntype]   , @agg(           A._variable[@columntype])         FROM @fromtables A GROUP BY         A.__[#GB1], A.__[#GB2]
+SELECT 23,            A._variable[#GB1 @comparabletype], A._variable[#GB2 @comparabletype], @agg(          A._variable[@comparabletype])     FROM @fromtables A GROUP BY         A.__[#GB1], A.__[#GB2]
 
 SELECT     @agg(                     @optionalfn(        A._variable[@columntype]     )), @agg(@optionalfn(A._variable[@columntype])) AS Q24 FROM @fromtables A
 SELECT     @agg(                     @optionalfn(        A._variable[@columntype]     )), COUNT(*)                                    AS Q25 FROM @fromtables A
@@ -91,7 +91,7 @@ SELECT     @agg(                     @optionalfn(        A._variable[@columntype
 -- group by alias (50 - 55)
 SELECT 50,                           @optionalfn(@onefun(A._variable[#GB @columntype])) as tag , @agg(            A._variable[@columntype] )        FROM @fromtables A GROUP BY tag
 SELECT 51,                           A._variable[#GB @columntype] as tag ,                       @agg(            A._variable[@columntype] )        FROM @fromtables A GROUP BY tag
-SELECT 52,              @optionalfn(@onefun(A._variable[#GB @columntype])) as tag1,  A._variable[@columntype] as tag2,  @agg(           A._variable[@columntype])         FROM @fromtables A GROUP BY        tag2, tag1
+SELECT 52, @optionalfn(@onefun(A._variable[#GB @comparabletype])) as tag1, A._variable[@comparabletype] as tag2, @agg(A._variable[@comparabletype]) FROM @fromtables A GROUP BY tag2, tag1
 
 -- DISTINCT expression (56 - 65)
 -- basic select template has covered multiple columns distinct
