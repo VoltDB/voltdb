46004704e13932fe6211f190f563a4b43c3586dd
peter shaw <pshaw@peters-MacBook-Pro.local>
2015-07-14 10:52:06 -0400

    ENG-8574 Integrate pieces.

--- tests/test_apps/socketimporter/ddl.sql
+++ tests/test_apps/socketimporter/ddl.sql
@@ -3,54 +3,58 @@
 -- file -inlinebatch END_OF_BATCH
 
 ------- Socket Importer Table -------
-CREATE TABLE importTable (
-key BIGINT NOT NULL
-, value BIGINT NOT NULL
-, CONSTRAINT PK_importTable PRIMARY KEY
-  (
-    key
-  )
-);
+CREATE TABLE importtable
+     (
+                  KEY   BIGINT NOT NULL ,
+                  value BIGINT NOT NULL ,
+                  insert_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                  CONSTRAINT pk_importtable PRIMARY KEY ( KEY )
+     );
 -- Partition on id
-PARTITION table importTable ON COLUMN key;
-
+PARTITION TABLE importtable ON COLUMN KEY;
 
 ------- Kafka Importer Tables -------
-create table KafkaImportTable1 (
-key BIGINT NOT NULL
-, value BIGINT NOT NULL
-, CONSTRAINT PK_kafka_import_table1 PRIMARY KEY
-  (
-    key
-  )
-);
+CREATE TABLE kafkaimporttable1
+     (
+                  KEY   BIGINT NOT NULL,
+                  value BIGINT NOT NULL,
+                  insert_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                  CONSTRAINT pk_kafka_import_table1 PRIMARY KEY ( KEY )
+     );
 -- Partition on id
-PARTITION table KafkaImportTable1 ON COLUMN key;
+PARTITION TABLE kafkaimporttable1 ON COLUMN KEY;
 
+CREATE TABLE kafkamirrortable1
+     (
+                  KEY   BIGINT NOT NULL ,
+                  value BIGINT NOT NULL ,
+                  insert_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                  CONSTRAINT pk_kafkamirrortable PRIMARY KEY ( KEY )
+     );
 
-create table kafkaMirrorTable1 (
-key BIGINT NOT NULL
-, value BIGINT NOT NULL
-, CONSTRAINT PK_kafkaMirrorTable PRIMARY KEY
-  (
-    key
-  )
-);
 -- Partition on id
-PARTITION table kafkaMirrorTable1 ON COLUMN key;
-
+PARTITION TABLE kafkamirrortable1 ON COLUMN KEY;
 
 -- Export table
+CREATE TABLE kafkaexporttable1
+     (
+                  KEY   BIGINT NOT NULL ,
+                  value BIGINT NOT NULL
+     );
 
-create table kafkaExportTable1 (
-key BIGINT NOT NULL
-, value BIGINT NOT NULL
-);
-
-export table kafkaExportTable1;
+EXPORT TABLE kafkaexporttable1;
 
 -- Stored procedures
-load classes sp.jar;
-create procedure from class socketimporter.db.procedures.InsertExport;
+
+LOAD classes sp.jar;
+
+CREATE PROCEDURE FROM class socketimporter.db.procedures.InsertExport;
+CREATE PROCEDURE FROM class socketimporter.db.procedures.InsertImport;
+CREATE PROCEDURE FROM class socketimporter.db.procedures.MatchRows;
+CREATE PROCEDURE FROM class socketimporter.db.procedures.DeleteRows;
+
+create procedure CountMirror as select count(*) from kafkamirrortable1;
 
 -- END_OF_BATCH
+           
+
