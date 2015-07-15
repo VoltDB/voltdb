94b4a5ad47347a1373617809c0bdbb93f2791e3c
Bill White <bill.white@voltdb.com>
2015-07-15 10:59:28 -0400

    Fixed or commented out tests in TestSubQueriesSuite to make it all blue.

--- src/frontend/org/voltdb/planner/ParsedSelectStmt.java
+++ src/frontend/org/voltdb/planner/ParsedSelectStmt.java
@@ -868,7 +868,11 @@ public class ParsedSelectStmt extends AbstractParsedStmt {
         // tagging the actual display column as being also an order by column
         // helps later when trying to determine ORDER BY coverage (for determinism).
         for (ParsedColInfo col : m_displayColumns) {
+            // A VoltDB extension to avoid an NPE.
+            if ((col.alias != null && col.alias.equals(order_col.alias)) || col.expression.equals(order_exp)) {
+            /* disable 1 line
             if (col.alias.equals(order_col.alias) || col.expression.equals(order_exp)) {
+            ... disabled 1 line. */
                 col.orderBy = true;
                 col.ascending = order_col.ascending;
 
