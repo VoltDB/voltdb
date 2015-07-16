d6d1bd04f8274037d526c27bff49d4d58ff60087
Alex Rogers <arogers@voltdb.com>
2015-07-16 17:50:48 -0400

    ENG-8608: Add update record to PartitionDRGateway tracing code

--- src/frontend/org/voltdb/PartitionDRGateway.java
+++ src/frontend/org/voltdb/PartitionDRGateway.java
@@ -183,6 +183,28 @@ public class PartitionDRGateway {
                               (recordType == DRRecordType.DELETE_BY_INDEX ? (" index checksum " + indexCrc) : ""));
                     break;
                 }
+                case UPDATE:
+                case UPDATE_BY_INDEX: {
+                    if (haveOpenTransaction.get() == -1) {
+                        log.error("Have update but no open transaction");
+                        break;
+                    }
+                    final long tableHandle = buf.getLong();
+                    final int oldRowLengthPrefix = buf.getInt();
+                    final int oldRowIndexCrc;
+                    if (recordType == DRRecordType.UPDATE_BY_INDEX) {
+                        oldRowIndexCrc = buf.getInt();
+                    } else {
+                        oldRowIndexCrc = 0;
+                    }
+                    buf.position(buf.position() + oldRowLengthPrefix);
+                    final int newRowLengthPrefix = buf.getInt();
+                    buf.position(buf.position() + newRowLengthPrefix);
+                    checksum = buf.getInt();
+                    log.trace("Version " + version + " type " + recordType + " table handle " + tableHandle + " old row length " + oldRowLengthPrefix +
+                              " new row length " + newRowLengthPrefix + " checksum " + checksum +
+                              (recordType == DRRecordType.UPDATE_BY_INDEX ? (" index checksum " + oldRowIndexCrc) : ""));
+                }
                 case BEGIN_TXN: {
                     //Begin txn
                     final long txnId = buf.getLong();
