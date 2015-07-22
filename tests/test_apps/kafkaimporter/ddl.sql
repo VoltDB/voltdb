26c1857dbb6f1774e0e219394c119b396d8bf58f
peter shaw <pshaw@peters-MacBook-Pro.local>
2015-07-22 15:25:27 -0400

    ENG-8000 Partition the SP's

--- tests/test_apps/kafkaimporter/ddl.sql
+++ tests/test_apps/kafkaimporter/ddl.sql
@@ -49,10 +49,17 @@ EXPORT TABLE kafkaexporttable1;
 LOAD classes sp.jar;
 
 CREATE PROCEDURE FROM class kafkaimporter.db.procedures.InsertExport;
+PARTITION PROCEDURE InsertExport ON TABLE Kafkamirrortable1 COLUMN key;
+
 CREATE PROCEDURE FROM class kafkaimporter.db.procedures.InsertFinal;
+
 CREATE PROCEDURE FROM class kafkaimporter.db.procedures.InsertImport;
+PARTITION PROCEDURE InsertImport ON TABLE KafkaImportTable1 COLUMN key;
+
 CREATE PROCEDURE FROM class kafkaimporter.db.procedures.MatchRows;
+
 CREATE PROCEDURE FROM class kafkaimporter.db.procedures.DeleteRows;
+PARTITION PROCEDURE DeleteRows ON TABLE KafkaMirrorTable1 COLUMN key;
 
 CREATE PROCEDURE CountMirror as select count(*) from kafkamirrortable1;
 CREATE PROCEDURE CountImport as select count(*) from kafkaimporttable1;
