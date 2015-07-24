34d15e0e34e2e6916a412fc84a20a614b960e946
Peter Shaw <pshaw@voltdb.com>
2015-07-24 09:05:25 -0400

    ENG-8472 revert to wildcard version

--- tools/copy-lib.sh
+++ tools/copy-lib.sh
@@ -11,22 +11,23 @@ if [ "$USER" != "test" ]; then
     exit 0
 fi
 
+echo '$1 is' $1
+echo '$2 is' $2
+echo 'lib is' obj/release/nativelibs/libvoltdb*
+
+
 OS=`uname`
 
-echo '$1 is ', $1
-echo '$2 is ', $2
-echo 'lib is ', obj/release/nativelibs/libvoltdb*.jnilib
-
-if [ "$OS" = "Darwin" -a -e obj/release/nativelibs/libvoltdb-${2}.jnilib ]
+if [ "$OS" = "Darwin" -a -e obj/release/nativelibs/libvoltdb*.jnilib ]
 then
     # the Mac case...
     tar cf - obj/release/nativelibs/ | ssh volt0 "cd libs; tar xf -"
     exit 0
 else
     # the Linux case...
-    if [ -e ~/libs/obj/release/nativelibs/libvoltdb-${2}.jnilib ]; then
+    if [ -e ~/libs/obj/release/nativelibs/* ]; then
         mkdir -p $1
-        cp ~/libs/obj/release/nativelibs/libvoltdb-${2}.jnilib $1
+        cp ~/libs/obj/release/nativelibs/* $1
         exit 0
     else
         echo "++++++++++++++nativelibs not found!"
