5b45ef07d623a7e6221702fcdbf0ea282ea68791
Xin Jia <xinjiacs@gmail.com>
2015-08-10 17:02:14 -0400

    fix various stuffs about the test case

--- src/frontend/org/voltdb/jni/ExecutionEngine.java
+++ src/frontend/org/voltdb/jni/ExecutionEngine.java
@@ -462,8 +462,9 @@ public abstract class ExecutionEngine implements FastDeserializer.Deserializatio
         // track cache misses
         m_cacheMisses++;
         // estimate the cache size by the number of misses
-        m_eeCacheSize = m_eeCacheSize + 1;
-        m_eeCacheSize = Math.min(m_eeCacheSize, EE_PLAN_CACHE_SIZE);
+        if (m_eeCacheSize < EE_PLAN_CACHE_SIZE) {
+            m_eeCacheSize++;
+        }
         // get the plan for realz
         return ActivePlanRepository.planForFragmentId(fragmentId);
     }
