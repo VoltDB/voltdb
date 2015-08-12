cc816ea831638ad684c14249be45199281e6b946
Yiqun Zhang <zhangyiqun9164@gmail.com>
2015-08-12 18:21:55 -0400

    ENG-8840 new MatViewQueriesXMLGenerator

--- src/frontend/org/voltdb/compiler/MatViewQueriesXMLGenerator.java
+++ src/frontend/org/voltdb/compiler/MatViewQueriesXMLGenerator.java
@@ -19,7 +19,11 @@ package org.voltdb.compiler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 import org.voltdb.planner.ParsedColInfo;
+import org.voltdb.catalog.CatalogMap;
+import org.voltdb.catalog.Table;
+import org.voltdb.catalog.Column;
 import org.voltdb.compiler.VoltXMLElementHelper;
 
 import org.hsqldb_voltpatches.VoltXMLElement;
@@ -28,22 +32,29 @@ import org.hsqldb_voltpatches.VoltXMLElement;
  * Tune XMLs for materialized view min/max recalculation queries.
  * ENG-8641
  */
-public class MatViewFallbackQueryXMLGenerator {
+public class MatViewQueriesXMLGenerator {
 
+    private Table m_destTable;
     private VoltXMLElement m_xml;
     private int m_maxElementId;
     private ArrayList<ParsedColInfo> m_groupByColumnsParsedInfo;
     private ArrayList<ParsedColInfo> m_displayColumnsParsedInfo;
     private ArrayList<VoltXMLElement> m_fallbackQueryXMLs;
+    private VoltXMLElement m_buildUpQueryXML;
 
-    public MatViewFallbackQueryXMLGenerator(VoltXMLElement xmlquery,
-                                            ArrayList<ParsedColInfo> groupByColumnsParsedInfo,
-                                            ArrayList<ParsedColInfo> displayColumnsParsedInfo) {
+    public MatViewQueriesXMLGenerator(Table destTable, VoltXMLElement xmlquery,
+                                      ArrayList<ParsedColInfo> groupByColumnsParsedInfo,
+                                      ArrayList<ParsedColInfo> displayColumnsParsedInfo) {
+        m_destTable = destTable;
         m_xml = xmlquery.duplicate();
         m_groupByColumnsParsedInfo = groupByColumnsParsedInfo;
         m_displayColumnsParsedInfo = displayColumnsParsedInfo;
         m_maxElementId = VoltXMLElementHelper.findMaxElementId( m_xml );
         m_fallbackQueryXMLs = new ArrayList<VoltXMLElement>();
+
+        // The following tasks should be called in fixed order!
+        adjustColumnAliases();
+        generateBuildUpQueryXMLs();
         generateFallbackQueryXMLs();
     }
 
@@ -54,6 +65,38 @@ public class MatViewFallbackQueryXMLGenerator {
 
     private String lastElementId() {
         return String.valueOf( m_maxElementId );
+    }
+
+    private void adjustColumnAliases() {
+        ArrayList<String> realColumnNames = new ArrayList<String>(Collections.nCopies(m_destTable.getColumns().size(), ""));
+        Collections.fill(realColumnNames, null);
+        for (Column info : m_destTable.getColumns()) {
+            realColumnNames.set(info.getIndex(), info.getName());
+        }
+        List<VoltXMLElement> viewColumns = VoltXMLElementHelper.getFirstChild(m_xml, "columns").children;
+        for (int i=0; i<viewColumns.size(); ++i) {
+            VoltXMLElement columnref = viewColumns.get(i);
+            String realColumnName = realColumnNames.get(i);
+            columnref.attributes.put("alias", realColumnName);
+        }
+    }
+
+    private void generateBuildUpQueryXMLs() {
+        // This function will generate the XML for materialized view build up query:
+        // INSERT INTO target_table SELECT ...
+        // 1. Create insert node.
+        m_buildUpQueryXML = new VoltXMLElement("insert");
+        m_buildUpQueryXML.attributes.put("table", m_destTable.getTypeName());
+        // 2. Add columns
+        VoltXMLElement columns = new VoltXMLElement("columns");
+        m_buildUpQueryXML.children.add(columns);
+        List<VoltXMLElement> viewColumns = VoltXMLElementHelper.getFirstChild(m_xml, "columns").children;
+        for (VoltXMLElement columnref : viewColumns) {
+            VoltXMLElement column = new VoltXMLElement("column");
+            column.attributes.put("name", columnref.attributes.get("alias"));
+            columns.children.add(column);
+        }
+        m_buildUpQueryXML.children.add(m_xml.duplicate());;
     }
 
     private void generateFallbackQueryXMLs() {
@@ -117,6 +160,10 @@ public class MatViewFallbackQueryXMLGenerator {
                 joincondList.clear();
             }
         }
+    }
+
+    public VoltXMLElement getBuildUpQueryXML() {
+        return m_buildUpQueryXML;
     }
 
     public List<VoltXMLElement> getFallbackQueryXMLs() {
