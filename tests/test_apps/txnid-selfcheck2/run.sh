38271cbf6031a3415d51dd569fe416b53443ab3e
liam <liam@voltdb.com>
2015-08-14 09:51:01 -0400

    Adding option to txnid2 app that allows disabling different threads when running the client.

--- tests/test_apps/txnid-selfcheck2/run.sh
+++ tests/test_apps/txnid-selfcheck2/run.sh
@@ -73,7 +73,7 @@ function server() {
 
 # run the client that drives the example
 function client() {
-    benchmark
+    async-benchmark
 }
 
 # Asynchronous benchmark sample
@@ -100,7 +100,9 @@ function async-benchmark() {
         --partfillerrowmb=128 \
         --progresstimeout=120 \
         --usecompression=false \
-        --allowinprocadhoc=false
+        --allowinprocadhoc=false \
+        --disabledthreads=
+#ddlt,clients,partBiglt,replBiglt,partCappedlt,replCappedlt,replLoadlt,partLoadlt,adHocMayhemThread,idpt,readThread,partTrunclt,replTrunclt
 }
 
 function help() {
