a3430b3b3de1f2f85076a97a3ea3a05d0c65ec97
Peter Shaw <pshaw@voltdb.com>
2015-08-24 11:15:43 -0400

    ENG-8884 remove debug line

--- tools/copy-lib.sh
+++ tools/copy-lib.sh
@@ -15,10 +15,7 @@ echo '$1 is' $1
 echo '$2 is' $2
 echo 'lib is' obj/release/nativelibs/libvoltdb*
 
-
 OS=`uname`
-
-echo '++++obj/release/nativelibs/libvoltdb-$2.jnilib is:' obj/release/nativelibs/libvoltdb-$2.jnilib
 
 if [ "$OS" = "Darwin" -a -e obj/release/nativelibs/libvoltdb-$2.jnilib ]
 then
