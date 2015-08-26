f3b8b4fc663bd6c2ed866497c00b7a5bab0ed13b
Xin Jia <xinjiacs@gmail.com>
2015-08-26 14:37:36 -0400

    very low risk simple fix for V5.6

--- src/ee/execution/VoltDBEngine.cpp
+++ src/ee/execution/VoltDBEngine.cpp
@@ -1940,19 +1940,10 @@ void VoltDBEngine::executePurgeFragment(PersistentTable* table) {
 static std::string dummy_last_accessed_plan_node_name("no plan node in progress");
 
 void VoltDBEngine::reportProgressToTopend() {
-    std::string tableName;
-    int64_t tableSize;
-
+    std::string tableName = "None";
+    int64_t tableSize = 0;
     assert(m_currExecutorVec);
 
-    if (m_lastAccessedTable == NULL) {
-        tableName = "None";
-        tableSize = 0;
-    }
-    else {
-        tableName = m_lastAccessedTable->name();
-        tableSize = m_lastAccessedTable->activeTupleCount();
-    }
     //Update stats in java and let java determine if we should cancel this query.
     m_tuplesProcessedInFragment += m_tuplesProcessedSinceReport;
     m_tupleReportThreshold = m_topend->fragmentProgressUpdate(m_currentIndexInBatch,
