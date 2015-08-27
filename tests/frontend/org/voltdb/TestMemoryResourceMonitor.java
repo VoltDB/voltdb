02a421ecfedde442af78bb17cf27af7700072d18
Manju James <mjames@voltdb.com>
2015-08-27 10:05:06 -0400

    ENG-8876: Allow percentages also for configuring memory limit.

--- tests/frontend/org/voltdb/TestMemoryResourceMonitor.java
+++ tests/frontend/org/voltdb/TestMemoryResourceMonitor.java
@@ -24,7 +24,10 @@
 package org.voltdb;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 import org.voltdb.VoltDB.Configuration;
@@ -35,24 +38,25 @@ import org.voltdb.common.Constants;
 import org.voltdb.compiler.VoltProjectBuilder;
 import org.voltdb.utils.FakeStatsProducer;
 import org.voltdb.utils.MiscUtils;
+import org.voltdb.utils.PlatformProperties;
 import org.voltdb.utils.SystemStatsCollector;
 import org.voltdb.utils.SystemStatsCollector.Datum;
 
 public class TestMemoryResourceMonitor extends TestCase
 {
-    private static final int DEFAULT_MONITORING_INTERVAL=60;
     private static final int MONITORING_INTERVAL = 2;
+    private static final int DEFAULT_MONITORING_INTERVAL = 60;
 
     private ServerThread m_localServer;
     private VoltDB.Configuration m_config;
     private Client m_client;
     private TestStatsProducer m_mockStatsProducer;
 
-    public void setUpServer(boolean setRssLimit, boolean setMonitoringInterval) throws Exception
+    public void setUpServer(String rssLimit, boolean setMonitoringInterval) throws Exception
     {
         VoltProjectBuilder builder = new VoltProjectBuilder();
-        if (setRssLimit) {
-            builder.setRssLimit(0.5);
+        if (rssLimit!=null) {
+            builder.setRssLimit(rssLimit);
         }
         if (setMonitoringInterval) {
             builder.setResourceCheckInterval(MONITORING_INTERVAL);
@@ -78,13 +82,51 @@ public class TestMemoryResourceMonitor extends TestCase
 
     @Override
     public void tearDown() throws Exception {
-        m_client.close();
-        m_localServer.shutdown();
+        if (m_client != null) {
+            m_client.close();
+        }
+        if (m_localServer != null) {
+            m_localServer.shutdown();
+        }
+    }
+
+    public void testBadRssValues() throws Exception {
+        String[] badValues = { "xx%",
+                "-1%",
+                "100%",
+                "150%",
+                "40.3%",
+                "-20",
+                "abc"
+        };
+        ResourceUsageMonitor monitor = new ResourceUsageMonitor(null, null);
+        for (int i=0; i<badValues.length; i++) {
+            try {
+                monitor.getMemoryLimitSize(badValues[i]);
+                fail("Must have failed for bad memory limit value " + badValues[i]);
+            } catch(IllegalArgumentException e) {
+                // expected
+            }
+        }
+    }
+
+    public void testGoodRssValues() throws Exception {
+        long totalSize = PlatformProperties.getPlatformProperties().ramInMegabytes*1048576L;
+        Map<String, Double> configToExpectedRss = new HashMap<>();
+        configToExpectedRss.put("0%", 0.0);
+        configToExpectedRss.put("1%", totalSize/100.0);
+        configToExpectedRss.put("15%", totalSize*15/100.0);
+        configToExpectedRss.put("40", 40.0*1073741824L);
+        configToExpectedRss.put("1.5", 1.5*1073741824L);
+        ResourceUsageMonitor monitor = new ResourceUsageMonitor(null, null);
+        for (String str : configToExpectedRss.keySet()) {
+            Assert.assertEquals(configToExpectedRss.get(str), monitor.getMemoryLimitSize(str));
+        }
     }
 
     public void testNoRssLimit() throws Exception
     {
-        setUpServer(false, true);
+        setUpServer(null, true);
 
         // Wait for monitoring interval time and verify server is still in running mode
         m_mockStatsProducer.m_rss = 2048L*1024*1024;
@@ -95,7 +137,7 @@ public class TestMemoryResourceMonitor extends TestCase
     // Disabling this test because it takes long. Enable it and run manually to test.
     public void notestNoMonitoringInterval() throws Exception
     {
-        setUpServer(true, false);
+        setUpServer("0.5", false);
 
         // Wait for monitoring interval time and verify server is still in running mode
         m_mockStatsProducer.m_rss = 2048L*1024*1024;
@@ -105,7 +147,7 @@ public class TestMemoryResourceMonitor extends TestCase
 
     public void testLimitNotExceeded() throws Exception
     {
-        setUpServer(true, true);
+        setUpServer("0.5", true);
 
         m_mockStatsProducer.m_rss = 10*1024*1024;
         assertEquals(m_mockStatsProducer.m_rss, SystemStatsCollector.getRSSMB());
@@ -115,9 +157,21 @@ public class TestMemoryResourceMonitor extends TestCase
         assertEquals(OperationMode.RUNNING, VoltDB.instance().getMode());
     }
 
+    public void testLimitNotExceededPerc() throws Exception
+    {
+        setUpServer("90%", true);
+
+        m_mockStatsProducer.m_rss = Double.valueOf(PlatformProperties.getPlatformProperties().ramInMegabytes*1048576L*80/100.0).longValue();
+        assertEquals(m_mockStatsProducer.m_rss, SystemStatsCollector.getRSSMB());
+
+        // Wait for monitoring interval time and verify server is still in running mode
+        resumeAndWait(MONITORING_INTERVAL+1);
+        assertEquals(OperationMode.RUNNING, VoltDB.instance().getMode());
+    }
+
     public void testLimitExceededWithResumePauseAgain() throws Exception
     {
-        setUpServer(true, true);
+        setUpServer("0.5", true);
 
         // Go above limit, wait for more than configured amt of time and verify server is paused
         m_mockStatsProducer.m_rss = 512*1024*1024;
@@ -132,7 +186,7 @@ public class TestMemoryResourceMonitor extends TestCase
 
     public void testLimitExceededWithResume() throws Exception
     {
-        setUpServer(true, true);
+        setUpServer("0.5", true);
 
         // Go above limit, wait for more than configured amt of time and verify server is paused
         m_mockStatsProducer.m_rss = 512*1024*1024;
@@ -145,15 +199,30 @@ public class TestMemoryResourceMonitor extends TestCase
         assertEquals(OperationMode.RUNNING, VoltDB.instance().getMode());
     }
 
+    public void testLimitExceededWithResumePerc() throws Exception
+    {
+        setUpServer("90%", true);
+
+        // Go above limit, wait for more than configured amt of time and verify server is paused
+        m_mockStatsProducer.m_rss = Double.valueOf(PlatformProperties.getPlatformProperties().ramInMegabytes*1048576L*95/100.0).longValue();
+        resumeAndWait(MONITORING_INTERVAL+1);
+        assertEquals(OperationMode.PAUSED, VoltDB.instance().getMode());
+
+        // Don't go above limit in mock, resume and make sure server does not go back into paused.
+        m_mockStatsProducer.m_rss = Double.valueOf(PlatformProperties.getPlatformProperties().ramInMegabytes*1048576L*80/100.0).longValue();
+        resumeAndWait(MONITORING_INTERVAL+1);
+        assertEquals(OperationMode.RUNNING, VoltDB.instance().getMode());
+    }
+
     public void testCatalogUpdate_PauseAfterUpdate() throws Exception
     {
-        setUpServer(false, true); // set up server with no rss limit
+        setUpServer(null, true); // set up server with no rss limit
         m_mockStatsProducer.m_rss = 2048L*1024*1024;
         resumeAndWait(MONITORING_INTERVAL+1);
         assertEquals(OperationMode.RUNNING, VoltDB.instance().getMode());
 
         // update server with rss limit
-        String newDepFile = getDeploymentPathWithRss(1);
+        String newDepFile = getDeploymentPathWithRss("1");
         String depBytes = new String(ClientUtils.fileToBytes(new File(newDepFile)), Constants.UTF8ENCODING);
         VoltTable[] results = m_client.callProcedure("@UpdateApplicationCatalog", null, depBytes).getResults();
         assertTrue(results.length == 1);
@@ -165,13 +234,13 @@ public class TestMemoryResourceMonitor extends TestCase
 
     public void testCatalogUpdate_ResumeAfterUpdate() throws Exception
     {
-        setUpServer(true, true); // set up server with rss limit
+        setUpServer("0.5", true); // set up server with rss limit
         m_mockStatsProducer.m_rss = 2048L*1024*1024;
         resumeAndWait(MONITORING_INTERVAL+1);
         assertEquals(OperationMode.PAUSED, VoltDB.instance().getMode());
 
         // update server with rss limit
-        String newDepFile = getDeploymentPathWithRss(0);
+        String newDepFile = getDeploymentPathWithRss("0");
         String depBytes = new String(ClientUtils.fileToBytes(new File(newDepFile)), Constants.UTF8ENCODING);
         VoltTable[] results = m_client.callProcedure("@UpdateApplicationCatalog", null, depBytes).getResults();
         assertTrue(results.length == 1);
@@ -181,7 +250,7 @@ public class TestMemoryResourceMonitor extends TestCase
         assertEquals(OperationMode.RUNNING, VoltDB.instance().getMode());
     }
 
-    private String getDeploymentPathWithRss(int rssLimit) throws Exception
+    private String getDeploymentPathWithRss(String rssLimit) throws Exception
     {
         VoltProjectBuilder builder = new VoltProjectBuilder();
         builder.setRssLimit(rssLimit);
