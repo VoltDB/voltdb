2ee864ce83f74b3a29d47695cbb90bb859fc4823
Huzefa Hakimi <hhakimi@voltdb.com>
2015-09-01 17:39:32 -0400

    porting Is Distinct from operator support to master

--- src/ee/common/types.cpp
+++ src/ee/common/types.cpp
@@ -540,6 +540,9 @@ string expressionToString(ExpressionType type)
     case EXPRESSION_TYPE_COMPARE_IN: {
         return "COMPARE_IN";
     }
+    case EXPRESSION_TYPE_COMPARE_NOTDISTINCT: {
+        return "COMPARE_NOTDISTINCT";
+    }
     case EXPRESSION_TYPE_CONJUNCTION_AND: {
         return "CONJUNCTION_AND";
     }
@@ -656,6 +659,8 @@ ExpressionType stringToExpression(string str )
         return EXPRESSION_TYPE_COMPARE_LIKE;
     } else if (str == "COMPARE_IN") {
         return EXPRESSION_TYPE_COMPARE_IN;
+    } else if (str == "COMPARE_NOTDISTINCT") {
+        return EXPRESSION_TYPE_COMPARE_NOTDISTINCT;
     } else if (str == "CONJUNCTION_AND") {
         return EXPRESSION_TYPE_CONJUNCTION_AND;
     } else if (str == "CONJUNCTION_OR") {
