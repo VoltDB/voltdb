d69e2fd2d48dd06a77dba916cfa5dfda6a00af31
Manju James <mjames@voltdb.com>
2015-09-03 09:22:48 -0400

    ENG-8950: Little more info in the failure message.

--- src/frontend/org/voltdb/EELibraryLoader.java
+++ src/frontend/org/voltdb/EELibraryLoader.java
@@ -78,15 +78,16 @@ public class EELibraryLoader {
                         hostLog.debug("Error loading VoltDB JNI shared library", e);
                     }
                     if (mustSuccede) {
-                        String msg = "Library VOLTDB JNI shared library loading failed. Library path " +
-                                System.getProperty("java.library.path") + "\n";
+                        String msg = "Library VOLTDB JNI shared library loading failed with error: " + e.getMessage() + "\n";
+                        msg += "Library path " + System.getProperty("java.library.path") + ", " +
+                               USE_JAVA_LIBRARY_PATH + "=" + System.getProperty(USE_JAVA_LIBRARY_PATH) + "\n";
                         msg += "The library may have failed to load because it can't be found in your " +
                                 "load library path, or because it is not compatible with the current " +
                                 "platform.\n";
                         msg +=  "VoltDB provides builds on our website for 64-bit OSX systems >= 10.6, " +
                                 "and 64-bit Linux systems with kernels >= 2.6.18.";
                         if (e instanceof UnsatisfiedLinkError) {
-                            msg += "Or the library may have failed to load because java.io.tmpdir should be set to a different directory. " +
+                            msg += "\nOr the library may have failed to load because java.io.tmpdir should be set to a different directory. " +
                                    "Use VOLTDB_OPTS='-Djava.io.tmpdir=<dirpath>' to set it.";
                         }
                         VoltDB.crashLocalVoltDB(msg, false, null);
