7a8d346e5d8128822b4e5364d09d46948d12c624
Daniel Weiss <dweiss@voltdb.com>
2015-09-10 12:27:59 -0400

    ENG-7982 remove unused imports

--- src/frontend/org/voltdb/ClientInterface.java
+++ src/frontend/org/voltdb/ClientInterface.java
@@ -113,9 +113,7 @@ import org.voltdb.messaging.MultiPartitionParticipantMessage;
 import org.voltdb.parser.SQLLexer;
 import org.voltdb.security.AuthenticationRequest;
 import org.voltdb.sysprocs.saverestore.SnapshotUtil;
-import org.voltdb.utils.CatalogUtil;
 import org.voltdb.utils.Encoder;
-import org.voltdb.utils.InMemoryJarfile;
 import org.voltdb.utils.MiscUtils;
 
 import com.google_voltpatches.common.base.Charsets;
