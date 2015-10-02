63282d4ea63cab423b17c245c4e05fecbf10689e
Prabim Manandhar <pmanandhar@deerwalk.com>
2015-10-02 15:36:30 +0545

    Modified code to solve the issue with scrolling in IE.

--- src/frontend/org/voltdb/dbmonitor/js/jquery.popup.js
+++ src/frontend/org/voltdb/dbmonitor/js/jquery.popup.js
@@ -114,8 +114,10 @@
 						    $('body').css("height", $(window).height());
 							$('body').css("overflow", "hidden");
 							$('body').css("position", "fixed");
-							$('body').css("overflow-y", "scroll");
-							$('body').css("width", "100%");
+						    if (navigator.userAgent.indexOf('MSIE') == -1) {
+						        $('body').css("overflow-y", "scroll");
+						    }
+						    $('body').css("width", "100%");
 							//$('body').bind('touchmove', function(e){e.preventDefault()});//mobile
 							// Call the open callback
 							plugin.o.afterOpen.call(plugin);
