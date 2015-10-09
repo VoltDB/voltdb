1609ad26e552ea2207210a5bce202839454be43f
Peter Shaw <pshaw@voltdb.com>
2015-10-09 15:09:59 -0400

    ENG-xxxx hotfix update buildstring and regex

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -167,9 +167,9 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback {
     // CatalogContext is immutable, just make sure that accessors see a consistent version
     volatile CatalogContext m_catalogContext;
     private String m_buildString;
-    static final String m_defaultVersionString = "5.2.6";
+    static final String m_defaultVersionString = "5.2.0.1";
     // by default set the version to only be compatible with itself
-    static final String m_defaultHotfixableRegexPattern = "^\\Q5.2.6\\E\\z";
+    static final String m_defaultHotfixableRegexPattern = "^5\\.2(\\.0\\.1)?$";
     // these next two are non-static because they can be overrriden on the CLI for test
     private String m_versionString = m_defaultVersionString;
     private String m_hotfixableRegexPattern = m_defaultHotfixableRegexPattern;
