3c9219006c4cfccbe1f069b9b699839ea2b2fcc3
Peter Shaw <pshaw@voltdb.com>
2015-10-27 17:21:49 -0400

    remove tabs

--- tests/test_apps/exportbenchmark2/client/exportbenchmark/ExportBenchmark.java
+++ tests/test_apps/exportbenchmark2/client/exportbenchmark/ExportBenchmark.java
@@ -434,19 +434,19 @@ public class ExportBenchmark {
         }
     }
 
-	void finalStats() {
-		// Write stats to file if requested
-		try {
-			if ((config.statsfile != null) && (config.statsfile.length() != 0)) {
-				FileWriter fw = new FileWriter(config.statsfile);
-				fw.append(String.format("%d,%f,%f,%f,%f\n",	benchmarkStartTS, min, max, (end-start)/start*100.0, (max-min)/min*100.0));
-				fw.close();
-			}
-		} catch (IOException e) {
-			System.err.println("Error writing stats file");
-			e.printStackTrace();
-		}
-	}
+    void finalStats() {
+        // Write stats to file if requested
+        try {
+            if ((config.statsfile != null) && (config.statsfile.length() != 0)) {
+                FileWriter fw = new FileWriter(config.statsfile);
+                fw.append(String.format("%d,%f,%f,%f,%f\n",    benchmarkStartTS, min, max, (end-start)/start*100.0, (max-min)/min*100.0));
+                fw.close();
+            }
+        } catch (IOException e) {
+            System.err.println("Error writing stats file");
+            e.printStackTrace();
+        }
+    }
 
 //    static void connectToOneServerWithRetry(String server) {
 //        int sleep = 1000;
@@ -491,11 +491,11 @@ public class ExportBenchmark {
 //                @Override
 //                public void run() {
 //                    try {
-//                    	connectToOneServerWithRetry(server);
-//                    	connections.countDown();
+//                        connectToOneServerWithRetry(server);
+//                        connections.countDown();
 //                    } catch (Throwable ex) {
-//                    	ex.printStackTrace();
-//                    	Thread.dumpStack();
+//                        ex.printStackTrace();
+//                        Thread.dumpStack();
 //                        System.out.println("Uncaught exception: " + ex.getMessage() + ", " + ex.toString());
 //                        printJStack();
 //                        System.exit(-1);
@@ -506,7 +506,7 @@ public class ExportBenchmark {
 //        // block until all have connected
 //        connections.await();
 //    }
-    
+
     static public void printJStack() {
         Map<String, List<String>> deduped = new HashMap<String, List<String>>();
 
@@ -544,36 +544,36 @@ public class ExportBenchmark {
         }
         System.out.println("Full thread dump:\n" + logline);
     }
-    
-    static void connect(String servers) throws InterruptedException, Exception {
-    	//final Splitter COMMA_SPLITTER = Splitter.on(",").omitEmptyStrings().trimResults();
-    	try {
-    		System.out.println("Connecting to VoltDB Interface...");
-    		ClientConfig clientConfig = new ClientConfig();
-    		clientConfig.setReconnectOnConnectionLoss(true);
-    		client = ClientFactory.createClient(clientConfig);
 
-    		for (String server: servers.split(",")) {
-    			System.out.println("..." + server);
-    			try {
-    				client.createConnection(server);
-    			} catch (Throwable ex) {
-    				ex.printStackTrace();
-    				Thread.dumpStack();
-    				System.out.println("Uncaught exception: " + ex.getMessage() + ", " + ex.toString());
-    				printJStack();
-    				System.exit(-1);
-    			}
-    		}
-    	} catch  (Throwable ex) {
-    		ex.printStackTrace();
-    		Thread.dumpStack();
-    		System.out.println("Uncaught exception: " + ex.getMessage() + ", " + ex.toString());
-    		printJStack();
-    		System.exit(-1);
-    	}
+    static void connect(String servers) throws InterruptedException, Exception {
+        //final Splitter COMMA_SPLITTER = Splitter.on(",").omitEmptyStrings().trimResults();
+        try {
+            System.out.println("Connecting to VoltDB Interface...");
+            ClientConfig clientConfig = new ClientConfig();
+            clientConfig.setReconnectOnConnectionLoss(true);
+            client = ClientFactory.createClient(clientConfig);
+
+            for (String server: servers.split(",")) {
+                System.out.println("..." + server);
+                try {
+                    client.createConnection(server);
+                } catch (Throwable ex) {
+                    ex.printStackTrace();
+                    Thread.dumpStack();
+                    System.out.println("Uncaught exception: " + ex.getMessage() + ", " + ex.toString());
+                    printJStack();
+                    System.exit(-1);
+                }
+            }
+        } catch  (Throwable ex) {
+            ex.printStackTrace();
+            Thread.dumpStack();
+            System.out.println("Uncaught exception: " + ex.getMessage() + ", " + ex.toString());
+            printJStack();
+            System.exit(-1);
+        }
     }
-	
+
     /* Main routine creates a benchmark instance and kicks off the
      * run method for each configuration variant.
      *
