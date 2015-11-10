ddffca6ee1871b65fe80f9216a0e6fa57b9ea421
Huzefa Hakimi <hhakimi@voltdb.com>
2015-11-10 13:42:33 -0500

    fix tabs

--- src/ee/expressions/geofunctions.cpp
+++ src/ee/expressions/geofunctions.cpp
@@ -247,14 +247,14 @@ template<> NValue NValue::call<FUNC_VOLT_CONTAINS>(const std::vector<NValue>& ar
 }
 
 template<> NValue NValue::callUnary<FUNC_VOLT_POLYGON_NUM_INTERIOR_RINGS>() const {
-	if (isNull()) {
-	    return NValue::getNullValue(VALUE_TYPE_INTEGER);
-	}
+    if (isNull()) {
+        return NValue::getNullValue(VALUE_TYPE_INTEGER);
+    }
     const Geography polygon = getGeography();
-	NValue retVal(VALUE_TYPE_INTEGER);
-	// exclude exterior ring
-	retVal.getInteger() = polygon.numLoops() - 1;
-	return retVal;
+    NValue retVal(VALUE_TYPE_INTEGER);
+    // exclude exterior ring
+    retVal.getInteger() = polygon.numLoops() - 1;
+    return retVal;
 }
 
 template<> NValue NValue::callUnary<FUNC_VOLT_POLYGON_NUM_POINTS>() const {
@@ -271,19 +271,19 @@ template<> NValue NValue::callUnary<FUNC_VOLT_POINT_LATITUDE>() const {
     if (isNull()) {
         return NValue::getNullValue(VALUE_TYPE_DOUBLE);
     }
-	const Point point = getPoint();
-	NValue retVal(VALUE_TYPE_DOUBLE);
-	retVal.getDouble() = point.getLatitude();
-	return retVal;
+    const Point point = getPoint();
+    NValue retVal(VALUE_TYPE_DOUBLE);
+    retVal.getDouble() = point.getLatitude();
+    return retVal;
 }
 template<> NValue NValue::callUnary<FUNC_VOLT_POINT_LONGITUDE>() const {
     if (isNull()) {
         return NValue::getNullValue(VALUE_TYPE_DOUBLE);
     }
     const Point point = getPoint();
-	NValue retVal(VALUE_TYPE_DOUBLE);
-	retVal.getDouble() = point.getLongitude();
-	return retVal;
+    NValue retVal(VALUE_TYPE_DOUBLE);
+    retVal.getDouble() = point.getLongitude();
+    return retVal;
 }
 
 } // end namespace voltdb
