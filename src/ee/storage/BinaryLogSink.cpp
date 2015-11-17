e5a233304a76914562075edbb540a4072561edde
Alex Rogers <arogers@voltdb.com>
2015-11-17 13:46:35 -0500

    ENG-9320, ENG-9435: Migrate DR version checks to use protocol version

--- src/ee/storage/BinaryLogSink.cpp
+++ src/ee/storage/BinaryLogSink.cpp
@@ -209,7 +209,7 @@ int64_t BinaryLogSink::apply(const char *taskParams, boost::unordered_map<int64_
         pool->purge();
         const char* recordStart = taskInfo.getRawPointer();
         const uint8_t drVersion = taskInfo.readByte();
-        if (drVersion > 2) {
+        if (drVersion != DRTupleStream::DR_VERSION) {
             throwFatalException("Unsupported DR version %d", drVersion);
         }
         const DRRecordType type = static_cast<DRRecordType>(taskInfo.readByte());
