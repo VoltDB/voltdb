40a0a83b3b4652192f659aaa999e6118ce2c1f47
Daniel Weiss <dweiss@voltdb.com>
2015-12-01 18:22:26 -0500

    ENG-9416 first commit

---
+++ src/frontend/org/voltdb/importer/transformer/SimpleTransformer.java
@@ -0,0 +1,75 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2015 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.voltdb.importer.transformer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.voltdb.common.Constants;
+
+import au.com.bytecode.opencsv_voltpatches.CSVParser;
+
+public class SimpleTransformer extends AbstractTransformer implements BundleActivator {
+    String m_format;
+    Boolean m_strictQuote;
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        context.registerService(this.getClass().getName(), this, null);
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        //Do any bundle related cleanup.
+    }
+
+    @Override
+    public void configure(Properties p) {
+        m_format = p.getProperty("format").trim();
+        if (!("csv".equalsIgnoreCase(m_format) || "tsv".equalsIgnoreCase(m_format))) {
+            throw new RuntimeException("Invalid format " + m_format + ", choices are either \"csv\" or \"tsv\".");
+        }
+        //m_strictQuote = Boolean.valueOf(p.getProperty("strict-quote", "false").trim());
+    };
+
+    @Override
+    public Object[] transform(ByteBuffer b) {
+        String line = new String(b.array(),b.arrayOffset(),b.limit(),StandardCharsets.UTF_8);
+        Object list[] = null;
+        try {
+            list = (new CSVParser()).parseLine(line);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (list != null) {
+            for (int i = 0; i < list.length; i++) {
+                if ("NULL".equals(list[i])
+                        || Constants.CSV_NULL.equals(list[i])
+                        || Constants.QUOTED_CSV_NULL.equals(list[i])) {
+                    list[i] = null;
+                }
+            }
+        }
+        return list;
+    }
+}
---
+++ tests/frontend/org/voltdb/importer/TestBuiltinTransformer.java
@@ -0,0 +1,122 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2015 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.voltdb.importer;
+
+import static org.voltcore.common.Constants.VOLT_TMP_DIR;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.ServiceLoader;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.launch.Framework;
+import org.osgi.framework.launch.FrameworkFactory;
+import org.voltdb.importer.transformer.AbstractTransformer;
+
+import com.google_voltpatches.common.base.Function;
+import com.google_voltpatches.common.base.Joiner;
+import com.google_voltpatches.common.collect.FluentIterable;
+import com.google_voltpatches.common.collect.ImmutableList;
+import com.google_voltpatches.common.collect.ImmutableMap;
+
+public class TestBuiltinTransformer extends TestCase {
+    private Bundle m_bundle;
+    private Framework m_framework;
+    private final static Joiner COMMA_JOINER = Joiner.on(",").skipNulls();
+
+    private final static Function<String,String> appendVersion = new Function<String, String>() {
+        @Override
+        public String apply(String input) {
+            return input + ";version=1.0.0";
+        }
+    };
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testHostFailure() throws Exception {
+        List<String> packages = ImmutableList.<String>builder()
+                .add("org.voltcore.network")
+                .add("org.voltcore.logging")
+                .add("org.voltdb.importer")
+                .add("org.apache.log4j")
+                .add("org.voltdb.client")
+                .add("org.slf4j")
+                .add("org.voltcore.utils")
+                .add("com.google_voltpatches.common.base")
+                .add("com.google_voltpatches.common.collect")
+                .add("com.google_voltpatches.common.net")
+                .add("com.google_voltpatches.common.io")
+                .add("com.google_voltpatches.common.util.concurrent")
+                .build();
+        String tmpFilePath = System.getProperty(VOLT_TMP_DIR, System.getProperty("java.io.tmpdir"));
+        //Create a directory in temp + username
+        File f = new File(tmpFilePath, System.getProperty("user.name"));
+        String systemPackagesSpec = FluentIterable.from(packages).transform(appendVersion).join(COMMA_JOINER);
+        Map<String, String> m_frameworkProps = ImmutableMap.<String,String>builder()
+        .put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, systemPackagesSpec)
+        .put("org.osgi.framework.storage.clean", "onFirstInit")
+        .put("felix.cache.rootdir", f.getAbsolutePath())
+        .put("felix.cache.locking", Boolean.FALSE.toString())
+        .build();
+        FrameworkFactory frameworkFactory = ServiceLoader.load(FrameworkFactory.class).iterator().next();
+        m_framework = frameworkFactory.newFramework(m_frameworkProps);
+        m_framework.start();
+        m_bundle = m_framework.getBundleContext().installBundle("file:/home/dweiss/dev/voltdb/bundles/builtintransformer.jar");
+        m_bundle.start();
+        System.out.println(AbstractTransformer.class.getClassLoader().toString());
+        ServiceReference refs[] = m_bundle.getRegisteredServices();
+        ServiceReference<AbstractTransformer> reference = refs[0];
+        AbstractTransformer o = m_bundle.getBundleContext().getService(reference);
+        System.out.println(o.getClass().getClassLoader().toString());
+        Properties prop = new Properties();
+        prop.setProperty("service", "csv");
+        o.configure(prop);
+        ByteBuffer input = ByteBuffer.wrap("\"test\",12,10.05,test".getBytes());
+        Object[] results = o.transform(input);
+        for (Object result : results) {
+            System.out.println(result);
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        m_bundle.stop();
+        m_framework.stop();
+    }
+}
+
