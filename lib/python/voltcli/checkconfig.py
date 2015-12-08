928c3399e02c08cac8e0e58c36a092e4bced89b2
Peter Shaw <pshaw@voltdb.com>
2015-12-08 15:52:20 -0500

    ENG-9576 Call just the hard requirement function expect if "voltdb check"

--- lib/python/voltcli/checkconfig.py
+++ lib/python/voltcli/checkconfig.py
@@ -29,10 +29,6 @@ import platform
 import subprocess
 import sys
 import os.path
-# Define HardRequirements (full name)
-# and possible SkippableleRequirements(fullname:init)
-hardRequirements = ['TransparentHugePage']
-skippableRequirements = {'TransparentHugePage':'thp'}
 
 # Helper functions
 
@@ -73,14 +69,14 @@ def _check_segmentation_offload():
         generic-receive-offload settings
     """
     results = []
-    tokenDevs = subprocess.Popen("ip link | grep -B 1 ether", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout.read().split('\n')[:-1][0::2]
+    tokenDevs = subprocess.Popen("ip link | grep -B 1 ether", stdout=subprocess.PIPE, shell=True).stdout.read().split('\n')[:-1][0::2]
     for d in map(lambda x: x.split(':'), tokenDevs):
         if len(d) < 2:
             continue
         dev = d[1].strip()
         tcpSeg = False
         genRec = False
-        features = subprocess.Popen("ethtool --show-offload " + dev, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).stdout.read().split('\n')
+        features = subprocess.Popen("ethtool --show-offload " + dev, stdout=subprocess.PIPE, shell=True).stdout.read().split('\n')
         for f in features:
             if "tcp-segmentation-offload" in f:
                 tcpSeg = f.split()[1] == "off"
@@ -234,8 +230,13 @@ def test_hard_requirements():
     """ Returns any errors resulting from hard config requirement violations
     """
     output = {}
-    test_full_config(output)
-    return dict([(k,v) for k,v in output.items() if k in hardRequirements])
-    # return _check_thp_config()
+    for k in hardRequirements:
+        print "calling hardReq k: " + k
+        hardRequirements[k](output)
+    return output
 
-
+# (Moved to end since Python does not handle forward reference.)
+# Define HardRequirements (full name : checker method)
+# and possible SkippableleRequirements(fullname:init)
+hardRequirements = {'TransparentHugePage' : test_thp_config, }
+skippableRequirements = {'TransparentHugePage':'thp'}
