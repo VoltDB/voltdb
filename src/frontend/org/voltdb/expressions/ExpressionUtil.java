027951b8a41129b4896fa1032081f09fb9688639
Huzefa Hakimi <hhakimi@voltdb.com>
2015-12-09 16:24:29 -0500

    delete extra line

--- src/frontend/org/voltdb/expressions/ExpressionUtil.java
+++ src/frontend/org/voltdb/expressions/ExpressionUtil.java
@@ -615,7 +615,6 @@ public abstract class ExpressionUtil {
             }
         } else if (ExpressionType.OPERATOR_NOT == expr.getExpressionType()) {
             AbstractExpression leftExpr = expr.getLeft();
-
             if (VoltType.BOOLEAN == leftExpr.getValueType()) {
                 // functions can also return boolean. so the left child expression can be
                 // function expression and there by don't evaluate every left child expr
