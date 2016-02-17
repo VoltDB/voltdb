ceacc58cc46e231862d5b9795c373ba7135fa237
alexeevm <michael.alexeev@gmail.com>
2016-02-17 15:27:57 -0500

    AbstractJoinExecutor::p_init_null_tuples param order (outer table, inner table). License check

--- src/ee/executors/abstractjoinexecutor.cpp
+++ src/ee/executors/abstractjoinexecutor.cpp
@@ -1,5 +1,5 @@
 /* This file is part of VoltDB.
- * Copyright (C) 2008-2015 VoltDB Inc.
+ * Copyright (C) 2008-2016 VoltDB Inc.
  *
  * This file contains original code and/or modifications of original code.
  * Any modifications made by VoltDB Inc. are licensed under the following
@@ -64,7 +64,7 @@ void AbstractJoinExecutor::outputTuple(CountingPostfilter& postfilter, TableTupl
     pmp.countdownProgress();
 }
 
-void AbstractJoinExecutor::p_init_null_tuples(Table* inner_table, Table* outer_table) {
+void AbstractJoinExecutor::p_init_null_tuples(Table* outer_table, Table* inner_table) {
     if (m_joinType != JOIN_TYPE_INNER) {
         assert(inner_table);
         m_null_inner_tuple.init(inner_table->schema());
--- src/ee/executors/abstractjoinexecutor.h
+++ src/ee/executors/abstractjoinexecutor.h
@@ -1,5 +1,5 @@
 /* This file is part of VoltDB.
- * Copyright (C) 2008-2015 VoltDB Inc.
+ * Copyright (C) 2008-2016 VoltDB Inc.
  *
  * This file contains original code and/or modifications of original code.
  * Any modifications made by VoltDB Inc. are licensed under the following
@@ -71,7 +71,7 @@ class AbstractJoinExecutor : public AbstractExecutor {
 
         bool p_init(AbstractPlanNode*, TempTableLimits* limits);
 
-        void p_init_null_tuples(Table* inner_table, Table* outer_table);
+        void p_init_null_tuples(Table* outer_table, Table* inner_table);
 
         // Write tuple to the output table
         void outputTuple(CountingPostfilter& postfilter, TableTuple& join_tuple, ProgressMonitorProxy& pmp);
