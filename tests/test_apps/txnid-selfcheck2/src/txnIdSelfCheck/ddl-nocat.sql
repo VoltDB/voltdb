965ec8e6183e21dcda1e4134964ca33dd2447598
peter shaw <pshaw@peters-MacBook-Pro.local>
2016-02-26 12:19:14 -0500

    ENG-9067 Change "export" to new "stream" keyword.

--- tests/test_apps/txnid-selfcheck2/src/txnIdSelfCheck/ddl-nocat.sql
+++ tests/test_apps/txnid-selfcheck2/src/txnIdSelfCheck/ddl-nocat.sql
@@ -140,7 +140,7 @@ CREATE TABLE forDroppedProcedure
 PARTITION TABLE forDroppedProcedure ON COLUMN p;
 
 -- export tables
-CREATE TABLE partitioned_export
+CREATE STREAM partitioned_export PARTITION ON COLUMN cid
 (
   txnid      bigint             NOT NULL
 , prevtxnid  bigint             NOT NULL
@@ -153,8 +153,8 @@ CREATE TABLE partitioned_export
 , adhocjmp   bigint             NOT NULL
 , value      varbinary(1048576) NOT NULL
 );
-PARTITION TABLE partitioned_export ON COLUMN cid;
-EXPORT TABLE partitioned_export;
+-- PARTITION TABLE partitioned_export ON COLUMN cid;
+-- EXPORT TABLE partitioned_export;
 
 CREATE VIEW ex_partview (
     cid,
@@ -180,7 +180,7 @@ CREATE TABLE ex_partview_shadow (
 );
 PARTITION TABLE ex_partview_shadow ON COLUMN cid;
 
-CREATE TABLE replicated_export
+CREATE STREAM replicated_export
 (
   txnid      bigint             NOT NULL
 , prevtxnid  bigint             NOT NULL
