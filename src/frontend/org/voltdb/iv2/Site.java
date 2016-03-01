bece341f95369b3c5e262a75c21d66a52c91194d
wweiss <wweiss@voltdb.com>
2016-03-01 16:33:00 -0500

    ENG-9849: Initialize Homogenious topology

--- src/frontend/org/voltdb/iv2/Site.java
+++ src/frontend/org/voltdb/iv2/Site.java
@@ -173,7 +173,8 @@ public class Site implements Runnable, SiteProcedureConnection, SiteSnapshotConn
      *  @ApplyBinaryLogSP and @ApplyBinaryLogMP invocation so it can be provided to the
      *  ReplicaDRGateway on repair
      */
-    private Map<Integer, Map<Integer, DRConsumerDrIdTracker>> m_maxSeenDrLogsBySrcPartition;
+    private final  Map<Integer, Map<Integer, DRConsumerDrIdTracker>> m_maxSeenDrLogsBySrcPartition =
+            new HashMap<Integer, Map<Integer, DRConsumerDrIdTracker>>();
     private long m_lastLocalSpUniqueId;   // Only populated by the Site for ApplyBinaryLog Txns
     private long m_lastLocalMpUniqueId;   // Only populated by the Site for ApplyBinaryLog Txns
 
