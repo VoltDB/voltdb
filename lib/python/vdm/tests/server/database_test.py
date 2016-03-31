c739d3aca5a26c9af08d2e471d8b41ba07171166
Manju James <mjames@voltdb.com>
2016-03-31 10:38:28 -0400

    Merging in changes from deerwalk/master.

--- lib/python/vdm/tests/server/database_test.py
+++ lib/python/vdm/tests/server/database_test.py
@@ -72,7 +72,7 @@ class Database(unittest.TestCase):
             # Delete database
             db_url = __url__ + str(last_db_id)
             response = requests.delete(db_url)
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 204)
         else:
             print "The database list is empty"
 
@@ -278,7 +278,7 @@ class DeleteDatabase(unittest.TestCase):
             # Delete database
             db_url = __url__ + str(last_db_id)
             response = requests.delete(db_url)
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 204)
         else:
             print "The database list is empty"
 
