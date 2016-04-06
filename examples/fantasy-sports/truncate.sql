f3d5d6577a4d22539f61547fff69ab11d1890a43
John Hugg <jhugg@voltdb.com>
2016-04-06 14:14:16 -0400

    Remove fantasy-sports example because it won't be ready for 6.3. Pushed it to a branch at app-fantasy-sports first.

--- examples/fantasy-sports/truncate.sql
+++
@@ -1,6 +0,0 @@
-truncate table fantasy_user;
-truncate table user_contest_roster;
-truncate table user_contest_score;
-truncate table nfl_contest_large;
-truncate table nfl_contest_small;
-truncate table nfl_player_game_score;
