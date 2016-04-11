83a4653c1ee1490672bcc7be634de2591fd96bec
Peter Shaw <pshaw@voltdb.com>
2016-04-11 15:03:29 -0400

    ENG-6772 sync ddl.sql & ddl-nocat.sql

--- tests/test_apps/genqa/ddl.sql
+++ tests/test_apps/genqa/ddl.sql
@@ -1,5 +1,3 @@
-IMPORT CLASS genqa.procedures.SampleRecord;
-
 -- Partitioned Data Table
 CREATE TABLE partitioned_table
 (
@@ -46,7 +44,7 @@ AS
  GROUP BY rowid_group;
 
 -- Export Table for Partitioned Data Table deletions
-CREATE STREAM export_partitioned_table EXPORT TO TARGET abc
+CREATE STREAM export_partitioned_table PARTITION ON COLUMN rowid EXPORT TO TARGET abc
 (
   txnid                     BIGINT        NOT NULL
 , rowid                     BIGINT        NOT NULL
@@ -61,10 +59,10 @@ CREATE STREAM export_partitioned_table EXPORT TO TARGET abc
 , type_not_null_bigint      BIGINT        NOT NULL
 , type_null_timestamp       TIMESTAMP
 , type_not_null_timestamp   TIMESTAMP     NOT NULL
-, type_null_decimal         DECIMAL
-, type_not_null_decimal     DECIMAL       NOT NULL
 , type_null_float           FLOAT
 , type_not_null_float       FLOAT         NOT NULL
+, type_null_decimal         DECIMAL
+, type_not_null_decimal     DECIMAL       NOT NULL
 , type_null_varchar25       VARCHAR(32)
 , type_not_null_varchar25   VARCHAR(32)   NOT NULL
 , type_null_varchar128      VARCHAR(128)
@@ -72,7 +70,6 @@ CREATE STREAM export_partitioned_table EXPORT TO TARGET abc
 , type_null_varchar1024     VARCHAR(1024)
 , type_not_null_varchar1024 VARCHAR(1024) NOT NULL
 );
-PARTITION TABLE export_partitioned_table ON COLUMN rowid;
 
 CREATE TABLE  export_mirror_partitioned_table 
 (
@@ -89,10 +86,10 @@ CREATE TABLE  export_mirror_partitioned_table
 , type_not_null_bigint      BIGINT        NOT NULL
 , type_null_timestamp       TIMESTAMP
 , type_not_null_timestamp   TIMESTAMP     NOT NULL
-, type_null_decimal         DECIMAL
-, type_not_null_decimal     DECIMAL       NOT NULL
 , type_null_float           FLOAT
 , type_not_null_float       FLOAT         NOT NULL
+, type_null_decimal         DECIMAL
+, type_not_null_decimal     DECIMAL       NOT NULL
 , type_null_varchar25       VARCHAR(32)
 , type_not_null_varchar25   VARCHAR(32)   NOT NULL
 , type_null_varchar128      VARCHAR(128)
@@ -103,11 +100,10 @@ CREATE TABLE  export_mirror_partitioned_table
 );
 PARTITION TABLE export_mirror_partitioned_table ON COLUMN rowid;
 
-CREATE STREAM  export_done_table EXPORT TO TARGET abc
+CREATE STREAM export_done_table PARTITION ON COLUMN txnid EXPORT TO TARGET abc
 (
   txnid                     BIGINT        NOT NULL
 );
-PARTITION TABLE export_done_table ON COLUMN txnid;
 
 -- Replicated Table
 CREATE TABLE replicated_table
@@ -181,12 +177,11 @@ CREATE STREAM  export_replicated_table EXPORT TO TARGET abc
 , type_not_null_varchar1024 VARCHAR(1024) NOT NULL
 );
 
-CREATE STREAM export_skinny_partitioned_table  EXPORT TO TARGET abc
+CREATE STREAM export_skinny_partitioned_table  PARTITION ON COLUMN rowid EXPORT TO TARGET abc
 (
   txnid                     BIGINT        NOT NULL
 , rowid                     BIGINT        NOT NULL
 );
-PARTITION TABLE export_skinny_partitioned_table ON COLUMN rowid;
 
 CREATE PROCEDURE FROM CLASS genqa.procedures.JiggleSkinnyExportSinglePartition;
 CREATE PROCEDURE FROM CLASS genqa.procedures.JiggleSinglePartition;
@@ -202,7 +197,4 @@ CREATE PROCEDURE FROM CLASS genqa.procedures.JiggleExportDoneTable;
 PARTITION PROCEDURE JiggleSkinnyExportSinglePartition
   ON TABLE export_skinny_partitioned_table COLUMN rowid;
 
--- EXPORT TABLE export_skinny_partitioned_table;
--- EXPORT TABLE export_partitioned_table;
--- EXPORT TABLE export_replicated_table;
--- EXPORT TABLE export_done_table;
+CREATE PROCEDURE SelectwithLimit as select * from export_mirror_partitioned_table where rowid between ? and ? order by rowid limit ?;
