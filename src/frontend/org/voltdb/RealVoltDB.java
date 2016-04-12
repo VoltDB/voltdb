4f359781e2607fc9f60c5ad4e8a9895c8bb23822
Ruth Morgenstein <rmorgenstein@voltdb.com>
2016-04-12 17:08:06 -0400

    Set Version V4.6.11.1

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -169,9 +169,9 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback
     // CatalogContext is immutable, just make sure that accessors see a consistent version
     volatile CatalogContext m_catalogContext;
     private String m_buildString;
-    static final String m_defaultVersionString = "4.6.11";
+    static final String m_defaultVersionString = "4.6.11.1";
     // by default set the version to only be compatible with itself
-    static final String m_defaultHotfixableRegexPattern = "^\\Q4.6.11\\E\\z";
+    static final String m_defaultHotfixableRegexPattern = "^\\Q4.6.11.1\\E\\z";
     // these next two are non-static because they can be overrriden on the CLI for test
     private String m_versionString = m_defaultVersionString;
     private String m_hotfixableRegexPattern = m_defaultHotfixableRegexPattern;
--- src/frontend/org/voltdb/studio/js/mainui.js
+++ src/frontend/org/voltdb/studio/js/mainui.js
@@ -4,7 +4,7 @@ var IMainUI = (function(){
 
 var tab_counter = 1;
 var $tabs = null;
-var $volt_version = '4.6.11';
+var $volt_version = '4.6.11.1';
 
 this.InitWorkspace = function()
 {
