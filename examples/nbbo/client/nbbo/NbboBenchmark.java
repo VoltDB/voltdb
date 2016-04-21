5cc20eac287ac5124528d94dff73c4045e35849b
John Piekos <jpiekos@voltdb.com>
2016-04-21 11:18:00 -0400

    Update NbboBenchmark.java
    Delete the term "volt ".

--- examples/nbbo/client/nbbo/NbboBenchmark.java
+++ examples/nbbo/client/nbbo/NbboBenchmark.java
@@ -93,10 +93,10 @@ public class NbboBenchmark {
         @Option(desc = "Comma separated list of the form server[:port] to connect to.")
         String servers = "localhost";
 
-        @Option(desc = "Volt user name")
+        @Option(desc = "User name")
         public String user = "";
 
-        @Option(desc = "Volt password")
+        @Option(desc = "Password")
         public String password = "";
 
         @Option(desc = "Benchmark duration, in seconds.")
