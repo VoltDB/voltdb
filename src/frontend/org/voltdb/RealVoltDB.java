41a17a99f9acbeffe283355e86f2a5c85620d528
Jenkins CI <test@voltdb.com>
2016-04-29 08:43:51 -0400

    Set Version 6.2.1

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -178,9 +178,9 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback {
     // CatalogContext is immutable, just make sure that accessors see a consistent version
     volatile CatalogContext m_catalogContext;
     private String m_buildString;
-    static final String m_defaultVersionString = "6.2";
+    static final String m_defaultVersionString = "6.2.1";
     // by default set the version to only be compatible with itself
-    static final String m_defaultHotfixableRegexPattern = "^\\Q6.2\\E\\z";
+    static final String m_defaultHotfixableRegexPattern = "^\\Q6.2.1\\E\\z";
     // these next two are non-static because they can be overrriden on the CLI for test
     private String m_versionString = m_defaultVersionString;
     private String m_hotfixableRegexPattern = m_defaultHotfixableRegexPattern;
