34683588255f9b722074cf107033df4eeb15368c
Zeeman He <zhe@voltdb.com>
2016-05-03 17:03:24 -0400

    add license info

--- src/frontend/org/voltdb/importclient/kinesis/KinesisStreamImporterException.java
+++ src/frontend/org/voltdb/importclient/kinesis/KinesisStreamImporterException.java
@@ -1,9 +1,24 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2016 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.voltdb.importclient.kinesis;
 
 import org.voltdb.importclient.ImportBaseException;
 
-public class KinesisStreamImporterException extends ImportBaseException
-{
+public class KinesisStreamImporterException extends ImportBaseException {
     private static final long serialVersionUID = 7668280657393399984L;
 
     public KinesisStreamImporterException() {
@@ -17,8 +32,7 @@ public class KinesisStreamImporterException extends ImportBaseException
         super(cause);
     }
 
-    public KinesisStreamImporterException(String format, Throwable cause,
-            Object... args) {
+    public KinesisStreamImporterException(String format, Throwable cause, Object... args) {
         super(format, cause, args);
     }
 }
