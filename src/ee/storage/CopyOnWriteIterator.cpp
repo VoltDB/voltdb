30b1a448e23974a66a71623959b6738fc35a12dd
Huzefa Hakimi <hhakimi@voltdb.com>
2016-05-20 18:42:56 -0400

    add check for empty table in COWIterator during snapshot

--- src/ee/storage/CopyOnWriteIterator.cpp
+++ src/ee/storage/CopyOnWriteIterator.cpp
@@ -28,6 +28,7 @@ CopyOnWriteIterator::CopyOnWriteIterator(
         m_location(NULL),
         m_blockOffset(0),
         m_currentBlock(NULL),
+        m_emptyTable(false),
         m_skippedDirtyRows(0),
         m_skippedInactiveRows(0) {
 
@@ -36,6 +37,7 @@ CopyOnWriteIterator::CopyOnWriteIterator(
         // has empty tuple storage block associated with it. So no need
         // to set it up for snapshot
         m_blockIterator = m_end;
+        m_emptyTable = true;
         return;
     }
 
@@ -57,6 +59,12 @@ CopyOnWriteIterator::CopyOnWriteIterator(
  * it skiped a dirty tuple and didn't end up with the right found tuple count upon reaching the end.
  */
 bool CopyOnWriteIterator::needToDirtyTuple(char *tupleAddress) {
+    if (m_emptyTable) {
+        // snapshot was activated when the table was empty.
+        // Tuple is not in  snapshot region, don't care about this tuple
+        assert(m_currentBlock == NULL);
+        return false;
+    }
     /**
      * Find out which block the address is contained in. Lower bound returns the first entry
      * in the index >= the address. Unless the address happens to be equal then the block
@@ -69,11 +77,8 @@ bool CopyOnWriteIterator::needToDirtyTuple(char *tupleAddress) {
         return false;
     }
 
-    // code will be removed. Not for check-in: for debugging purpose only
-    if (m_currentBlock == NULL) {
-        StackTrace::printStackTrace();
-    }
     assert(m_currentBlock != NULL);
+
     /**
      * Now check where this is relative to the COWIterator.
      */
