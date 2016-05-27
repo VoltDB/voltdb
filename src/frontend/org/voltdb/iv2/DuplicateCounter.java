d3b5013d6bfbc41e58e4027f1dca2b522cb7ad15
John Hugg <jhugg@voltdb.com>
2016-05-27 15:54:34 -0400

    ENG-10389: Remove extra logging and un-comment-out code (accident).

--- src/frontend/org/voltdb/iv2/DuplicateCounter.java
+++ src/frontend/org/voltdb/iv2/DuplicateCounter.java
@@ -77,7 +77,6 @@ public class DuplicateCounter
     int updateReplicas(List<Long> replicas) {
         Set<Long> newSet = new HashSet<Long>(replicas);
         m_expectedHSIds.retainAll(newSet);
-        tmLog.info(String.format("*** RELEASING TXN %d", m_txnId));
         if (m_expectedHSIds.size() == 0) {
             return DONE;
         }
@@ -93,7 +92,7 @@ public class DuplicateCounter
                 m_responseHash = Long.valueOf(hash);
             }
             else if (!m_responseHash.equals(hash)) {
-                /*tmLog.fatal("Stored procedure " + getStoredProcedureName()
+                tmLog.fatal("Stored procedure " + getStoredProcedureName()
                         + " generated different SQL queries at different partitions."
                         + " Shutting down to preserve data integrity.");
                 String msg = String.format("HASH MISMATCH COMPARING: %d to %d\n"
@@ -102,7 +101,7 @@ public class DuplicateCounter
                         hash, m_responseHash,
                         m_lastResponse.toString(), message.toString());
                 tmLog.error(msg);
-                return MISMATCH;*/
+                return MISMATCH;
             }
             /*
              * Replicas will return a response to a write with no result tables
@@ -134,7 +133,6 @@ public class DuplicateCounter
 
         m_expectedHSIds.remove(message.m_sourceHSId);
         if (m_expectedHSIds.size() == 0) {
-            tmLog.info("satisfied");
             return DONE;
         }
         else {
