7726ba44b56dc42219f97f32b08cfc9b6e2664a6
Bill White <bill.white@voltdb.com>
2016-06-10 11:26:29 -0400

    Fixed a licenses check problem.

--- tests/frontend/org/voltdb/planner/TestPlansSubQueries.java
+++ tests/frontend/org/voltdb/planner/TestPlansSubQueries.java
@@ -2321,10 +2321,10 @@ public class TestPlansSubQueries extends PlannerTestCase {
      * user column names.
      */
     public void testGeneratedNamesDontConflict() {
-    	String sql = "select C1 from ( select cast(a as varchar), c as c1 from r5 ) as SQ where SQ.C1 < 0;";
-	AbstractPlanNode pn = compile(sql);
-	assertNotNull(pn);
-	VoltType vt = pn.getOutputSchema().getColumns().get(0).getType();
-	assert(VoltType.INTEGER.equals(vt));
+        String sql = "select C1 from ( select cast(a as varchar), c as c1 from r5 ) as SQ where SQ.C1 < 0;";
+    AbstractPlanNode pn = compile(sql);
+    assertNotNull(pn);
+    VoltType vt = pn.getOutputSchema().getColumns().get(0).getType();
+    assert(VoltType.INTEGER.equals(vt));
     }
 }
