d18a1b66aafe318f3c0271604d9716bb5ebaa409
John Hugg <jhugg@voltdb.com>
2016-06-15 17:16:00 -0400

    ENG-10599: Print way more info on hash mismatch. This isn't really enough, but it's a lot more to go on.

--- src/frontend/org/voltdb/SQLStmt.java
+++ src/frontend/org/voltdb/SQLStmt.java
@@ -17,8 +17,6 @@
 
 package org.voltdb;
 
-import java.nio.ByteBuffer;
-
 import org.apache.hadoop_voltpatches.util.PureJavaCrc32C;
 import org.voltdb.common.Constants;
 import org.voltdb.planner.ActivePlanRepository;
@@ -54,7 +52,7 @@ public class SQLStmt {
     String sqlTextStr;
     String joinOrder;
     // hash of the SQL string for determinism checks
-    byte[] sqlCRC;
+    int sqlCRC;
 
     byte statementParamJavaTypes[];
 
@@ -100,8 +98,7 @@ public class SQLStmt {
         // create a hash for determinism purposes
         PureJavaCrc32C crc = new PureJavaCrc32C();
         crc.update(sqlText);
-        // ugly hack to get bytes from an int
-        this.sqlCRC = ByteBuffer.allocate(4).putInt((int) crc.getValue()).array();
+        this.sqlCRC = (int) crc.getValue();
 
         inCatalog = true;
     }
