8b30fdac0e6449dc9eea1018efd11e6bf37a6b9b
Stefano Santoro <ssantoro@voltdb.com>
2016-06-22 12:42:32 -0400

    ENG-10404 fix flaky TestJDBCAutoReconnectOnLoss

--- tests/frontend/org/voltdb/ServerThread.java
+++ tests/frontend/org/voltdb/ServerThread.java
@@ -71,7 +71,7 @@ public class ServerThread extends Thread {
             m_config.m_pathToLicense = getTestLicensePath();
         }
         m_config.m_leader = "";
-        m_config.m_coordinators = JoinerCriteria.hosts(m_config.m_leader);
+        m_config.m_coordinators = JoinerCriteria.hosts(m_config.m_internalPort);
         VoltDB.instance().setMode(OperationMode.INITIALIZING);
 
 
@@ -91,7 +91,7 @@ public class ServerThread extends Thread {
             m_config.m_pathToLicense = getTestLicensePath();
         }
         m_config.m_leader = "";
-        m_config.m_coordinators = JoinerCriteria.hosts(m_config.m_leader);
+        m_config.m_coordinators = JoinerCriteria.hosts(m_config.m_internalPort);
         VoltDB.instance().setMode(OperationMode.INITIALIZING);
 
 
@@ -129,7 +129,7 @@ public class ServerThread extends Thread {
             m_config.m_pathToLicense = getTestLicensePath();
         }
         m_config.m_leader = MiscUtils.getHostnameColonPortString("localhost", leaderPort);
-        m_config.m_coordinators = JoinerCriteria.hosts(m_config.m_leader);
+        m_config.m_coordinators = JoinerCriteria.hosts(internalPort);
         m_config.m_internalPort = internalPort;
         m_config.m_zkInterface = "127.0.0.1:" + zkPort;
         VoltDB.instance().setMode(OperationMode.INITIALIZING);
