858a56f9f47df4b977d0595140c32740edd2f8a1
Femi Olukoya <oolukoya@voltdb.com>
2016-06-23 11:09:47 -0400

    Error handling and helper functions

--- tools/jenkins/jenkinsbot.py
+++ tools/jenkins/jenkinsbot.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+
+# This file is part of VoltDB.
+# Copyright (C) 2008-2016 VoltDB Inc.
+
+# Script that runs jenkinsbot for VoltDB Slack
+
+from mysql.connector.errors import Error as MySQLError
 from slackclient import SlackClient
 from tabulate import tabulate
 
@@ -5,7 +13,6 @@ import mysql.connector
 import os
 import sys
 import time
-
 
 tl_query = ("""
 SELECT t.name AS 'Test name', COUNT(*) AS 'Failures'
@@ -31,16 +38,16 @@ RIGHT JOIN `junit-job-results` AS j
 ON j.name=t.job AND t.name=%(test)s
 """)
 
+
 def jenkins_session():
     """
     Establishes session and responds to commands
     """
-    help_text = """
-    test-leaderboard <job> <build>
-    build-range <job> <build #>-<build #>
-    test-on-master <test>
-    help
-    """
+
+    help_text = ['test-leaderboard <job> <build>\n',
+                 'build-range <job> <build #>-<build #>\n',
+                 'test-on-master <test>\n',
+                 'help\n']
 
     token = os.environ.get('token', None)
     if token is None:
@@ -49,70 +56,81 @@ def jenkins_session():
 
     try:
         sc = SlackClient(token)
-        db = mysql.connector.connect(host='volt2.voltdb.lan', user='oolukoya', password='oolukoya', database='qa')
+        db = mysql.connector.connect(host=os.environ.get('dbhost', None),
+                                     user=os.environ.get('dbuser', None),
+                                     password=os.environ.get('dbpass', None),
+                                     database=os.environ.get('dbdb', None))
         cursor = db.cursor()
+    except Exception as e:
+        print('Could not connect to database')
+        print(e)
+        return
 
-        # Connect to real time messaging
-        if sc.rtm_connect():
-            while True:
-                # Wait for and respond to commands.
-                incoming = list(sc.rtm_read())
-                print incoming
-                if len(incoming) > 0 and incoming[0].get('text', None) is not None and incoming[0].get('bot_id', None) is None:
-                    text = incoming[0]['text']
-                    channel = incoming[0]['channel']
-                    if 'end-session' in text:
-                        print sc.api_call(
-                            'chat.postMessage', channel=channel, text='Exiting..', as_user=True
-                        )
-                        return
-                    elif 'help' in text:
-                        print sc.api_call(
-                            'chat.postMessage', channel=channel, text=help_text, as_user=True
-                        )
-                    elif 'test-leaderboard' in text:
-                        params = {
-                            'job': text.split(' ')[1],
-                            'beginning': text.split(' ')[2]
-                        }
-                        cursor.execute(tl_query, params)
-                        headers = list(cursor.column_names)
-                        table = list(cursor.fetchall())
-                        print sc.api_call(
-                            'files.upload', channels=[channel], content=tabulate(table, headers), filetype='text', filename='testleaderboard.txt'
-                        )
-                    elif 'build-range' in text:
-                        args = text.split(' ')
-                        builds = args[2]
-                        params = {
-                            'job': args[1],
-                            'build_low': builds.split('-')[0],
-                            'build_high': builds.split('-')[1]
-                        }
-                        cursor.execute(br_query, params)
-                        headers = list(cursor.column_names)
-                        table = list(cursor.fetchall())
-                        print sc.api_call(
-                            'files.upload', channels=[channel], content=tabulate(table, headers), filetype='text', filename='buildrange.txt'
-                        )
-                    elif 'test-on-master' in text:
-                        params = {
-                            'test': text.split(' ')[1]
-                        }
-                        cursor.execute(tom_query, params)
-                        headers = list(cursor.column_names)
-                        table = list(cursor.fetchall())
-                        print sc.api_call(
-                            'files.upload', channels=[channel], content=tabulate(table, headers), filetype='text', filename='testonmaster.txt'
-                        )
-                time.sleep(1)
-        else:
-            print "Connection Failed, invalid token?"
-    except:
-        print(sys.exc_info())
-    finally:
-        cursor.close()
-        db.close()
+    # Connect to real time messaging
+    if not sc.rtm_connect():
+        print "Connection Failed, invalid token?"
+        return
+
+    while True:
+        try:
+            # Wait for and respond to commands.
+            incoming = list(sc.rtm_read())
+            if len(incoming) > 0 and incoming[0].get('text', None) is not None and incoming[0].get('bot_id', None) is None:
+                text = incoming[0]['text']
+                channel = incoming[0]['channel']
+                if 'end-session' in text:
+                    post_message(sc, channel, 'Leaving...')
+                    sys.exit(0)
+                elif 'help' in text:
+                    post_message(sc, channel, ''.join(help_text))
+                elif 'test-leaderboard' in text:
+                    params = {
+                        'job': text.split(' ')[1],
+                        'beginning': text.split(' ')[2]
+                    }
+                    make_query(cursor, tl_query, params, [channel], 'testleaderboard.txt')
+                elif 'build-range' in text:
+                    args = text.split(' ')
+                    builds = args[2]
+                    params = {
+                        'job': args[1],
+                        'build_low': builds.split('-')[0],
+                        'build_high': builds.split('-')[1]
+                    }
+                    make_query(cursor, br_query, params, [channel], 'buildrange.txt')
+                elif 'test-on-master' in text:
+                    params = {
+                        'test': text.split(' ')[1]
+                    }
+                    make_query(cursor, tom_query, params, [channel], 'testonmaster.txt')
+            time.sleep(1)
+        except (KeyboardInterrupt, SystemExit):
+            # Turning off the bot
+            cursor.close()
+            db.close()
+            break
+        except MySQLError as e:
+            print(e)
+            post_message(sc, channel, 'Something went wrong with the query. Please try again.')
+        except Exception as e:
+            # Something unexpected went wrong
+            print(e)
+
+def make_query(cursor, query, params, channels, filename):
+    """
+    Make a query then upload a text file with tables to the channels.
+    """
+    cursor.execute(query, params)
+    headers = list(cursor.column_names)
+    table = list(cursor.fetchall())
+    sc.api_call(
+        'files.upload', channels=channels, content=tabulate(table, headers), filetype='text', filename=filename
+    )
+
+def post_message(sc, channel, text):
+    sc.api_call(
+        'chat.postMessage', channel=channel, text=text, as_user=True
+    )
 
 if __name__ == '__main__':
     jenkins_session()
