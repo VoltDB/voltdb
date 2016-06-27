bd08f8f0222064adcf7fdfda9f046d1856c708d6
Femi Olukoya <oolukoya@voltdb.com>
2016-06-27 14:55:53 -0400

    Pro and community specific queries

--- tools/jenkins/jenkinsbot.py
+++ tools/jenkins/jenkinsbot.py
@@ -15,8 +15,8 @@ from mysql.connector.errors import Error as MySQLError
 from slackclient import SlackClient
 from tabulate import tabulate
 
-COMMUNITY = 'branch-2-community-junit-master'
-PRO = 'branch-2-pro-junit-master'
+COMMUNITY = os.environ.get('community', None)
+PRO = os.environ.get('pro', None)
 
 TL_QUERY = ("""
   SELECT tf.name AS 'Test name',
@@ -24,6 +24,17 @@ TL_QUERY = ("""
     FROM `junit-test-failures` AS tf
    WHERE NOT tf.status='FIXED' AND
          tf.build >= %(beginning)s AND
+         tf.job=%(job)s
+GROUP BY tf.name
+ORDER BY 2 DESC
+""")
+
+D_QUERY = ("""
+  SELECT tf.name AS 'Test name',
+         COUNT(*) AS 'Failures'
+    FROM `junit-test-failures` AS tf
+   WHERE NOT tf.status='FIXED' AND
+         NOW() - INTERVAL %(days)s DAY <= tf.stamp AND
          tf.job=%(job)s
 GROUP BY tf.name
 ORDER BY 2 DESC
@@ -68,15 +79,13 @@ PL_QUERY = ("""
                   name,
                   (
                    SELECT COUNT(*)
-                     FROM `junit-builds` jr
+                     FROM `junit-builds` AS jr
                     WHERE jr.name = tf.job
                   ) AS total,
                   COUNT(*) AS fails
              FROM `junit-test-failures` AS tf
             WHERE NOT status='FIXED' AND
-                  job=%(job)s AND
-                  %(build_low)s <= build AND
-                  build <= %(build_high)s
+                  job=%(job)s
          GROUP BY job,
                   name,
                   total
@@ -97,7 +106,6 @@ ORDER BY 2 DESC
 class JenkinsBot(object):
 
     def __init__(self):
-        self.cursor = None
         self.client = None
 
     def connect(self):
@@ -108,115 +116,121 @@ class JenkinsBot(object):
         self.client = SlackClient(token)
         return True
 
-    def jenkins_session(self):
+    def session(self):
         """
         Establishes session and responds to commands
         """
 
-        help_text = ['See which tests are failing the most since this build: test-leaderboard <job> <build>\n',
-                     'Failing the most in this build range: build-range <job> <build #>-<build #>\n',
-                     'Most recent failure on master: test-on-master <test>\n',
-                     'See failure percentages per test: percent-leaderboard <job> <build #>-<build #>\n',
-                     'All failures for a job: all-failures <job>\n'
+        help_text = ['See which tests are failing the most since this build:\n\ttest-leaderboard <job> <build>\n',
+                     'See which tests are failing in the past x days:\n\tdays <job> <days>\n',
+                     'Failing the most in this build range:\n\tbuild-range <job> <build #>-<build #>\n',
+                     'Most recent failure on master:\n\ttest-on-master <job> <testname> (testname is full test name '
+                     'including the suite)\n',
+                     'All failures for a job:\n\tall-failures <job>\n',
+                     'For any <job>, you can specify "pro" or "com" for the master jobs\n',
                      'help\n']
-
-        if not self.connect():
-            return
-
-        try:
-            database = mysql.connector.connect(host=os.environ.get('dbhost', None),
-                                               user=os.environ.get('dbuser', None),
-                                               password=os.environ.get('dbpass', None),
-                                               database=os.environ.get('dbdb', None))
-            self.cursor = database.cursor()
-        except MySQLError as error:
-            print('Could not connect to database')
-            print(error)
-            return
 
         # Connect to real time messaging
         if not self.client.rtm_connect():
-            print "Connection Failed, invalid token?"
+            print "Connection failed for Real Time Messaging, invalid token?"
             return
 
         while True:
             try:
                 # Wait for and respond to commands.
                 incoming = list(self.client.rtm_read())
-                if len(incoming) > 0 and incoming[0].get('text', None) is not None and incoming[0].get('bot_id', None) is None:
+                if (len(incoming) > 0 and incoming[0].get('text', None) is not None and
+                    incoming[0].get('bot_id', None) is None):
                     text = incoming[0]['text']
                     channel = incoming[0]['channel']
                     if 'end-session' in text:
+                        # Keyword for making jenkinsbot inactive. Script has to be run again to turn jenkinsbot on.
                         self.post_message(channel, 'Leaving...')
                         sys.exit(0)
                     elif 'help' in text:
                         self.post_message(channel, ''.join(help_text))
-                    elif 'test-leaderboard' in text:
-                        args = text.split(' ')
-                        params = {
-                            'job': args[1],
-                            'beginning': args[2]
-                        }
-                        self.query_and_response(TL_QUERY, params, [channel], 'testleaderboard.txt')
-                    elif 'build-range' in text:
-                        args = text.split(' ')
-                        builds = args[2]
-                        params = {
-                            'job': args[1],
-                            'build_low': builds[0],
-                            'build_high': builds[1]
-                        }
-                        self.query_and_response(BR_QUERY, params, [channel], 'buildrange.txt')
-                    elif 'test-on-master' in text:
-                        args = text.split(' ')
-                        params = {
-                            'test': args[1],
-                            'job': args[2]
-                        }
-                        self.query_and_response(TOM_QUERY, params, [channel], 'testonmaster.txt')
-                    elif 'percent-leaderboard' in text:
-                        args = text.split(' ')
-                        builds = args[2].split('-')
-                        params = {
-                            'job': args[1],
-                            'build_low': builds[0],
-                            'build_high': builds[1]
-                        }
-                        self.query_and_response(PL_QUERY, params, [channel], 'percentleaderboard.txt')
-                    elif 'all-failures' in text:
-                        args = text.split(' ')
-                        params = {
-                            'job': args[1]
-                        }
-                        self.query_and_response(AF_QUERY, params, [channel], 'allfailures.txt')
+                    else:
+                        if 'pro' in text:
+                            job = PRO
+                        elif 'com' in text:
+                            job = COMMUNITY
+                        elif len(text) > 1:
+                            job = text.split(' ')[1]
+                        if 'test-on-master' in text:
+                            args = text.split(' ')
+                            params = {
+                                'job': job,
+                                'test': args[2],
+                            }
+                            self.query_and_response(TOM_QUERY, params, [channel], 'testonmaster.txt')
+                        elif 'all-failures' in text:
+                            params = {
+                                'job': job
+                            }
+                            self.query_and_response(AF_QUERY, params, [channel], 'allfailures.txt')
+                        elif 'days' in text:
+                            args = text.split(' ')
+                            params = {
+                                'job': job,
+                                'days': args[2]
+                            }
+                            self.query_and_response(D_QUERY, params, [channel], '%s-leaderboard-%s.txt' % args[2])
+                        elif 'test-leaderboard' in text:
+                            args = text.split(' ')
+                            params = {
+                                'job': job,
+                                'beginning': args[2]
+                            }
+                            self.query_and_response(TL_QUERY, params, [channel], 'testleaderboard.txt')
+                        elif 'build-range' in text:
+                            args = text.split(' ')
+                            builds = args[2]
+                            params = {
+                                'job': job,
+                                'build_low': builds[0],
+                                'build_high': builds[1]
+                            }
+                            self.query_and_response(BR_QUERY, params, [channel], 'buildrange.txt')
                 time.sleep(1)
             except (KeyboardInterrupt, SystemExit):
                 # Turning off the bot
-                self.cursor.close()
-                database.close()
                 break
             except IndexError as error:
                 print(error)
-                self.post_message(channel, 'Hint: Incorrect number of arguments\n\n' + ''.join(help_text))
+                self.post_message(channel, 'Incorrect number of arguments\n\n' + ''.join(help_text))
             except MySQLError as error:
                 print(error)
                 self.post_message(channel, 'Something went wrong with the query.')
             except Exception as error:
                 # Something unexpected went wrong
                 print(error)
-
+            sys.stdout.flush()
 
     def query_and_response(self, query, params, channels, filename):
         """
         Make a query then upload a text file with tables to the channels.
         """
-        self.cursor.execute(query, params)
-        headers = list(self.cursor.column_names)
-        table = list(self.cursor.fetchall())
+        try:
+            database = mysql.connector.connect(host=os.environ.get('dbhost', None),
+                                               user=os.environ.get('dbuser', None),
+                                               password=os.environ.get('dbpass', None),
+                                               database=os.environ.get('dbdb', None))
+            cursor = database.cursor()
+        except MySQLError as error:
+            print('Could not connect to database')
+            print(error)
+            self.post_message(channel, 'Something went wrong with the query.')
+            return
+
+        cursor.execute(query, params)
+        headers = list(cursor.column_names)
+        table = list(cursor.fetchall())
         self.client.api_call(
             'files.upload', channels=channels, content=tabulate(table, headers), filetype='text', filename=filename
         )
 
+        cursor.close()
+        database.close()
 
     def post_message(self, channel, text):
         """
@@ -228,4 +242,18 @@ class JenkinsBot(object):
 
 if __name__ == '__main__':
     jenkinsbot = JenkinsBot()
-    jenkinsbot.jenkins_session()
+    if jenkinsbot.connect() and len(sys.argv) > 1:
+        if sys.argv[1] == 'session':
+            jenkinsbot.session()
+        elif sys.argv[1] == 'pro-leaderboard':
+            jenkinsbot.query_and_response(PL_QUERY,
+                                    {'job': PRO},
+                                    ['#junit'],
+                                    'pro-leaderboard.txt'
+            )
+        elif sys.argv[1] == 'community-leaderboard':
+            jenkinsbot.query_and_response(PL_QUERY,
+                                    {'job': COMMUNITY},
+                                    ['#junit'],
+                                    'community-leaderboard.txt'
+            )
