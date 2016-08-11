7de5000c6de77e7802a2388feaec8a9aa0d7381a
Yiqun Zhang <zhangyiqun9164@gmail.com>
2016-08-11 10:46:32 -0400

    Fix test error

--- src/hsqldb19b3/org/hsqldb_voltpatches/FunctionSQL.java
+++ src/hsqldb19b3/org/hsqldb_voltpatches/FunctionSQL.java
@@ -2066,7 +2066,8 @@ public class FunctionSQL extends Expression {
             case FUNC_CURRENT_TIMESTAMP : {
                 int precision = DateTimeType.defaultTimestampFractionPrecision;
 
-                if (nodes[0] != null) {
+                // Because this function supports empty parameter list, the nodes array could be empty.
+                if (nodes.length > 0 && nodes[0] != null) {
                     precision = ((Number) nodes[0].valueData).intValue();
                 }
 
