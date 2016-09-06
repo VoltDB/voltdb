0c03cbf7699877a2cd5f997a778c69ad6519ced6
Huzefa Hakimi <hhakimi@voltdb.com>
2016-09-06 15:14:34 -0400

    add switch for kit version and add license headers

--- tools/docker/docker-entrypoint.sh
+++ tools/docker/docker-entrypoint.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
+
+############################################################################################
+# This file is part of VoltDB.
+# Copyright (C) 2008-2016 VoltDB Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+############################################################################################
+
 set -e
 
 function init() {
     if [ -f  ${CUSTOM_CONFIG} ]; then
         DEPLOYMENT=${CUSTOM_CONFIG}
-        #echo "using custom deployment ${DEPLOYMENT} (${CUSTOM_CONFIG})"
     else
         DEPLOYMENT=${DEFAULT_DEPLOYMENT}
-        #echo "using default deployment ${DEPLOYMENT} (${DEFAULT_DEPLOYMENT})"
-
     fi
-    #cat ${DEPLOYMENT}
+
     OPTIONS="-C ${DEPLOYMENT} -D ${DIRECTORY_SPEC}"
     echo "Run voltdb init $OPTIONS"
     bin/voltdb init ${OPTIONS}
--- tools/docker/docker_run.sh
+++ tools/docker/docker_run.sh
@@ -1,22 +1,77 @@
 #!/bin/bash
+############################################################################################
+# This file is part of VoltDB.
+# Copyright (C) 2008-2016 VoltDB Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Helper script for creating VOLTDB cluster using voltdb's docker community image
+#
+# Environment variables
+# HOSTCOUNT     Required        Specifies number of the node to initialize the cluster with
+# CONFIGFILE    Optional        Specifies the deployment file to be used instead of default
+#                               deployment packaged with voltdb community the image. Path to
+#                               deployment should be absolute
+# LICENSE       Optional        Specifies the license file. Path to the file should be absolute
+# STORAGE       Optional        Specifies the host location to use as the location for voltdbroot
+#                               for the cluster nodes. Path to the file should be absolute
+############################################################################################
 
 # script assumes there is docker image called voltdb. If image name is different, update DOCKER_IMAGE
+
+usage() {
+
+echo "Helper script for creating VOLTDB cluster using voltdb's docker community image
+
+Usage:
+    HOSTCOUNT=<number of nodes> CONFIGFILE=<config file absolute path> LICENSE=<license file absolute path> STORAGE=<config file absolute path> ./docker_run.sh
+
+Details:
+    Environment variables
+    HOSTCOUNT     Required        Specifies number of the node to initialize the cluster with
+    CONFIGFILE    Optional        Specifies the deployment file to be used instead of default
+                                  deployment packaged with voltdb community the image. Path to
+                                  deployment should be absolute
+    LICENSE       Optional        Specifies the license file. Path to the file should be absolute
+    STORAGE       Optional        Specifies the host location to use as the location for voltdbroot
+                                  for the cluster nodes. Path to the file should be absolute
+
+Usage examples:
+    HOSTCOUNT=3 CONFIGFILE=/home/foo/deployment_t.xml LICENSE=/home/foo/license.xml STORAGE=/home/foo/voltdbCluster/ ./docker_run.sh
+    HOSTCOUNT=3 ./docker_run.sh"
+exit 0;
+}
+
 function start() {
-    : ${HOSTCOUNT?"Need to set HOSTCOUNT"}
-    #: ${STORAGE?"Need to specify storage location"}
+
+    case ${HOSTCOUNT} in
+        (*[!0-9]*|'') usage;
+    esac
 
     # form the host list
     NODE="node"
-    for (( i=1; i<=$HOSTCOUNT; i++ ))
+    for (( i=1; i<=${HOSTCOUNT}; i++ ))
     do
         if [ -n "${NODE_LIST}" ]; then
-            NODE_LIST="$NODE_LIST,$NODE$i"
+            NODE_LIST="${NODE_LIST},${NODE}${i}"
         else
-            NODE_LIST="$NODE$i"
+            NODE_LIST="${NODE}${i}"
         fi
     done
 
-    echo "server list: " $NODE_LIST
+    echo "Host list: " ${NODE_LIST}
 
     # image to run
     DOCKER_IMAGE="voltdb/voltdb-community"
@@ -29,7 +84,7 @@ function start() {
     # set the docker subnet to voltLocalCluster
     if [ -z ${NETWORK} ]; then
         NETWORK=voltLocalCluster
-        #TODO: check if netowkr called voltLocalCluster exists, if doesn't create bridged network called voltLocalCluster
+        #TODO: check if network called voltLocalCluster exists, if doesn't create bridged network called voltLocalCluster
     fi
 
     echo "DOCKER file paths - voltdbroot: ${DOCKER_DATA_STORE};  custom config: ${DOCKER_CUSTOM_CONFIG}; licensefile: ${DOCKER_LICENSE}"
@@ -54,7 +109,7 @@ function start() {
         echo "Use license file: ${LICENSE}"
     fi
 
-    for (( i=1; i<=$HOSTCOUNT; i++ ))
+    for (( i=1; i<=${HOSTCOUNT}; i++ ))
     do
         echo ""
         MOUNT_POINTS="${CUSTOM_CONFIG_MOUNT} ${LICENSE_MOUNT}"
@@ -73,9 +128,9 @@ function start() {
             echo "Node ${NODE}${i}: mounted host directory ${HOST_LOCATION} to the container's voltdbroot at ${DOCKER_DATA_STORE}"
             MOUNT_POINTS="${MOUNT_POINTS} -v ${HOST_LOCATION}:${DOCKER_DATA_STORE}"
         fi
-        OPTIONS="-d -P -e HOST_COUNT="$HOSTCOUNT" -e HOSTS="$NODE_LIST" ${MOUNT_POINTS} --name=${NODE}${i} --network=${NETWORK} ${DOCKER_IMAGE}"
+
+        OPTIONS="-d -P -e HOST_COUNT="${HOSTCOUNT}" -e HOSTS="${NODE_LIST}" ${MOUNT_POINTS} --name=${NODE}${i} --network=${NETWORK} ${DOCKER_IMAGE}"
         echo "---- docker run ${OPTIONS} ----"
-        #docker run -d -P -e HOST_COUNT="$HOSTCOUNT" -e HOSTS="$NODE_LIST"  ${MOUNT_POINTS} --name=$NODE$i --network=voltLocalCluster $DOCKER_IMAGE
         docker run ${OPTIONS}
     done
 }
