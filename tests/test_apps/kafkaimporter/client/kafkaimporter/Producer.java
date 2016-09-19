037a39a044d732d9d8af928c917f94003df43505
Your Name <you@example.com>
2016-09-19 17:27:55 -0400

    ENG-11094 Add compression option to producer

--- tests/test_apps/kafkaimporter/client/kafkaimporter/Producer.java
+++ tests/test_apps/kafkaimporter/client/kafkaimporter/Producer.java
@@ -56,6 +56,7 @@ public class Producer extends Thread {
     long m_cycles;
     long m_rangemin;
     long m_rangemax;
+    String m_compression;
     boolean m_producerrunning = false;
     JSONObject m_json_obj;
 
@@ -71,6 +72,10 @@ public class Producer extends Thread {
         m_servers = config.brokers;
         m_rate = config.producerrate;
         m_cycletime = config.cycletime;
+        if (topicnum % 2 == 0)      // alternate compression strategies, if any
+            m_compression = config.compression;
+        else
+            m_compression = "none";
         m_pausetime = (int) (config.pausetime * Math.random()); // let each thread have its own wait time between 0 and pausetime
         m_rows = config.totalrows;
         long possiblecycles = m_rows / (m_rate * m_cycletime);
@@ -84,6 +89,7 @@ public class Producer extends Thread {
         m_json_obj = new JSONObject();
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, m_servers);
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, m_compression); // compression.type
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.ACKS_CONFIG, "all");
@@ -163,8 +169,12 @@ public class Producer extends Thread {
         @Option(desc = "Number of producer cycles")
         int cycles = 5;
 
+        @Option(desc = "Compression codec: none, gzip, snappy, lz4")
+        String compression = "gzip";
+
         @Override
         public void validate() {
+            final String compression_types = "none gzip snappy lz4";
             if (ntopics == 0) ntopics = 1;
             if (topic.length() <= 0) exitWithMessageAndUsage("Topic name required");
             if (brokers.length() < 0) exitWithMessageAndUsage("Broker list required");
@@ -173,6 +183,8 @@ public class Producer extends Thread {
             if (pausetime <= 0) exitWithMessageAndUsage("Pause time must be > 0");
             if (totalrows <= 0) exitWithMessageAndUsage("Total rows must be > 0");
             if (cycles <= 0) exitWithMessageAndUsage("Cycle count must be > 0");
+            if (! compression_types.contains(compression))
+                exitWithMessageAndUsage("Compression value unknown");
         }
     }
 
