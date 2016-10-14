1b6479d5856cdcd745db9ba8db61bd6468f83b59
Michael Teixeira <mteixeira@voltdb.com>
2016-10-14 14:46:09 -0400

    ENG-10878: use existing buffer for serialization

--- src/frontend/org/voltcore/utils/SSLDeferredSerialization.java
+++ src/frontend/org/voltcore/utils/SSLDeferredSerialization.java
@@ -17,45 +17,24 @@ public class SSLDeferredSerialization implements DeferredSerialization {
 
     @Override
     public ByteBuffer serialize(ByteBuffer dst) throws IOException {
-
-        ByteBuffer encryptInto = ByteBuffer.allocate(m_src.capacity() * 5);
+        int initialDstPosition = dst.position();
+        dst.position(dst.position() + 4);
+        ByteBuffer encryptInto = dst.slice();
         ByteBuffer allocated = encrypt(m_src, encryptInto);
-        if (allocated != null) {
-            int encryptedSize = allocated.remaining();
-            ByteBuffer full = ByteBuffer.allocate(encryptedSize + 4);
-            full.putInt(encryptedSize);
-            full.put(allocated);
-            full.flip();
-            return full;
+        if (allocated == null) {
+            int amountEncrypted = encryptInto.position();
+            dst.position(initialDstPosition);
+            dst.putInt(amountEncrypted);
+            dst.position(dst.position() + amountEncrypted);
         } else {
-            int encryptedSize = encryptInto.remaining();
-            ByteBuffer full = ByteBuffer.allocate(encryptedSize + 4);
-            full.putInt(encryptedSize);
-            full.put(encryptInto);
-            full.flip();
-            return full;
+            dst.position(initialDstPosition);
+            int amountEncrypted = allocated.position() - 4;
+            allocated.position(0);
+            allocated.putInt(amountEncrypted);
+            allocated.position(4 + amountEncrypted);
+            allocated.flip();
         }
-
-//        int initialDstPosition = dst.position();
-//        dst.position(dst.position() + 4);
-//        ByteBuffer encryptInto = dst.slice();
-//        ByteBuffer allocated = encrypt(m_src, encryptInto);
-//        if (allocated == null) {
-//            // encrypt used dst, put the length on dst.
-//            int amountEncrypted = encryptInto.position();
-//            dst.position(initialDstPosition);
-//            dst.putInt(amountEncrypted);
-//            dst.position(dst.position() + amountEncrypted);
-//        } else {
-//            // reset the position on dst
-//            dst.position(initialDstPosition);
-//            // encrypt used allocated, put the length on allocated.
-//            int newPosition = allocated.position();
-//            allocated.position(0);
-//            dst.putInt(newPosition - 4);
-//            dst.position(newPosition);
-//        }
-//        return allocated;
+        return allocated;
     }
 
     @Override
@@ -69,21 +48,12 @@ public class SSLDeferredSerialization implements DeferredSerialization {
     }
 
     private ByteBuffer encrypt(ByteBuffer src, ByteBuffer dst) throws IOException {
-        if (dst == null) {
-            ByteBuffer allocated = ByteBuffer.allocate((int) (src.capacity() * 1.2));
-            return encryptWithAllocated(src, allocated);
-        }
         SSLEngineResult result = m_sslEngine.wrap(src, dst);
         switch (result.getStatus()) {
             case OK:
-                // added this
-                dst.flip();
-
-
-
                 return null;
             case BUFFER_OVERFLOW:
-                ByteBuffer allocated = ByteBuffer.allocate((int) (dst.capacity() * 1.2));
+                ByteBuffer allocated = ByteBuffer.allocate(dst.remaining() * 2);
                 return encryptWithAllocated(src, allocated);
             case BUFFER_UNDERFLOW:
                 throw new IOException("Underflow on ssl wrap of buffer.");
@@ -97,19 +67,12 @@ public class SSLDeferredSerialization implements DeferredSerialization {
     private ByteBuffer encryptWithAllocated(ByteBuffer src, ByteBuffer dst) throws IOException {
         ByteBuffer allocated = dst;
         while (true) {
-
-            // TODO: also changed this:
-//            allocated.position(4);
-//            ByteBuffer encryptInto = allocated.slice();
-//            SSLEngineResult result = m_sslEngine.wrap(src, encryptInto);
-            SSLEngineResult result = m_sslEngine.wrap(src, allocated);
-
-
+            allocated.position(4);
+            ByteBuffer encryptInto = allocated.slice();
+            SSLEngineResult result = m_sslEngine.wrap(src, encryptInto);
             switch (result.getStatus()) {
                 case OK:
-                    // allocated.position(allocated.position() + 4);
-                    // added flip
-                    allocated.flip();
+                    allocated.position(encryptInto.position() + 4);
                     return allocated;
                 case BUFFER_OVERFLOW:
                     allocated = ByteBuffer.allocate(allocated.capacity() * 2);
@@ -121,7 +84,6 @@ public class SSLDeferredSerialization implements DeferredSerialization {
                 default:
                     throw new IOException("Unexpected SSLEngineResult.Status");
             }
-
         }
     }
 }
