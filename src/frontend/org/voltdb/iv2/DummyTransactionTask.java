d1f8d20e7eb3160a34f7efe4bbb7f881d4c1dd02
Xin Jia <xinjiacs@gmail.com>
2016-10-17 09:23:59 -0400

    ENG-11110: Add a dummy transaction task message at the end of SP lead… (#3978)
    * ENG-11110: Add a dummy transaction task message at the end of SP leader promotion. The request will be duplicated to replicas like a SP write. It does not do anything useful work, but as a signal to flush everyone's transaction queue before the newly promoted leader starting to run new transactions.
    
    * ENG-11110: give dummy transaction an unique id as command log assuming it advancing always
    
    * ENG-11110: Fix to deliver the dummy transaction task as the first message after leader promotion, by embedded in mailbox synchronized function
    
    * ENG-11110: Try counting max schedule sphandle by transactions (multi-fragments MP txn counts as 1 sphandle)

---
+++ src/frontend/org/voltdb/iv2/DummyTransactionTask.java
@@ -0,0 +1,66 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2016 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.voltdb.iv2;
+
+import java.io.IOException;
+
+import org.voltcore.messaging.Mailbox;
+import org.voltdb.SiteProcedureConnection;
+import org.voltdb.dtxn.TransactionState;
+import org.voltdb.messaging.DummyTransactionResponseMessage;
+import org.voltdb.rejoin.TaskLog;
+
+/**
+ * This dummy transaction task is a SP read only transaction that will be put into transaction task queue.
+ * And it will be duplicated to followers as well. This task acts as a synchronization transaction on all
+ * partition replicas like normal SP write transaction.
+ */
+public class DummyTransactionTask extends TransactionTask {
+    final Mailbox m_initiator;
+
+    public DummyTransactionTask(Mailbox initiator,
+            TransactionState txnState, TransactionTaskQueue queue) {
+        super(txnState, queue);
+        m_initiator = initiator;
+    }
+
+    private void generateDummyResponse() {
+        doCommonSPICompleteActions();
+        DummyTransactionResponseMessage response = new DummyTransactionResponseMessage(this);
+        response.m_sourceHSId = m_initiator.getHSId();
+        m_initiator.deliver(response);
+    }
+
+    @Override
+    public void run(SiteProcedureConnection siteConnection) {
+        generateDummyResponse();
+    }
+
+    @Override
+    public void runFromTaskLog(SiteProcedureConnection siteConnection) {
+        generateDummyResponse();
+    }
+
+    @Override
+    public void runForRejoin(SiteProcedureConnection siteConnection, TaskLog rejoinTaskLog) throws IOException {
+        generateDummyResponse();
+    }
+
+    public long getSPIHSId() {
+        return m_txnState.initiatorHSId;
+    }
+}
