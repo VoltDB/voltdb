8c632ab0eddc3a097914b6bf0892f0956739cc0e
Stefano Santoro <ssantoro@voltdb.com>
2016-10-18 16:06:26 -0400

    ENG-11130 recover using shutdown snapshot

--- src/frontend/org/voltdb/Iv2TransactionCreator.java
+++ src/frontend/org/voltdb/Iv2TransactionCreator.java
@@ -18,6 +18,7 @@
 package org.voltdb;
 
 import org.voltcore.network.Connection;
+import org.voltdb.AuthSystem.AuthUser;
 import org.voltdb.dtxn.TransactionCreator;
 
 /**
@@ -66,6 +67,30 @@ public class Iv2TransactionCreator implements TransactionCreator
                 messageSize,
                 nowNanos,
                 true);
+    }
+
+    @Override
+    public ClientResponseImpl dispatch(StoredProcedureInvocation invocation,
+            Connection connection, boolean isAdmin) {
+
+        final InvocationClientHandler handler = new InvocationClientHandler() {
+
+            final boolean adminFlag = isAdmin;
+            final long connectionId = connection.connectionId();
+
+            @Override
+            public boolean isAdmin() {
+                return adminFlag;
+            }
+
+            @Override
+            public long connectionId() {
+                return connectionId;
+            }
+        };
+
+        AuthUser admin = m_ci.getCatalogContext().authSystem.getInternalAdminUser();
+        return m_ci.getDispatcher().dispatch(invocation, handler, connection, admin);
     }
 
     @Override
