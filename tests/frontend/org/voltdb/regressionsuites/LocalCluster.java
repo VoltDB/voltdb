b344262cde8023f2515f56b9ef2758415287d703
Stefano Santoro <ssantoro@voltdb.com>
2016-10-20 12:21:48 -0400

    ENG-11130 fixed guava related imports

--- tests/frontend/org/voltdb/regressionsuites/LocalCluster.java
+++ tests/frontend/org/voltdb/regressionsuites/LocalCluster.java
@@ -45,8 +45,8 @@ import org.voltdb.utils.CommandLine;
 import org.voltdb.utils.MiscUtils;
 import org.voltdb.utils.VoltFile;
 
-import com.google.common.collect.Maps;
 import com.google_voltpatches.common.collect.ImmutableSortedSet;
+import com.google_voltpatches.common.collect.Maps;
 
 /**
  * Implementation of a VoltServerConfig for a multi-process
