a0a6f98bef43cb0507efe363c9a9a9b086398bec
Michael Teixeira <mteixeira@voltdb.com>
2016-11-01 14:20:38 -0400

    ENG-10878: minor cleanup

--- examples/voter/client/voter/AsyncBenchmark.java
+++ examples/voter/client/voter/AsyncBenchmark.java
@@ -465,53 +465,6 @@ public class AsyncBenchmark {
     }
 
     /**
-     * Creates the key managers required to initiate the {@link SSLContext}, using a JKS keystore as an input.
-     *
-     * @param filepath - the path to the JKS keystore.
-     * @param keystorePassword - the keystore's password.
-     * @param keyPassword - the key's passsword.
-     * @return {@link KeyManager} array that will be used to initiate the {@link SSLContext}.
-     * @throws Exception
-     */
-    protected static KeyManager[] createKeyManagers(String filepath, String keystorePassword, String keyPassword) throws Exception {
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        InputStream keyStoreIS = new FileInputStream(filepath);
-        try {
-            keyStore.load(keyStoreIS, keystorePassword.toCharArray());
-        } finally {
-            if (keyStoreIS != null) {
-                keyStoreIS.close();
-            }
-        }
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-        kmf.init(keyStore, keyPassword.toCharArray());
-        return kmf.getKeyManagers();
-    }
-
-    /**
-     * Creates the trust managers required to initiate the {@link SSLContext}, using a JKS keystore as an input.
-     *
-     * @param filepath - the path to the JKS keystore.
-     * @param keystorePassword - the keystore's password.
-     * @return {@link TrustManager} array, that will be used to initiate the {@link SSLContext}.
-     * @throws Exception
-     */
-    protected static TrustManager[] createTrustManagers(String filepath, String keystorePassword) throws Exception {
-        KeyStore trustStore = KeyStore.getInstance("JKS");
-        InputStream trustStoreIS = new FileInputStream(filepath);
-        try {
-            trustStore.load(trustStoreIS, keystorePassword.toCharArray());
-        } finally {
-            if (trustStoreIS != null) {
-                trustStoreIS.close();
-            }
-        }
-        TrustManagerFactory trustFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        trustFactory.init(trustStore);
-        return trustFactory.getTrustManagers();
-    }
-
-    /**
      * Main routine creates a benchmark instance and kicks off the run method.
      *
      * @param args Command line arguments.
