5e70cf54662b47c93c31aa77e9b1cc93af3eab4e
Ruth Morgenstein <rmorgenstein@voltdb.com>
2016-11-04 16:04:50 -0400

    Allow 4.6.11.2 to upgrade from 4.6.11.1
    Fix regex match.

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -170,8 +170,8 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback
     volatile CatalogContext m_catalogContext;
     private String m_buildString;
     static final String m_defaultVersionString = "4.6.11.2";
-    // Allow V4.6.11.1 to hot upgrade V4.6.11
-    static final String m_defaultHotfixableRegexPattern = "^\\Q4.6.11.2\\E\\z";
+    // Allow V4.6.11.2 to hot upgrade V4.6.11.1
+    static final String m_defaultHotfixableRegexPattern = "^4.6\\.11\\.(1|2)?$";
     // these next two are non-static because they can be overrriden on the CLI for test
     private String m_versionString = m_defaultVersionString;
     private String m_hotfixableRegexPattern = m_defaultHotfixableRegexPattern;
