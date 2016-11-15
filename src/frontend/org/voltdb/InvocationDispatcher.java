2444e988f12724d12cad827ae819d1946527b635
Stefano Santoro <stefano@santoro.org>
2016-11-15 16:33:10 -0500

    ENG-11475 determine local replicas first before client interface accepts connections (#4069)

--- src/frontend/org/voltdb/InvocationDispatcher.java
+++ src/frontend/org/voltdb/InvocationDispatcher.java
@@ -253,8 +253,8 @@ public final class InvocationDispatcher {
      * Populate the map in the background and it will be used to route
      * requests to local replicas once the info is available
      */
-    public void asynchronouslyDetermineLocalReplicas() {
-        VoltDB.instance().getSES(false).submit(new Runnable() {
+    public Future<?> asynchronouslyDetermineLocalReplicas() {
+        return VoltDB.instance().getSES(false).submit(new Runnable() {
 
             @Override
             public void run() {
