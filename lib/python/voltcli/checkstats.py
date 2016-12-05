9ab404ef6229b3ed7482bd5de8d6e8f608150698
Zeeman He <zhe@voltdb.com>
2016-12-05 13:14:33 -0500

    ENG-11434 added comments

--- lib/python/voltcli/checkstats.py
+++ lib/python/voltcli/checkstats.py
@@ -137,6 +137,8 @@ def dr_producer_stats(runner, partition_min_host, partition_min, partition_max):
         else:
             last_acked = r[9]
 
+        #The partition is in the initial state. no stats updated yet-no transactions are queued and acknowledged 
+        #thus no need to track. 
         if last_queued == -1 and last_acked == -1:
             continue
 
