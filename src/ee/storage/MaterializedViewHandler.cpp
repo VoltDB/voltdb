ec67d7de4207b2aebf2a3813deab7dccf39a4470
Yiqun Zhang <yzhang@voltdb.com>
2016-12-12 14:46:45 -0500

    ENG-11573 populate joined table view on snapshot restore (#4113)
    * Refactor catchUpWithExistingData
    * Add inserted tuple to delta table on snapshot restore.
    * Put insertTupleIntoDeltaTable into insertTupleCommon

--- src/ee/storage/MaterializedViewHandler.cpp
+++ src/ee/storage/MaterializedViewHandler.cpp
@@ -166,9 +166,9 @@ namespace voltdb {
     // to catch up with the existing data.
     //TODO: *non-grouped views could instead set up a hard-coded initial
     // row as they do in the single-table case to avoid querying empty tables.
-    void MaterializedViewHandler::catchUpWithExistingData(VoltDBEngine *engine, bool fallible) {
+    void MaterializedViewHandler::catchUpWithExistingData(bool fallible) {
         ExecutorContext* ec = ExecutorContext::getExecutorContext();
-        UniqueTempTableResult viewContent = engine->executePlanFragment(m_createQueryExecutorVector.get());
+        UniqueTempTableResult viewContent = ec->getEngine()->executePlanFragment(m_createQueryExecutorVector.get());
         TableIterator ti = viewContent->iterator();
         TableTuple tuple(viewContent->schema());
         while (ti.next(tuple)) {
