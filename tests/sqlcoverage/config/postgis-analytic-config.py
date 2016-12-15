f6b9d544a7b91e5bb189bc0dbacdb54c4a2c4f5a
jcrawford <jcrawford@voltdb.com>
2016-12-15 15:52:25 -0500

    Added new, smaller config files, to be used by faster, "split" versions of the various sqlCoverage Jenkins jobs, in order to run in parallel: default-basic-config.py, default-advanced-config.py, default-advanced-mv-subq-config.py, default-regression-config.py, extended-config.py, extended-subq-joins-config.py, matview-config.py, matview-int-config.py, postgis-analytic-config.py, postgis-geo-config.py, postgis-upsert-jmv-config.py.
    Also, renamed the geo test suite files, to be more consistent with other
    test suite file names: basic-geo-point.sql (was geo-basic-point.sql),
    basic-geo-polygon.sql (was geo-basic-polygon.sql),
    advanced-geo-point.sql (was geo-advanced-point.sql),
    advanced-geo-polygon.sql (was geo-advanced-polygon.sql),
    geo-functions.sql (was geo-basic.sql).

---
+++ tests/sqlcoverage/config/postgis-analytic-config.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+# This file is part of VoltDB.
+# Copyright (C) 2008-2016 VoltDB Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+
+# This file contains the "analytic" tests, which are a subset of those run
+# by the "postgis" sqlcoverage Jenkins job.
+
+# These tests only work when run against PostgreSQL, not HSQL, and some require
+# PostGIS. Hence, to run them on the command line, use something like the following:
+#     ant -Dpostgis=1 -Dsql_coverage_suite=postgis-analytic-config.py sqlcoverage
+{
+    # Tests of SQL Analytic Windowed Functions (e.g. RANK, DENSE_RANK)
+    "analytic": {"schema": "schema.py",
+                 "ddl": "DDL.sql",
+                 "template": "analytic.sql",
+                 "normalizer": "normalizer.py"},
+    "analytic-ints": {"schema": "int-schema.py",
+                      "ddl": "int-DDL.sql",
+                      "template": "analytic-ints.sql",
+                      "normalizer": "normalizer.py"},
+    "analytic-strings": {"schema": "strings-schema.py",
+                         "ddl": "strings-DDL.sql",
+                         "template": "analytic-strings.sql",
+                         "normalizer": "normalizer.py"},
+    "analytic-timestamp": {"schema": "timestamp-schema.py",
+                           "ddl": "DDL.sql",
+                           "template": "analytic-timestamp.sql",
+                           "normalizer": "normalizer.py"},
+    "analytic-decimal": {"schema": "decimal-schema.py",
+                         "ddl": "DDL.sql",
+                         "template": "analytic-decimal.sql",
+                         "normalizer": "normalizer.py"},
+    "analytic-geo-point": {"schema": "geo-point-schema.py",
+                           "ddl": "geo-DDL.sql",
+                           "template": "analytic-geo-point.sql",
+                           "normalizer": "normalizer.py"},
+    "analytic-geo-polygon": {"schema": "geo-polygon-schema.py",
+                             "ddl": "geo-DDL.sql",
+                             "template": "analytic-geo-polygon.sql",
+                             "normalizer": "normalizer.py"},
+
+    # May want to add something like these, someday:
+#     "analytic-float": {"schema": "float-schema.py",
+#                        "ddl": "float-DDL.sql",
+#                        "template": "analytic-float.sql",
+#                        "normalizer": "normalizer.py"},
+#     "analytic-varbinary": {"schema": "int-schema.py",
+#                            "ddl": "index-DDL.sql",
+#                            "template": "analytic-varbinary.sql",
+#                            "normalizer": "normalizer.py"},
+}
