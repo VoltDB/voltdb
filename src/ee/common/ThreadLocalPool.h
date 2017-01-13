6f5d54330343e0a72311c2d594118c6d5d8b68cc
Bill White <bill.white@voltdb.com>
2017-01-12 16:52:17 -0800

    Fix several problems with strings, and with aliases in order by lists.
    The SPACE sql function did not check for arguments being too large.  As
    a result we would try to allocate a large amount of string space, which
    caused allocator failures.
    
    These same queries showed up problems in the planner with aliases in
    order by lists.  If a select list element is a constant, and there is a
    reference to it as an alias in the order by list, we didn't expect it
    and we threw an error. There are also cases where data types for select
    list aliases are not set in the Volt expression structures when we
    expect them to be.  This can cause NPEs when converting from strings to
    BIGINT values.  These have been fixed.

--- src/ee/common/ThreadLocalPool.h
+++ src/ee/common/ThreadLocalPool.h
@@ -48,7 +48,10 @@ public:
         Sized(int32_t requested_size) : m_size(requested_size) { }
     };
 
-    static const int POOLED_MAX_VALUE_LENGTH;
+    // This needs to be >= the VoltType.MAX_VALUE_LENGTH defined in java, currently 1048576.
+    // The rationale for making it any larger would be to allow calculating wider "temp"
+    // values for use in situations where they are not being stored as column values.
+    static const int POOLED_MAX_VALUE_LENGTH = 1024 * 1024;
 
     /**
      * Allocate space from a page of objects of the requested size.
