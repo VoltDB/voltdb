688ab7d9d42b19d085851576a25e7b13b2f6f196
Anish Khanzode <akhanzode@voltdb.com>
2017-01-21 08:08:40 -0500

    ENG-11856: Fix delete zk node.

--- src/frontend/org/voltdb/export/ExportGeneration.java
+++ src/frontend/org/voltdb/export/ExportGeneration.java
@@ -751,10 +751,8 @@ public class ExportGeneration implements Generation {
         if (m_mbox != null && messenger != null) {
             for (Integer partition : m_dataSourcesByPartition.keySet()) {
                 final String partitionDN =  m_mailboxesZKPath + "/" + partition;
-                final String partitionNodeDN =  m_mailboxesZKPath + "/" + partition + "/" + m_mbox.getHSId();
                 try {
-                    messenger.getZK().delete(partitionNodeDN, -1);
-                    messenger.getZK().delete(partitionDN, -1);
+                    ZKUtil.deleteRecursively(messenger.getZK(), partitionDN);
                 } catch (InterruptedException | KeeperException ex) {
                     exportLog.warn("Failed to delete generation partition mailboxes from zookeeper.", ex);
                 }
