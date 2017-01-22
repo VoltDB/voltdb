180e30b6562b8974371a1ccdcf80bafb82946709
Anish Khanzode <akhanzode@voltdb.com>
2017-01-22 10:32:06 -0500

    ENG-11856: Hold off deleting zk nodes until mastership change is addressed. (#4217)

--- src/frontend/org/voltdb/export/ExportGeneration.java
+++ src/frontend/org/voltdb/export/ExportGeneration.java
@@ -749,17 +749,8 @@ public class ExportGeneration implements Generation {
         shutdown = true;
         //We need messenger NULL guard for tests.
         if (m_mbox != null && messenger != null) {
-            for (Integer partition : m_dataSourcesByPartition.keySet()) {
-                final String partitionDN =  m_mailboxesZKPath + "/" + partition;
-                try {
-                    ZKUtil.deleteRecursively(messenger.getZK(), partitionDN);
-                } catch (InterruptedException | KeeperException ex) {
-                    exportLog.warn("Failed to delete generation partition mailboxes from zookeeper.", ex);
-                }
-            }
             messenger.removeMailbox(m_mbox);
         }
-        m_dataSourcesByPartition.clear();
         m_onAllSourcesDrained = null;
     }
 
