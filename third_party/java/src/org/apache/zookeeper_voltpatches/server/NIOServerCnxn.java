0fd8daf5cac4fd69b808cdfc20132c8116491152
Anish Khanzode <akhanzode@voltdb.com>
2017-01-26 14:53:23 -0500

    ENG-9258: messaging changes.

--- third_party/java/src/org/apache/zookeeper_voltpatches/server/NIOServerCnxn.java
+++ third_party/java/src/org/apache/zookeeper_voltpatches/server/NIOServerCnxn.java
@@ -455,7 +455,7 @@ public class NIOServerCnxn implements Watcher, ServerCnxn {
             }
 
         } catch(Exception e) {
-            LOG.warn("Unexpected Exception, server probably doing graceful shutdown: " + e.getMessage());
+            LOG.warn("Unexpected Exception: " + e.getMessage());
         }
     }
 
