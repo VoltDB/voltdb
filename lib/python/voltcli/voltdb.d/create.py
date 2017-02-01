d8e779c7bf9b98e2ae01cae3828a86b71a526ef7
Anish Khanzode <akhanzode@voltdb.com>
2017-02-01 17:27:29 -0500

    ENG-11917: Hide help for legacy verbs, and update help.

--- lib/python/voltcli/voltdb.d/create.py
+++ lib/python/voltcli/voltdb.d/create.py
@@ -30,7 +30,8 @@
                            'start replica cluster (deprecated, please use role="replica" in the deployment file)',
                            default = False),
     ),
-    description = 'WARNING: The start is deprecated. Please use INIT and START. Start a new, empty database.'
+    description = 'WARNING: The create is deprecated. Please use INIT and START. Start a new, empty database.',
+    hideverb=True
 )
 def create(runner):
     runner.go()
