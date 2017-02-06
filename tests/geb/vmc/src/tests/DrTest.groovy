e0a71be8c96f78645ef529e724c9a2175d349c40
anil <anrai@deerwalk.com>
2017-02-06 14:23:03 +0545

    Created Dr Test and Page

---
+++ tests/geb/vmc/src/tests/DrTest.groovy
@@ -0,0 +1,67 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2017 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package vmcTest.tests
+
+import vmcTest.pages.*
+import geb.Page.*
+
+class DrTest extends TestBase {
+
+    def setup() { // called before each test
+        int count = 0
+
+        while(count<2) {
+            count ++
+            try {
+                setup: 'Open VMC page'
+                to VoltDBManagementCenterPage
+                expect: 'to be on VMC page'
+                at VoltDBManagementCenterPage
+
+                when: 'click the DR link (if needed)'
+                page.openDrPage()
+                then: 'should be on DR page'
+                at DrPage
+
+                break
+            } catch (org.openqa.selenium.ElementNotVisibleException e) {
+                println("ElementNotVisibleException: Unable to Start the test")
+                println("Retrying")
+                try {
+                    waitFor(waitTime) { 1 == 0 }
+                } catch (geb.waiting.WaitTimeoutException exception) {
+
+                }
+            }
+        }
+    }
+
+    def hello() {
+        when:
+        println("hello")
+        println("hello")
+        then:
+        println("hello0")
+    }
+}
