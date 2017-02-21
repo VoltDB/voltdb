21e03576e6440cb7282057d1bce165515a1fdb5e
Ruth Morgenstein <rmorgenstein@voltdb.com>
2017-02-21 17:08:00 -0500

    ENG-11837. Review comments + some fixes, additions - Took Paul's review comments. - Added in TPS calculation. - Fixed TPS calculation to round, not truncate

--- src/frontend/org/voltdb/utils/LatencyLogger.java
+++ src/frontend/org/voltdb/utils/LatencyLogger.java
@@ -51,125 +51,125 @@ public class LatencyLogger {
         final String hostname;
         if (args.length == 1) {
             readHistogramFromFile(args[0]);
-        } else {
-            if (args.length != 3) {
-                if (args.length == 4) {
-                    hostname = args[3];
-                } else {
-                    System.out.println(usage());
-                    return;
-                }
+            return;
+        }
+        if (args.length != 3) {
+            if (args.length == 4) {
+                hostname = args[3];
             } else {
-                hostname = args[0];
-            }
-
-            final String server = args[0];
-            int dur = 0;
-            try {
-                dur = Integer.valueOf(args[2]);
-                if (dur < 1) {
-                    throw new NumberFormatException();
-                }
-            } catch (NumberFormatException e) {
-                System.out.println(
-                        "reportIntervalSeconds should be greater than or equal to 1");
                 System.out.println(usage());
-                System.exit(0);
+                return;
             }
-            final int duration = dur;
-
-            final Client c = ClientFactory.createClient();
-            int port = 0;
-            try {
-                port = Integer.valueOf(args[1]);
-            } catch (NumberFormatException e) {
-                System.out.println("Failed to parse port number.");
-                System.out.println("Usage server port reportIntervalSeconds");
-                System.exit(0);
+        } else {
+            hostname = args[0];
+        }
+        final String server = args[0];
+        int dur = 0;
+        try {
+            dur = Integer.valueOf(args[2]);
+            if (dur < 1) {
+                throw new NumberFormatException();
             }
-            System.out.println("Connecting to " + server + " port " + port);
-            c.createConnection(args[0], port);
+        } catch (NumberFormatException e) {
+            System.out.println(
+                    "reportIntervalSeconds should be greater than or equal to 1");
+            System.out.println(usage());
+            System.exit(0);
+        }
+        final int duration = dur;
 
-            System.out.printf(
-                    "%12s, %10s, %10s, %10s, %10s, %10s, %10s, %10s\n",
-                    "TIMESTAMP", "COUNT", "TPS", "95", "99", "99.9", "99.99",
-                    "99.999");
+        final Client c = ClientFactory.createClient();
+        int port = 0;
+        try {
+            port = Integer.valueOf(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Failed to parse port number.");
+            System.out.println("Usage server port reportIntervalSeconds");
+            System.exit(0);
+        }
+        System.out.println("Connecting to " + server + " port " + port);
+        c.createConnection(args[0], port);
 
-            final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss:SSS");
+        System.out.printf(
+                "%12s, %10s, %10s, %10s, %10s, %10s, %10s, %10s\n",
+                "TIMESTAMP", "COUNT", "TPS", "95", "99", "99.9", "99.99",
+                "99.999");
 
-            ScheduledExecutorService ses = Executors
-                    .newSingleThreadScheduledExecutor();
-            ses.scheduleAtFixedRate(new Runnable() {
-                @Override
-                public void run() {
-                    VoltTable table = null;
-                    try {
-                        table = c.callProcedure("@Statistics",
-                                "LATENCY_HISTOGRAM", 0).getResults()[0];
-                    } catch (IOException | ProcCallException e) {
-                        System.out.println("Failed to get statistics:");
-                        e.printStackTrace();
+        final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+
+        ScheduledExecutorService ses = Executors
+                .newSingleThreadScheduledExecutor();
+       ses.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                VoltTable table = null;
+                try {
+                    table = c.callProcedure("@Statistics",
+                            "LATENCY_HISTOGRAM", 0).getResults()[0];
+                } catch (IOException | ProcCallException e) {
+                    System.out.println("Failed to get statistics:");
+                    e.printStackTrace();
+                    System.exit(0);
+                }
+                List<String> hostnames = new ArrayList<String>();
+                String tableHostname = "";
+                while (!hostname.equalsIgnoreCase(tableHostname)) {
+                    if (!table.advanceRow()) {
+                        System.out.println("Server host name " + server
+                                + " not found. Valid host names are "
+                                + hostnames.toString());
                         System.exit(0);
                     }
-                    List<String> hostnames = new ArrayList<String>();
-                    String tableHostname = "";
-                    while (!hostname.equalsIgnoreCase(tableHostname)) {
-                        if (!table.advanceRow()) {
-                            System.out.println("Server host name " + server
-                                    + " not found. Valid host names are "
-                                    + hostnames.toString());
-                            System.exit(0);
-                        }
-                        tableHostname = table.getString(2);
-                        hostnames.add(tableHostname);
-                    }
-                    Date now = new Date(table.getLong(0));
-                    Histogram newHistogram = AbstractHistogram
-                            .fromCompressedBytes(table.getVarbinary(4),
-                                    CompressionStrategySnappy.INSTANCE);
-                    Histogram diffHistogram;
-                    if (m_histogramData == null) {
-                        diffHistogram = newHistogram;
-                    } else {
-                        diffHistogram = Histogram.diff(newHistogram,
-                                m_histogramData);
-                    }
-                    long totalCount = diffHistogram.getTotalCount();
-                    if (totalCount > 0) {
-                        System.out.printf(
-                                "%12s, %10d, %10d, %8.2fms, %8.2fms, %8.2fms, %8.2fms, %8.2fms\n",
-                                sdf.format(now), totalCount,
-                                (totalCount / duration),
-                                (diffHistogram.getValueAtPercentile(95.0D)
-                                        / 1000.0D),
-                                (diffHistogram.getValueAtPercentile(99)
-                                        / 1000.0D),
-                                (diffHistogram.getValueAtPercentile(99.9)
-                                        / 1000.0D),
-                                (diffHistogram.getValueAtPercentile(99.99)
-                                        / 1000.0D),
-                                (diffHistogram.getValueAtPercentile(99.999)
-                                        / 1000.0D));
-                    } else {
-                        System.out.printf(
-                                "%12s, %10d, %10d, %8.2fms, %8.2fms, %8.2fms, %8.2fms, %8.2fms\n",
-                                sdf.format(now), totalCount, 0, 0D, 0D, 0D, 0D,
-                                0D);
-                    }
-
-                    m_histogramData = AbstractHistogram.fromCompressedBytes(
-                            table.getVarbinary(4),
-                            CompressionStrategySnappy.INSTANCE);
-                    ;
+                    tableHostname = table.getString(2);
+                    hostnames.add(tableHostname);
                 }
-            }, 0, duration, TimeUnit.SECONDS);
-        }
+                Date now = new Date(table.getLong(0));
+                Histogram newHistogram = AbstractHistogram
+                        .fromCompressedBytes(table.getVarbinary(4),
+                                CompressionStrategySnappy.INSTANCE);
+                Histogram diffHistogram;
+                if (m_histogramData == null) {
+                    diffHistogram = newHistogram;
+                } else {
+                    diffHistogram = Histogram.diff(newHistogram,
+                            m_histogramData);
+                }
+                long totalCount = diffHistogram.getTotalCount();
+                if (totalCount > 0) {
+                    System.out.printf(
+                            "%12s, %10d, %10.0f, %8.2fms, %8.2fms, %8.2fms, %8.2fms, %8.2fms\n",
+                            sdf.format(now), totalCount,
+                            ((double) totalCount / duration),
+                            (diffHistogram.getValueAtPercentile(95.0D)
+                                    / 1000.0D),
+                            (diffHistogram.getValueAtPercentile(99)
+                                    / 1000.0D),
+                            (diffHistogram.getValueAtPercentile(99.9)
+                                    / 1000.0D),
+                            (diffHistogram.getValueAtPercentile(99.99)
+                                    / 1000.0D),
+                            (diffHistogram.getValueAtPercentile(99.999)
+                                    / 1000.0D));
+                } else {
+                    System.out.printf(
+                            "%12s, %10d, %10d, %8.2fms, %8.2fms, %8.2fms, %8.2fms, %8.2fms\n",
+                            sdf.format(now), totalCount, 0, 0D, 0D, 0D, 0D,
+                            0D);
+                }
+
+                m_histogramData = AbstractHistogram.fromCompressedBytes(
+                        table.getVarbinary(4),
+                        CompressionStrategySnappy.INSTANCE);
+                ;
+            }
+        }, 0, duration, TimeUnit.SECONDS);
 
     }
 
     public static void readHistogramFromFile(String filename) {
         System.out.println("Reading histograms from " + filename);
         Hashtable<String, Histogram> histograms = new Hashtable<String, Histogram>();
+        Hashtable<String, Long> timestamps = new Hashtable<String, Long>();
 
         System.out.printf(
                 "%23s, %32s, %10s, %10s, %10s, %10s, %10s, %10s, %10s\n",
@@ -198,27 +198,33 @@ public class LatencyLogger {
 
                 // Look for a prior histogram for this host
                 Histogram priorHistogram = histograms.get(host_name);
+                Long priorTime = timestamps.get(host_name);
                 Histogram diffHistogram;
+                long diffTime;
                 if (priorHistogram == null) {
                     diffHistogram = newHistogram;
+                    diffTime = 1;
                 } else {
                     diffHistogram = Histogram.diff(newHistogram,
                             priorHistogram);
+
+                    diffTime = (Long.parseLong(time) - priorTime)/1000;
                 }
                 // Set the most recent histogram as the prior histogram for this hostname.
                 histograms.put(host_name, newHistogram);
+                timestamps.put(host_name, Long.parseLong(time));
 
                 long totalCount = diffHistogram.getTotalCount();
                 if (totalCount > 0) {
                     System.out.printf(
-                            "%23s, %32s, %10d, %10d, %8.2fms, %8.2fms, %8.2fms, %8.2fms, %8.2fms\n",
-                            friendly_ts, host_name, totalCount, 0, // (totalCount
-                                                                   // /
-                                                                   // duration),
+                            "%23s, %32s, %10d, %10.0f, %8.2fms, %8.2fms, %8.2fms, %8.2fms, %8.2fms\n",
+                            friendly_ts, host_name, totalCount,  (double) totalCount / diffTime,
                             (diffHistogram.getValueAtPercentile(95.0D)
                                     / 1000.0D),
-                            (diffHistogram.getValueAtPercentile(99) / 1000.0D),
-                            (diffHistogram.getValueAtPercentile(99.9) / 1000.0D),
+                            (diffHistogram.getValueAtPercentile(99)
+                                    / 1000.0D),
+                            (diffHistogram.getValueAtPercentile(99.9)
+                                    / 1000.0D),
                             (diffHistogram.getValueAtPercentile(99.99)
                                     / 1000.0D),
                             (diffHistogram.getValueAtPercentile(99.999)
@@ -240,7 +246,7 @@ public class LatencyLogger {
             System.err.println(e.getMessage());
             System.exit(-1);
         }
-        System.out.println("Number of Histograms: " + histograms.size());
+        //System.out.println("Number of Histograms: " + histograms.size());
 
     }
 
