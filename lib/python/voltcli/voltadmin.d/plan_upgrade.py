d64f75c80df12d54a141fcc27049a22bad8a0cd1
Yang Lu <ylu@voltdb.com>
2017-02-22 15:06:32 -0500

    ENG-11806, allow 'voltadmin stop' command to stop node with specified internal port.
    Change-Id: Ife91516f05200a8967550062358ed39e2036bb4f

--- lib/python/voltcli/voltadmin.d/plan_upgrade.py
+++ lib/python/voltcli/voltadmin.d/plan_upgrade.py
@@ -18,6 +18,7 @@
 from voltcli.hostinfo import Host
 from voltcli.hostinfo import Hosts
 from xml.etree import ElementTree
+from collections import defaultdict
 import os.path
 
 # TODO: change to actual release version when in-service upgrade is released
@@ -28,27 +29,28 @@ RELEASE_MINOR_VERSION = 0
     bundles = VOLT.AdminBundle(),
     description = "Generate a checklist before performing in-service upgrade.",
     arguments=(
-            VOLT.PathArgument('new_kit', 'path to new VoltDB kit directory', absolute=True, optional=False),
-            VOLT.PathArgument('new_root', 'path to new VoltDB root directory', absolute=True, optional=False)
+            VOLT.PathArgument('newKit', 'path to new VoltDB kit directory', absolute=True, optional=False),
+            VOLT.PathArgument('newRoot', 'path to the parent of new VoltDB root directory', absolute=True, optional=False),
+            VOLT.StringArgument('newNode', 'hostname or IP of the extra node', optional=True)
     )
 )
 
 def plan_upgrade(runner):
     
-    clusterSize = basicCheck(runner)
+    hosts, kfactor = basicCheck(runner)
     
     # first check the existence of root path on all the existing nodes
     # 1. how to know the nodes information?
     # 2. how to check path on every node? ( a new system pro0cedure? ) 
     # 3. how to check the new root is valid?
     # 4. does the directory contain .initialized file? If it has, then this is a initialized root.
-    # runner.call_proc('@UpgradeCheck',[VOLT.FastSerializer.VOLTTYPE_STRING], [runner.opts.new_kit, runner.opts.new_root])
+    # runner.call_proc('@UpgradeCheck',[VOLT.FastSerializer.VOLTTYPE_STRING], [runner.opts.newKit, runner.opts.newRoot])
     
-    # assume both new_kit and new_root exist
+    # assume both newKit and newRoot exist
     
     # verify the version of new kit is above the feature release version (e.g. 7.3)
     try:
-        versionF = open(os.path.join(runner.opts.new_kit, 'version.txt'), 'r')
+        versionF = open(os.path.join(runner.opts.newKit, 'version.txt'), 'r')
     except IOError:
         runner.abort("Couldn't find version information in new VoltDB kit.")
     
@@ -61,26 +63,17 @@ def plan_upgrade(runner):
         int(majorVersion) == RELEASE_MAJOR_VERSION and int(minorVersion) < RELEASE_MINOR_VERSION):
         runner.abort("The version of new VoltDB kit is too low. In-service upgrade is supported from V%d.%d" 
                      % (RELEASE_MAJOR_VERSION, RELEASE_MINOR_VERSION));
+                     
+    # newRoot should contains a voltdbroot directory
+    if not os.path.isdir(runner.opts.newRoot):
+        runner.abort("New root directory is not initialized. Please noted that NEWROOT should be pointed to the parent of new VoltDB root directory.")
     
-    # verify that the new root is initialized
-    initialized = False
-    if os.path.isfile(os.path.join(runner.opts.new_root, 'voltdbroot', '.initialized')) :
-        initialized = True
-        
-    # Need a new node?
-    needNewNode = False
-    if clusterSize % 2 == 1:
-        needNewNode = True
-    
-    generateCommands(initialized, needNewNode)
+    generateCommands(runner.opts,
+                     hosts, 
+                     os.path.isfile(os.path.join(runner.opts.newRoot, 'voltdbroot', '.initialized')), 
+                     kfactor)
 
 def basicCheck(runner):
-    # Know about the current cluster
-    # 1. existing number of nodes
-    # 2. k-factor
-    # 3. partition detection on/off
-    # 4. current root
-    # 5. current version
     response = runner.call_proc('@SystemInformation',
                                 [VOLT.FastSerializer.VOLTTYPE_STRING],
                                 ['OVERVIEW'])
@@ -91,6 +84,9 @@ def basicCheck(runner):
         hosts.update(tuple[0], tuple[1], tuple[2])
     
     numberOfNodes = len(hosts.hosts_by_id)
+    if numberOfNodes % 2 == 1 and runner.opts.newNode is None:
+        runner.abort("The cluster has odd number of nodes, plan_upgrade needs an extra node to generate the instructions")
+        
     # get current version and root directory from an arbitrary node
     host = hosts.hosts_by_id.itervalues().next();
     currentVersion = host['version']
@@ -110,21 +106,6 @@ def basicCheck(runner):
     if pd is None:
         runner.abort("Couldn't find partition-detection setting in current deployment file")
     
-    pd_tag = pd.get('enabled') # can be True, False or None
-    if pd_tag is None:
-        pd_enabled = True # true by default
-    elif pd_tag == 'True':
-        pd_enabled = True
-    else:
-        pd_enabled = False
-        
-    
-    print "Size of Cluster: %d" % numberOfNodes
-    print "Current version: %s" % currentVersion
-    print "Current VoltDB Root: %s" % currentVoltDBRoot
-    print "K-factor: %d" % int(kfactor)
-    print "Is partition-detection enabled? %s" % pd_enabled
-    
     # sanity check, in case of nodes number or K-factor is less than required
     # K = 0, abort with error message
     if kfactor == 0:
@@ -134,20 +115,58 @@ def basicCheck(runner):
     if numberOfNodes == 1: 
         runner.abort("Current cluster doesn't have enough node to perform in-service upgrade, at least two nodes are required")
         
-    # N = 2, don't recommend, must turn partition detection off
-    if numberOfNodes == 2 and pd_enabled:
-        runner.abort("In two-nodes cluster, you can't preform in-service upgrade when partition detection is enabled ")
-    
-    return numberOfNodes
+    return hosts, kfactor
 
-def generateCommands(initialized, needNewNode):
-    file = open('upgradePlan.txt', 'w+')
+def generateCommands(opts, hosts, initialized, kfactor):
+    
+    
+    toKill = chooseNodeToKill(hosts, kfactor)
+    
+    for host in hosts.hosts_by_id.values():
+        print host.hostname
     
     #1 for the new cluster, initialize the new root path if not being initialized already
     
-    #2 based on the information from @SystemInformation, choose half of the nodes to kill (how to choose?)
+    #2 kill half of the cluster
+    files = {}
+    delimiter = '-' * 5
+    for hostId,hostInfo in hosts.hosts_by_id.items():
+        filename = "upgradePlan-%s:%s-%s.txt" % (hostInfo.ipaddress, hostInfo.internalport, hostInfo.hostname)
+        file = open(filename, 'w+')
+        files[hostId] = file
+        if hostId in toKill:
+            file.write(delimiter + 'stop node' + delimiter)
+            file.write('\n')
+            if (hostInfo.hostname is None):
+                file.write('voltadmin stop %s:%d' % (hostInfo.ipaddress, hostInfo.internalport))
+            else:
+                file.write('voltadmin stop %s:%d' % (hostInfo.hostname, hostInfo.internalport))
+            file.write('\n')
     
     #3 start the new cluster ( need the license path, may need hostname or ip from the extra node )
+    missing = len(hosts.hosts_by_id) - len(toKill)
+    if opts.newNode is not None:
+        missing -= 1
+    for hostId,hostInfo in hosts.hosts_by_id.items():
+        if hostId in toKill:
+            file = files[hostId]
+            file.write('\n')
+            file.write(delimiter + 'start new cluster' + delimiter)
+            file.write('\n')
+            hostsString = []
+            for hostId, hostInfo in hosts.hosts_by_id.items():
+                if hostId in toKill:
+                    hostname = hostInfo.hostname
+                    if hostname is None:
+                        hostsString.append(hostInfo.ipaddress + ':' + str(hostInfo.internalport))
+                    else:
+                        hostsString.append(hostname + ':' + str(hostInfo.internalport))
+            
+            if opts.newNode is not None:
+                hostsString.append(opts.newNode)
+            
+            file.write("%s start --dir=%s -H %s --missing=%d" % (os.path.join(opts.newKit, 'voltdb'), opts.newRoot, ','.join(hostsString), missing))
+            file.write('\n')
     
     #4 set up XDCR replication between two clusters (how to generate command for it? call @UAC?)
     
@@ -158,4 +177,26 @@ def generateCommands(initialized, needNewNode):
     #7 rejoin the nodes being shutdown recently to the new cluster
     
     #8 turn on XDCR (optional), to avoid annoying 'Couldn't connect to host XX' message 
-        
+
+# Choose half of nodes (lower bound) in the cluster which can be killed without violating k-safety
+def chooseNodeToKill(hosts, kfactor):
+    victims = []
+    expectation = len(hosts.hosts_by_id) / 2;
+    if kfactor >= expectation:
+        for hostId, hostInfo in hosts.hosts_by_id.items():
+            victims.append(hostId)
+            if len(victims) == expectation:
+                break;
+        return victims
+    
+    partitionGroup = defaultdict(list)
+    for hostId, hostInfo in hosts.hosts_by_id.items():
+        partitions = hostInfo['partitiongroup']
+        partitionGroup[partitions].append(hostId)
+    while len(victims) != expectation:
+        for hostIds in partitionGroup.values():
+            if len(hostIds) > kfactor:
+                victims.append(hostIds.pop())
+            if len(victims) == expectation:
+                break;
+    return victims
