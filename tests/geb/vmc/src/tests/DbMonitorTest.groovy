2ca71aad1f6e56f8f994055096aeeeaaba39e58c
anil <anrai@deerwalk.com>
2017-03-16 12:38:28 +0545

    changed selector from css selector to class and index

--- tests/geb/vmc/src/tests/DbMonitorTest.groovy
+++ tests/geb/vmc/src/tests/DbMonitorTest.groovy
@@ -3372,10 +3372,10 @@ class DbMonitorTest extends TestBase {
           resultingStoredProcedureName = $("#storeProcedureBody > tr > td:nth-child(1)").text()
         } catch(geb.error.RequiredPageContentNotPresent e) {
           waitFor(waitTime) { $("#tblSP > tbody > tr > td.sorting_1").isDisplayed() }
-          resultingStoredProcedureName = $("#tblSP > tbody > tr > td.sorting_1").text()
+          resultingStoredProcedureName = $(class:"sorting_1", 0).text()
         } catch(geb.waiting.WaitTimeoutException e) {
           waitFor(waitTime) { $("#tblSP > tbody > tr > td.sorting_1").isDisplayed() }
-          resultingStoredProcedureName = $("#tblSP > tbody > tr > td.sorting_1").text()
+          resultingStoredProcedureName = $(class:"sorting_1", 0).text()
         }
         then: 'set created status'
         println("The resultingStoredProcedureName is " + resultingStoredProcedureName)
