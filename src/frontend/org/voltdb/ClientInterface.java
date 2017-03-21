1619a416d828384976540d84f367de321d07ec1f
Xin Jia <xinjiacs@gmail.com>
2017-03-21 10:29:26 -0400

    Add the remote ip address to the authentication log message; Log read… (#4334)
    * Add the remote ip address to the authentication log message; Log readable catalog changes as info level.
    
    * Add the from ip address for the HTTP client
    
    * Update the UpdateClasses detection message in the diff engine report
    
    * Output the successful authentication message for different user at different IP; Change the error message

--- src/frontend/org/voltdb/ClientInterface.java
+++ src/frontend/org/voltdb/ClientInterface.java
@@ -704,7 +704,7 @@ public class ClientInterface implements SnapshotDaemon.DaemonInitiator {
                 /*
                  * Authenticate the user.
                  */
-                boolean authenticated = arq.authenticate(hashScheme);
+                boolean authenticated = arq.authenticate(hashScheme, socket.socket().getRemoteSocketAddress().toString());
 
                 if (!authenticated) {
                     Exception faex = arq.getAuthenticationFailureException();
