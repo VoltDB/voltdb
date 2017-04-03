e7d023e5809a2ba774f43d676296ec385938e6f1
sujesh shakya <sujeshakya@deerwalk.com>
2017-04-03 14:08:56 +0545

    master: Reverted added system stored procedures

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.core.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.core.js
@@ -588,25 +588,22 @@
                     '@ExplainProc': { '1': ['Stored Procedure Name (varchar)', 'Returns Table[]'] },
                     '@ExplainView': { '1': ['Materialized View Name (varchar)', 'Returns Table[]'] },
                     '@Pause': { '0': ['Returns bit'] },
-                    '@Promote': { '0': ['Returns bit'] },
                     '@Quiesce': { '0': ['Returns bit'] },
-                    '@ResetDR': { '0': ['Returns bit'] },
                     '@Resume': { '0': ['Returns bit'] },
                     '@Shutdown': { '0': ['Returns bit'] },
                     '@SnapshotDelete': { '2': ['DirectoryPath (varchar)', 'UniqueId (varchar)', 'Returns Table[]'] },
                     '@SnapshotRestore': { '2': ['DirectoryPath (varchar)', 'UniqueId (varchar)', 'Returns Table[]'], '1': ['JSON (varchar)', 'Returns Table[]'] },
-                    '@SnapshotSave': { '0': ['Returns Table[]'], '3': ['DirectoryPath (varchar)', 'UniqueId (varchar)', 'Blocking (bit)', 'Returns Table[]'], '1': ['JSON (varchar)', 'Returns Table[]'] },
+                    '@SnapshotSave': { '3': ['DirectoryPath (varchar)', 'UniqueId (varchar)', 'Blocking (bit)', 'Returns Table[]'], '1': ['JSON (varchar)', 'Returns Table[]'] },
                     '@SnapshotScan': { '1': ['DirectoryPath (varchar)', 'Returns Table[]'] },
                     '@SnapshotStatus': { '0': ['Returns Table[]'] },
                     '@Statistics': { '2': ['Statistic (StatisticsComponent)', 'Interval (bit)', 'Returns Table[]'] },
-                    '@StopNode':{'1': ['Host ID (Int)', 'Returns Int']},
-                    '@SwapTables':{'1': ['Table Name (varchar)', 'Table-Name (varchar)', 'Returns Int']},
                     '@SystemCatalog': { '1': ['SystemCatalog (CatalogComponent)', 'Returns Table[]'] },
                     '@SystemInformation': { '1': ['Selector (SysInfoSelector)', 'Returns Table[]'] },
                     '@UpdateApplicationCatalog': { '2': ['CatalogPath (varchar)', 'DeploymentConfigPath (varchar)', 'Returns Table[]'] },
-                    '@UpdateClasses': { '1': ['Jar File (varchar)', 'Class Selector (varchar)', 'Returns Table[]'] },
                     '@UpdateLogging': { '1': ['Configuration (xml)', 'Returns Table[]'] },
-                    '@ValidatePartitioning': { '2': ['HashinatorType (int)', 'Config (varbinary)', 'Returns Table[]'] }
+                    '@Promote': { '0': ['Returns bit'] },
+                    '@ValidatePartitioning': { '2': ['HashinatorType (int)', 'Config (varbinary)', 'Returns Table[]'] },
+                    '@GetPartitionKeys': { '1': ['VoltType (varchar)', 'Returns Table[]'] }
                 };
 
                 var childConnectionQueue = connection.getQueue();
