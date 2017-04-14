6943640897431bb10989e217c4ddc54d41512ef8
Rui Shen <rshen@voltdb.com>
2017-04-14 16:39:34 -0400

    Do not tolerate lack of tracker in site dr idempotency check because there will be at least dummy tracker

--- src/frontend/org/voltdb/iv2/Site.java
+++ src/frontend/org/voltdb/iv2/Site.java
@@ -452,32 +452,18 @@ public class Site implements Runnable, SiteProcedureConnection, SiteSnapshotConn
         {
             Map<Integer, DRConsumerDrIdTracker> clusterSources = m_maxSeenDrLogsBySrcPartition.get(producerClusterId);
             if (clusterSources == null) {
-                // Don't have a tracker for this cluster
-                if (DRLogSegmentId.isEmptyDRId(lastReceivedDRId)) {
-                    return DRIdempotencyResult.SUCCESS;
-                } else {
-                    if (drLog.isTraceEnabled()) {
-                        drLog.trace(String.format("P%d binary log site idempotency check failed. " +
-                                        "Site doesn't have tracker for this cluster while the last received is %s",
-                                producerPartitionId,
-                                DRLogSegmentId.getDebugStringFromDRId(lastReceivedDRId)));
-                    }
-                }
+                drLog.warn(String.format("P%d binary log site idempotency check failed. " +
+                                "Site doesn't have tracker for this cluster while the last received is %s",
+                        producerPartitionId,
+                        DRLogSegmentId.getDebugStringFromDRId(lastReceivedDRId)));
             }
             else {
                 DRConsumerDrIdTracker targetTracker = clusterSources.get(producerPartitionId);
                 if (targetTracker == null) {
-                    // Don't have a tracker for this partition
-                    if (DRLogSegmentId.isEmptyDRId(lastReceivedDRId)) {
-                        return DRIdempotencyResult.SUCCESS;
-                    } else {
-                        if (drLog.isTraceEnabled()) {
-                            drLog.trace(String.format("P%d binary log site idempotency check failed. " +
-                                                      "Site's tracker is null while the last received is %s",
-                                                      producerPartitionId,
-                                                      DRLogSegmentId.getDebugStringFromDRId(lastReceivedDRId)));
-                        }
-                    }
+                    drLog.warn(String.format("P%d binary log site idempotency check failed. " +
+                                    "Site's tracker is null while the last received is %s",
+                            producerPartitionId,
+                            DRLogSegmentId.getDebugStringFromDRId(lastReceivedDRId)));
                 }
                 else {
                     assert (targetTracker.size() > 0);
