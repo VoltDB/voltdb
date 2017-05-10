da495f009d2bab33929c4bb4703bc5d968fbd111
wweiss <wweiss@voltdb.com>
2017-05-10 16:57:35 -0400

    ENG-12445: Fix typos

---
+++ Default/sources.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+CC_SRCS := 
+ASM_SRCS := 
+CPP_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+CC_DEPS := 
+C++_DEPS := 
+EXECUTABLES := 
+C_UPPER_DEPS := 
+CXX_DEPS := 
+OBJS := 
+CPP_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src/ee/catalog \
+src/ee/common \
+src/ee/execution \
+src/ee/executors \
+src/ee/expressions \
+src/ee/indexes \
+src/ee/logging \
+src/ee/plannodes \
+src/ee/stats \
+src/ee/storage \
+src/ee/structures \
+src/ee \
+
