be35490763f80448bd52c594ae7323f6e07acdd1
Yang Lu <ylu@voltdb.com>
2017-05-11 17:24:50 -0400

    ENG-10599, show additional information about plan in hash mismatch error message.
    Change-Id: I0e63326e59b31361e643aa0bf8b808331fd8aa9c

--- src/frontend/org/voltdb/iv2/DeterminismHash.java
+++ src/frontend/org/voltdb/iv2/DeterminismHash.java
@@ -65,7 +65,6 @@ public class DeterminismHash {
     final int[] m_hashes = new int[MAX_HASHES_COUNT + HEADER_OFFSET];
 
     protected final HybridCrc32 m_inputCRC = new HybridCrc32();
-    protected final HybridCrc32 m_stmtParamCRC = new HybridCrc32();
 
     public void reset(int catalogVersion) {
         m_catalogVersion = catalogVersion;
@@ -102,17 +101,11 @@ public class DeterminismHash {
     public void offerStatement(SQLStmt stmt, int offset, ByteBuffer psetBuffer) {
         int stmtHash = SQLStmtAdHocHelper.getHash(stmt);
         m_inputCRC.update(stmtHash);
+        m_inputCRC.updateFromPosition(offset, psetBuffer);
 
         if (m_hashCount < MAX_HASHES_COUNT) {
-            m_stmtParamCRC.reset();
-            m_stmtParamCRC.updateFromPosition(offset, psetBuffer);
-
-            int perStmtCRC = (int) m_stmtParamCRC.getValue();
             m_hashes[m_hashCount] = stmtHash;
-            m_hashes[m_hashCount + 1] = perStmtCRC;
-            m_inputCRC.update(perStmtCRC);
-        } else {
-            m_inputCRC.updateFromPosition(offset, psetBuffer);
+            m_hashes[m_hashCount + 1] = (int) m_inputCRC.getValue();
         }
         m_hashCount += 2;
     }
