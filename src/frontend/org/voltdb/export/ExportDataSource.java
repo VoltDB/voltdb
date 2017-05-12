0668a0f8ddf1e69731ed1e7e97187746f535dea1
Anish Khanzode <akhanzode@voltdb.com>
2017-05-12 13:09:20 -0400

    ENG-12430: Update coments.

--- src/frontend/org/voltdb/export/ExportDataSource.java
+++ src/frontend/org/voltdb/export/ExportDataSource.java
@@ -809,8 +809,7 @@ public class ExportDataSource implements Comparable<ExportDataSource> {
                 m_pollFuture = null;
 
                 if (m_drainTraceForDebug != null) {
-                    //Making this an ERROR. Looks like this is happening when ack starts drains and pollImpl is submitted to execute.
-                    //Since this does not happen outside of our system test I am going to make ENG-12430 non blocker.
+                    //Making this an ERROR. Looks like this is happening when ackImpl initiates drains and pollImpl is submitted to execute.
                     exportLog.error("Rolling generation " + m_generation + " before it is fully drained. " +
                                             "Drain was called from " + Throwables.getStackTraceAsString(m_drainTraceForDebug));
                 }
