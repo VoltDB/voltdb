2db06acfd363a1e7741bec070d3fcb4bc4e9a976
JadenW <qwang@voltdb.com>
2017-05-15 16:13:46 -0400

    replace dr reset self whit dr drop

--- lib/python/voltcli/voltadmin.d/dr.py
+++ lib/python/voltcli/voltadmin.d/dr.py
@@ -19,7 +19,7 @@ from voltcli.checkstats import StatisticsProcedureException
 
 def reset(runner):
     result = runner.call_proc('@ResetDR', [VOLT.FastSerializer.VOLTTYPE_TINYINT, VOLT.FastSerializer.VOLTTYPE_TINYINT, VOLT.FastSerializer.VOLTTYPE_TINYINT],
-                              [runner.opts.clusterId, runner.opts.forcing * 1, runner.opts.resetAll * 1 - runner.opts.resetSelf * 1]).table(0)
+                              [runner.opts.clusterId, runner.opts.forcing * 1, runner.opts.resetAll * 1]).table(0)
     status = result.tuple(0).column_integer(0)
     message = result.tuple(0).column_string(1)
     if status == 0:
@@ -28,20 +28,30 @@ def reset(runner):
         runner.error(message)
         sys.exit(1)
 
-    # post check for resetSelf
-    if runner.opts.resetSelf:
-        actionMessage = 'Not all connected clusters report received reset. You may continue monitoring remaining consumer clusters with @Statistics'
-        try:
-            checkstats.check_no_dr_consumer(runner)
-        except StatisticsProcedureException as proex:
-            runner.info('The previous command has timed out and stopped waiting... The cluster is in resetting DR phase.')
-            runner.error(proex.message)
-            if proex.isTimeout:
-                runner.info(actionMessage)
-            sys.exit(proex.exitCode)
-        except (KeyboardInterrupt, SystemExit):
-            runner.info('The previous command has stopped waiting... The cluster is in resetting DR phase.')
-            runner.abort(actionMessage)
+def drop(runner):
+    result = runner.call_proc('@ResetDR', [VOLT.FastSerializer.VOLTTYPE_TINYINT, VOLT.FastSerializer.VOLTTYPE_TINYINT, VOLT.FastSerializer.VOLTTYPE_TINYINT],
+                              [-1, runner.opts.forcing * 1, -1]).table(0)
+    status = result.tuple(0).column_integer(0)
+    message = result.tuple(0).column_string(1)
+    if status == 0:
+        runner.info(message)
+    else:
+        runner.error(message)
+        sys.exit(1)
+
+    # post check for drop
+    actionMessage = 'Not all connected clusters report received reset. You may continue monitoring remaining consumer clusters with @Statistics'
+    try:
+        checkstats.check_no_dr_consumer(runner)
+    except StatisticsProcedureException as proex:
+        runner.info('The previous command has timed out and stopped waiting... The cluster is in dropping phase.')
+        runner.error(proex.message)
+        if proex.isTimeout:
+            runner.info(actionMessage)
+        sys.exit(proex.exitCode)
+    except (KeyboardInterrupt, SystemExit):
+        runner.info('The previous command has stopped waiting... The cluster is in dropping phase.')
+        runner.abort(actionMessage)
 
 @VOLT.Multi_Command(
     bundles = VOLT.AdminBundle(),
@@ -50,11 +60,11 @@ def reset(runner):
             VOLT.BooleanOption('-f', '--force', 'forcing', 'bypass precheck', default = False),
             VOLT.IntegerOption('-c', '--cluster', 'clusterId', 'cluster ID', default = -1),
             VOLT.BooleanOption('-a', '--all', 'resetAll', 'reset all connected cluster(s)', default = False),
-            VOLT.BooleanOption('-s', '--self', 'resetSelf', 'gracefully reset self', default = False),
             VOLT.IntegerOption('-t', '--timeout', 'timeout', 'The timeout value in seconds if @Statistics is not progressing.', default = 0),
     ),
     modifiers = (
             VOLT.Modifier('reset', reset, 'reset one/all remote dr cluster(s).'),
+            VOLT.Modifier('drop', drop, 'gracefully dropping current cluster off the mesh, inform all its connected cluster(s) to reset it.'),
     )
 )
 
@@ -63,12 +73,10 @@ def dr(runner):
         runner.abort_with_help('You cannot specify both --force and --all options.')
     if runner.opts.clusterId >= 0 and runner.opts.resetAll:
         runner.abort_with_help('You cannot specify both --cluster and --all options.')
-    if runner.opts.clusterId >= 0 and runner.opts.resetSelf:
-        runner.abort_with_help('You cannot specify both --cluster and --self options.')
-    if runner.opts.resetAll and runner.opts.resetSelf:
-        runner.abort_with_help('You cannot specify both --all and --self options.')
     if runner.opts.clusterId < -1 or runner.opts.clusterId > 127:
         runner.abort_with_help('The cluster ID must be in the range of 0 to 127.')
+    if runner.opts.modifier == "drop" and (runner.opts.forcing or runner.opts.clusterId >=0 or runner.opts.resetAll):
+        runner.abort_with_help('You cannot specify either --force, --cluster or --all for drop.')
     if runner.opts.timeout < 0:
         runner.abort_with_help('The timeout value must be more than zero seconds.')
     runner.go()
