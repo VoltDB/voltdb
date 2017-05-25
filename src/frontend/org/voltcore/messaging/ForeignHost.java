3d92ce140f95b9133e10ee360522b3bb665e0808
Tianhe Wu <twu@voltdb.com>
2017-05-25 12:03:24 -0400

    Minor change.

--- src/frontend/org/voltcore/messaging/ForeignHost.java
+++ src/frontend/org/voltcore/messaging/ForeignHost.java
@@ -109,6 +109,18 @@ public class ForeignHost {
                 }
                 m_hostMessenger.reportForeignHostFailed(m_hostId);
             }
+
+            if (!m_closing && !isPrimary()) {
+                if (!m_hostMessenger.isShuttingDown()) {
+                    hostLog.warn("******************************************************************************************");
+                    hostLog.warn("********* Received remote hangup from foreign host " + hostnameAndIPAndPort() + " *********");
+                    hostLog.warn("******************************************************************************************");
+                } else {
+                    hostLog.warn("******************************************************************************************");
+                    hostLog.warn("********* Received remote shutdown from foreign host " + hostnameAndIPAndPort() + " *********");
+                    hostLog.warn("******************************************************************************************");
+                }
+            }
         }
 
         @Override
