1d486d385f7e4c1d48c8c02cd7023dfaac33ce3f
Bill White <bill.white@voltdb.com>
2017-06-01 11:54:28 -0700

    Small error message fix in a test function.

--- tests/frontend/org/voltdb/planner/PlannerTestCase.java
+++ tests/frontend/org/voltdb/planner/PlannerTestCase.java
@@ -713,7 +713,7 @@ public class PlannerTestCase extends TestCase {
                     fail(String.format("Expected %d plan nodes, but found more.", types.length));
                 }
                 if (types[idx] instanceof PlanNodeType) {
-                    assertEquals(String.format("Expected %s plan node by found %s", types[idx], plan.getPlanNodeType()),
+                    assertEquals(String.format("Expected %s plan node but found %s", types[idx], plan.getPlanNodeType()),
                                  types[idx], plan.getPlanNodeType());
                 } else if (types[idx] instanceof PlanWithInlineNodes) {
                     PlanWithInlineNodes branch = (PlanWithInlineNodes)types[idx];
