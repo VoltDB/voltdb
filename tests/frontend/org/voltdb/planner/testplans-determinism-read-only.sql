19d158e5a5fccbfe6cc76e597fa5637a247a29e5
Tianhe Wu <twu@voltdb.com>
2017-06-01 15:10:28 -0400

    Some change to the sql file.

--- tests/frontend/org/voltdb/planner/testplans-determinism-read-only.sql
+++ tests/frontend/org/voltdb/planner/testplans-determinism-read-only.sql
@@ -3,7 +3,7 @@ partition table test on column a;
 
 create procedure tp as select top 3 * from test;
 
-create table test2 (a int primary key, b int not null);
+create table test2 (a int, b int not null, primary key (a, b));
 partition table test2 on column b;
 
 create procedure tp2 as select top 1 * from test2;
