5552fdd3f239f3c4eb3c634d94ddc1ca4e54c29c
Dinesh Majeti <dmajeti@voltdb.com>
2017-06-08 14:15:51 -0400

    comments

--- tests/frontend/org/voltdb/regressionsuites/TestFixedSQLSuite.java
+++ tests/frontend/org/voltdb/regressionsuites/TestFixedSQLSuite.java
@@ -1774,7 +1774,7 @@ public class TestFixedSQLSuite extends RegressionSuite {
                                       var1.length() * 2, var1 + var1, 10)));
         }
 
-        // test error message for upsert
+        // Test upsert
         try {
             client.callProcedure("@AdHoc", "Upsert into VARLENGTH (id, var1) VALUES (2,'" + var1 + "')");
             fail();
