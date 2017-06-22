c5d42391d582eae6bb1bd27a49cd6a143b584069
sujesh shakya <sujeshakya@deerwalk.com>
2017-06-22 17:27:58 +0545

    VMC-358: Fixed various design and spelling mistakes in Latency Details popup

--- src/frontend/org/voltdb/dbmonitor/js/voltdb.analysis.js
+++ src/frontend/org/voltdb/dbmonitor/js/voltdb.analysis.js
@@ -98,12 +98,14 @@ function loadAnalysisPage(){
             procedureDetails["PROCEDURE_DETAIL"].forEach (function(item){
                 VoltDbAnalysis.latencyDetail[item.STATEMENT + '(' + item.PARTITION_ID + ')'] =
                     {
-                        AVG: item.AVG_EXECUTION_TIME,
-                        MIN: item.MIN_EXECUTION_TIME,
-                        MAX: item.MAX_EXECUTION_TIME
+                        AVG: item.AVG_EXECUTION_TIME/1000000,
+                        MIN: item.MIN_EXECUTION_TIME/1000000,
+                        MAX: item.MAX_EXECUTION_TIME/1000000,
+                        PARTITION_ID: item.PARTITION_ID,
+                        INVOCATIONS: item.INVOCATIONS
                     }
 
-                VoltDbAnalysis.latencyDetailValue.push({"label": item.STATEMENT + '(' + item.PARTITION_ID + ')' , "value": item.AVG_EXECUTION_TIME, "PROCEDURE": item.PROCEDURE, "TIMESTAMP": item.TIMESTAMP});
+                VoltDbAnalysis.latencyDetailValue.push({"label": item.STATEMENT + '(' + item.PARTITION_ID + ')' , "value": item.AVG_EXECUTION_TIME/1000000, "PROCEDURE": item.PROCEDURE, "TIMESTAMP": item.TIMESTAMP});
             });
 
             MonitorGraphUI.initializeProcedureDetailGraph();
