6cb5fc313c567dcb22e36dced2d10964bebf9192
Dinesh Majeti <dmajeti@voltdb.com>
2017-07-10 15:52:17 -0400

    box byte array proc file

---
+++ tests/testprocs/org/voltdb_testprocs/regressionsuites/fixedsql/BoxedByteArrays.java
@@ -0,0 +1,67 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2017 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.voltdb_testprocs.regressionsuites.fixedsql;
+
+import org.voltdb.*;
+
+@ProcInfo (
+    singlePartition = false
+)
+public class BoxedByteArrays extends VoltProcedure {
+
+    public final SQLStmt anInsert = new SQLStmt
+            ("INSERT INTO ENG_539 VALUES (?, ?, ?);");
+
+//    public VoltTable[] run(String inpType, Integer id, String varbinString) {
+//
+//        voltQueueSQL(anInsert, id, varbinString);
+//        return voltExecuteSQL();
+//    }
+
+    public final SQLStmt aBIGSelect = new SQLStmt
+            ("SELECT * FROM ENG_539 WHERE BIG IN (?);");
+
+    public VoltTable[] run(String inpType, Integer id, byte[] varbin,
+            byte[][] varbinArr, long[] intArr, String inpString) {
+
+        if (inpType.equals("VARBIN")) {
+            voltQueueSQL(anInsert, id, varbin, null);
+        } else if (inpType.equals("STR")) {
+            voltQueueSQL(anInsert, id, inpString, null);
+        } else if (inpType.equals("DSTR")) {
+            voltQueueSQL(anInsert, id, null, inpString);
+        } else if (inpType.equals("BIGD")) {
+            voltQueueSQL(anInsert, id, null, id);
+        } else if (inpType.equals("INTARR")) {
+            voltQueueSQL(aBIGSelect, intArr);
+        }
+//        else if (inpType.equals("SEL_VARBIN")) {
+//            voltQueueSQL(aSelect, varbinArr);
+//        }
+//        else if (inpType.equals("SEL_STR")) {
+//            voltQueueSQL(aSelect, strArr);
+//        }
+        return voltExecuteSQL();
+    }
+}
