b180a7e1e86e0a297883e98356c6352cd1ea53d7
Dinesh Majeti <dmajeti@voltdb.com>
2017-07-12 16:30:06 -0400

    remove code

--- src/frontend/org/voltdb/parser/SQLLexer.java
+++ src/frontend/org/voltdb/parser/SQLLexer.java
@@ -387,18 +387,7 @@ public class SQLLexer extends SQLPatternFactory
                     // Move past an ordinary character.
                     iCur++;
                 }
-//            } else if (inBegin) {
-//                // inside BEGIN ... END for multi stmt procedure
-//                // proceed till END
-//                // TODO: take care of CASE END
-//                if ( (buf[iCur] == 'E' || buf[iCur] == 'e')
-//                        && (iCur + 2 < buf.length)
-//                        && String.copyValueOf(buf, iCur, 3).equalsIgnoreCase("END") ) {
-//                        inBegin = false;
-//                        iCur += 3;
-//                } else
-//                    iCur++;
-                }  else {
+            } else {
                 // Outside of a quoted string - watch for the next separator, quote or comment.
                 if ( (buf[iCur] == 'B' || buf[iCur] == 'b')
                         && (iCur <= buf.length - 5)
