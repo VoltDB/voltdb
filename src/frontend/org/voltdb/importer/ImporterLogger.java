c215f78c96db44fa86087ca378db5826e2223c68
Jeff Crump <jeffreycrump@users.noreply.github.com>
2017-07-12 09:07:22 -0400

    ENG-12457: Kafka Loader refactor, with manual offset management (#4579)
    * WIP
    
    * KafkaLoader with manual offset management. WIP.
    
    * License check.
    
    * WIP
    
    * WIP
    
    * Javadoc
    
    * Command-line argument tweaks.
    
    * WIP
    
    * Temporarily shim the new loader into the old scripts. TBD whether we want to make new scripts or just make the new implementation the default.
    
    * Add support for —brokers argument.
    
    * Fix typo
    
    * Comments.
    
    * Comments, tweaks.
    
    * Comments
    
    * Use —hosts instead of separate —server and —port
    
    * Pass commit policy through from the command line.
    
    * Better handling of —commitPolicy.  With a unit test.
    
    * Better shutdown handling.
    
    * Comment
    
    * Callback for success so that we can manage offsets for batches in the face of errors.
    
    * Comments.
    
    * Rename BulkLoaderFailureCallback and hook it up to properly manage offsets.
    
    * Use the original argument name, -s, for servers to avoid having to change legacy tests.  Fix a typo in one of the groovy tools.
    
    * Better ZK connection logic.
    
    * Fix license check.
    
    * Only apply the “success” callback when necessary.
    
    * Handle new rules around port, server, and host arguments. Moved CLI implementation into its own file to facilitate unit testing.
    
    * Extract groupid from properties file if present, log warnings as appropriate. More unit tests, and a way to check the warning messages.
    
    * Properties file handling, in progress.
    
    * Fix problem with config file handling.
    
    * Deprecate —kpartitions
    
    * Cleanup arg processing.
    
    * Pass Kafka SSL-related consumer properties from command line.
    
    * Incorporate review comments.
    
    * Changes per code review.
    
    * Revert name change BulkLoaderFailureCallback -> BulkLoaderCallback.  There’s a bug with the Javadoc, but it turns out this package is client-visible after all.
    
    * Pass in the callback executor for the procedure (i.e., not bulk import) code path.
    
    * Only create the executor in the procedure case.
    
    * Re-arrange constructors to avoid calling instanceof. This has the happy side effect of making the callback mechanism much cleaner.  Renamed ImportSuccessCallback to BulkLoaderSuccessCallback to be consistent with the failure interface.
    
    * Add diagnostic logging of the Kafka connection properties. Useful for troubleshooting.
    
    * Clean up properties warnings since we changed tack there.
    
    * Fix test code.
    
    * Move logging out from the guts to the tool itself.
    
    * Back out previous change due to packaging constraints.
    
    * Add some additional logging per support request.
    
    * Changes per code review.
    
    * Fix unit test.
    
    * More code review requests.
    
    * Rename some member fields for clarity and consistency.
    
    * Manually merge fix for ENG-12756
    
    * More code review changes.
    
    * Remove empty line.
    
    * Javadoc fix for a build breaker (and to fix a few other warnings)
    
    * Changes per code review.
    
    * Fix unit test the right way.
    
    * One more code review comment.
    
    * Changes per code review.

---
+++ src/frontend/org/voltdb/importer/ImporterLogger.java
@@ -0,0 +1,34 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2017 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.voltdb.importer;
+
+import org.voltcore.logging.Level;
+
+/**
+ * Interface for providing logging support to Importer implementations.
+ * @author jcrump
+ */
+public interface ImporterLogger {
+
+    public void rateLimitedLog(Level level, Throwable cause, String format, Object... args);
+    public void info(Throwable t, String msgFormat, Object... args);
+    public void warn(Throwable t, String msgFormat, Object... args);
+    public void error(Throwable t, String msgFormat, Object... args);
+    public void debug(Throwable t, String msgFormat, Object... args);
+    public boolean isDebugEnabled();
+}
