a0ca57e20ea794546ad68816db7d7a8a36ebe358
Dinesh Majeti <dmajeti@voltdb.com>
2017-07-19 10:24:47 -0400

    more tests

--- tests/frontend/org/voltdb/utils/TestSplitSQLStatements.java
+++ tests/frontend/org/voltdb/utils/TestSplitSQLStatements.java
@@ -300,6 +300,14 @@ public class TestSplitSQLStatements {
                 + "end;";
         checkSplitter(sql, sql.substring(0, sql.length() - 1));
 
+        // case/end with no whitespace before and after it
+        sql = "create procedure thisproc as "
+                + "begin "
+                + "SELECT a, "
+                + "select case when id < 0 then (id+0)end+100 from aaa;"
+                + "end;";
+        checkSplitter(sql, sql.substring(0, sql.length() - 1));
+
     }
 
 }
