cb2d29f183dee58860a29f693dbe797d7a7747a1
Dinesh Majeti <dmajeti@voltdb.com>
2017-07-20 18:32:38 -0400

    sqlcmd space error diff

--- src/frontend/org/voltdb/parser/SQLLexer.java
+++ src/frontend/org/voltdb/parser/SQLLexer.java
@@ -496,7 +496,7 @@ public class SQLLexer extends SQLPatternFactory
         } else {
             // we only check for incomplete multi statement procedures right now
             // add a mandatory space..
-            results.incompleteStmt = String.copyValueOf(buf, iStart, iCur - iStart).trim() + ' ';
+            results.incompleteStmt = String.copyValueOf(buf, iStart, iCur - iStart);
         }
 
         results.completelyParsedStmts = statements;
