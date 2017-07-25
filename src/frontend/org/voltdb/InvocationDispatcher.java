72cb83afe24e00002b5af06c78f84e340eee36ba
Yang Lu <ylu@voltdb.com>
2017-07-25 10:32:08 -0400

    Eng 12307 stopnode bypass partition detection (#4676)
    * ENG-12307, don't trigger partition detection if the shutdown is caused by @StopNode.
    
    Change-Id: Ib27577c4a1d5fdef419277cd377c7586a3c178c7
    
    * ENG-12307, protect cluster from being stopped by multiple sequential @StopNode.
    
    Also improved the test.
    
    Change-Id: I325e5ae42c28f4c16c07c4286a825a0475a03fb8
    
    * ENG-12307, improve the junit.
    
    Change-Id: I242ab128a7fe115f21b47377230168a13776aec6
    
    * ENG-12307, add a new test.
    
    Change-Id: Ie0f4e521efd0db8a582fd21c304b74d25b941ea6
    
    * ENG-12307, add comment, update error response text.
    
    Change-Id: I7f1d1c7db5c1448ebb2046077e8840c3e4780ad8
    
    * ENG-12307, uncomment test code.
    
    Change-Id: I2cb1d6ac316f008388a01d891c1292236a55e2b9

--- src/frontend/org/voltdb/InvocationDispatcher.java
+++ src/frontend/org/voltdb/InvocationDispatcher.java
@@ -43,7 +43,6 @@ import org.json_voltpatches.JSONObject;
 import org.json_voltpatches.JSONStringer;
 import org.voltcore.logging.Level;
 import org.voltcore.logging.VoltLogger;
-import org.voltcore.messaging.ForeignHost;
 import org.voltcore.messaging.HostMessenger;
 import org.voltcore.messaging.Mailbox;
 import org.voltcore.network.Connection;
@@ -669,26 +668,17 @@ public final class InvocationDispatcher {
                     "Invalid Host Id or Host Id not member of cluster: " + ihid,
                     task.clientHandle);
         }
-        if (!m_cartographer.isClusterSafeIfNodeDies(liveHids, ihid)) {
-            hostLog.info("Its unsafe to shutdown node with hostId: " + ihid
-                    + " Cannot stop the requested node. Stopping individual nodes is only allowed on a K-safe cluster."
-                    + " And all rejoin nodes should be completed."
-                    + " Use shutdown to stop the cluster.");
+        String reason = m_cartographer.stopNodeIfClusterIsSafe(liveHids, ihid);
+        if (reason != null) {
+            hostLog.info("It's unsafe to shutdown node " + ihid
+                    + ". Cannot stop the requested node. " + reason
+                    + ". Use shutdown to stop the cluster.");
             return gracefulFailureResponse(
-                    "Cannot stop the requested node. Stopping individual nodes is only allowed on a K-safe cluster."
-                  + " And all rejoin nodes should be completed."
-                  + " Use shutdown to stop the cluster.", task.clientHandle);
+                    "It's unsafe to shutdown node " + ihid
+                  + ". Cannot stop the requested node. " + reason
+                  + ". Use shutdown to stop the cluster.", task.clientHandle);
         }
 
-
-        int hid = hostMessenger.getHostId();
-        if (hid == ihid) {
-            //Killing myself no pill needs to be sent
-            VoltDB.instance().halt();
-        } else {
-            //Send poison pill with target to kill
-            hostMessenger.sendPoisonPill("@StopNode", ihid, ForeignHost.CRASH_ME);
-        }
         return new ClientResponseImpl(ClientResponse.SUCCESS, new VoltTable[0], "SUCCESS", task.clientHandle);
     }
 
