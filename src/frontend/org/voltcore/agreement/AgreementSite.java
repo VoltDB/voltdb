6e8e40d1e0ca4d2355088ec088b724105c81111a
Nan Deng <ndeng@voltdb.com>
2017-07-26 11:01:41 -0400

    wrap into RateLimitedLogger.tryLogForMessage()

--- src/frontend/org/voltcore/agreement/AgreementSite.java
+++ src/frontend/org/voltcore/agreement/AgreementSite.java
@@ -102,7 +102,7 @@ public class AgreementSite implements org.apache.zookeeper_voltpatches.server.Zo
     private volatile boolean m_shouldContinue = true;
     private volatile boolean m_recovering = false;
     private static final VoltLogger m_recoveryLog = new VoltLogger("REJOIN");
-    RateLimitedLogger logger = new RateLimitedLogger(10000, m_recoveryLog, Level.INFO);
+    //RateLimitedLogger logger = new RateLimitedLogger(10000, m_recoveryLog, Level.INFO);
     private static final VoltLogger m_agreementLog = new VoltLogger("AGREEMENT");
     private long m_minTxnIdAfterRecovery = Long.MIN_VALUE;
     private final CountDownLatch m_shutdownComplete = new CountDownLatch(1);
@@ -356,7 +356,14 @@ public class AgreementSite implements org.apache.zookeeper_voltpatches.server.Zo
     private long m_lastHeartbeatTime = System.nanoTime();
     private void processMessage(VoltMessage message) throws Exception {
         if (!m_hsIds.contains(message.m_sourceHSId)) {
-            logger.log("Dropping message " + message + " because it is not from a known up site",System.currentTimeMillis(), Level.INFO);
+        	String messageFormat = "Dropping message " + message + " because it is not from a known up site";
+            //logger.log("Dropping message " + message + " because it is not from a known up site",System.currentTimeMillis(), Level.INFO);
+            RateLimitedLogger.tryLogForMessage(m_lastHeartbeatTime,
+                                               10000,
+                                               TimeUnit.MILLISECONDS,
+                                               m_agreementLog,
+                                               Level.INFO,
+                                               messageFormat);
             return;
         }
         if (message instanceof TransactionInfoBaseMessage) {
