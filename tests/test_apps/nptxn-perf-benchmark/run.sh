ca15cb3ae38a142f50bda44f4c30494029aaaf99
Tianhe Wu <twu@voltdb.com>
2017-07-31 17:45:55 -0400

    initial commit.

--- tests/test_apps/nptxn-perf-benchmark/run.sh
+++ tests/test_apps/nptxn-perf-benchmark/run.sh
@@ -106,7 +106,7 @@ function client() {
     init
     java -classpath $BENCHMARK_JAR_NAME:$CLIENTCLASSPATH np.NPBenchmark \
          --servers="$SERVERS" \
-         --sprate=0.9 \
+         --sprate=0.1 \
          --cardcount=500000 \
          --mprate=0.005 \
          --skew=0.0 \
