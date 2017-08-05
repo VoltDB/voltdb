723315ff7afec615504cec6251370d1d63046220
Phil Rosegay <prosegay@gmail.com>
2017-08-05 15:47:54 -0400

    update findchangedpaths.py add parameter to match

--- tools/jenkins/findchangedpaths.py
+++ tools/jenkins/findchangedpaths.py
@@ -2,25 +2,24 @@
 
 import sys, os
 import urllib2
+from fnmatch import fnmatch
 
 
 def getdata(url, build):
     url = 'http://ci.voltdb.lan:8080/' + url + str(build) + "/api/python"
-    #print url
     return eval(urllib2.urlopen(url).read())
 
 if __name__ == '__main__':
 
+    pattern = sys.argv[1]
+    result = -1
+
     #DATA = {"_class":"hudson.model.FreeStyleBuild","actions":[{"_class":"hudson.model.ParametersAction","parameters":[{"_class":"hudson.model.StringParameterValue","name":"GIT_BRANCH","value":"origin/ENG-12931-PoisonPillInEE"},{"_class":"hudson.model.StringParameterValue","name":"GIT_REPO","value":"voltdb"},{"_class":"hudson.model.StringParameterValue","name":"REPOSITORY_VOLTDB","value":"git@github.com:VoltDB/voltdb.git"},{"_class":"hudson.model.StringParameterValue","name":"REPOSITORY_PRO","value":"git@github.com:VoltDB/pro.git"},{"_class":"hudson.model.StringParameterValue","name":"GIT_REPOSITORY_PRO","value":"git@github.com:VoltDB/pro.git"},{"_class":"hudson.model.StringParameterValue","name":"GIT_REPOSITORY_VOLTDB","value":"git@github.com:VoltDB/voltdb.git"}]},{"_class":"hudson.model.CauseAction","causes":[{"_class":"hudson.model.Cause$UpstreamCause","shortDescription":"Started by upstream project \"SyncProjectBranchesWithJenkins\" build number 33,941","upstreamBuild":33941,"upstreamProject":"SyncProjectBranchesWithJenkins","upstreamUrl":"job/SyncProjectBranchesWithJenkins/"}]},{},{},{},{}],"artifacts":[],"building":False,"description":None,"displayName":"#31944.voltdb origin/ENG-12931-PoisonPillInEE","duration":23487,"estimatedDuration":24740,"executor":None,"fullDisplayName":"SyncStartBranchJobs #31944.voltdb origin/ENG-12931-PoisonPillInEE","id":"31944","keepLog":False,"number":31944,"queueId":13006,"result":"SUCCESS","timestamp":1501871991747,"url":"http://ci:8080/job/SyncStartBranchJobs/31944/","builtOn":"","changeSet":{"_class":"hudson.scm.EmptyChangeLogSet","items":[],"kind":None}}
-    #print DATA
-    DATA = getdata('job/'+os.environ['JOB_BASE_NAME'], '/'+ os.environ['BUILD_NUMBER'])
+    DATA = getdata('job/'+os.environ['JOB_BASE_NAME'], '/'+os.environ['BUILD_NUMBER'])
 
     affectedPaths = []
 
     while True:
-        #for f in DATA.iteritems():
-        #    print f
-
         # look at job's changeset
         cs = dict(DATA['changeSet'])
 
@@ -40,5 +39,9 @@ if __name__ == '__main__':
                     DATA = getdata(upstream['upstreamUrl'], str(upstream['upstreamBuild']))
                     break
                 else:
-                    print affectedPaths
-                    sys.exit(len(affectedPaths) == 0)
+                    for p in affectedPaths:
+                        if fnmatch(p, pattern):
+                            print p
+                            result = 0
+                            break
+                    sys.exit(result)
