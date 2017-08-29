fcd761d3c86d9d1f77353c582104b8991862c563
Yang Lu <ylu@voltdb.com>
2017-08-29 15:12:09 -0400

    Eng 12852 support concurrent NT procedures (#4810)
    * Small code cleanup and re-orgnization.
    
    Change-Id: Ide9438ebcbe23c65b260236ab34e372b61f93702
    
    * ENG-12852, stop using the implicit deterministic order to notify client the dropped transactions.
    
    Change-Id: Id9997fb123da503d51295f1db83899f96cc980e8
    
    * ENG-12852, set the size of NT proc service thread pool to [2, 20], decrease the queue size to 1000.
    
    Change-Id: Ica8a4ad4b35e061f80260c3696d902bf69952f72
    
    * ENG-12852, add unit tests to prove server doesn't lost responses (can be a failure response) even during mastership change.
    
    Change-Id: Iec3295da89c16c61e61b6dd486baede6af9c8b2d
    
    * ENG-12852, fix failing unit tests.
    
    Change-Id: I7647231d982b0be6e0681c5e6ca45e6fc62ef2bd
    
    * ENG-12852, move TestNTProcMultipleNode to pro repo.
    
    Change-Id: I37806b1fd90ea841684e79af007ae1f36e26b8cb
    
    * ENG-12852, update comment.
    
    Change-Id: I70fc0fee2abf6e73d258500ebd1d79e928c25a27

--- src/frontend/org/voltdb/InvocationDispatcher.java
+++ src/frontend/org/voltdb/InvocationDispatcher.java
@@ -720,7 +720,6 @@ public final class InvocationDispatcher {
                                      nowNanos,
                                      task.getProcName(),
                                      NTPROC_JUNK_ID,
-                                     true,
                                      true); // We are using shortcut read here on purpose
                                             // it's the simplest place to keep the handle because it
                                             // doesn't do as much work with partitions.
@@ -1224,7 +1223,7 @@ public final class InvocationDispatcher {
         }
 
         long handle = cihm.getHandle(isSinglePartition, isSinglePartition ? partitions[0] : -1, invocation.getClientHandle(),
-                messageSize, nowNanos, invocation.getProcName(), initiatorHSId, isReadOnly, isShortCircuitRead);
+                messageSize, nowNanos, invocation.getProcName(), initiatorHSId, isShortCircuitRead);
 
         Iv2InitiateTaskMessage workRequest =
             new Iv2InitiateTaskMessage(m_siteId,
