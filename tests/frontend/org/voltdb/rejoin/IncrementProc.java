c39012db3d39d66d9947e515344ca7da2f03ab11
Yang Lu <ylu@voltdb.com>
2017-09-26 15:36:55 -0400

    Eng 13137 community k (#4885)
    * ENG-13144 undo cached pool size optimzation (#4871)
    
    * ENG-13144 test change
    
    * ENG-13144 test change
    
    * ENG-13144 undo pool size optimization
    
    * ENG-13144 set undo cache pool size to 256
    
    * ENG-13144 set undo cached pool size to 192
    
    * ENG-13046: Add WARN log for HOST logger in case of MP routed to SPI (#4861)
    
    * ENG-13046: Add debug log for MISROUTE logger in case of MP routed to SPI
    
    * ENG-12837 (#4866)
    
    * Fraud Detection Example Application
    
    * ENG-13130, remove the duplicated debug information in hash mismatch detection. (#4876)
    
    Change-Id: I88bd787308bf4c3820ec4d39082ab01638eb7ac3
    
    * ENG-13137, move rejoin to community.
    
    Change-Id: I29bcc946a41209904e637d69509e0dd83643a389
    
    * ENG-13137, lift the enterprise k-safety restriction in deployment.
    
    Change-Id: I2cd2920760064b76106dee3cc44d82dfda2aad22
    
    * ENG-13137, move non-truncation snapshot part of testSnapshotsWithFailures to community.
    
    Change-Id: I1d052fbe304abb9e85c777593e30297d2d64be90

---
+++ tests/frontend/org/voltdb/rejoin/IncrementProc.java
@@ -0,0 +1,67 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2017 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.voltdb.rejoin;
+
+import org.voltdb.SQLStmt;
+import org.voltdb.VoltProcedure;
+import org.voltdb.VoltTable;
+
+/**
+ * Used to test pause-less rejoin under load.
+ * If the data diverges, this should crash VoltDB.
+ */
+public class IncrementProc extends VoltProcedure {
+    public final static byte USER_ABORT = 66;
+
+    public final SQLStmt read = new SQLStmt(
+            "select ival from blah_replicated order by ival limit 1;");
+    public final SQLStmt write = new SQLStmt(
+            "update blah_replicated set ival = ? where ival = ?;");
+
+    public VoltTable[] run() {
+        // batch one does a read
+        voltQueueSQL(read, EXPECT_SCALAR_LONG);
+        long current = voltExecuteSQL()[0].asScalarLong();
+
+        // batch 2 does a write based on the read
+        voltQueueSQL(write, EXPECT_SCALAR_MATCH(1), current + 1, current);
+        voltExecuteSQL();
+
+        if (getSeededRandomNumberGenerator().nextBoolean()) {
+            setAppStatusCode(USER_ABORT);
+            throw new VoltAbortException("Decided to roll this baby back y'all!");
+        }
+
+        // batch 2 just does a read
+        voltQueueSQL(read, EXPECT_SCALAR_MATCH(current + 1));
+        voltExecuteSQL();
+
+        // batch three does a write and a read together
+        voltQueueSQL(read, EXPECT_SCALAR_MATCH(current + 1));
+        voltQueueSQL(write, EXPECT_SCALAR_MATCH(1), current + 2, current + 1);
+        VoltTable[] results = voltExecuteSQL();
+
+        return results;
+    }
+}
