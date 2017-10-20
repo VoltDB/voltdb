966f83e58f67f07ea4611f616392b173b4f661ce
Rui Shen <rshen@voltdb.com>
2017-10-20 11:24:26 -0700

    ENG-12827: Handle replicated table binary log correctly with no replicated stream

--- src/ee/storage/DRTupleStream.h
+++ src/ee/storage/DRTupleStream.h
@@ -44,7 +44,9 @@ public:
     static const uint8_t COMPATIBLE_PROTOCOL_VERSION = 7;
 
     static const uint8_t ELASTICADD_PROTOCOL_VERSION = 8;
-    static const uint8_t NO_REPLICATED_STREAM_PROTOCOL_VERSION = 9;
+    static const uint8_t NO_REPLICATED_STREAM_PROTOCOL_VERSION = 8;
+
+    static const int8_t REPLICATED_TABLE_MASK = INT8_MIN;
 
     DRTupleStream(int partitionId, size_t defaultBufferSize, uint8_t drProtocolVersion=PROTOCOL_VERSION);
 
@@ -137,6 +139,9 @@ private:
     DRTxnPartitionHashFlag m_hashFlag;
     int64_t m_firstParHash;
     int64_t m_lastParHash;
+    bool m_hasReplicatedStream;
+    bool m_wasFirstChangeReplicatedTable;
+    bool m_wasLastChangeReplicatedTable;
     size_t m_beginTxnUso;
 
     int64_t m_lastCommittedSpUniqueId;
