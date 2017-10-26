3743302119ee47d351eff685ffe544a7dffd9d79
Rui Shen <rshen@voltdb.com>
2017-10-26 15:32:34 -0700

    fix typo

--- src/ee/storage/persistenttable.cpp
+++ src/ee/storage/persistenttable.cpp
@@ -855,7 +855,7 @@ void PersistentTable::insertTupleCommon(TableTuple& source, TableTuple& target,
 
     if (m_name == "PARTITIONED") {
         char message[2048];
-        snprintf(message, 2048, "insertTupleCommon PK %jd %jd isDirty %d\n", target.getNValue(3).toString(), target.getNValue(0).toString(), tuple.isDirty());
+        snprintf(message, 2048, "insertTupleCommon PK %jd %jd isDirty %d\n", target.getNValue(3).toString(), target.getNValue(0).toString(), target.isDirty());
         LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_INFO, message);
     }
 
@@ -1016,7 +1016,7 @@ void PersistentTable::updateTupleWithSpecificIndexes(TableTuple& targetTupleToUp
 
     if (m_name == "PARTITIONED") {
         char message[2048];
-        snprintf(message, 2048, "updateTuple PK %jd %jd isDirty %d\n", targetTupleToUpdate.getNValue(3).toString(), targetTupleToUpdate.getNValue(0).toString(), tuple.isDirty());
+        snprintf(message, 2048, "updateTuple PK %jd %jd isDirty %d\n", targetTupleToUpdate.getNValue(3).toString(), targetTupleToUpdate.getNValue(0).toString(), targetTupleToUpdate.isDirty());
         LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_INFO, message);
     }
 
