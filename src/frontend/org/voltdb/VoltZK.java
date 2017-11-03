5abaf12382124ee9a65482f2577d8240aa59edc0
JadenW <qwang@voltdb.com>
2017-11-03 16:50:40 -0400

    ENG-13341: Add more comment to elastic join blockers

--- src/frontend/org/voltdb/VoltZK.java
+++ src/frontend/org/voltdb/VoltZK.java
@@ -110,10 +110,16 @@ public class VoltZK {
      */
     public static final String actionBlockers = "/db/action_blockers";
     // being able to use as constant string
+
+    // two elastic join blockers
+    // elasticJoinInProgress blocks the rejoin (create in init state, release before data migration start)
+    // banElasticJoin blocks elastic join (currently create in DRProducer connect to a cluster has version before v7.8,
+    //                               release after no more connected cluster has version < v7.8)
     public static final String leafNodeElasticJoinInProgress = "join_blocker";
     public static final String elasticJoinInProgress = actionBlockers + "/" + leafNodeElasticJoinInProgress;
     public static final String leafNodeBanElasticJoin = "no_join_blocker";
     public static final String banElasticJoin = actionBlockers + "/" + leafNodeBanElasticJoin;
+
     public static final String leafNodeRejoinInProgress = "rejoin_blocker";
     public static final String rejoinInProgress = actionBlockers + "/" + leafNodeRejoinInProgress;
     public static final String leafNodeCatalogUpdateInProgress = "uac_nt_blocker";
