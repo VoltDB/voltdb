ba82cd741d514ab557b856b95a9d509013e4dd4e
Zeeman He <zhe@voltdb.com>
2017-12-28 12:15:13 -0500

    review update

--- src/frontend/org/voltdb/utils/CatalogUtil.java
+++ src/frontend/org/voltdb/utils/CatalogUtil.java
@@ -2173,7 +2173,7 @@ public abstract class CatalogUtil {
             catUser.setShadowpassword(hashedPW);
             catUser.setSha256shadowpassword(hashedPW256);
             //use fixed seed for comparison
-            catUser.setPassword( BCrypt.hashpw(sha1hex, "$2a$10$pWO/a/OQkFyQWQDpchZdEe"));
+            catUser.setPassword( BCrypt.hashpw(sha256hex, "$2a$10$pWO/a/OQkFyQWQDpchZdEe"));
             // process the @groups and @roles comma separated list
             for (final String role : roles) {
                 final Group catalogGroup = db.getGroups().get(role);
