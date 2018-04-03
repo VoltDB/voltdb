a1c8111a191e5d670c26342bb587d9622ad08e5d
Jenkins CI <test@voltdb.com>
2018-04-03 12:01:00 -0400

    Set Version 8.1

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -238,9 +238,9 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, HostM
     // Cluster settings reference and supplier
     final ClusterSettingsRef m_clusterSettings = new ClusterSettingsRef();
     private String m_buildString;
-    static final String m_defaultVersionString = "8.1.SRTpreview1";
+    static final String m_defaultVersionString = "8.1";
     // by default set the version to only be compatible with itself
-    static final String m_defaultHotfixableRegexPattern = "^\\Q8.1.SRTpreview1\\E\\z";
+    static final String m_defaultHotfixableRegexPattern = "^\\Q8.1\\E\\z";
     // these next two are non-static because they can be overrriden on the CLI for test
     private String m_versionString = m_defaultVersionString;
     private String m_hotfixableRegexPattern = m_defaultHotfixableRegexPattern;
