fd1407d899c090968afc52f062e8a47593ff4bc0
Yang Lu <ylu@voltdb.com>
2018-04-12 14:10:37 -0400

    ENG-13769, add missing new classes.
    Change-Id: I070845d3dad34e03b674e4ae26c33574a5557b82

---
+++ src/frontend/org/voltdb/iv2/MpRestartSequenceGenerator.java
@@ -0,0 +1,52 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2018 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.voltdb.iv2;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MpRestartSequenceGenerator {
+    // bit sizes for each of the fields in the 64-bit id
+    // note, these add up to 63 bits to make dealing with
+    // signed / unsigned conversions easier.
+    // scale up to 1024-node cluster, should be enough for now.
+    static final long NODEID_BITS = 10;
+    static final long COUNTER_BITS = 54;
+
+    static final long NODEID_MAX_VALUE = (1L << NODEID_BITS) - 1L;
+    static final long COUNTER_MAX_VALUE = (1L << COUNTER_BITS) - 1L;
+    private int m_leaderElectorId;
+    private AtomicLong m_counter = new AtomicLong(0);
+
+    public MpRestartSequenceGenerator(int nodeId) {
+        m_leaderElectorId = nodeId;
+    }
+
+    public long getNextSeqNum() {
+        long counter = m_counter.incrementAndGet();
+        return makeSequenceNumber(m_leaderElectorId, counter);
+    }
+
+    private long makeSequenceNumber(int nodeId, long counter) {
+        assert (nodeId <= NODEID_MAX_VALUE);
+        assert (counter <= COUNTER_MAX_VALUE);
+        long seq = nodeId << COUNTER_BITS;
+        seq |= counter;
+        return seq;
+    }
+
+}
