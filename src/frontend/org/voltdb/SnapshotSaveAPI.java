91d0a0b128a43e5eba2be42818eefe9294e1d720
Zeeman He <zhe@voltdb.com>
2018-04-13 15:14:17 -0400

    update replicas for stream snapshot

--- src/frontend/org/voltdb/SnapshotSaveAPI.java
+++ src/frontend/org/voltdb/SnapshotSaveAPI.java
@@ -294,7 +294,9 @@ public class SnapshotSaveAPI
                                     VoltDB.crashLocalVoltDB("Unexpected exception while attempting to create truncation snapshot headers",
                                             true, deferredSnapshotSetup.getError());
                                 }
-                                ((RealVoltDB)VoltDB.instance()).updateReplicaForJoin(context.getSiteId());
+                                if(format == SnapshotFormat.STREAM) {
+                                    ((RealVoltDB)VoltDB.instance()).updateReplicaForJoin(context.getSiteId());
+                                }
                                 context.getSiteSnapshotConnection().startSnapshotWithTargets(
                                         deferredSnapshotSetup.getPlan().getSnapshotDataTargets());
                             }
