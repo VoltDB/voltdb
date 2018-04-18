9b4346a5196a5d9c384b8cbd26ae110ac21041da
Christopher M. Wolff <cwolff@voltdb.com>
2018-04-18 10:21:29 -0400

    Fix EE unit test

--- tests/ee/test_utils/plan_testing_baseclass.h
+++ tests/ee/test_utils/plan_testing_baseclass.h
@@ -253,9 +253,7 @@ public:
             throw std::logic_error(oss.str());
         }
         assert(pTable != NULL);
-        int64_t dummyTrackerForExceptions = 0;
-        voltdb::ConditionalSynchronizedExecuteWithMpMemory
-                setMpMemoryIfNeeded(pTable->isCatalogTableReplicated(), true, dummyTrackerForExceptions);
+        voltdb::ConditionalSynchronizedExecuteWithMpMemory setMpMemoryIfNeeded(pTable->isCatalogTableReplicated(), true);
         for (int row = 0; row < nRows; row += 1) {
             if (row > 0 && (row % 100 == 0)) {
                 std::cout << '.';
