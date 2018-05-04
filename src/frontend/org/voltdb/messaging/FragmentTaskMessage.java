d7b3fc37b2d48c50ec5506a097cec47132c7dac0
wweiss <wweiss@voltdb.com>
2018-05-04 11:36:42 -0400

    ENG-13777: Randomly failing unit tests.

--- src/frontend/org/voltdb/messaging/FragmentTaskMessage.java
+++ src/frontend/org/voltdb/messaging/FragmentTaskMessage.java
@@ -509,6 +509,11 @@ public class FragmentTaskMessage extends TransactionInfoBaseMessage
                                       Collection<Integer> involvedPartitions) {
         m_initiateTask = initiateTask;
         m_involvedPartitions = ImmutableSet.copyOf(involvedPartitions);
+        if (m_nPartCount != 0 && m_involvedPartitions.size() != m_nPartCount) {
+            hostLog.error("FragmentTaskMessage has inconsistent fields: nPartCount=" + m_nPartCount +
+                    ", involvedPartitions=" + m_involvedPartitions);
+            assert(false);
+        }
         assert(m_nPartCount == 0 || m_involvedPartitions.size() == m_nPartCount);
         // this function may be called for the same instance twice, with slightly different
         // but same size initiateTask. The second call is intended to update the spHandle in
