15ba977cbe788a253c91f5c746adc5c0970b4520
Zeeman He <zhe@voltdb.com>
2018-07-05 23:10:46 -0400

    ENG-14213 do not replay before snapshotted (#5447)

--- src/frontend/org/voltdb/SiteProcedureConnection.java
+++ src/frontend/org/voltdb/SiteProcedureConnection.java
@@ -235,6 +235,7 @@ public interface SiteProcedureConnection {
     public void updateHashinator(TheHashinator hashinator);
     public void setViewsEnabled(String viewNames, boolean enabled);
     public long[] validatePartitioning(long tableIds[], byte hashinatorConfig[]);
+    public void notifyOfSnapshotNonce(String nonce, long snapshotSpHandle);
     public long applyBinaryLog(long txnId, long spHandle, long uniqueId, int remoteClusterId, int remotePartitionId, byte logData[]);
     public void setDRProtocolVersion(int drVersion);
     /*
