e4bdbaa0b09bd85e66668858284b7ac1d9e5da9e
Zeeman He <zhe@voltdb.com>
2018-08-07 08:17:32 -0400

    ENG-14413 test case fix (#5515)

--- src/frontend/org/voltdb/iv2/MpPromoteAlgo.java
+++ src/frontend/org/voltdb/iv2/MpPromoteAlgo.java
@@ -150,7 +150,7 @@ public class MpPromoteAlgo implements RepairAlgo
             //remove the flag for the partition if the repair process is not cancelled.
             //The flag is registered upon host failure or MPI promotion. The repair may be interrupted but will
             //be eventually completed.
-            if (!m_promotionResult.isCancelled()) {
+            if (!m_promotionResult.isCancelled() && m_mailbox.m_messenger != null) {
                 VoltZK.removeMpRepairBlocker(m_mailbox.m_messenger.getZK(), repairLogger);
             }
         }
