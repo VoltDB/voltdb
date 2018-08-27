77c9fe4a4e5f4627faf5413f1e40e61965870030
Chao Zhou <czhou@voltdb.com>
2018-08-27 10:58:43 -0400

    fix voltsql license

--- lib/python/voltsql/tests/parseutils/test_ctes.py
+++ lib/python/voltsql/tests/parseutils/test_ctes.py
@@ -1,3 +1,19 @@
+# This file is part of VoltDB.
+# Copyright (C) 2008-2018 VoltDB Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
@@ -24,22 +40,6 @@
 # ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# This file is part of VoltDB.
-# Copyright (C) 2008-2018 VoltDB Inc.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
 
 import pytest
 from sqlparse import parse
