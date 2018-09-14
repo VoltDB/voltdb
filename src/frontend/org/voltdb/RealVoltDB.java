2b07347976cd499d95162c314e2bd3aefe5f4eaf
Chao Zhou <czhou@voltdb.com>
2018-09-14 11:50:14 -0400

    ENG-14551-fix large mode ratio test failure (#5599)
    * fix ENG-14551
    
    * add comments
    
    * add comments

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -1627,7 +1627,10 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, HostM
 
             // If -Dlarge_mode_ratio=xx is specified via ant, the value will show up in the environment variables and
             // take higher priority. Otherwise, the value specified via VOLTDB_OPTS will take effect.
-            final double largeModeRatio = Double.valueOf(System.getenv("LARGE_MODE_RATIO") == null ? System.getProperty("LARGE_MODE_RATIO", "0") : System.getenv("LARGE_MODE_RATIO"));
+            // If the test is started by ant and -Dlarge_mode_ratio is not set, it will take a default value "-1" which
+            // we should ignore.
+            final double largeModeRatio = Double.valueOf((System.getenv("LARGE_MODE_RATIO") == null ||
+                    System.getenv("LARGE_MODE_RATIO").equals("-1")) ? System.getProperty("LARGE_MODE_RATIO", "0") : System.getenv("LARGE_MODE_RATIO"));
             if (largeModeRatio > 0) {
                 hostLog.info(String.format("The large_mode_ratio property is set as %.2f", largeModeRatio));
             }
