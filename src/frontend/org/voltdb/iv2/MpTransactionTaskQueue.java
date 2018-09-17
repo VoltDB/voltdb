66f5f8bfc4bacfaeab9626237f86e7dcfde0dd07
wweiss <wweiss@voltdb.com>
2018-09-17 15:56:52 -0400

    ENG-14614: It is possible for the MP Deliver and the MP Site threads to debug print the Invocation for the same transaction at the same time. When this happens the parameters can be printed as well. It appears that some parameters (specifically VoltTable) are not thread safe at least for ToString(). To avoid this, only the TxnSummary will be printed out on non site threads and the invocation will only be printed out in detail on the site thread.

--- src/frontend/org/voltdb/iv2/MpTransactionTaskQueue.java
+++ src/frontend/org/voltdb/iv2/MpTransactionTaskQueue.java
@@ -125,7 +125,7 @@ public class MpTransactionTaskQueue extends TransactionTaskQueue
             if (e.getValue() instanceof MpProcedureTask) {
                 MpProcedureTask next = (MpProcedureTask)e.getValue();
                 if (tmLog.isDebugEnabled()) {
-                    tmLog.debug("MpTTQ: poisoning task: " + next);
+                    tmLog.debug("MpTTQ: poisoning task: " + next.toShortString());
                 }
                 next.doRestart(masters, partitionMasters);
 
@@ -145,7 +145,7 @@ public class MpTransactionTaskQueue extends TransactionTaskQueue
                     poison.setStatus(FragmentResponseMessage.UNEXPECTED_ERROR, restart);
                     txn.offerReceivedFragmentResponse(poison);
                     if (tmLog.isDebugEnabled()) {
-                        tmLog.debug("MpTTQ: restarting:" + next);
+                        tmLog.debug("MpTTQ: restarting:" + next.toShortString());
                     }
                 }
             }
@@ -159,7 +159,7 @@ public class MpTransactionTaskQueue extends TransactionTaskQueue
                 MpProcedureTask next = (MpProcedureTask)tt;
 
                 if (tmLog.isDebugEnabled()) {
-                    tmLog.debug("Repair updating task: " + next + " with masters: " + CoreUtils.hsIdCollectionToString(masters));
+                    tmLog.debug("Repair updating task: " + next.toShortString() + " with masters: " + CoreUtils.hsIdCollectionToString(masters));
                 }
                 next.updateMasters(masters, partitionMasters);
             }
