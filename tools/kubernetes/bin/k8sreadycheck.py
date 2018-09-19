dbc51da941d9ad776c356a5c352d84aeb9579cfb
Phil Rosegay <prosegay@voltdb.com>
2018-09-19 17:01:28 -0400

    ENG-14641_k8s_readiness_checks

--- tools/kubernetes/bin/k8sreadycheck.py
+++ tools/kubernetes/bin/k8sreadycheck.py
@@ -9,25 +9,57 @@
 # contact support
 
 import subprocess
-import sys, os
-HOST = os.environ["HOSTNAME"]
 
-# PingPartitions will transactionally check all partitions for availibility, it returns 0 on success.
-# nb. the cluster will appear not-ready if the database is paused.
-cmd = "curl -sg http://localhost:8080/api/1.0/?Procedure=@PingPartitions\&Parameters=\[0\] | jq '.status,.statusstring' | xargs"
-r = subprocess.check_output(cmd, shell=True).strip()
-if r != "1":
-        print r
+
+def do_cmd(cmd):
+    r = subprocess.check_output(cmd, shell=True).strip().replace('null', 'None')
+    R = eval(r)
+    return R
+
+
+# subprocess.check_output was introduced in 2.7, let's be sure.
+if sys.hexversion < 0x02070000:
+    raise Exception("Python version 2.7 or greater is required.")
+
+HOSTNAME = os.environ['HOSTNAME']
+
+URL = "curl -sg http://localhost:8080/api/2.0/"
+
+# Check PingPartitions verifies that transactions are being processed
+# If the database is Paused, it will appear not-ready to k8s
+cmd = URL+"""?Procedure=@PingPartitions\&Parameters=\[0\] \
+        | jq -c '[.status,.statusstring]' """
+r = do_cmd(cmd)
+if r[0] != 1:
+        print "Database is not processing transactions '%s'" % r[1]
         sys.exit(1)
-#sys.exit(0)
 
-# DR PRODUCER ACTIVE: returns null (DR not configured), true (DR is ok), false (DR is down)
-#cmd = """curl -sg http://localhost:8080/api/1.0/?Procedure=@Statistics\&Parameters=\['DRPRODUCER'\] | jq '.results[].data[] | [.[%s],.[%s]] | select(.[0] == "%s") | .[1]' """ % (HOSTNAME, ISSYNCED, HOST)
-cmd = """curl -sg http://localhost:8080/api/1.0/?Procedure=@Statistics\&Parameters=\['DRPRODUCER'\] | jq '.results[].data[] | select(length == 10 and .[2] == "%s") | .[5]' """ % (HOST)
-r = subprocess.check_output(cmd, shell=True).strip()
-if r != '"ACTIVE"':
-        print "Database replication is NOT syncing (%s)" % r
+# Check DRROLE for this host, STATE=='STOPPED' fail readiness
+cmd = URL+"""?Procedure=@Statistics\&Parameters=["DRROLE"] | jq -c '[.results."0"[] | .ROLE,.STATE]'"""
+r = do_cmd(cmd)
+DR_ROLE = r[0]
+
+# if Database Replication (DR) is not configured, skip the remaining DR checks
+if DR_ROLE == "NONE":
+        sys.exit(0)
+
+# if DRROLE reports anything but ACTIVE, a DR exception has most likely occurred
+if r[1] != "ACTIVE":
+        print "Database replication has failed, DRROLE is '%s'" % r  # print error for k8s
         sys.exit(1)
 
-# everything is ok
-sys.exit(0)
+# Check DRCONSUMER IS_COVERED for this host, any false responses fail readiness
+cmd = URL+"""?Procedure=@Statistics\&Parameters=\["DRCONSUMER",0\] \
+         | jq -c '[[.results][]."1"[] | select(.HOSTNAME | startswith("%s")) | select(.IS_COVERED == "false") | .REMOTE_CLUSTER_ID] | unique'""" % HOSTNAME
+r = do_cmd(cmd)
+if len(r) > 0:
+        print "Database Replication Consumer has failed, some remote cluster partitions are not covered: '%s'" % r
+        sys.exit(1)
+
+# Check DRPRODUCER MODE for this host's consumer connections, if any "NOT_CONNECTED" for this host, fail readiness
+cmd = URL+"""?Procedure=@Statistics\&Parameters=\["DRPRODUCER",0\] \
+        | jq -c '[[.results][]."0"[] | select(.HOSTNAME | startswith("%s")) | select(.MODE == "NOT_CONNECTED") | .REMOTE_CLUSTER_ID] | unique'""" % HOSTNAME
+r = do_cmd(cmd)
+if len(r) > 0:
+        print "Database Replication Producer connection to cluster-id(s) '%s' has failed." % r
+        sys.exit(1)
