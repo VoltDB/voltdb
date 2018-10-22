66d102fb29fdba436ace8e7b255f9f4c86539685
JadenW <qwang@voltdb.com>
2018-10-22 18:34:45 -0400

    ENG-14768: System Proc for starting elastic removal

--- src/frontend/org/voltdb/SystemProcedureCatalog.java
+++ src/frontend/org/voltdb/SystemProcedureCatalog.java
@@ -222,6 +222,7 @@ public class SystemProcedureCatalog {
         builder.put("@NibbleDeleteMP",          new Config("org.voltdb.sysprocs.NibbleDeleteMP",           false, false, false, 0,    VoltType.INVALID,   false, false, true,  true,      true,   false,            true,         true ));
         builder.put("@LowImpactDeleteNT",       new Config("org.voltdb.sysprocs.LowImpactDeleteNT",        true,  false, false, 0,    VoltType.INVALID,   false, false, false, true,      false,  false,            false,        false ));
         builder.put("@ExportControl",           new Config("org.voltdb.sysprocs.ExportControl",            false, false, false, 0,    VoltType.INVALID,   false, false, true,  true,      false,  false,            true,         false ));
+        builder.put("@ElasticRemoveNT",         new Config("org.voltdb.sysprocs.ElasticRemoveNT",        false, false, false, 0,    VoltType.INVALID,   true, false, true, true,      false,  false,            false,        false ));
 
         listing = builder.build();
     }
