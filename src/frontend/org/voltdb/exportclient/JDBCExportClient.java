76ea709ebb2c3d85205ecd57012c19ab4dbd13d6
Ben Ballard <bballard@voltdb.com>
2018-10-24 13:47:57 -0400

    ENG-14496 catch more JDBC Export exceptions

--- src/frontend/org/voltdb/exportclient/JDBCExportClient.java
+++ src/frontend/org/voltdb/exportclient/JDBCExportClient.java
@@ -499,7 +499,7 @@ public class JDBCExportClient extends ExportClientBase {
                 try {
                     conn = m_ds.getDataSource().getConnection();
                     conn.setAutoCommit(false);
-                } catch (SQLException e) {
+                } catch (Exception e) {
                     m_logger.warn("JDBC export unable to connect", e);
                     closeConnection();
                     throw new RestartBlockException(true);
@@ -523,7 +523,7 @@ public class JDBCExportClient extends ExportClientBase {
                         m_logger.debug(pstmtString);
                     }
                     pstmt = conn.prepareStatement(pstmtString);
-                } catch (SQLException e) {
+                } catch (Exception e) {
                     m_logger.warn("JDBC export unable to prepare insert statement", e);
                     closeConnection();
                     throw new RestartBlockException(true);
@@ -543,6 +543,9 @@ public class JDBCExportClient extends ExportClientBase {
                 throw new RestartBlockException(true);
             } catch (SQLException e) {
                 rateLimitedLogError(m_logger, "commit() failed for row in table %s %s", m_source.tableName, Throwables.getStackTraceAsString(e));
+                throw new RestartBlockException(true);
+            } catch (Exception e) {
+                rateLimitedLogError(m_logger, "Exception while executing batch or commiting row in table %s %s", m_source.tableName, Throwables.getStackTraceAsString(e));
                 throw new RestartBlockException(true);
             } finally{
                 m_dataRows.clear();
