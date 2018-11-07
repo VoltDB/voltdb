646937d329063c5ddc996d6072bd325c35a582fd
jwiebema1 <jwiebema1@users.noreply.github.com>
2018-11-07 16:20:12 -0500

    Eng 14811 fix bad merge (#5737)
    * bad merge with reverted stream names.

--- tests/test_apps/genqa/src/genqa/ExportKafkaOnServerVerifier.java
+++ tests/test_apps/genqa/src/genqa/ExportKafkaOnServerVerifier.java
@@ -255,9 +255,9 @@ public class ExportKafkaOnServerVerifier {
 
     //Submit consumer tasks to executor and wait for EOS message then continue on.
     void createAndConsumeKafkaStreams(String topicPrefix, boolean skinny) throws Exception {
-        final String topic = topicPrefix + "EXPORT_PARTITIONED_STREAM";
-        final String topic2 = topicPrefix + "EXPORT_PARTITIONED_STREAM2";
-        final String doneTopic = topicPrefix + "EXPORT_DONE_STREAM";
+        final String topic = topicPrefix + "EXPORT_PARTITIONED_TABLE";
+        final String topic2 = topicPrefix + "EXPORT_PARTITIONED_TABLE2";
+        final String doneTopic = topicPrefix + "EXPORT_DONE_TABLE";
 
         List<Future<Long>> doneFutures = new ArrayList<>();
 
