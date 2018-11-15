0ce4f3e47523b6c4a9c1d6b2f428724e1b96d5c5
Nate Clark <nclark@voltdb.com>
2018-11-15 12:33:12 -0500

    ENG-19512: Backport: Shutdown client interface after dr
    [ backport 71121a61b1bfffc54f0e118aefdd530c0b99f1bf ]

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -3525,11 +3525,6 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, HostM
                     m_adminListener.stop();
                 }
 
-                // shut down the client interface
-                if (m_clientInterface != null) {
-                    m_clientInterface.shutdown();
-                    m_clientInterface = null;
-                }
                 // send hostDown trap as client interface is
                 // no longer available
                 m_snmp.hostDown(FaultLevel.INFO, m_messenger.getHostId(), "Host is shutting down");
@@ -3568,6 +3563,12 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, HostM
                 }
 
                 shutdownReplicationConsumerRole();
+
+                // shut down the client interface
+                if (m_clientInterface != null) {
+                    m_clientInterface.shutdown();
+                    m_clientInterface = null;
+                }
 
                 if (m_snapshotIOAgent != null) {
                     m_snapshotIOAgent.shutdown();
