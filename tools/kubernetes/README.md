e7aaf621f539a7a7515eb0c7682efc3a78e3be8f
Phil Rosegay <prosegay@gmail.com>
2018-11-21 15:43:03 -0500

    ENG-14903 ENG-14904 Kubernetes VoltDB deployment using configmaps

--- tools/kubernetes/README.md
+++ tools/kubernetes/README.md
@@ -13,76 +13,135 @@ Here's the procedure to setup a k8s deployment of VoltDB:
 
 1. Download and untar the voltdb release kit.
 
-2. cd kubernetes folder (this folder)
+2. cd kubernetes folder (this folder). In this folder you will find:
 
-3. Make a new CONFIG_FILE by copying config_template.cfg, then customize it as follows:
+    voltdb-k8s-utils.sh             Script to deploy voltdb clusters
+    config_template.cfg             Deployment Parameter template (requires customization)
+    voltdb-statefulset.yaml         k8s deployment spec (template)
 
-    1. Set REP to the docker repository to push the image to
-    2. Set IMAGE_TAG to the image tag
-    3. Set NODECOUNT to the number of voltdb nodes
-    4. Set MEMORYSIZE to the required memory size to request from k8s for each node
-    5. Set CPU_COUNT to the required number of cpus to request from k8s for each node
-    6. Set PVOLUME_SIZE to the size of the persistent volume needed for each node
-    7. Set LICENSE_FILE to the location of your voltdb license file
-    8. Set DEPLOYMENT_FILE to the location of your VoltDB deployment file
+
+3. The script voltdb-k8s-utils.sh along with an accompanying parameter file will enable you to deploy a VoltDB Cluster
+   in k8s. The script has the following functions:
+
+    voltdb-k8s-utils.sh <cluster-parameter-file> <option>
+
+    <cluster-parameter-file> has the key-value pairs for the cluster settings
+    <options> one or more of the following options.
+
+    See the description of the individual functions for details
+
+    -B, --build-voltdb-image        Builds the VoltDB docker image using docker build
+
+    -M, --install-configmap         Installs the configmap for VoltDB init using kubectl create configmap
+
+    -C, --configure-voltdb          Generates a statefulset.yaml to deploy the VoltDB cluster using
+                                    voltdb-statefulset.yaml as a master template
+
+    -S, --start-voltdb              Deploys the VoltDB cluster and starts the nodes using kubectl scale/create
+
+    -P, --stop-voltdb               Gracefully shutdown VoltDB using kubectl exec voltadmin shutdown
+
+    -D, --purge-persistent-claims   Purge persistent volume (PERSISTENT DATA WILL BE LOST)
+                                    using kubectl delete pv ...
+
+    -F, --force-voltdb-set          Terminate VoltDB statefulset ungracefully (POSSIBLE DATA LOSS)
+                                    using kubectl delete pods and statefulsets for the cluster
+
+    If you want to more than one option in an invocation, specify them separately and in the order
+    in which you want them to execute. ie. "-B -M -C -S"
+
+
+
+4. Make a new CONFIG_FILE by copying config_template.cfg, name this file with your new cluster's name.
+
+    The filename that you choose for your template will be used as the Cluster Name for the voltdb cluster,
+    all outputs will be identified by this name.
+
+    1. Set REP to the docker repository to push the image to (optional)
+    2. Set IMAGE_TAG to the image tag (optional)
+    3. Set NODECOUNT to the number of voltdb nodes (required)
+    4. Set MEMORYSIZE to the required memory size to request from k8s for each node (default 4Gi)
+    5. Set CPU_COUNT to the required number of cpus to request from k8s for each node (default 1)
+    6. Set PVOLUME_SIZE to the size of the persistent volume needed for each node (default 1Gi)
+    7. Set LICENSE_FILE to the location of your voltdb license file (optional, default from the voltdb kit)
+    8. Set DEPLOYMENT_FILE to the location of your VoltDB deployment file (optional)
     9. Set SCHEMA_FILE to the location of your database startup schema (optional)
    10. Set CLASSES_JAR to the location of your database startup classes (options)
-   11. Set EXTENSION_DIR, BUNDLES_DIR, LOG4J_FILE  to the location of your lib-extension, bundes, or logging properties (optional)
+   11. Set EXTENSION_DIR, BUNDLES_DIR, LOG4J_FILE  to the location of your lib-extension, bundles, or logging properties (optional)
+   12. Set VOLTDB_START_ARGS additional command line arguments for voltdb start (optional)
+   13. Set VOLTDB_OPTS runtime options for voltdb start (optional)
+   14. Set VOLTDB_HEAPMAX runtime java heap allocation for voltdb start (optional)
 
-    Other customizations to consider:
-
-    To set the Java Heap Size for VoltDB, edit the statefulset yaml file and set the following into the environment (env:) section:
-          env:
-            ...
-            - name: MAX_HEAP_SIZE
-              value: <required-heap-size> ex. 2G
-            ...
-     See VoltDB documentation for more information.
+    Notes:
 
     Passing startup parameters to VoltDB:
 
     You can pass parameters to voltdb at startup by setting VOLTDB_START_ARGS in the k8s deployment yaml:
-        env:
-            ...
-            - name: VOLTDB_START_ARGS
-              value: "--missing=1 ..."
-            ...
-    See VoltDB documentation for more information.
+
+        VOLTDB_START_ARGS="--ignore=thp --pause ..."
+
+    See VoltDB documentation for more information about startup options.
 
     For your deployment there may be other things to configure such as persistent volume properties, sizes, etc.,
     you'll need to edit the voltdb-statefulset.yaml template and set your specific requirements there, and check
     it into your source control.
 
-4. Build the image and voltdb-statefulset deployment file:
+    For these voltdb start options you may specify either a single value or a comma separated list of values.
+    If a list is given, a single value corresponding to the pod ordinal will be selected from the list
+    at voltdb startup.
 
-    This step creates the voltdb image and configures a kubernetes deployment file.
-    The resulting deployment file will be named... The container image is customized
-    with provided assets, and a initial database root is created with your settings.
-    This root will be copied to persistent storage on first run of the database (node).
+                '--externalinterface'
+                '--internalinterface'
+                '--publicinterface'
+                '--admin'
+                '--client'
+                '--http'
+                '--internal'
+                '--replication'
+                '--zookeeper'
+                '--drpublic'
 
-        ./build_image.sh CONFIG_FILE
+                ex: --drpublic '10.0.0.1,10.0.0.2,10.0.0.3,10.0.0.4,...'
+                for statefulset pod-1 --drpublic 10.0.0.2 will be used at startup
+
+
+5. Build the image config-maps, and customized voltdb-statefulset deployment file:
+
+        ./voltdb-k8s-utils.sh CONFIG_FILE -B -M -C
+
+        chained comands:
+        -B: build the image
+        -M: configure and install the config-maps (both init-time and run-time)
+        -C: configure the customized statefulset yaml
+
+6. Start the deployment:
+
+        ./voltdb-k8s-utils.sh CONFIG_FILE -S
+
 
 ## Starting and Stopping the Cluster
 
-* To start the cluster:
+* To stop the cluster:
 
-        kubectl create -f K8S_DEPLOYMENT
+        ./voltdb-k8s-utils.sh CONFIG_FILE -P  (or -F to force it off without saving)
 
-* To stop the cluster, retaining the persistent volume(s):
+* To restart the cluster:
 
-        voltadmin pause --wait
-        kubectl delete -f K8S_DEPLOYMENT
+        ./voltdb-k8s-utils.sh CONFIG_FILE -R
+
+        the database will be quiesced, followed by a scale down to zero pods.
+
+* To purge the persistent volumes the cluster:
+
+        !!! This will result in unercoverable loss of the database and its data !!!
+
+        ./voltdb-k8s-utils.sh CONFIG_FILE -D
 
 * To display persistent volume(s) and claim(s):
 
         kubectl get pvc
         kubectl get pv
 
-* To delete volumes (all data in the database will be lost):
-
-        kubectl delete <pv/pvc name>
-
-    nb. next time the database is brought up it will be in the initial state
 
 ## Other Useful Commands
 
@@ -90,7 +149,7 @@ Here are some other commands that are useful (assuming the name of the statefuls
 
 * To connect sqlcmd to a running cluster
 
-        kubectl exec -it voltdb-0 -- sqlcmd --servers=localhost
+        kubectl exec -it pod-0 -- sqlcmd --servers=localhost
 
 * To proxy VoltDB ports to localhost ports (ports are remote[:local])
 
--- tools/kubernetes/bin/voltdbk8s.py
+++ tools/kubernetes/bin/voltdbk8s.py
@@ -5,53 +5,80 @@
 
 # Author: Phil Rosegay
 
-# VOLTDB K8S node startup controller
+# VOLTDB K8S node startup adapter
 #
 # Uses DNS SRV records to discover nodes and edit the VoltDB configuration when nodes startup.
-# Copy "pristine" voltdbroot from docker image to persistent storage on first run.
+# Initialize a new VoltDB Database on first run.
 #
 # args are voltdb ... (ie. voltdb start command)
-# Invoke from the container ENTRYPOINT ex.
+# Invoke from the container ENTRYPOINT, or k8s command
 #
 #   voltdbk8s.py voltdb start <parms>
 
 #
-# voltdb start cmd with -H will be ignored, -H will be set appropriately.
+# voltdb start cmd with -H will be ignored, -H will be set to the pod name.
 #
 
-
+from __future__ import print_function
 import sys, os
 import socket
 import subprocess
 import httplib2
-from shutil import copytree
+import shlex
+from time import sleep
+from subprocess import Popen, STDOUT
 
 
 # mount point for persistent volume voltdbroot
-PV_VOLTDBROOT = "/voltdbroot"
+PV_VOLTDBROOT = os.getenv('VOLTDB_K8S_ADAPTER_PVVOLTDBROOT', "/voltdbroot")
 
 
 # TODO: need to implement internal-interface option (voltdb command) support
-VOLTDB_INTERNAL_INTERFACE=3021
-VOLTDB_HTTP_PORT = 8080
+VOLTDB_INTERNAL_INTERFACE = int(os.getenv('VOLTDB_K8S_ADAPTER_INTERNAL_PORT', 3021))
+VOLTDB_HTTP_PORT = int(os.getenv('VOLTDB_K8S_ADAPTER_HTTP_PORT', 8080))
 
+
+# These VoltDB start args may be comma separated lists, the value passed to voltdb will be selected
+# using the pod-pod_ordinal as index to the list of values. If there is only a single value, that value
+# will be used for all pods.
+MAYBE_ORDINAL_START_ARGS = ['--externalinterface',
+                        '--internalinterface',
+                        '--publicinterface',
+                        '--admin',
+                        '--client',
+                        '--http',
+                        '--internal',
+                        '--replication',
+                        '--zookeeper',
+                        '--drpublic',
+                     ]
+
+def printerr(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def printdbg(*args, **kwargs):
+    if __debug__:
+        print(*args, file=sys.stdout, **kwargs)
 
 def query_dns_srv(query):
     m_list = []
     try:
         # SRV gives us records for each node in the cluster like ...
+        # voltdb stateful set pods are registered on startup not on readiness.
         # _service._proto.name.  TTL   class SRV priority weight port target.
         #nginx.default.svc.cluster.local    service = 10 100 0 voltdb-0.nginx.default.svc.cluster.local.
         # the fqdn is structured as ... headless-service-name.namespace.svc.cluster.local
         # this is similar to etcd
-        answers = subprocess.check_output(("nslookup -type=SRV %s" % query).split(' ')).split('\n')[2:]
-    except:
+        cmd = "nslookup -type=SRV %s | awk '/%s/ {print $NF}'" % ((query,)*2)
+        #cmd = "host -t SRV %s | awk '{print $NF}'" % query
+        answers = subprocess.check_output(cmd, shell=True)
+    except Exception as e:
+        printerr(str(e))
         return m_list
-    for rdata in answers:
+    for rdata in answers.split('\n'):
         if len(rdata):
             m_list.append(rdata.split(' ')[-1][:-1])  # drop the trailing '.'
-            print rdata
-    print m_list
+    printdbg(m_list)
     # return a list of fq hostnames of pods in the service domain
     return sorted(m_list)
 
@@ -62,7 +89,7 @@ def try_to_connect(host, port):
         s.connect((host, port))
         return True
     except Exception as e:
-        print str(e)
+        printdbg(str(e))
         return False
     finally:
         s.close()
@@ -73,7 +100,7 @@ def http_get(url, host, port):
     proto = "http"
     admin = "true"
     urlp = "%s://%s:%d/api/1.0/?%s&admin=%s" % (proto, host, port, url, admin)
-    print urlp
+    printdbg(urlp)
     h = httplib2.Http(".cache")
     return h.request(urlp, "GET")
 
@@ -81,7 +108,6 @@ def http_get(url, host, port):
 def get_system_information(host, port, section='OVERVIEW'):
     try:
         resource = "Procedure=@SystemInformation&Parameters=[\"" + section + "\"]"
-        print resource
         resp_headers, content = http_get(resource, host, port)
         return resp_headers, content
     except:
@@ -89,115 +115,237 @@ def get_system_information(host, port, section='OVERVIEW'):
 
 
 def find_arg_index(args, arg):
-    for i in range(0, len(args)):
-        if args[i] == arg:
-            return i+1
+    _args = arg.split(',')
+    for a in _args:
+        for i in range(0, len(args)):
+            if args[i] == a:
+                return i+1
     return None
 
 
-def fork_voltdb(host, voltdbroot):
-    # before we fork over, see if /voltdbroot (persistent storage mount) is empty
-    # if it is, copy our pristine voltdbroot from the image
-    if not os.path.exists(PV_VOLTDBROOT):
-        print "ERROR: '%s' is not mounted!!!!" % PV_VOLTDBROOT
-        sys.exit(-1)
-    working_voltdbroot = os.path.join(PV_VOLTDBROOT, voltdbroot)
-    print os.listdir(PV_VOLTDBROOT)
-    print working_voltdbroot
-    if voltdbroot not in os.listdir(PV_VOLTDBROOT):
-        # our docker image has pwd pointing to the pristine voltdbroot
-        # TODO: this is ok for now (maybe) but when catalogs change we need to persist back to our image?
-        pristine_root = os.getcwd()
-        print "Copying pristine voltdbroot to '%s'" % working_voltdbroot
-        copytree(pristine_root, working_voltdbroot, symlinks=True, ignore=None)
-        os.chdir(PV_VOLTDBROOT)
-        os.system("rm -f " + ssname)
-        os.system("ln -sf " + voltdbroot + " " + ssname)
-    os.chdir(working_voltdbroot)
-    args = sys.argv[:]  # copy
-    for i in range(0, len(args)):
-        if args[i] == "-D" or args[i] == '--directory':
-            args[i+1] = working_voltdbroot
-    # run voltdb replacing current process
-    hni = find_arg_index(args, '-H')
-    if hni == 0:
-        hni = len(args)
-        args[hni] = "-H"
-        hni += 1
-    args[hni] = host
-    print "VoltDB cmd is '%s'" % args[1:]
-    # flush so we see our output in k8s logs
-    sys.stdout.flush()
-    sys.stderr.flush()
-    d = os.path.dirname(args[0])
-    os.execv(os.path.join(d, args[1]), args[1:])
-    sys.exit(0)
+def add_or_replace_arg(args, option, value, action=None):
+    # option is comma separated list of option formats to be treated equally, ie. "-L,--license"
+    # we'll reasonably assume that only one of the option formats is present
+    options = option.split(",")
+    for o in options:
+        ix = find_arg_index(args, o)
+        if ix is not None:
+            break
+    if ix is None:
+        if action == 'replace':
+            return
+        args.append(o)
+        args.append(value)
+        return
+    args[ix] = value
+    return
+
+
+def str_to_arg_list(text):
+    # strip any leading and trailing quotes
+    al = []
+    text = text.strip("'\"")
+    # returns a list of shell-like arguments
+    for a in shlex.split(text):
+        if '=' in a:
+            al.extend(a.split('='))
+        else:
+            al.append(a)
+    return al
+
+#def init_voltdbroot(working_voltdbroot, assets_dir):
 
 
 def get_hostname_tuple(fqdn):
     hostname, domain = fqdn.split('.', 1)
     ssp = hostname.split('-')
-    hn = ('-'.join(ssp[0:-1]), ssp[-1], hostname, domain)  # statefulset hostnames are podname-ordinal
-    return hn   # returns a tuple (ss-name, ordinal, hostname, domain)
+    hn = ('-'.join(ssp[0:-1]), ssp[-1], hostname, domain)  # statefulset hostnames are podname-pod_ordinal
+    return hn   # returns a tuple (ss-name, pod_ordinal, hostname, domain)
 
 
 if __name__ == "__main__":
 
-    print sys.argv
-    print os.environ
+    printdbg(sys.argv)
+    printdbg(os.environ)
 
     # check that our args look like a voltdb start command line and only that
     if not (sys.argv[1] == 'voltdb' and sys.argv[2] == 'start'):
-        print "ERROR: expected voltdb start command but found '%s'" % sys.argv[1]
-        sys.exit(-1)
+        printerr("WARNING: expected voltdb start command but found '%s'" % sys.argv[1])
+        ###sys.exit(-1)
 
-    fqhostname = socket.getfqdn()
-    print fqhostname
+    fqhostname = os.getenv('VOLTDB_K8S_ADAPTER_FQHOSTNAME', socket.getfqdn())
 
-    # for maintenance mode, don't bring up the database just hang
-    # nb. in maintenance mode, liveness checks will probably timeout if enabled
-    if '--k8s-maintenance' in sys.argv:
-        while True:
-            from time import sleep
-            sleep(10000)
-        #os.execv('tail' '-f', '/dev/null')
+    printdbg(fqhostname)
 
     # use the domain of the leader address to find other pods in our cluster
     hn = get_hostname_tuple(fqhostname)
-    print hn
+    printdbg(hn)
 
-    ssname, ordinal, my_hostname, domain = hn
+    ssname, pod_ordinal, my_hostname, domain = hn
 
-    if len(hn) != 4 or not ordinal.isdigit():
-        # TODO: need a better check for running with k8s statfulset???
+    if len(hn) != 4 or not pod_ordinal.isdigit():
         # we don't know what do with this, just fork
-        fork_voltdb(ssname+"-0."+domain, None)
+        #fork_voltdb(ssname+"-0."+domain)
+        printerr("ERROR: Hostname parse error, is this a statefulset pod?, cannot continue")
+        printerr(fqhostname)
+        sys.exit(-1)
 
-    # if there are some pods up in our cluster, connect to the first one we find
-    # if we fail to form/rejoin/join a cluster, k8s will likely just restart the pod
+    pod_ordinal = int(pod_ordinal)
 
-    # get a list of fq hostnames of pods in the domain from DNS SRV records
-    my_cluster_members = query_dns_srv(domain)
+    # For maintenance mode, don't bring up the database just sleep indefinitely
+    # nb. in maintenance mode, liveness checks will timeout if enabled
+    if '--k8s-maintenance' in sys.argv:
+        while True:
+            sleep(10000)
+        #os.execv('tail' '-f', '/dev/null')
 
-    din = find_arg_index(sys.argv, '-D')
+    # TODO: -D not tested
+    din = find_arg_index(sys.argv, '-D,--dir')
     if din:
         voltdbroot = '.'.join(my_hostname, sys.argv[din])
     else:
         voltdbroot = fqhostname
 
+    # discover pods in our cluster, if any
+    # nb. voltdb pods are registered when then are started, and therefore may not appear ready to k8s
+    cluster_pods = query_dns_srv(domain)
+
+    # find nodes which are actually up and
     # nodes may be "published before they are ready to receive traffic"
-    for host in my_cluster_members:
-        print "Connecting to '%s'" % host
+    cluster_pods_up = []
+    for host in cluster_pods:
+        print("Connecting to '%s'" % host)
         if try_to_connect(host, VOLTDB_INTERNAL_INTERFACE):
             # we may have found a running node, get voltdb SYSTEMINFORMATION
             if try_to_connect(host, VOLTDB_HTTP_PORT):
                 sys_info = None
                 try:
                     sys_info = get_system_information(host, VOLTDB_HTTP_PORT)
+                    cluster_pods_up.append(host)
                 except:
-                    raise
-                print "sysinfo: " + str(sys_info)
-            # try to connect to mesh
-            fork_voltdb(host, voltdbroot)
+                    pass
+    # in the event that no pods are up, direct fork to pod-0
+    if len(cluster_pods_up) == 0:
+        cluster_pods_up = [ssname+"-0."+domain]
 
-    fork_voltdb(ssname+"-0."+domain, voltdbroot)
+    printdbg(cluster_pods_up)
+
+    # before we fork over, see if /voltdbroot (persistent storage mount) is empty
+    # if it is, initialize a new database there from our assets
+    if not os.path.exists(PV_VOLTDBROOT):
+        printerr("ERROR: '%s' is not mounted!!!!" % PV_VOLTDBROOT)
+        sys.exit(-1)
+    assets_dir = os.path.join(os.getenv('VOLTDB_INIT_VOLUME', '/etc/voltdb'))
+    working_voltdbroot = os.path.join(PV_VOLTDBROOT, voltdbroot)
+
+    try:
+        pv = os.listdir(working_voltdbroot)
+    except OSError:
+        pv = []
+    printdbg(pv)
+
+    # initialize the voltdbroot if necessary
+    if len(pv) == 0:
+        print("Initializing a new voltdb database at '%s'" % working_voltdbroot)
+        try:
+            os.mkdir(working_voltdbroot)
+        except OSError:
+            pass
+        olddir = os.getcwd()
+        os.chdir(working_voltdbroot)
+        cmd = [sys.argv[1], 'init']
+        """
+        These asset files are mounted from a configmap
+        !!!Updating the configmap will have NO EFFECT on a running or restarted instance!!!
+        The command to create the configmap is typically:
+
+        kubectl create configmap --from-file=deployment=mydeployment.xml [--from-file=classes=myclasses.jar] [--from-file=schema=myschema.sql]
+
+        The deployment file node count is ignored, node count is specified in the runtime environment variable $NODECOUNT
+        """
+        if os.path.isdir(assets_dir):
+            deployment_file = os.path.join(assets_dir, 'deployment')
+            if os.path.isfile(deployment_file):
+                cmd.extend(['--config', deployment_file])
+            classes_file = os.path.join(assets_dir, 'classes')
+            if os.path.isfile(classes_file):
+                cmd.extend(['--classes', classes_file])
+            schema_file = os.path.join(assets_dir, 'schema')
+            if os.path.isfile(schema_file):
+                cmd.extend(['--schema', schema_file])
+        extra_init_args = os.getenv('VOLTDB_INIT_ARGS')
+        if extra_init_args:
+            cmd.extend(str_to_arg_list(extra_init_args))
+        print("Init command: " + str(cmd))
+        sp = Popen(cmd, shell=False, stderr=STDOUT)
+        sp.wait()
+        if sp.returncode != 0:
+            print("ERROR failed Initializing voltdb database at '%s' (did you forget --force?)" %
+                  working_voltdbroot)
+            sys.exit(-1)
+        print("Initialize new voltdb succeeded!!!")
+        os.chdir(olddir)
+        os.system("rm -f " + ssname)
+        os.system("ln -sf " + voltdbroot + " " + ssname)
+
+    # check that we have the correct/consistent PV for this node
+    try:
+        pv = os.listdir(PV_VOLTDBROOT)
+    except OSError:
+        pv = []
+    if voltdbroot not in pv:
+        printerr("WARNING voltdbroot expected: '%s' actual: '%s'" % (voltdbroot, pv))
+        ###sys.exit(1)  # not enforcing this but beware pv's might go to any pod on reuse?
+
+    os.chdir(working_voltdbroot)
+    args = sys.argv[:]  # copy
+
+    # normalize args
+    # some of our "args" might be environment strings of args, if so break them up for the shell
+    nargs = []
+    for a in args:
+        if ' ' in a:
+            nargs.extend(str_to_arg_list(a))
+        else:
+            nargs.append(a)
+    args = nargs
+
+    # filter pod-pod_ordinal args: ex: '--foo a,b,c,d' when run time podname is 'podname-2' results in the
+    # value '--foo c' rendered to the commandline
+    for oa in MAYBE_ORDINAL_START_ARGS:
+        ix = find_arg_index(args, oa)
+        if ix is not None:
+            printdbg(args[ix])
+            if ',' in args[ix]:
+                li = args[ix].split(',')
+                printdbg(li)
+                printdbg(pod_ordinal)
+                printdbg(len(li))
+                if len(li) < pod_ordinal:
+                    printerr("ERROR treating '%s' as a pod_ordinal comma separated list but there appear to be"
+                             " insufficient entries for host '%s'" % (oa, pod_ordinal))
+                    sys.exit(-1)
+                args[ix] = li[pod_ordinal]
+
+    # build the voltdb start command line
+    printdbg(args)
+    args = shlex.split(' '.join(args))
+
+    if os.path.isdir(assets_dir):
+        license_file = os.path.join(assets_dir, 'license')
+        if os.path.isfile(license_file):
+            add_or_replace_arg(args, "-l,--license", license_file)
+    add_or_replace_arg(args, "-D,--dir", working_voltdbroot, action="replace")
+    add_or_replace_arg(args, "-H,--host", ','.join(cluster_pods_up))
+
+    print("VoltDB cmd is '%s'" % args[1:])
+    print("Starting VoltDB...")
+
+    # flush so we see our output in k8s logs
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    # fork voltdb
+    d = os.path.dirname(args[0])
+    os.execv(os.path.join(d, args[1]), args[1:])
+    sys.exit(0)
