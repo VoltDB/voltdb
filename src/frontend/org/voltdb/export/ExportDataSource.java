c5273d1233cd314e5f7c52777f8ac03a62d4c262
Yang Lu <ylu@voltdb.com>
2018-12-11 16:31:23 +0800

    ENG-14941, add debug stmt.
    Change-Id: I757a91065b3db9584c36069beba25e3e6beeceec

--- src/frontend/org/voltdb/export/ExportDataSource.java
+++ src/frontend/org/voltdb/export/ExportDataSource.java
@@ -587,6 +587,11 @@ public class ExportDataSource implements Comparable<ExportDataSource> {
                     }
                     maxLatency = m_overallMaxLatency;
                 }
+                if (exportLog.isDebugEnabled()) {
+                    exportLog.debug("Export Stats: " + m_partitionId + " " + m_siteId + " " + m_tableName + " " + m_exportTargetName + " " + m_mastershipAccepted.get() +
+                            " " + m_tupleCount + " " + m_tuplesPending.get() + " " + m_lastQueuedTimestamp + " " + m_lastAckedTimestamp + " " + avgLatency + " " +
+                            maxLatency + " " + m_queueGap + " " + m_status.toString());
+                }
                 return new ExportStatsRow(m_partitionId, m_siteId, m_tableName, m_exportTargetName,
                         m_mastershipAccepted.get(), m_tupleCount, m_tuplesPending.get(),
                         m_lastQueuedTimestamp, m_lastAckedTimestamp,
