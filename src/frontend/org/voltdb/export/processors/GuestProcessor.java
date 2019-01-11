358fb7bcc11062be719c33ee5848cecbf29990d2
Yang Lu <ylu@voltdb.com>
2019-01-11 23:45:34 +0800

    ENG-15180, fix NPE in UAC. (#5850)
    Change-Id: Icfb68808b8cb5c96e9e6004b689de1fb4a65e342

--- src/frontend/org/voltdb/export/processors/GuestProcessor.java
+++ src/frontend/org/voltdb/export/processors/GuestProcessor.java
@@ -140,10 +140,13 @@ public class GuestProcessor implements ExportDataProcessor {
                         return;
                     }
                     String tableName = source.getTableName().toLowerCase();
+                    String groupName = m_targetsByTableName.get(tableName);
                     if (source.getClient() == null) {
                         m_logger.warn("Table " + tableName + " has no configured connector.");
                         continue;
                     }
+                    //If we configured a new client we already mapped it if not old client will be placed for cleanup at shutdown.
+                    m_clientsByTarget.putIfAbsent(groupName, source.getClient());
                     ExportRunner runner = new ExportRunner(m_targetsByTableName.get(tableName), source.getClient(), source);
                     // DataSource should start polling only after command log replay on a recover
                     source.setReadyForPolling(m_startPolling);
