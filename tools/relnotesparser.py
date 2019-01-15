70ab6832f691a3e25dc315953ac1d140e47ed13d
Andrew Gent <ajgent@voltdb.com>
2019-01-15 16:10:47 -0500

    Rewrite the release notes updating tools. Make parser a callable module. MAke updater a single command.
    In addition, change the structure of the data from parser to updater, so it is a fixed, 2 column array with the keys (comma separated) in one column and text in the other.

--- tools/relnotesparser.py
+++ tools/relnotesparser.py
@@ -3,25 +3,31 @@
 # Written by Andrew Gent
 #
 # This program parses the XML source file for the release notes and extracts the text of
-# each note, along with the ticket number coded in the <revision> tag, and writes them
-# out as CSV data. The CSV file can then be compared to the # releasenote field in JIRA 
-# using the accompanying relnotesupdater.py script.
+# each note, along with the ticket number coded in the <revision> tag.
 #
-# HOW TO USE:
+# Normally, this is called as a module by relnotesupdater.py. However, it can be run
+# standalone, in which case it writes the release note data as CSV to stdout.
+#
+# HOW TO USE STANDALONE:
 # - in the voltdb-docs repo, make sure you've checked out the latest docs
 # - in the voltdb repo:
 #       python ./relnotesparser.py ~/workspace/voltdb-doc/userdocs/releasenotes.xml > /tmp/relnotes.csv
 #
 
 import sys
+import csv
+import StringIO
+
 from xml.dom.minidom import parse
 
+relnotes = []
 
 def parsefile(filename):
     tree = parse(filename)
     #root = tree.getroot()
     # Look for all the "revision=" attributes
     findattribute(tree,"revision",0)
+    return relnotes
 
 def findattribute(xml,attribute, depth):
     if depth > 100:
@@ -36,7 +42,10 @@ def findattribute(xml,attribute, depth):
             if attribute in atts.keys():
                 foundattribute = atts[attribute].value
         if foundattribute:
-            print foundattribute + "," + quote(smash(innertext(child) ))
+            row = [foundattribute,smash(innertext(child))]
+            relnotes.append(row)
+            #relnotes.append([foundattribute,smash(innertext(child))])
+            #print foundattribute + "," + quote(smash(innertext(child) ))
         else:
             findattribute(child,attribute,depth+1)
 
@@ -62,4 +71,19 @@ def quote(text):
 
 if __name__ == "__main__":
     f = sys.argv[1]
-    parsefile(f)
+    parsed = parsefile(f)
+    
+    # print the array
+    strfile = StringIO.StringIO()
+    fid = csv.writer(strfile)
+    for r in parsed:
+        fid.writerow(r)
+        #outputstr = ""
+        #c = len(r)
+        #for i in range (0,c-1):
+        #    outputstr = outputstr + r[i] + ","
+        #outputstr = outputstr + quote(r[c-1])
+        #print outputstr
+    sys.stdout.write( strfile.getvalue() )
+    sys.stdout.flush()
+
