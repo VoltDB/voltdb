9c29236c79dad75a3d175259a3716bb17208c9c9
Yiqun Zhang <yzhang1991@users.noreply.github.com>
2019-02-20 16:55:33 -0500

    ENG-15174-snap-restore-replicated (#5916)
    Fix a bug that SnapshotRestore falls into a deadlock when a multi-block replicated table's snapshot data needs to be transferred from remote nodes.

--- src/frontend/org/voltdb/sysprocs/saverestore/ReplicatedTableSaveFileState.java
+++ src/frontend/org/voltdb/sysprocs/saverestore/ReplicatedTableSaveFileState.java
@@ -19,6 +19,7 @@ package org.voltdb.sysprocs.saverestore;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -29,27 +30,32 @@ import org.voltdb.catalog.Table;
 import org.voltdb.dtxn.SiteTracker;
 import org.voltdb.sysprocs.SysProcFragmentId;
 
-public class ReplicatedTableSaveFileState extends TableSaveFileState
-{
-    ReplicatedTableSaveFileState(String tableName, long txnId)
-    {
+public class ReplicatedTableSaveFileState extends TableSaveFileState {
+    ReplicatedTableSaveFileState(String tableName, long txnId) {
         super(tableName, txnId);
     }
 
     @Override
-    void addHostData(VoltTableRow row) throws IOException
-    {
+    void addHostData(VoltTableRow row) throws IOException {
         assert(row.getString("TABLE").equals(getTableName()));
-
         checkSiteConsistency(row); // throws if inconsistent
         // this cast should be safe; site_ids are ints but get
         // promoted to long in the VoltTable.row.getLong return
         m_hostsWithThisTable.add((int) row.getLong("CURRENT_HOST_ID"));
     }
 
+    public String debug() {
+        StringBuilder builder = new StringBuilder("Replicated table ");
+        builder.append(getTableName()).append(" exists on host ");
+        for (Integer host : m_hostsWithThisTable) {
+            builder.append(host).append(", ");
+        }
+        builder.setLength(builder.length() - 2);
+        return builder.toString();
+    }
+
     @Override
-    public boolean isConsistent()
-    {
+    public boolean isConsistent() {
         // right now there is nothing to check across all rows
         m_consistencyResult = "Table: " + getTableName() +
             " has consistent savefile state.";
@@ -60,30 +66,23 @@ public class ReplicatedTableSaveFileState extends TableSaveFileState
         return m_hostsWithThisTable;
     }
 
-    @Override
-    public SynthesizedPlanFragment[]
-    generateRestorePlan(Table catalogTable, SiteTracker st)
-    {
+    @Override public SynthesizedPlanFragment[]
+    generateRestorePlan(Table catalogTable, SiteTracker st) {
         for (int hostId : m_hostsWithThisTable) {
             m_sitesWithThisTable.addAll(st.getSitesForHost(hostId));
         }
 
         SynthesizedPlanFragment[] restore_plan = null;
-        if (catalogTable.getIsreplicated())
-        {
+        if (catalogTable.getIsreplicated()) {
             restore_plan = generateReplicatedToReplicatedPlan(st);
-        }
-        else
-        {
+        } else {
             restore_plan = generateReplicatedToPartitionedPlan(st);
         }
         return restore_plan;
     }
 
-    private void checkSiteConsistency(VoltTableRow row) throws IOException
-    {
-        if (!row.getString("IS_REPLICATED").equals("TRUE"))
-        {
+    private void checkSiteConsistency(VoltTableRow row) throws IOException {
+        if (! row.getString("IS_REPLICATED").equals("TRUE")) {
             String error = "Table: " + getTableName() + " was replicated " +
             "but has a savefile which indicates partitioning at site: " +
             row.getLong("CURRENT_HOST_ID");
@@ -92,9 +91,7 @@ public class ReplicatedTableSaveFileState extends TableSaveFileState
         }
     }
 
-    private SynthesizedPlanFragment[]
-    generateReplicatedToPartitionedPlan(SiteTracker st)
-    {
+    private SynthesizedPlanFragment[] generateReplicatedToPartitionedPlan(SiteTracker st) {
         SynthesizedPlanFragment[] restore_plan = null;
 
         Integer host = m_hostsWithThisTable.iterator().next();
@@ -131,52 +128,46 @@ public class ReplicatedTableSaveFileState extends TableSaveFileState
     }
 
     private SynthesizedPlanFragment[]
-    generateReplicatedToReplicatedPlan(SiteTracker st)
-    {
-        SynthesizedPlanFragment[] restore_plan = null;
-        Set<Long> execution_site_ids =
-            st.getAllSites();
-        Set<Long> sites_missing_table =
-            getSitesMissingTable(execution_site_ids);
-        // not sure we want to deal with handling expected load failures,
-        // so let's send an individual load to each site with the table
-        // and then pick sites to send the table to those without it
-        restore_plan =
-            new SynthesizedPlanFragment[execution_site_ids.size() + 1];
-        int restore_plan_index = 0;
-        for (Long site_id : m_sitesWithThisTable)
-        {
-            restore_plan[restore_plan_index] =
-                constructLoadReplicatedTableFragment();
-            restore_plan[restore_plan_index].siteId = site_id;
-            ++restore_plan_index;
+    generateReplicatedToReplicatedPlan(SiteTracker siteTracker) {
+        Set<Long> allSiteIds = siteTracker.getAllSites();
+        Set<Integer> hostsMissingTable = getHostsMissingTable(allSiteIds);
+        // send a fragment to all the sites with this table to load the local copy of this table.
+        // send a fragment to selected sites to send the table data to all the HOSTS
+        // (not sites) without this table to load.
+        int planFragmentCount = m_sitesWithThisTable.size() + hostsMissingTable.size() + 1;
+        SynthesizedPlanFragment[] restorePlan = new SynthesizedPlanFragment[planFragmentCount];
+        int planIndex = 0;
+        for (Long siteId : m_sitesWithThisTable) {
+            restorePlan[planIndex] = constructLoadReplicatedTableFragment();
+            restorePlan[planIndex].siteId = siteId;
+            ++planIndex;
         }
-        for (Long site_id : sites_missing_table)
-        {
-            long source_site_id =
-                m_sitesWithThisTable.iterator().next();
-            restore_plan[restore_plan_index] =
-                constructDistributeReplicatedTableAsReplicatedFragment(source_site_id,
-                                                           site_id);
-            ++restore_plan_index;
+        Iterator<Long> sourceSitePicker = m_sitesWithThisTable.iterator();
+        for (Integer hostId : hostsMissingTable) {
+            if (! sourceSitePicker.hasNext()) {
+                sourceSitePicker = m_sitesWithThisTable.iterator();
+            }
+            // pick a site to be responsible for sending the data.
+            long sourceSiteId = sourceSitePicker.next();
+            restorePlan[planIndex] =
+                constructDistributeReplicatedTableAsReplicatedFragment(
+                        sourceSiteId, hostId);
+            ++planIndex;
         }
-        assert(restore_plan_index == execution_site_ids.size());
-        restore_plan[restore_plan_index] =
+        assert(planIndex == planFragmentCount - 1);
+        restorePlan[planIndex] =
             constructLoadReplicatedTableAggregatorFragment(false);
-        return restore_plan;
+        return restorePlan;
     }
 
-    private Set<Long> getSitesMissingTable(Set<Long> clusterSiteIds)
-    {
-        Set<Long> sites_missing_table = new HashSet<Long>();
-        for (long site_id : clusterSiteIds)
-        {
-            if (!m_sitesWithThisTable.contains(site_id))
-            {
-                sites_missing_table.add(site_id);
+    private Set<Integer> getHostsMissingTable(Set<Long> clusterSiteIds) {
+        Set<Integer> hostsMissingTable = new HashSet<Integer>();
+        for (long siteId : clusterSiteIds) {
+            if (! m_sitesWithThisTable.contains(siteId)) {
+                hostsMissingTable.add(SiteTracker.getHostForSite(siteId));
             }
         }
-        return sites_missing_table;
+        return hostsMissingTable;
     }
 
     private SynthesizedPlanFragment
@@ -222,6 +213,7 @@ public class ReplicatedTableSaveFileState extends TableSaveFileState
     constructLoadReplicatedTableAggregatorFragment(boolean asPartitioned)
     {
         int result_dependency_id = getNextDependencyId();
+        //setRootDependencyId(resultDependencyId);
         SynthesizedPlanFragment plan_fragment = new SynthesizedPlanFragment();
         plan_fragment.fragmentId =
             SysProcFragmentId.PF_restoreReceiveResultTables;
