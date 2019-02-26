d3d6eeb4f6472c186b79dae52c38c431bffabcd0
JadenW <qwang@voltdb.com>
2019-02-26 15:43:11 -0500

    upgrade mockito and powerMock uptodate for java9+

--- tests/frontend/org/voltdb/export/TestExportDataSource.java
+++ tests/frontend/org/voltdb/export/TestExportDataSource.java
@@ -25,13 +25,13 @@ package org.voltdb.export;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.hamcrest.MockitoHamcrest.argThat;
 import static org.voltdb.export.ExportMatchers.ackPayloadIs;
 
 import java.io.File;
