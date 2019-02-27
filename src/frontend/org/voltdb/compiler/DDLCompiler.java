296e818b8ccc9545f20db24975a347f537892adf
Zeeman He <zhe@voltdb.com>
2019-02-27 10:46:00 -0500

    update error message

--- src/frontend/org/voltdb/compiler/DDLCompiler.java
+++ src/frontend/org/voltdb/compiler/DDLCompiler.java
@@ -1390,7 +1390,7 @@ public class DDLCompiler {
             final String migrationTarget = ttlNode.attributes.get("migrationTarget");
             if (!StringUtil.isEmpty(migrationTarget)) {
                 if (table.getIsreplicated()) {
-                    throw m_compiler.new VoltCompilerException("Nibble export is not supported for replicated table:" + table.getTypeName());
+                    throw m_compiler.new VoltCompilerException("'MIGRATE TO TARGET' is not supported for replicated table:" + table.getTypeName());
                 }
                 ttl.setMigrationtarget(migrationTarget);
                 table.setTabletype(TableType.PERSISTENT_MIGRATE.get());
