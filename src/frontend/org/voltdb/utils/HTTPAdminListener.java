c6cd44172d3f8f20271cf53256c63fb1893c054d
JadenW <qwang@voltdb.com>
2019-03-11 15:35:19 -0400

    update Session Management for jetty 9.4

--- src/frontend/org/voltdb/utils/HTTPAdminListener.java
+++ src/frontend/org/voltdb/utils/HTTPAdminListener.java
@@ -35,11 +35,9 @@ import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.security.ConstraintMapping;
 import org.eclipse.jetty.security.ConstraintSecurityHandler;
 import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.HttpChannel;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -61,8 +59,7 @@ import javax.servlet.SessionTrackingMode;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.gzip.GzipHandler;
-import org.eclipse.jetty.server.session.HashSessionIdManager;
-import org.eclipse.jetty.server.session.HashSessionManager;
+import org.eclipse.jetty.server.session.DefaultSessionIdManager;
 import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
@@ -82,8 +79,7 @@ public class HTTPAdminListener {
     static final String HTML_CONTENT_TYPE = "text/html;charset=utf-8";
 
     final Server m_server;
-    final HashSessionIdManager m_idmanager = new HttpSessionIdManager();
-    final HashSessionManager m_manager = new HashSessionManager();
+    final DefaultSessionIdManager m_idmanager;
     final SessionHandler m_sessionHandler = new SessionHandler();
 
     final HTTPClientInterface httpClientInterface = new HTTPClientInterface();
@@ -196,12 +192,12 @@ public class HTTPAdminListener {
                 );
 
         //Keep inactive session only for 10 seconds VMC will ping every 5 second and thus keep session alive.
-        m_manager.setMaxInactiveInterval(HTTPClientInterface.MAX_SESSION_INACTIVITY_SECONDS);
+        m_sessionHandler.setMaxInactiveInterval(HTTPClientInterface.MAX_SESSION_INACTIVITY_SECONDS);
+        m_idmanager = new HttpSessionIdManager(m_server);
         m_server.setSessionIdManager(m_idmanager);
         m_idmanager.setWorkerName("vmc");
-        m_manager.setSessionIdManager(m_idmanager);
+        m_sessionHandler.setSessionIdManager(m_idmanager);
         m_sessionHandler.setServer(m_server);
-        m_sessionHandler.setSessionManager(m_manager);
 
         m_mustListen = mustListen;
         // PRE-LOAD ALL HTML TEMPLATES (one for now)
@@ -242,14 +238,14 @@ public class HTTPAdminListener {
             rootContext.setMaxFormContentSize(HTTPClientInterface.MAX_QUERY_PARAM_SIZE);
             // close another attack vector where potentially one may send a large number of keys
             rootContext.setMaxFormKeys(HTTPClientInterface.MAX_FORM_KEYS);
-            rootContext.getSessionHandler().getSessionManager().getSessionCookieConfig().setHttpOnly(true);
+            rootContext.getSessionHandler().getSessionCookieConfig().setHttpOnly(true);
             //Only use cookie mode and dont support URL
             Set<SessionTrackingMode> trackModes = new HashSet<>();
             trackModes.add(SessionTrackingMode.COOKIE);
-            rootContext.getSessionHandler().getSessionManager().setSessionTrackingModes(trackModes);
+            rootContext.getSessionHandler().setSessionTrackingModes(trackModes);
             if (useSecure) {
                 //Make cookie secure when using SSL
-                rootContext.getSessionHandler().getSessionManager().getSessionCookieConfig().setSecure(useSecure);
+                rootContext.getSessionHandler().getSessionCookieConfig().setSecure(useSecure);
             }
 
             ContextHandler cssResourceHandler = new ContextHandler("/css");
@@ -370,7 +366,11 @@ public class HTTPAdminListener {
         }
     }
 
-    private static class HttpSessionIdManager extends HashSessionIdManager {
+    private static class HttpSessionIdManager extends DefaultSessionIdManager {
+
+        public HttpSessionIdManager(Server server) {
+            super(server);
+        }
 
         //remove all sessions in cache
         public void cleanSessions() throws Exception {
