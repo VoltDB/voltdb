023ab8e98c7ffb84d7108df9980b9a44c82c27ca
wweiss <wweiss@voltdb.com>
2019-03-14 13:51:27 -0400

    Missed unstaged file.

--- src/frontend/org/voltdb/TableType.java
+++ src/frontend/org/voltdb/TableType.java
@@ -40,6 +40,7 @@ public enum TableType {
     public static boolean isStreamViewOnly(int e) {
         return (e == STREAM_VIEW_ONLY.get());
     }
+
     public static boolean isStream(int e) {
         return (e == STREAM.get() || e == STREAM_VIEW_ONLY.get());
     }
