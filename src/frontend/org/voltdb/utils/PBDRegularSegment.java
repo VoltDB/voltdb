0db59c2b9f610bd73ae8ff759d03cb1b0a01d5c4
Manju James <manjujames@Manjus-MacBook-Pro.local>
2019-03-22 13:32:16 -0400

    ENG-15706: CRC was not getting reset in uncompressed mode, which resulted in a CRC error. In addition, the buffer wasn't getting discarded on a CRC error, which resulted in the error seen in the system test.

--- src/frontend/org/voltdb/utils/PBDRegularSegment.java
+++ src/frontend/org/voltdb/utils/PBDRegularSegment.java
@@ -585,6 +585,7 @@ public class PBDRegularSegment extends PBDSegment {
                     }
                     retcont.b().flip();
                     if (checkCRC) {
+                        m_crc32.reset();
                         m_crc32.update(length);
                         m_crc32.update(flags);
                         m_crc32.update(retcont.b());
@@ -596,6 +597,7 @@ public class PBDRegularSegment extends PBDSegment {
                             openForWrite(false);
                             initNumEntries(m_objectReadIndex, m_bytesRead);
                             m_fc.truncate(m_readOffset);
+                            retcont.discard();
                             return null;
                         }
                     }
