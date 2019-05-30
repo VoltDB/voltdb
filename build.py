0242e19b17d9816dce7c630bfa094018c8c8f575
Manju James <manjujames@Manjus-MacBook-Pro.local>
2019-05-30 10:24:32 -0400

    Options to make C++ compiler less strict to make compilation pass.

--- build.py
+++ build.py
@@ -41,8 +41,9 @@ CTX = BuildContext(sys.argv)
 # this does not include header/lib search paths or specific flags for
 #  specific targets
 CTX.CPPFLAGS += """-Wall -Wextra -Werror -Woverloaded-virtual
-            -Wpointer-arith -Wcast-qual -Wwrite-strings
+            -Wpointer-arith -Wno-cast-qual -Wwrite-strings
             -Winit-self -Wno-sign-compare -Wno-unused-parameter
+            -Wno-user-defined-warnings
             -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -DNOCLOCK
             -fno-omit-frame-pointer
             -fvisibility=default
