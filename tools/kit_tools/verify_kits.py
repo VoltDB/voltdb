94497ae2629751d857e1884d84590df94149e318
Jenkins CI <test@voltdb.com>
2019-11-20 13:28:23 -0500

    Set Version 7.7.5

--- tools/kit_tools/verify_kits.py
+++ tools/kit_tools/verify_kits.py
@@ -5,7 +5,7 @@ from fabric.api import run, cd, local, get, settings, lcd
 from fabric_ssh_config import getSSHInfoForHost
 
 verifydir = "/tmp/" + os.getenv('USER') + "/verifytemp"
-version = "7.7.4"
+version = "7.7.5"
 
 ################################################
 # SETUP A DIST & TOOLS IN A TEMP DIR
