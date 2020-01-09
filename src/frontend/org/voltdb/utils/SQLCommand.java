b319edc652a4d9d4f23171f2687541a695aaadc5
dave porter <dporter@voltdb.com>
2020-01-09 15:17:26 -0500

    strip trailing spaces

--- src/frontend/org/voltdb/utils/SQLCommand.java
+++ src/frontend/org/voltdb/utils/SQLCommand.java
@@ -383,7 +383,7 @@ public class SQLCommand {
                     execListConfigurations();
                     break;
                 */
-                
+
             case "tasks":
                     executeListTasks();
                     break;
