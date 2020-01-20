26031e1ab8d558684f3cb79ec831c5fcd6d03d4f
John McGrail <jmcgrail@voltdb.com>
2020-01-20 15:44:31 -0500

    ENG-18786: Eliminate the bulk of ExportOnServerVerifier
    The only code in use in ExportOnServerVerifier is the static
        VerifyRows()
    method.  Moved this to new RowVerifier class.

--- tests/test_apps/genqa/src/genqa/ExportRabbitMQVerifier.java
+++ tests/test_apps/genqa/src/genqa/ExportRabbitMQVerifier.java
@@ -154,7 +154,7 @@ public class ExportRabbitMQVerifier {
                 }
                 ValidationErr err = null;
                 try {
-                    err = ExportOnServerVerifier.verifyRow(row);
+                    err = RowVerifier.verifyRow(row);
                 } catch (ValidationErr validationErr) {
                     validationErr.printStackTrace();
                 }
