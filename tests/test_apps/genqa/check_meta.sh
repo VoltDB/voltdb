d294bc353d943f403fbd1783d7cfe58d16b052a7
Peter Shaw <pshaw@voltdb.com>
2020-01-31 08:19:51 -0500

    ENG-18894: WIP

--- tests/test_apps/genqa/check_meta.sh
+++ tests/test_apps/genqa/check_meta.sh
@@ -1,10 +1,10 @@
 echo 'dollar star is: ' $*
-rows=`cat $*/* | wc -l`
+rows=`cat $* | wc -l`
 tmpfile=`mktemp`
-echo "sort -u $*/* to $tmpfile"
-sort -u $*/* > $tmpfile
+echo "sort -u $* to $tmpfile"
+sort -u $* > $tmpfile
 dedupedrows=`cat $tmpfile | wc -l`
 duperows=$(($rows - $dedupedrows))
 echo Dupes: $duperows
 python 123-nodupcheck.py $tmpfile
-rm -f $tmpfile
+# rm -f $tmpfile
