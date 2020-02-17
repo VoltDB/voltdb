7abd44e88dfdea483925b5a0e5923ffcd7fd665b
Lukai Liu <lliu@voltdb.com>
2020-02-17 11:06:45 -0500

    CMake changes

--- src/ee/storage/TableTupleAllocator.hpp
+++ src/ee/storage/TableTupleAllocator.hpp
@@ -557,7 +557,7 @@ namespace voltdb {
             // NOTE: the deletion event need to happen before
             // calling add(...), unlike insertion/update.
             void add(CompactingChunks const&, ChangeType, void const*);
-            void const* operator()(void const*) const;               // revert history at this place!
+            void const* operator()(void const*) const;             // revert history at this place!
             void release(void const*);                             // local memory clean-up. Client need to call this upon having done what is needed to record current address in snapshot.
             // auxillary buffer that client must need for tuple deletion/update operation,
             // to hold value before change.
