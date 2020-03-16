7d40b51322262f22a45a4f1e21a2654d8d67b19c
John T. Crawford <johnTcrawford@gmail.com>
2020-03-16 02:46:02 -0400

    ENG-15712-scov-misc: Miscellaneous SqlCoverage issues
    Several improvements: a more complete fix for ENG-15712 ('timeout:
    procedure call took longer than 5 seconds'); a fix for ENG-19187, part 8
    ('TypeError: cannot concatenate 'str' and 'NoneType' objects')
    [SQLCoverageReport.py]; added test queries with (& without) LIMIT, per
    ENG-18533 [advanced-select.sql]; added test queries with GROUP BY,
    somewhat like ENG-18549, but without the '?' parameter, which
    SqlCoverage cannot handle [advanced-select.sql, advanced-ints.sql];
    trivial changes to grammar.sql.

--- tests/sqlcoverage/template/int/advanced-ints.sql
+++ tests/sqlcoverage/template/int/advanced-ints.sql
@@ -1,2 +1,5 @@
 <configure-for-ints.sql>
 <advanced-template.sql>
+
+-- Additional test of GROUP BY and aggregate functions, with CAST function (like ENG-18549, but w/o ?)
+SELECT _variable[#GB], CAST(_value[byte] AS INTEGER) + @agg(_variable[@columntype]) FROM @fromtables Q44 GROUP BY __[#GB]
