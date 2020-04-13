f084f4c4e1b534dff20f48ef250bef0e243effa5
Nate Clark <nclark@voltdb.com>
2020-04-13 09:27:23 -0400

    ENG-19355: Move ExportTupleStream to new StreamedTable
    When recreating a table because of a truncate if the table has a shadow
    stream the ExportTupleStream in the StreamedTable needs to be moved from
    the original StreamedTable to the new one. If the transaction is undone
    the stream needs to be moved back.

--- src/ee/storage/persistenttable.cpp
+++ src/ee/storage/persistenttable.cpp
@@ -225,6 +225,10 @@ void PersistentTable::truncateTableUndo(TableCatalogDelegate* tcd,
         unsetTableForStreamIndexing();
     }
 
+    if (m_shadowStream != nullptr) {
+        m_shadowStream->moveWrapperTo(originalTable->m_shadowStream);
+    }
+
     VoltDBEngine* engine = ExecutorContext::getEngine();
     auto views = originalTable->views();
     // reset all view table pointers
@@ -382,6 +386,10 @@ void PersistentTable::truncateTable(VoltDBEngine* engine, bool replicatedTable, 
         vassert(! emptyTable->hasPurgeFragment());
         boost::shared_ptr<ExecutorVector> evPtr = getPurgeExecutorVector();
         emptyTable->swapPurgeExecutorVector(evPtr);
+    }
+
+    if (m_shadowStream != nullptr) {
+        m_shadowStream->moveWrapperTo(emptyTable->m_shadowStream);
     }
 
     engine->rebuildTableCollections(replicatedTable, false);
