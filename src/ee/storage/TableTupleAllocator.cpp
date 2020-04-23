ab93e53372729220deeca6c41d282045d25de76a
wweiss <wweiss@voltdb.com>
2020-04-23 12:42:19 -0400

    ENG-19410: Stop updates from finalizing the txn memory tuple. This also requires a small rework of the delete path.

--- src/ee/storage/TableTupleAllocator.cpp
+++ src/ee/storage/TableTupleAllocator.cpp
@@ -1200,17 +1200,6 @@ inline size_t CompactingChunks::DelayedRemover::finalize() const {
             for_each(m_removed.cbegin(), m_removed.cend(),
                     [this](void const* p) { m_chunks.finalizerAndCopier().finalize(p); });
         }
-        if (! m_movements.empty()) {
-            /**
-             * The frozen case: finalize need to by called on
-             * destination unless a fresh entry is added to hook
-             * memory. This depends on CompactingChunks::add(...) to handle it
-             */
-            for_each(m_movements.cbegin(), m_movements.cend(),
-                    [this](pair<void*, void*> const& entry) {
-                        m_chunks.finalizerAndCopier().finalize(entry.first);
-                    });
-        }
         return m_removed.size() + m_movements.size();
     } else {
         return 0;
@@ -1878,9 +1867,6 @@ template<typename IteratorObserver, typename E2> inline void TxnPreHook<Alloc, T
                     m_finalizerAndCopier ? m_finalizerAndCopier.copy(fresh, dst) :
                     memcpy(fresh, dst, m_changeStore.tupleSize()));
         }
-    }
-    if (m_recording && m_finalizerAndCopier) {
-        m_finalizerAndCopier.finalize(dst);
     }
 }
 
