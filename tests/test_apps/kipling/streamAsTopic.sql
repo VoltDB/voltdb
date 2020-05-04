438a335fbb5497f8422961dbfba97a65a453bb8c
root@pshaw-DH310 <pshaw@voltdb.com>
2020-05-04 15:09:24 -0400

    ENG-19404: more cleanup/remove extraneous files

--- tests/test_apps/kipling/streamAsTopic.sql
+++
@@ -1,8 +0,0 @@
-CREATE STREAM  T8_KAFKAEXPORTTABLENUM partition on column key as topic
-     (
-                  KEY   BIGINT NOT NULL,
-                  value BIGINT NOT NULL,
-                  insert_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                  CONSTRAINT pk_kafka_export_tableNUM PRIMARY KEY ( KEY )
-     );
-
