8397be3df8147ad2a7ddd07bb4879b6f4ca197b8
Nate Clark <nclark@voltdb.com>
2020-06-16 14:08:05 -0400

    ENG-19743: Backport: Unregister dr consumer from global service leader
    [ backport 79475e81fb52bb207d0d43bf1ac1e969dc5650ab ]
    
    When a cluster is promoted the ConsumerDRGateway is shutdown and set to
    null in RealVoltDB, however an instace of the gateway is still
    registered with the GlobalServiceElector. When the gateway is shutdown
    deregister it from the GlobalServiceElector.

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -4340,6 +4340,7 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, HostM
                 hostLog.warn("Interrupted shutting down dr replication", e);
             }
             finally {
+                m_globalServiceElector.unregisterService(m_consumerDRGateway);
                 m_consumerDRGateway = null;
             }
         }
