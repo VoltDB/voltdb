48c6216f585424bcd9b4e3be840b5573b0bfe15c
Zeeman He <zhe@voltdb.com>
2020-06-23 07:50:49 -0400

    ENG-19758

--- src/frontend/org/voltdb/exportclient/JDBCExportClient.java
+++ src/frontend/org/voltdb/exportclient/JDBCExportClient.java
@@ -603,9 +603,7 @@ public class JDBCExportClient extends ExportClientBase {
                     throw new RestartBlockException(true);
                 }
             }
-            if (!ignoreGenerations) {
-                checkSchemas();
-            }
+            checkSchemas();
         }
 
         @Override
@@ -820,6 +818,9 @@ public class JDBCExportClient extends ExportClientBase {
 
         @Override
         public void sourceNoLongerAdvertised(AdvertisedDataSource source) {
+            if (m_es == null) {
+                return;
+            }
             m_es.shutdown();
             try {
                 if (!m_es.awaitTermination(SHUTDOWN_TIMEOUT_S, TimeUnit.SECONDS)) {
