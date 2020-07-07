df0f7d5e8c6e4e18c7f70328e823701abecbc3f1
John Wiebe <jwiebe@voltdb.com>
2020-07-07 10:13:22 -0400

    scripts for building and running kvbenchmark inside a docker container

---
+++ tests/test_apps/kvbenchmark/build_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -x
+
+mkdir -p voltdb/lib
+mkdir -p voltdb/bin
+mkdir -p voltdb/voltdb
+VOLTDB_HOME=../../../
+if [ ! -e voltdb/lib/felix-framework-5.6.12.jar ]; then
+    cp $VOLTDB_HOME/lib/*.jar voltdb/lib
+fi
+if [ ! -e voltdb/bin/sqlcmd ]; then
+    cp $VOLTDB_HOME/bin/sqlcmd voltdb/bin/
+fi
+cp $VOLTDB_HOME/voltdb/* voltdb/voltdb/
+#cp $VOLTDB_HOME/voltdb/log4j.xml  voltdb/
+
+#cp $VOLTDB_HOME/voltdb/license.xml voltdb/
+./run.sh jars
+
+docker build -t voltdb/kvbenchmark .
+
+#docker run -it -v $(pwd):/build --network=voltdb_network -p 21212:21212 -p 21211:21211 -p 8081:8080 -p 3021:3021 -p 5555:5555 -p 7181:7181 -w /build --name="voltdb" voltdb/voltdb-enterprise:latest bash -c "cd /opt/voltdb/bin && ./voltdb init && ./voltdb start"
+# verify it is reachable locally:
+#/workspace/VoltDB/voltdb/bin (master)$>./sqlcmd
+#SQL Command :: localhost:21212
+#1>
+# verify we can reach it from another container
+# docker run --network=voltdb_network --name=kvbenchmark jwiebe/kvbenchmark
+
+# try sqlcmd on inside that container
+#docker exec kvbenchmark bash -c "/kvbenchmark/voltdb/bin/sqlcmd --servers=voltdb --query='select * from store'"
+
+#select * from store;
+#SQL error while compiling query: Error in "select * from store" - object not found: STORE
+
+# ~/workspace/VoltDB/voltdb/tests/test_apps/kvbenchmark (master)$>docker exec kvbenchmark /usr/bin/java -cp /kvbenchmark/kvbenchmark.jar:/kvbenchmark/voltdb/voltdb/voltdb-10.0.beta5.jar:/kvbencharmmark/voltdb/lib/*.jar kvbench.SyncBenchmark --servers=voltdb
---
+++ tests/test_apps/kvbenchmark/docker_run.sh
@@ -0,0 +1,58 @@
+#/bin/sh
+# this file is used as an entry point inside a docker container
+# execute the application
+# example usage:
+# create a network for the containers to talk over
+# > docker network create --driver bridge voltdb_network
+# start volt
+# > docker run -it -v $(pwd):/build --network=voltdb_network -p 21212:21212 -p 21211:21211 -p 8081:8080 -p 3021:3021 -p 5555:5555 -p 7181:7181 -w /build --name="voltdb" voltdb/voltdb-enterprise:latest bash -c "cd /opt/voltdb/bin && ./voltdb init && ./voltdb start"
+# build the docker image with necessary volt files.
+# > build_docker.sh voltdb/kvbenchmark
+# this file will get run by default with no command options.
+# > docker run --rm --network=voltdb_network --name kvbenchmark -w /kvbenchmark -e SERVERS=voltdb voltdb/kvbenchmark
+# to debug the contents of the kvbenchmark data
+# > docker run --rm --network=voltdb_network --name kvbenchmark -w /kvbenchmark -e SERVERS=voltdb voltdb/kvbenchmark  voltdb/bin/sqlcmd --servers=voltdb --query='select key from store limit 1'
+
+
+set -x
+[ -z "$LOG4J" ] && LOG4J=voltdb/log4j.xml
+[ -z "$DISPLAYINTERVAL" ] && DISPLAYINTERVAL=5
+[ -z "$SERVERS" ] && SERVERS=localhost && echo "SERVERS:$SERVERS"
+[ -z "$DURATION" ] && DURATION=120
+[ -z "$POOLSIZE" ] && POOLSIZE=100000
+[ -z "$PRELOAD" ] && PRELOAD=true
+[ -z "$GETPUTRATIO" ] && GETPUTRATIO=0.90
+[ -z "$KEYSIZE" ] && KEYSIZE=32
+[ -z "$MINVALUESIZE" ] && MINVALUESIZE=1024
+[ -z "$MAXVALUESIZE" ] && MAXVALUESIZE=1024
+[ -z "$USECOMPRESSION" ] && USECOMPRESSION=false
+[ -z "$THREADS" ] && THREADS=40
+[ -z "$CSVFILE" ] && CSVFILE=periodic.csv.gz
+[ -z "$TOPOLOGYAWARE" ] && TOPOLOGYAWARE=true
+[ -z "$LOG4J" ] && LOG4J=voltdb/log4j.xml
+[ -z "$VJAR" ] && VJAR=`ls -1 voltdb/voltdb/*.jar | grep "voltdb-"`
+
+voltdb/bin/sqlcmd --servers=$SERVERS --query="select key from store limit 1"
+if [ $? != 0 ]; then
+    echo "loading ddl"
+    voltdb/bin/sqlcmd < ddl.sql
+else
+    voltdb/bin/sqlcmd --query="truncate table store"
+fi
+JAVA=`which java`
+$JAVA -classpath kvbenchmark.jar:$VJAR:voltdb/lib/*.jar -Dlog4j.configuration=file://$LOG4J \
+        kvbench.SyncBenchmark \
+        --displayinterval=$DISPLAYINTERVAL \
+        --duration=$DURATION \
+        --servers=$SERVERS \
+        --poolsize=$POOLSIZE \
+        --preload=$PRELOAD \
+        --getputratio=$GETPUTRATIO \
+        --keysize=$KEYSIZE \
+        --minvaluesize=$MINVALUESIZE \
+        --maxvaluesize=$MAXVALUESIZE \
+        --usecompression=$USECOMPRESSION \
+        --threads=$THREADS \
+        --csvfile=periodic.csv.gz \
+        --topologyaware=$TOPOLOGYAWARE
+
