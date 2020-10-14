5bc1436197cede97711a56eeb48420fe0cf789a9
Nate Clark <nclark@voltdb.com>
2020-10-14 17:28:23 -0400

    ENG-20120: Update consumer.key property key

--- tests/frontend/org/voltdb/LocalClustersTestBase.java
+++ tests/frontend/org/voltdb/LocalClustersTestBase.java
@@ -38,6 +38,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.function.LongConsumer;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -98,7 +99,7 @@ public class LocalClustersTestBase extends JUnit4LocalClusterTest {
 
     public static final MessageFormat TOPIC_FMT = new MessageFormat(
             "create stream {0}" + TOPIC_TAG + "{1} partition on column key (key bigint not null, value bigint not null);"
-                    + "create topic using stream {0}" + TOPIC_TAG + "{1} {2} PROPERTIES (key.consumer.columns=key);"
+                    + "create topic using stream {0}" + TOPIC_TAG + "{1} {2} PROPERTIES (consumer.key=key);"
                     + "create procedure " + INSERT_PREFIX + "{0}" + TOPIC_TAG + "{1} as insert into {0}" + TOPIC_TAG + "{1} (key, value) values (?, ?);");
 
     // Track the current running clusters so they can be reused between tests if the configuration doesn't change
@@ -356,9 +357,11 @@ public class LocalClustersTestBase extends JUnit4LocalClusterTest {
             throws Exception {
         String schemaDDL = createSchemaDDL(partitionedTableCount, replicatedTableCount, topicsCount, streamTargets);
 
-        for (Pair<LocalCluster, Client> clusterAndClient : CLUSTERS_AND_CLIENTS) {
-            Client client = clusterAndClient.getSecond();
-            assertEquals(ClientResponse.SUCCESS, callDMLProcedure(client, "@AdHoc", schemaDDL).getStatus());
+        if (StringUtils.isNotBlank(schemaDDL)) {
+            for (Pair<LocalCluster, Client> clusterAndClient : CLUSTERS_AND_CLIENTS) {
+                Client client = clusterAndClient.getSecond();
+                assertEquals(ClientResponse.SUCCESS, callDMLProcedure(client, "@AdHoc", schemaDDL).getStatus());
+            }
         }
     }
 
