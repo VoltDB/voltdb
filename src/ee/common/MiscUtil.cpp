6ac8e940148220fb8d0456272ebf0437b2aa9da7
Lukai Liu <lliu@voltdb.com>
2020-10-30 13:35:03 -0400

    Added EE micro profiler (#147)
    * Added EE micro profiler
    
    * Added detailed documentation

--- src/ee/common/MiscUtil.cpp
+++ src/ee/common/MiscUtil.cpp
@@ -15,16 +15,18 @@
  * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics.hpp>
 #include "common/MiscUtil.h"
 
 namespace voltdb {
-
-std::vector<std::string> MiscUtil::splitString(const std::string &str, char delimiter) {
-    std::vector<std::string> vec;
+using namespace std;
+vector<string> MiscUtil::splitString(string const& str, char delimiter) {
+    vector<string> vec;
     size_t begin = 0;
     while (true) {
         size_t end = str.find(delimiter, begin);
-        if (end == std::string::npos) {
+        if (end == string::npos) {
             if (begin != str.size()) {
                 vec.push_back(str.substr(begin));
             }
@@ -36,16 +38,221 @@ std::vector<std::string> MiscUtil::splitString(const std::string &str, char deli
     return vec;
 }
 
-std::vector<std::string> MiscUtil::splitToTwoString(const std::string &str, char delimiter) {
-    std::vector<std::string> vec;
+vector<string> MiscUtil::splitToTwoString(string const& str, char delimiter) {
+    vector<string> vec;
     size_t end = str.find(delimiter);
-    if (end == std::string::npos) {
+    if (end == string::npos) {
         vec.push_back(str);
     } else {
         vec.push_back(str.substr(0, end));
         vec.push_back(str.substr(end + 1));
     }
     return vec;
+}
+
+AbstractTimer::AbstractTimer(FILE* sink, char const* op):
+    m_fp(sink), m_operation(op), m_started(now()) {}
+
+double AbstractTimer::format_duration(usec const& elapsed, char const*& unit) {
+    static char const* usec = "usec", *msec = "msec";
+    auto const val = elapsed.count();
+    if (val < 1e3) {
+        unit = usec;
+        return val;
+    } else {
+        unit = msec;
+        return val / 1e3;
+    }
+}
+
+SimpleTimer::~SimpleTimer() {
+    report(diff(m_started, now()));
+}
+
+void SimpleTimer::report(usec const& elapsed) const {
+    char const* unit;
+    auto const val = format_duration(elapsed, unit);
+    fprintf(file(), "%s took %.1g %s\n", m_operation.c_str(), val, unit);
+}
+
+using namespace boost::accumulators;
+struct statistics_calculator {
+    accumulator_set<double,
+        features<tag::count, tag::min, tag::max, tag::mean, tag::median>> m_stats;
+public:
+    template<typename iterator>
+    statistics_calculator(iterator from, iterator to) {
+        for_each(from, to, bind<void>(ref(m_stats), std::placeholders::_1));
+    }
+#define getter(T, name)                              \
+    T name() const noexcept {                        \
+        return extract_result<tag::name>(m_stats);   \
+    }
+    getter(size_t, count);
+    getter(double, min);
+    getter(double, max);
+    getter(double, mean);
+    getter(double, median);
+#undef getter
+};
+
+/**
+ * Use unit 1 with given stat values, if average value is below 5000;
+ * use unit 2 with values divided by 1000 otherwise
+ */
+string to_string(char const* prefix, char const* unit1, char const* unit2,
+        statistics_calculator const& stat) {
+    ostringstream oss;
+    bool const converted = stat.mean() > 1e3;
+    char const* unit = converted ? unit2 : unit1;
+    if (converted) {
+        // convert from usec to msec
+        oss << prefix << "{ count = " << stat.count()
+            << ", min = " << stat.min() / 1e3 << " " << unit
+            << ", max = " << stat.max() / 1e3 << " " << unit
+            << ", mean = " << stat.mean() / 1e3 << " " << unit
+            << ", median = " << stat.median() / 1e3 << " " << unit
+            << "}\n";
+    } else {
+        oss << prefix << "{ count = " << stat.count()
+            << ", min = " << stat.min() << " " << unit
+            << ", max = " << stat.max() << " " << unit
+            << ", mean = " << stat.mean() << " " << unit
+            << ", median = " << stat.median() << " " << unit
+            << "}\n";
+    }
+    return oss.str();
+}
+
+RestartableTimer::RestartableTimer(FILE* sink, char const* op,
+        size_t periodic_report, size_t binWidth):
+    AbstractTimer(sink, op),
+    m_flushPeriod(periodic_report), m_binWidth(binWidth) {
+    m_elapsed.reserve(m_flushPeriod * 64);
+}
+
+void RestartableTimer::report(bool final_report) const {
+    auto const* prefix = final_report ? "Final" : "Periodic";
+    if (count()) {
+        fprintf(file(), "[%s] %s summary:\n# # # # # # # # # # %s\n# # # # # # # # # #\n",
+                m_operation.c_str(), prefix,
+                statistics(const_cast<std::vector<size_t>&>(m_elapsed),
+                    m_binWidth).c_str());
+    } else {
+        fprintf(file(), "[%s] %s summary: No timed events occurred\n",
+                m_operation.c_str(), prefix);
+    }
+}
+
+void RestartableTimer::stop() {
+    assert(m_active);
+    m_elapsed.emplace_back(diff(m_started, now()).count());
+    m_active = false;
+    if (m_flushPeriod && count() % m_flushPeriod == 0) {
+        report(false);
+    }
+}
+
+void RestartableTimer::restart() {
+    assert(! m_active);
+    m_active = true;
+    const_cast<time_point&>(m_started) = now();
+}
+
+size_t RestartableTimer::count() const noexcept {
+    return m_elapsed.size();
+}
+
+RestartableTimer::~RestartableTimer() {
+    if (active()) {
+        stop();
+    }
+    report(true);
+}
+
+inline RestartableTimer::ScopedTimer::ScopedTimer(RestartableTimer& tm) noexcept:
+    m_tm(tm) {
+    m_tm.restart();
+}
+
+RestartableTimer::ScopedTimer::~ScopedTimer() noexcept {
+    m_tm.stop();
+}
+
+string RestartableTimer::statistics(
+        vector<size_t> const& elapsed, size_t bin_size) {
+    using iterator = typename vector<size_t>::const_iterator;
+    size_t from = 0;
+    size_t const full = elapsed.size(),
+           est_bin_size = full / bin_size + 1;
+    vector<statistics_calculator> bin_stats;
+    bin_stats.reserve(est_bin_size);
+    vector<pair<size_t, size_t>> bin_positions;
+    bin_positions.reserve(est_bin_size);
+    while (from < full) {
+        auto const to = std::min(from + bin_size, full);
+        bin_stats.emplace_back(next(elapsed.begin(), from), next(elapsed.begin(), to));
+        bin_positions.emplace_back(from, to);
+        from = to;
+    }
+    ostringstream oss;
+    auto const full_stat = statistics_calculator{elapsed.begin(), elapsed.end()};
+    oss << to_string("Total statistics\n# # # # # # # # # #\n", "usec", "msec", full_stat)
+        << "\nBreak-down statistics\n# # # # # # # # # #\n";
+    for (size_t i = 0; i < bin_positions.size(); ++i) {
+        string prefix("[#");
+        oss << to_string(
+                prefix.append(std::to_string(bin_positions[i].first))
+                .append(" - #").append(std::to_string(bin_positions[i].second - 1))
+                .append("]: ").c_str(),
+                "usec", "msec", bin_stats[i])
+            << endl;
+    }
+    return oss.str();
+}
+
+RestartableTimers::RestartableTimers(FILE* sink, char const* op,
+        size_t binWidth, vector<string> const&& names):
+    super(sink, op, 0/* disables periodic report */, binWidth) {
+    if (names.empty()) {
+        printf("Warning: named timers not provided");
+    } else {
+        for_each(names.cbegin(), names.cend(),
+                [sink, op, binWidth, this] (string const& nm) {
+                    if (! m_subTimers.emplace(make_pair(nm,
+                                RestartableTimer(sink,
+                                        string(op).append(".").append(nm).c_str(),
+                                        0, binWidth))).second) {
+                        char buf[128];
+                        snprintf(buf, sizeof buf,
+                                "Duplicated name \"%s\" found for RestartableTimers",
+                                nm.c_str());
+                        buf[sizeof buf - 1] = '\0';
+                        throw logic_error(buf);
+                    }
+                });
+    }
+}
+
+typename RestartableTimer::ScopedTimer RestartableTimers::get(char const* k) {
+    auto iter = m_subTimers.find(k);
+    if (iter == m_subTimers.cend()) {
+        char buf[128];
+        snprintf(buf, sizeof buf, "Cannot find sub-timer named \"%s\". Typo?", k);
+        buf[sizeof buf - 1] = '\0';
+        throw logic_error(buf);
+    } else {
+        return iter->second.get();
+    }
+}
+
+inline typename RestartableTimer::ScopedTimer RestartableTimer::ScopedTimer::create(
+        RestartableTimer& tm) noexcept {
+    return {tm};
+}
+
+typename RestartableTimer::ScopedTimer RestartableTimer::get() {
+    return ScopedTimer::create(*this);
 }
 
 } // namespace voltdb
--- src/ee/common/MiscUtil.h
+++ src/ee/common/MiscUtil.h
@@ -17,10 +17,12 @@
 
 #pragma once
 
+#include <chrono>
 #include <string>
 #include <vector>
 
 #include "boost/functional/hash.hpp"
+#include "common/debuglog.h"
 
 namespace voltdb {
 
@@ -28,39 +30,239 @@ namespace voltdb {
  * Miscellaneous static utility methods.
  */
 class MiscUtil {
-
   public:
 
     /**
      * Split string on delimiter into any number of sub-strings.
      */
-    static std::vector<std::string> splitString(const std::string &str, char delimiter);
+    static std::vector<std::string> splitString(std::string const& str, char delimiter);
 
     /**
      * Split string on delimiter into two sub-strings.
      */
-    static std::vector<std::string> splitToTwoString(const std::string &str, char delimiter);
+    static std::vector<std::string> splitToTwoString(std::string const& str, char delimiter);
 
     /**
      * A hashCombine function that can deal with the quirks of floating point math
      * on the various platforms that we support.
      */
     static void hashCombineFloatingPoint(std::size_t &seed, double value) {
-        // This method was observed to fail on Centos 5 / GCC 4.1.2, returning different hashes
-        // for identical inputs, so the conditional was added,
-        // mutated from the one in boost/type_traits/intrinsics.hpp,
-        // and the broken overload for "double" was by-passed in favor of the more reliable
-        // one for int64 -- even if this may give sub-optimal hashes for typical collections of double.
-        // This conditional can be dropped when Centos 5 support is dropped.
-#if defined(__GNUC__) && ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 2) && !defined(__GCCXML__))) && !defined(BOOST_CLANG)
-        boost::hash_combine( seed, value);
+#if defined(__GNUC__) && ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 2) && ! defined(__GCCXML__))) && ! defined(BOOST_CLANG)
+        boost::hash_combine(seed, value);
 #else
-        {
-            const int64_t proxyForDouble =  *reinterpret_cast<const int64_t*>(&value);
-            boost::hash_combine(seed, proxyForDouble);
-        }
+#error "Unsupported compiler"
 #endif
     }
+};
+
+/**
+ * As the name suggests, this is not ready for use.
+ * Use one of the following sub-classes, and create
+ * the timer with the help of TimerProxy instead.
+ */
+class AbstractTimer {
+protected:
+    using time_point = std::chrono::time_point<std::chrono::steady_clock>;
+    using usec = std::chrono::duration<double, std::micro>;
+    FILE* m_fp;
+    std::string const m_operation;
+    time_point const m_started;
+
+    static time_point now() noexcept {
+        return std::chrono::steady_clock::now();
+    }
+    static usec diff(time_point const& from, time_point const& to) {
+        return std::chrono::duration_cast<usec>(to - from);
+    }
+    static double format_duration(usec const&, char const*&);
+public:
+    FILE* file() const noexcept {
+        return m_fp;
+    }
+    AbstractTimer(FILE*, char const*);
+};
+
+/**
+ * A simple, one-shot timer, which starts at object creation
+ * time, and stop at destruction time by RAII.
+ * Timing is written to the file handle, which is not validated
+ * nor closed.
+ *
+ * Use TimerProxy<SimpleTimer> instead of SimpleTimer for safer
+ * file handling.
+ */
+class SimpleTimer: public AbstractTimer {
+    void report(usec const&) const;
+public:
+    SimpleTimer(FILE* sink, char const* op) : AbstractTimer(sink, op) {}
+    ~SimpleTimer();
+};
+
+/**
+ * A RestartableTimer is a timer which, is not automatically
+ * started upon RestartableTimer object creation. Instead, you
+ * call RestartableTimer::get() to get a fire of the timer. It,
+ * too, is managed using RAII; but RestartableTimer::get() may be
+ * called as many times as you like. When the RestartableTimer
+ * object gets destructed, a detailed report is written to the
+ * file handle.
+ * Use TimerProxy<RestartableTimer> instead of bare
+ * RestartableTimer for safer file handling.
+ */
+class RestartableTimer: public AbstractTimer {
+    bool m_active = false;
+    size_t const m_flushPeriod, m_binWidth;
+    std::vector<size_t> m_elapsed;
+    static std::string statistics(std::vector<size_t> const&, size_t bin_size);
+    void report(bool final_report) const;
+    void stop();
+    void restart();
+protected:
+    bool active() const noexcept {
+        return m_active;
+    }
+public:
+    /**
+     * Ctor.
+     * \arg file handle
+     * \arg name of current restartable timer
+     * \arg period - statistics calculation/flushing period.
+     * For example, if get() is called 1,000 times, you get
+     * 1,000 timing events. Then, if period = 200, a detailed
+     * report is generated every 200 fires.
+     * Set to 0 to disable periodic flushing.
+     *
+     * \arg binWidht - statististics aggregation bin width. In
+     * the detailed report, it summarizes important statistics of
+     * **all** firings, as well as firings as separated into
+     * non-overlapping bins. For example, if get() is called
+     * 1,000 times cumulatively, with a bin width of 200, then, a
+     * full report of statistics based on 1,000 timings as well
+     * as every 200 timings are calculated and presented in the
+     * report.
+     */
+    RestartableTimer(FILE*, char const*, size_t period, size_t binWidth);
+    size_t count() const noexcept;
+    class ScopedTimer {
+        RestartableTimer& m_tm;
+        ScopedTimer(RestartableTimer&) noexcept;
+    public:
+        static ScopedTimer create(RestartableTimer&) noexcept;
+        ~ScopedTimer() noexcept;
+    };
+    /**
+     * Usage:
+     * {
+     *   auto tm = instance_RestartableTimer.get();
+     *   ...
+     * }
+     */
+    ScopedTimer get();
+    ~RestartableTimer();
+};
+
+/**
+ * A total, RestartableTimer that also includes any number of
+ * one-level, named, sub-timers that is scoped by the "total"
+ * timer.
+ *
+ * e.g.
+ * RestartableTimer totalTm("TotalTimer", "stdout",
+ *      512, // summarize in every 512-events window
+ *      {{"foo", "bar", "baz"}});   // sub timers
+ *  ...
+ *  {
+ *      // start total timer, the same way as RestartableTimer
+ *      auto const totalTm = TotalTm.get();
+ *      if (foo) {
+ *          // start named sub timer
+ *          auto const fooTm = TotalTm.get("foo");
+ *          ...
+ *      }
+ *      while (bar) {
+ *          // start anoher named sub timer
+ *          auto const barTm = TotalTm.get("bar");
+ *      }
+ *  }
+ */
+class RestartableTimers: RestartableTimer {
+    using super = RestartableTimer;
+    std::unordered_map<std::string, super> m_subTimers;
+public:
+    RestartableTimers(FILE*, char const*, size_t,
+            std::vector<std::string> const&&/* named sub-timers */);
+    using super::get; using super::file;
+    typename super::ScopedTimer get(char const*);
+};
+
+/**
+ * Wrapper that scopes file handle, by requesting for file name
+ * only, and transforms into 2nd arg of respective type's ctor.
+ *
+ * e.g.
+ * auto tm = TimerProxy<RestartableTimer>("/tmp/foo.z", "Timer", 0,
+ *          500);
+ *  // fwds to:
+ *  // RestartableTimer tm(fopen("/tmp/foo.z", "w"), "Timer", 0,
+ *                  500);
+ *  // only with scoped control of fclose when tm is destructed.
+ *
+ *  When the 1st \arg sink is "stdout" or "stderr", the standard
+ *  output/error instead of actual file is used for writing
+ *  report.
+ */
+template<typename T>
+class TimerProxy {
+#ifndef NDEBUG
+    // debug build fwds to underlying timer
+    T* m_instance;
+public:
+    template<typename... Args> inline
+    TimerProxy(char const* sink, Args&&... args):
+        m_instance(new T{! strcmp(sink, "stdout") ? stdout :
+                ! strcmp(sink, "stderr") ? stderr : fopen(sink, "w"),
+            std::forward<Args&&>(args)...}) {
+        if (! m_instance->file()) {
+            char buf[128];
+            snprintf(buf, sizeof buf,
+                    "Failed to open sink \"%s\" for write or append", sink);
+            buf[sizeof buf - 1] = '\0';
+            throw std::runtime_error(buf);
+        }
+    }
+    ~TimerProxy() {
+        auto* fp = m_instance->file();
+        delete m_instance;
+        if (fp) {
+            if (fp != stdout && fp != stderr) {
+                fclose(fp);
+            } else {
+                fflush(fp);
+            }
+        }
+    }
+    T& operator()() const {
+        return *m_instance;
+    }
+#else
+    // release build uses zero-cost dummy object that does
+    // absolutely nothing. The class template arg T is simply
+    // ignored.
+    struct DummyTimer {
+        constexpr bool get(char const*) const noexcept {
+            return get();
+        }
+        constexpr bool get() const noexcept {
+            return false;
+        }
+    } m_dummyTimer;
+public:
+    template<typename... Args> inline constexpr
+    TimerProxy(char const*, Args&&...) noexcept {}
+    constexpr DummyTimer const& operator()() const noexcept {
+        return m_dummyTimer;
+    }
+#endif
 };
 
 } // namespace voltdb
