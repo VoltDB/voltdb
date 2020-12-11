4dbcda2ba91ef268bd899599ffa3546fc88bb91e
Nate Clark <nclark@voltdb.com>
2020-12-10 23:00:24 -0500

    ENG-20411: Add encoders for topics
    Add interfaces and simple implementations for encoding a tuple for
    topics. This includes support for int, bigint, double, string and
    varbinary columns.

---
+++ src/ee/topics/encode/Encoder.h
@@ -0,0 +1,229 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2020 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "common/ExportSerializeIo.h"
+#include "common/tabletuple.h"
+
+namespace voltdb { namespace topics {
+
+/**
+ * Interface for serializing a TableTuple into an ExportSerializeOutput
+ */
+class TupleEncoder {
+public:
+    virtual ~TupleEncoder() {}
+
+    /**
+     * @param tuple that will be serialized
+     * @return the maximum size that the tuple will consume when serialized or -1 if the value to be encoded is null
+     */
+    virtual int32_t maxSizeOf(const TableTuple& tuple) = 0;
+
+    /**
+     * @param tuple that will be serialized
+     * @return the exact size that the tuple will consume when serialized or -1 if tuple to be encoded is null
+     */
+    virtual int32_t exactSizeOf(const TableTuple& tuple) {
+        return maxSizeOf(tuple);
+    }
+
+    /**
+     * Serialize tuple into out
+     *
+     * @param out to write serialized data
+     * @param tuple to serialize to out
+     * @return amount of data written  or -1 if the value encoded was null
+     */
+    virtual int32_t encode(ExportSerializeOutput& out, const TableTuple& tuple) = 0;
+};
+
+/**
+ * Interface for serializing a NValue into an ExportSerializeOutput
+ */
+class NValueEncoder {
+public:
+    virtual ~NValueEncoder() {}
+
+    /**
+     * @param value that will be serialized
+     * @return the maximum size that the value will consume when serialized or -1 if value to be encoded is null
+     */
+    virtual int32_t maxSizeOf(const NValue& value) = 0;
+
+    /**
+     * @param value that will be serialized
+     * @return the exact size that the value will consume when serialized or -1 if value to be encoded is null
+     */
+    virtual int32_t exactSizeOf(const NValue& value) {
+        return maxSizeOf(value);
+    }
+
+    /**
+     * Serialize tuple into out
+     *
+     * @param out to write serialized data
+     * @param value to serialize to out
+     * @return amount of data written or -1 if value to be encoded is null
+     */
+    virtual int32_t encode(ExportSerializeOutput& out, const NValue& value) = 0;
+
+protected:
+    /**
+     * Utility method for calculating the max variable length serialized size of an integer type when zigzag encoding is used
+     */
+    template <typename type>
+    static int32_t maxSerializedSizeOfVarInt() {
+        return (sizeof(type) * 8 + 6) / 7;
+    }
+    /**
+     * Utility method for calculating the serialized size of an integer value written in a zigzag variable length encoding
+     *
+     * https://en.wikipedia.org/wiki/Variable-length_quantity#Zigzag_encoding
+     */
+    static int32_t serializedSizeOfVarInt(int64_t value) {
+        int64_t v = ((value << 1) ^ (value >> 63));
+        size_t bytes = 1;
+        while (v >>= 7) {
+            ++bytes;
+        }
+        return bytes;
+    }
+};
+
+/**
+ * TupleEncoder which does not encode anything and always returns -1 to indicate value is null
+ */
+class NullEncoder : public TupleEncoder {
+public:
+    NullEncoder() = default;
+
+    int32_t maxSizeOf(const TableTuple& tuple) override {
+        return -1;
+    }
+
+    int32_t encode(ExportSerializeOutput& out, const TableTuple& tuple) override {
+        return -1;
+    }
+};
+
+/**
+ * Simple tuple encoder which uses a single NValueEncoder to encode one value at a constant index from the tuple
+ */
+template <class ENCODER>
+class SingleValueEncoder : public TupleEncoder {
+public:
+    SingleValueEncoder(int32_t index) : m_index(index) {};
+
+    int32_t maxSizeOf(const TableTuple& tuple) override {
+        const NValue value = tuple.getNValue(m_index);
+        if (value.isNull()) {
+            return -1;
+        }
+        return m_encoder.maxSizeOf(value);
+    }
+
+    int32_t encode(ExportSerializeOutput& out, const TableTuple& tuple) override {
+        const NValue value = tuple.getNValue(m_index);
+        if (value.isNull()) {
+            return -1;
+        }
+        return m_encoder.encode(out, value);
+    }
+
+private:
+    ENCODER m_encoder;
+    const int32_t m_index;
+};
+
+/**
+ * NValueEncoder for encoding ValueType::tINTEGER
+ */
+class IntEncoder : public NValueEncoder {
+public:
+    IntEncoder() = default;
+
+    int32_t maxSizeOf(const NValue& value) override {
+        vassert(ValuePeeker::peekValueType(value) == ValueType::tINTEGER);
+        return sizeof(int32_t);
+    }
+
+    int32_t encode(ExportSerializeOutput& out, const NValue& value) override {
+        out.writeInt(ValuePeeker::peekInteger(value));
+        return sizeof(int32_t);
+    }
+};
+
+/**
+ * NValueEncoder for encoding ValueType::tBIGINT
+ */
+class BigIntEncoder : public NValueEncoder {
+public:
+    BigIntEncoder() = default;
+
+    int32_t maxSizeOf(const NValue& value) override {
+        vassert(ValuePeeker::peekValueType(value) == ValueType::tBIGINT);
+        return sizeof(int64_t);
+    }
+
+    int32_t encode(ExportSerializeOutput& out, const NValue& value) override {
+        out.writeLong(ValuePeeker::peekBigInt(value));
+        return sizeof(int64_t);
+    }
+};
+
+/**
+ * NValueEncoder for encoding ValueType::tDOUBLE
+ */
+class DoubleEncoder : public NValueEncoder {
+public:
+    DoubleEncoder() = default;
+
+    int32_t maxSizeOf(const NValue& value) override {
+        vassert(ValuePeeker::peekValueType(value) == ValueType::tDOUBLE);
+        return sizeof(double);
+    }
+
+    int32_t encode(ExportSerializeOutput& out, const NValue& value) override {
+        out.writeDouble(ValuePeeker::peekDouble(value));
+        return sizeof(double);
+    }
+};
+
+/**
+ * NValueEncoder for encoding ValueType::tVARCHAR or ValueType::tVARBINARY without a preceding length value
+ */
+class PlainVarLenEncoder : public NValueEncoder {
+public:
+    PlainVarLenEncoder() = default;
+
+    int32_t maxSizeOf(const NValue& value) override {
+        int32_t length;
+        ValuePeeker::peekObject_withoutNull(value, &length);
+        return length;
+    }
+
+    int32_t encode(ExportSerializeOutput& out, const NValue& value) override {
+        int32_t length;
+        const char* string = ValuePeeker::peekObject_withoutNull(value, &length);
+        out.writeBytes(string, length);
+        return length;
+    }
+};
+
+} }
