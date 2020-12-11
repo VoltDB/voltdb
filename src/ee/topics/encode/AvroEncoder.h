324bf1c065c5f8c57cdb311466c0159c76586c8e
Nate Clark <nclark@voltdb.com>
2020-12-10 23:02:09 -0500

    ENG-20410: Add encoder for the avro format
    Add a high level encoder for the avro format which encodes specific
    columns from a tuple. This also adds individual encoders for each
    supported type and a few encoding formats for geography and timestamp
    types.

---
+++ src/ee/topics/encode/AvroEncoder.h
@@ -0,0 +1,125 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2020 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <numeric>
+#include <unordered_map>
+#include "topics/encode/Encoder.h"
+
+namespace voltdb { namespace topics {
+
+/**
+ * A simplified avro encoder for encoding a single NValue from a TableTuple. This encoder supports the value being
+ * nullable or not
+ */
+class AvroValueEncoder : public TupleEncoder {
+public:
+    AvroValueEncoder() : m_encoder(nullptr), m_index(-1), m_nullable(true) {}
+
+    AvroValueEncoder(NValueEncoder *encoder, int32_t index, bool nullable) :
+        m_encoder(encoder), m_index(index), m_nullable(nullable) {}
+
+    AvroValueEncoder(const AvroValueEncoder& other) :
+        m_encoder(other.m_encoder.get()), m_index(other.m_index), m_nullable(other.m_nullable) {}
+
+    int32_t maxSizeOf(const TableTuple& tuple) override {
+        const NValue value = tuple.getNValue(m_index);
+        int32_t length = m_nullable ? 1 : 0;
+        if (!value.isNull()) {
+            length += m_encoder->maxSizeOf(value);
+        } else {
+            vassert(m_nullable);
+        }
+        return length;
+    }
+
+    int32_t encode(ExportSerializeOutput& out, const TableTuple& tuple) override {
+        const NValue value = tuple.getNValue(m_index);
+        int32_t length = 0;
+        if (value.isNull()) {
+            vassert(m_nullable);
+            length = out.writeVarLong(1);
+        } else {
+            if (m_nullable) {
+                length = out.writeVarLong(0);
+            }
+            length += m_encoder->encode(out, value);
+        }
+        return length;
+    }
+
+private:
+    const std::unique_ptr<NValueEncoder> m_encoder;
+    const int32_t m_index;
+    const bool m_nullable;
+};
+
+/**
+ * Implementation of TupleEncoder which encodes specific columns from a tuple using the avro serialization format
+ */
+class AvroEncoder : public TupleEncoder {
+public:
+    /**
+     * @param schemaId which identifies the schema in a registry
+     * @param schema of topic stream
+     * @param index vector of indexes which are to be serialized
+     * @param props map with properties which affect how values are serialized
+     */
+    AvroEncoder(int32_t schemaId, const TupleSchema& schema, const std::vector<int32_t>& indexes,
+            const std::unordered_map<std::string, std::string>& props);
+
+
+    int32_t maxSizeOf(const TableTuple& tuple) override {
+        return std::accumulate(m_encoders.begin(), m_encoders.end(), s_headerSize,
+                [&tuple](int32_t sum, AvroValueEncoder& encoder) { return sum + encoder.maxSizeOf(tuple); });
+    }
+
+    int32_t exactSizeOf(const TableTuple& tuple) override {
+        return std::accumulate(m_encoders.begin(), m_encoders.end(), s_headerSize,
+                [&tuple](int32_t sum, AvroValueEncoder& encoder) { return sum + encoder.exactSizeOf(tuple); });
+    }
+
+    int32_t encode(ExportSerializeOutput& out, const TableTuple& tuple) override {
+        int32_t length = s_headerSize;
+
+        // Write out header to indicate which schema from the registry this is using
+        out.writeByte(s_magicValue);
+        out.writeInt(m_schemaId);
+
+        // Encode all of the selected columns
+        for (AvroValueEncoder& encoder : m_encoders) {
+            length += encoder.encode(out, tuple);
+        }
+
+        return length;
+    }
+
+    // Property keys used to configure avro encoding
+    static const std::string PROP_TIMESTAMP_ENCODING;
+    static const std::string PROP_POINT_ENCODING;
+    static const std::string PROP_GEOGRAPHY_ENCODING;
+
+private:
+    const int32_t m_schemaId;
+    std::vector<AvroValueEncoder> m_encoders;
+
+    static const int8_t s_magicValue = 0;
+    static const int32_t s_headerSize = sizeof(s_magicValue) + sizeof(m_schemaId);
+};
+
+} }
---
+++ tests/ee/topics/encode/AvroEncoderTest.cpp
@@ -0,0 +1,479 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2020 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "harness.h"
+#include "topics/encode/AvroEncoder.h"
+
+using namespace voltdb;
+using namespace voltdb::topics;
+
+class AvroEncoderTest : public Test {
+public:
+    virtual ~AvroEncoderTest() {
+        TupleSchema::freeTupleSchema(m_schema);
+    }
+
+protected:
+    void validateHeader(ExportSerializeInput& in, int32_t schemaId) {
+        ASSERT_EQ(0, in.readByte()); // Magic value
+        ASSERT_EQ(schemaId, in.readInt()); // schema ID
+    }
+
+    void setupAllSchema(bool nullable) {
+        std::vector<ValueType> types { ValueType::tTINYINT, ValueType::tSMALLINT, ValueType::tINTEGER,
+                        ValueType::tBIGINT, ValueType::tDOUBLE, ValueType::tTIMESTAMP, ValueType::tDECIMAL, ValueType::tVARCHAR,
+                        ValueType::tVARBINARY, ValueType::tPOINT, ValueType::tGEOGRAPHY };
+        std::vector<int32_t> sizes { 1, 2, 4, 8, 8, 8, 16, 256, 256, 16, 1024 };
+        std::vector<bool> nullables(11, nullable);
+        m_schema = TupleSchema::createTupleSchemaForTest(types, sizes, nullables);
+        setupTuple();
+    }
+
+    void setupTuple() {
+        m_tuple = TableTuple(m_schema);
+        m_tupleData.reset(new char[m_tuple.tupleLength()]);
+        m_tuple.moveAndInitialize(m_tupleData.get());
+    }
+
+    void insertValues(int8_t tinyint, int16_t smallint, int32_t integer, int64_t bigint, double dbl, int64_t timestamp,
+            double decimal, const std::string& varChar, const std::vector<uint8_t>& varBinary,
+            const GeographyPointValue* point, const Polygon* geography) {
+        m_tuple.setNValue(0, ValueFactory::getTinyIntValue(tinyint));
+        m_tuple.setNValue(1, ValueFactory::getSmallIntValue(smallint));
+        m_tuple.setNValue(2, ValueFactory::getIntegerValue(integer));
+        m_tuple.setNValue(3, ValueFactory::getBigIntValue(bigint));
+        m_tuple.setNValue(4, ValueFactory::getDoubleValue(dbl));
+        m_tuple.setNValue(5, ValueFactory::getTimestampValue(timestamp));
+        m_tuple.setNValue(6, ValueFactory::getDecimalValue(decimal));
+        m_tuple.setNValue(7, ValueFactory::getStringValue(varChar, &m_pool));
+        m_tuple.setNValue(8, ValueFactory::getBinaryValue(varBinary.data(), varBinary.size(), &m_pool));
+        m_tuple.setNValue(9, ValueFactory::getGeographyPointValue(point));
+        m_tuple.setNValue(10, ValueFactory::getGeographyValue(geography, &m_pool));
+    }
+
+    TupleSchema *m_schema;
+    Pool m_pool;
+    std::unique_ptr<char[]> m_tupleData;
+    TableTuple m_tuple;
+};
+
+// Test that serialization of fields which cannot be null works
+TEST_F(AvroEncoderTest, NonNullableAvro) {
+    setupAllSchema(false);
+
+    std::vector<int32_t> indexes { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    AvroEncoder ae(25, *m_schema, indexes, std::unordered_map<std::string, std::string>());
+    GeographyPointValue point(12.5, 78.9);
+
+    // The dumbest geography encoding I could figure out
+    std::string varchar = "some silly string";
+    std::vector<uint8_t> binary { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    std::vector<std::unique_ptr<S2Loop> > loops;
+    std::vector<S2Point> points( {S2Point(50, 5000, 100), S2Point(40, 900, 50), S2Point(900, 2000, 300)});
+    loops.emplace_back(new S2Loop(points));
+    Polygon geography;
+    geography.init(&loops, false);
+    insertValues(1, 2, 3, 4, 5, 6, 7, varchar, binary, &point, &geography);
+
+    int32_t size = ae.maxSizeOf(m_tuple);
+    std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+
+    ExportSerializeOutput eos(encoded.get(), static_cast<size_t>(size));
+    int32_t written = ae.encode(eos, m_tuple);
+    ASSERT_EQ(written, eos.position());
+
+    ExportSerializeInput esi(encoded.get(), size);
+    validateHeader(esi, 25);
+    ASSERT_EQ(1, esi.readVarInt()); // tinyint
+    ASSERT_EQ(2, esi.readVarInt()); // smallint
+    ASSERT_EQ(3, esi.readVarInt()); // integer
+    ASSERT_EQ(4, esi.readVarLong()); // bigint
+    ASSERT_EQ(5, esi.readDouble()); // double
+    ASSERT_EQ(6, esi.readVarLong()); // timestamp
+
+    ASSERT_EQ(16, esi.readVarInt()); // Size of decimal
+    int64_t val = esi.readLong();
+    ASSERT_EQ(0, ntohll(val)); // Highest significant bits
+    val = esi.readLong();
+    ASSERT_EQ(7000000000000, ntohll(val)); // Lowest significant bits with scale of 12
+
+    // Validate the string  can be deserialized
+    int64_t len = esi.readVarInt();
+    ASSERT_EQ(varchar.length(), len);
+    std::unique_ptr<char[]> strDecoded(new char[len]);
+    esi.readBytes(strDecoded.get(), len);
+    ASSERT_EQ(varchar, std::string(strDecoded.get(), len));
+
+    // Validate the var binary can be deserialized
+    len = esi.readVarInt();
+    ASSERT_EQ(binary.size(), len);
+    std::unique_ptr<char[]> binaryDecoded(new char[len]);
+    esi.readBytes(binaryDecoded.get(), len);
+    ASSERT_EQ(0, ::memcmp(binary.data(), binaryDecoded.get(), len));
+
+    // Validate the point
+    ASSERT_EQ(point.getLongitude(), esi.readDouble());
+    ASSERT_EQ(point.getLatitude(), esi.readDouble());
+
+    // Validate the geography
+    len = esi.readVarInt();
+    ASSERT_EQ(geography.serializedLength(), len);
+    std::unique_ptr<char[]> geoBytes(new char[len]);
+    esi.readBytes(geoBytes.get(), len);
+    GeographyValue geographyDecoded(geoBytes.get(), len);
+    ASSERT_EQ(0, ValuePeeker::peekGeographyValue(m_tuple.getNValue(10)).compareWith(geographyDecoded));
+}
+
+// Test that serialization of fields which can be null works
+TEST_F(AvroEncoderTest, NullableAvro) {
+    setupAllSchema(true);
+
+    std::vector<int32_t> indexes { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    AvroEncoder ae(25, *m_schema, indexes, std::unordered_map<std::string, std::string>());
+    GeographyPointValue point(12.5, 78.9);
+
+    // The dumbest geography encoding I could figure out
+    std::string varchar = "some silly string";
+    std::vector<uint8_t> binary { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    std::vector<std::unique_ptr<S2Loop> > loops;
+    std::vector<S2Point> points( {S2Point(50, 5000, 100), S2Point(40, 900, 50), S2Point(900, 2000, 300)});
+    loops.emplace_back(new S2Loop(points));
+    Polygon geography;
+    geography.init(&loops, false);
+    insertValues(1, 2, 3, 4, 5, 6, 7, varchar, binary, &point, &geography);
+
+    int32_t size = ae.maxSizeOf(m_tuple);
+    std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+
+    ExportSerializeOutput eos(encoded.get(), static_cast<size_t>(size));
+    int32_t written = ae.encode(eos, m_tuple);
+    ASSERT_EQ(written, eos.position());
+
+    ExportSerializeInput esi(encoded.get(), size);
+    validateHeader(esi, 25);
+
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    ASSERT_EQ(1, esi.readVarInt()); // tinyint
+
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    ASSERT_EQ(2, esi.readVarInt()); // smallint
+
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    ASSERT_EQ(3, esi.readVarInt()); // integer
+
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    ASSERT_EQ(4, esi.readVarLong()); // bigint
+
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    ASSERT_EQ(5, esi.readDouble()); // double
+
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    ASSERT_EQ(6, esi.readVarLong()); // timestamp
+
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    ASSERT_EQ(16, esi.readVarLong()); // Size of decimal
+    int64_t val = esi.readLong();
+    ASSERT_EQ(0, ntohll(val)); // Highest significant bits
+    val = esi.readLong();
+    ASSERT_EQ(7000000000000, ntohll(val)); // Lowest significant bits with scale of 12
+
+    // Validate the string  can be deserialized
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    int64_t len = esi.readVarInt();
+    ASSERT_EQ(varchar.length(), len);
+    std::unique_ptr<char[]> strDecoded(new char[len]);
+    esi.readBytes(strDecoded.get(), len);
+    ASSERT_EQ(varchar, std::string(strDecoded.get(), len));
+
+    // Validate the var binary can be deserialized
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    len = esi.readVarInt();
+    ASSERT_EQ(binary.size(), len);
+    std::unique_ptr<char[]> binaryDecoded(new char[len]);
+    esi.readBytes(binaryDecoded.get(), len);
+    ASSERT_EQ(0, ::memcmp(binary.data(), binaryDecoded.get(), len));
+
+    // Validate the point
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    ASSERT_EQ(point.getLongitude(), esi.readDouble());
+    ASSERT_EQ(point.getLatitude(), esi.readDouble());
+
+    // Validate the geography
+    ASSERT_EQ(0, esi.readVarInt()); // Union index indicating not null
+    len = esi.readVarInt();
+    ASSERT_EQ(geography.serializedLength(), len);
+    std::unique_ptr<char[]> geoBytes(new char[len]);
+    esi.readBytes(geoBytes.get(), len);
+    GeographyValue geographyDecoded(geoBytes.get(), len);
+    ASSERT_EQ(0, ValuePeeker::peekGeographyValue(m_tuple.getNValue(10)).compareWith(geographyDecoded));
+}
+
+// Test that nulls of all types are correctly serialized
+TEST_F(AvroEncoderTest, AllNullAvro) {
+    setupAllSchema(true);
+    m_tuple.setAllNulls();
+
+    std::vector<int32_t> indexes { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    AvroEncoder ae(25, *m_schema, indexes, std::unordered_map<std::string, std::string>());
+    int32_t size = ae.maxSizeOf(m_tuple);
+    std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+
+    ExportSerializeOutput eos(encoded.get(), static_cast<size_t>(size));
+    int32_t written = ae.encode(eos, m_tuple);
+    ASSERT_EQ(written, eos.position());
+
+    ExportSerializeInput esi(encoded.get(), size);
+    validateHeader(esi, 25);
+
+    // Validate all union indexers are 1 which indicates null
+    for (int i = 0; i < m_schema->columnCount(); ++i) {
+        ASSERT_EQ(1, esi.readVarInt());
+    }
+}
+
+// Test the encoding a subset of columns in any order works
+TEST_F(AvroEncoderTest, SomeColumnsEncoded) {
+    setupAllSchema(false);
+
+    GeographyPointValue point(12.5, 78.9);
+
+    // The dumbest geography encoding I could figure out
+    std::string varchar = "some silly string";
+    std::vector<uint8_t> binary { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    std::vector<std::unique_ptr<S2Loop> > loops;
+    std::vector<S2Point> points( {S2Point(50, 5000, 100), S2Point(40, 900, 50), S2Point(900, 2000, 300)});
+    loops.emplace_back(new S2Loop(points));
+    Polygon geography;
+    geography.init(&loops, false);
+    insertValues(1, 2, 3, 4, 5, 6, 7, varchar, binary, &point, &geography);
+
+    {
+        std::vector<int32_t> indexes { 1, 3, 5,};
+        AvroEncoder ae(90, *m_schema, indexes, std::unordered_map<std::string, std::string>());
+
+        int32_t size = ae.maxSizeOf(m_tuple);
+        std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+
+        ExportSerializeOutput eos(encoded.get(), static_cast<size_t>(size));
+        int32_t written = ae.encode(eos, m_tuple);
+        ASSERT_EQ(written, eos.position());
+
+        ExportSerializeInput esi(encoded.get(), size);
+        validateHeader(esi, 90);
+
+        ASSERT_EQ(2, esi.readVarInt()); // smallint
+        ASSERT_EQ(4, esi.readVarLong()); // bigint
+        ASSERT_EQ(6, esi.readVarLong()); // timestamp
+    }
+
+    {
+        std::vector<int32_t> indexes { 5, 3, 1,};
+        AvroEncoder ae(90, *m_schema, indexes, std::unordered_map<std::string, std::string>());
+
+        int32_t size = ae.maxSizeOf(m_tuple);
+        std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+
+        ExportSerializeOutput eos(encoded.get(), static_cast<size_t>(size));
+        int32_t written = ae.encode(eos, m_tuple);
+        ASSERT_EQ(written, eos.position());
+
+        ExportSerializeInput esi(encoded.get(), size);
+        validateHeader(esi, 90);
+
+        ASSERT_EQ(6, esi.readVarLong()); // timestamp
+        ASSERT_EQ(4, esi.readVarLong()); // bigint
+        ASSERT_EQ(2, esi.readVarInt()); // smallint
+    }
+}
+
+// Test the different encodings of timestamp work
+TEST_F(AvroEncoderTest, TimestampEncoding) {
+    std::vector<ValueType> types { ValueType::tTIMESTAMP };
+    std::vector<int32_t> sizes { 8 };
+    std::vector<bool> nullable { false };
+    m_schema = TupleSchema::createTupleSchemaForTest(types, sizes, nullable);
+    setupTuple();
+
+    const int64_t time = 1607698898846;
+    m_tuple.setNValue(0, ValueFactory::getTimestampValue(time));
+
+    std::unordered_map<std::string, std::string> props;
+    props[AvroEncoder::PROP_TIMESTAMP_ENCODING] = std::string("MICROSECONDS");
+
+    std::vector<int32_t> indexes { 0 };
+    {
+        AvroEncoder encoder(30, *m_schema, indexes, props);
+        int32_t size = encoder.maxSizeOf(m_tuple);
+        std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+        ExportSerializeOutput out(encoded.get(), size);
+
+        encoder.encode(out, m_tuple);
+
+        ExportSerializeInput in(encoded.get(), size);
+        validateHeader(in, 30);
+        ASSERT_EQ(time, in.readVarLong());
+    }
+
+    props[AvroEncoder::PROP_TIMESTAMP_ENCODING] = std::string("MILLISECONDS");
+    {
+        AvroEncoder encoder(30, *m_schema, indexes, props);
+        int32_t size = encoder.maxSizeOf(m_tuple);
+        std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+        ExportSerializeOutput out(encoded.get(), size);
+
+        encoder.encode(out, m_tuple);
+
+        ExportSerializeInput in(encoded.get(), size);
+        validateHeader(in, 30);
+        ASSERT_EQ(time / 1000, in.readVarLong());
+    }
+}
+
+// Test the different encodings of a point work
+TEST_F(AvroEncoderTest, GeographyPointEncoding) {
+    std::vector<ValueType> types { ValueType::tPOINT };
+    std::vector<int32_t> sizes { 16 };
+    std::vector<bool> nullable { false };
+    m_schema = TupleSchema::createTupleSchemaForTest(types, sizes, nullable);
+    setupTuple();
+
+    GeographyPointValue point(78.5, -25.98);
+    m_tuple.setNValue(0, ValueFactory::getGeographyPointValue(&point));
+
+    std::unordered_map<std::string, std::string> props;
+    props[AvroEncoder::PROP_POINT_ENCODING] = std::string("FIXED_BINARY");
+
+    std::vector<int32_t> indexes { 0 };
+    {
+        AvroEncoder encoder(30, *m_schema, indexes, props);
+        int32_t size = encoder.maxSizeOf(m_tuple);
+        std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+        ExportSerializeOutput out(encoded.get(), size);
+
+        encoder.encode(out, m_tuple);
+
+        ExportSerializeInput in(encoded.get(), size);
+        validateHeader(in, 30);
+        ASSERT_EQ(point.getLongitude(), in.readDouble());
+        ASSERT_EQ(point.getLatitude(), in.readDouble());
+    }
+
+    props[AvroEncoder::PROP_POINT_ENCODING] = std::string("BINARY");
+    {
+        AvroEncoder encoder(30, *m_schema, indexes, props);
+        int32_t size = encoder.maxSizeOf(m_tuple);
+        std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+        ExportSerializeOutput out(encoded.get(), size);
+
+        encoder.encode(out, m_tuple);
+
+        ExportSerializeInput in(encoded.get(), size);
+        validateHeader(in, 30);
+        ASSERT_EQ(16, in.readVarInt()); // size of encoding
+        ASSERT_EQ(point.getLongitude(), in.readDouble());
+        ASSERT_EQ(point.getLatitude(), in.readDouble());
+    }
+
+    props[AvroEncoder::PROP_POINT_ENCODING] = std::string("STRING");
+    {
+        AvroEncoder encoder(30, *m_schema, indexes, props);
+        int32_t size = encoder.maxSizeOf(m_tuple);
+        std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+        ExportSerializeOutput out(encoded.get(), size);
+
+        encoder.encode(out, m_tuple);
+
+        std::string string = point.toWKT();
+
+        ExportSerializeInput in(encoded.get(), size);
+        validateHeader(in, 30);
+        ASSERT_EQ(string.length(), in.readVarInt()); // size of encoding
+        std::unique_ptr<char[]> strBytes(new char[string.length()]);
+        in.readBytes(strBytes.get(), string.length());
+        ASSERT_EQ(string, std::string(strBytes.get(), string.length()));
+    }
+}
+
+// Test the different encodings of geography value work
+TEST_F(AvroEncoderTest, GeographyEncoding) {
+    std::vector<ValueType> types { ValueType::tGEOGRAPHY };
+    std::vector<int32_t> sizes { 512 };
+    std::vector<bool> nullable { false };
+    m_schema = TupleSchema::createTupleSchemaForTest(types, sizes, nullable);
+    setupTuple();
+
+    std::vector<std::unique_ptr<S2Loop> > loops;
+    std::vector<S2Point> points( {S2Point(50, 5000, 100), S2Point(40, 900, 50), S2Point(900, 2000, 300)});
+    loops.emplace_back(new S2Loop(points));
+    Polygon polygon;
+    polygon.init(&loops, false);
+    NValue nvalue = ValueFactory::getGeographyValue(&polygon, &m_pool);
+    GeographyValue geography = ValuePeeker::peekGeographyValue(nvalue);
+    m_tuple.setNValue(0, nvalue);
+
+    std::unordered_map<std::string, std::string> props;
+    props[AvroEncoder::PROP_GEOGRAPHY_ENCODING] = std::string("BINARY");
+
+    std::vector<int32_t> indexes { 0 };
+    {
+        AvroEncoder encoder(30, *m_schema, indexes, props);
+        int32_t size = encoder.maxSizeOf(m_tuple);
+        std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+        ExportSerializeOutput out(encoded.get(), size);
+
+        encoder.encode(out, m_tuple);
+
+        ExportSerializeInput in(encoded.get(), size);
+        validateHeader(in, 30);
+        ASSERT_EQ(geography.length(), in.readVarInt());
+        std::unique_ptr<char[]> geoBytes(new char[geography.length()]);
+        in.readBytes(geoBytes.get(), geography.length());
+        ASSERT_EQ(0, ::memcmp(geography.data(), geoBytes.get(), geography.length()));
+    }
+
+    props[AvroEncoder::PROP_GEOGRAPHY_ENCODING] = std::string("STRING");
+    {
+        AvroEncoder encoder(30, *m_schema, indexes, props);
+        int32_t size = encoder.maxSizeOf(m_tuple);
+        std::unique_ptr<uint8_t[]> encoded(new uint8_t[size]);
+        ExportSerializeOutput out(encoded.get(), size);
+
+        encoder.encode(out, m_tuple);
+
+        std::string string = ValuePeeker::peekGeographyValue(m_tuple.getNValue(0)).toWKT();
+
+        ExportSerializeInput in(encoded.get(), size);
+        validateHeader(in, 30);
+        ASSERT_EQ(string.length(), in.readVarInt()); // size of encoding
+        std::unique_ptr<char[]> strBytes(new char[string.length()]);
+        in.readBytes(strBytes.get(), string.length());
+        ASSERT_EQ(string, std::string(strBytes.get(), string.length()));
+    }
+}
+
+int main() {
+    return TestSuite::globalInstance()->runAll();
+}
