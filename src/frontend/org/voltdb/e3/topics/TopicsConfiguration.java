c3ba77aacdf8deaa265d55a5b30eaaff3f2b4fe6
Richard Dykiel <rdykiel@voltdb.com>
2020-12-14 14:18:45 -0500

    Eng 20413 20414 inline encoding java (#231)
    * Move topic properties to community
    
    * Create PBD segment metadata according to opaque topic or record transcoding policy (inline or consumer)
    
    * Extend typed properties to look up individual keys

---
+++ src/frontend/org/voltdb/e3/topics/TopicsConfiguration.java
@@ -0,0 +1,139 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2020 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.voltdb.e3.topics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import org.voltdb.compiler.deploymentfile.PropertiesType;
+import org.voltdb.compiler.deploymentfile.PropertyType;
+import org.voltdb.utils.PBDUtils;
+import org.voltdb.utils.PBDUtils.ConfigurationException;
+
+import com.google_voltpatches.common.collect.ImmutableMap;
+
+/**
+ * Class to take in topics configuration parameters specified in deployment file and get it to a form easily usable in
+ * the topics subsystem.
+ */
+public class TopicsConfiguration extends TypedPropertiesBase<TopicsConfiguration.Entry<?>> {
+
+    /**
+     * Defines all TopicsConfiguration parameters, with their types and default values.
+     */
+    public static class Entry<T> extends TypedPropertiesBase.KeyBase<T> {
+
+        private static final Map<String, Entry<?>> s_validConfigs = new HashMap<>();
+
+        /* Other configuration parameters to consider in the future:
+         * max.connections
+         * max.connections.per.ip
+         * message.max.bytes
+         * offsets.retention.minutes
+         * offsets.retention.check.interval.ms
+         * offset.metadata.max.bytes
+         */
+        public static final Entry<Integer> PORT = new Entry<Integer>("port", Integer.class, 9092, value -> {
+            if (value == null || value < 1 || value > 65536) {
+                throw new IllegalArgumentException("Port number must be between 1 and 65536");
+            }
+        });
+        public static final Entry<Integer> NETWORK_THREAD_COUNT = new Entry<>("network.thread.count", Integer.class, 20);
+        public static final Entry<String> CLUSTER_ID = new Entry<>("cluster.id", String.class);
+        public static final Entry<Integer> NEW_MEMBER_TIMEOUT = new Entry<>("newmember.timeout.ms", Integer.class, 60000);
+        public static final Entry<Integer> GROUP_MAX_SESSION_TIMEOUT = new Entry<>("group.max.session.timeout.ms", Integer.class, 1_800_000);
+        public static final Entry<Integer> GROUP_MIN_SESSION_TIMEOUT = new Entry<>("group.min.session.timeout.ms", Integer.class, 6000);
+        public static final Entry<Integer> GROUP_MAX_SIZE = new Entry<>("group.max.size", Integer.class, Integer.MAX_VALUE);
+        public static final Entry<Integer> GROUP_INITIAL_REBALANCE_DELAY_MS = new Entry<>("group.initial.rebalance.delay.ms", Integer.class, 3000);
+        public static final Entry<Integer> GROUP_MAX_LOAD_SIZE = new Entry<>("group.load.max.size", Integer.class,
+                32 * 1024 * 1024);
+        public static final Entry<Integer> OFFSET_RETENTION_CHECK = new Entry<>("offsets.retention.check.interval.ms",
+                Integer.class, 600_000);
+        public static final Entry<Integer> OFFSET_RETENTION = new Entry<>("offsets.retention.minutes", Integer.class,
+                10080 /* 7 days */);
+        public static final Entry<Integer> RETENTION_POLICY_THREADS = new Entry<Integer>("retention.policy.threads",
+                Integer.class, 2, numThreads -> {
+                    if (numThreads == null || numThreads < 1 || numThreads > 20) {
+                        throw new IllegalArgumentException("Number of PBD retention threads must be between 1 and 20");
+                    }
+                });
+        public static final Entry<Integer> QUOTA_MAX_THROTTLE_MS = new Entry<>("quota.throttle.max_ms", Integer.class,
+                1000, Entry::mustBePositive);
+        public static final Entry<Long> QUOTA_REQUEST_BPS = new Entry<>("quota.request.bytes_per_second", Long.class,
+                -1L, Entry::mustBePositive);
+        public static final Entry<Long> QUOTA_RESPONSE_BPS = new Entry<>("quota.response.bytes_per_second", Long.class,
+                -1L, Entry::mustBePositive);
+        public static final Entry<Integer> QUOTA_REQUEST_PROCESS_PCT = new Entry<>("quota.request.processing_percent",
+                Integer.class, -1, Entry::mustBePositive);
+        public static final Entry<Integer> COMPACTION_THREADS = new Entry<>("log.cleaner.threads", Integer.class, 1,
+                Entry::mustBePositive);
+        public static final Entry<Long> COMPACTION_DELETE_RETENTION_MS = new Entry<>("log.cleaner.delete.retention.ms",
+                Long.class, 86_400_000L, Entry::mustBePositive);
+        public static final Entry<Long> COMPACTION_BUFFER_SIZE = new Entry<>("log.cleaner.dedupe.buffer.size",
+                Long.class, 134_217_728L, Entry::mustBePositive);
+
+        public static final Entry<Long> TOPIC_SEGMENT_ROLL_TIME = new SegmentRollTimeEntry(PBDUtils.TOPIC_SEGMENT_ROLL_TIME, TimeUnit.DAYS.toNanos(7));
+
+        private Entry(String propertyName, Class<T> clazz) {
+            this(propertyName, clazz, null);
+        }
+
+        private Entry(String propertyName, Class<T> clazz, T defValue) {
+            this(propertyName, clazz, defValue, null);
+        }
+
+        private Entry(String propertyName, Class<T> clazz, T defValue, Consumer<? super T> validator) {
+            super(propertyName, clazz, defValue, validator);
+            Entry<?> prev = s_validConfigs.put(propertyName, this);
+            assert prev == null;
+        }
+    }
+
+    private static class SegmentRollTimeEntry extends Entry<Long> {
+        SegmentRollTimeEntry(String propertyName, Long defValue) {
+            super(propertyName, null, defValue);
+        }
+
+        @Override
+        protected Long parseValue(String strValue) {
+            try {
+                return TimeUnit.MILLISECONDS.toNanos(PBDUtils.parseTimeValue(strValue));
+            } catch (ConfigurationException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
+    }
+
+    public TopicsConfiguration(PropertiesType input) {
+        super(input == null ? ImmutableMap.of()
+                : input.getProperty().stream()
+                        .collect(Collectors.toMap(PropertyType::getName, PropertyType::getValue)));
+    }
+
+    public <T> T getConfigValue(Entry<T> entry) {
+        return super.getProperty(entry);
+    }
+
+    @Override
+    protected Map<String, Entry<?>> getValidKeys() {
+        return Entry.s_validConfigs;
+    }
+}
---
+++ src/frontend/org/voltdb/e3/topics/TypedPropertiesBase.java
@@ -0,0 +1,180 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2020 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.voltdb.e3.topics;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.voltdb.ParameterConverter;
+import org.voltdb.e3.topics.TypedPropertiesBase.KeyBase;
+import org.voltdb.utils.CompoundErrors;
+
+import com.google_voltpatches.common.collect.ImmutableMap;
+import com.google_voltpatches.common.collect.Sets;
+
+/**
+ * Base class for Properties map where all property keys are predefined and have parsers and potentially validated
+ *
+ * @param <K> Type of key
+ */
+public abstract class TypedPropertiesBase<K extends KeyBase<?>> {
+    private final Map<K, Object> m_properties;
+
+    protected TypedPropertiesBase(Map<String, String> properties) {
+        CompoundErrors errors = new CompoundErrors();
+        Map<String, K> validKeys = getValidKeys();
+        Set<String> unsupportedKeys = Sets.difference(properties.keySet(), validKeys.keySet());
+        if (!unsupportedKeys.isEmpty()) {
+            errors.addErrorMessage("Unsupported properties: " + unsupportedKeys);
+        }
+
+        ImmutableMap.Builder<K, Object> builder = ImmutableMap.builder();
+        for (K property : validKeys.values()) {
+            try {
+                String strValue = properties.get(property.name());
+                Object value = strValue == null ? property.getDefaultValue() : property.parseAndValidate(strValue);
+                if (value != null) {
+                    builder.put(property, value);
+                }
+            } catch (Exception e) {
+                errors.addErrorMessage(e.getMessage());
+            }
+        }
+
+        if (errors.hasErrors()) {
+            throw new IllegalArgumentException(errors.getErrorMessage());
+        }
+
+        m_properties = builder.build();
+    }
+
+    /**
+     * Retrieve the value associated with {@code key}
+     * <p>
+     * Note: Most implementations should override this method and make it public
+     *
+     * @param <T> Type of value associated with {@code key}
+     * @param key instance
+     * @return Value associated with key or {@code null}
+     */
+    @SuppressWarnings("unchecked")
+    protected <T> T getProperty(K key) {
+        return (T) m_properties.get(key);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof TypedPropertiesBase)) {
+            return false;
+        }
+
+        return m_properties.equals(((TypedPropertiesBase<?>) obj).m_properties);
+    }
+
+    @Override
+    public String toString() {
+        return m_properties.toString();
+    }
+
+    protected Map<K, Object> asMap() {
+        return m_properties;
+    }
+
+    /**
+     * @return A {@link Map} of all valid keys of {@link KeyBase#name()} to {@link KeyBase} instance
+     */
+    protected abstract Map<String, K> getValidKeys();
+
+    /**
+     * Base class for property keys
+     *
+     * @param <T> Type of value associated with this key
+     */
+    public static abstract class KeyBase<T> {
+        private final String m_name;
+        private final Class<T> m_class;
+        private final boolean m_mutable;
+        private final T m_default;
+        private final Consumer<? super T> m_validator;
+
+        protected static void mustBePositive(Number number) {
+            if (number.doubleValue() <= 0) {
+                throw new IllegalArgumentException("value must be positive: " + number);
+            }
+        }
+
+        // Properties are mutable by default
+        protected KeyBase(String name, Class<T> clazz, T defValue, Consumer<? super T> validator) {
+            this(name, clazz, true, defValue, validator);
+        }
+
+        protected KeyBase(String name, Class<T> clazz, boolean mutable, T defValue, Consumer<? super T> validator) {
+            m_name = name;
+            m_class = clazz;
+            m_mutable = mutable;
+            m_default = defValue;
+            m_validator = validator;
+        }
+
+        public String name() {
+            return m_name;
+        }
+
+        public boolean isMutable() {
+            return m_mutable;
+        }
+
+        public T getDefaultValue() {
+            return m_default;
+        }
+
+        public T get(Map<String, String> properties) {
+            String strValue = properties.get(m_name);
+            return strValue == null ? m_default : parseAndValidate(strValue);
+        }
+
+        @Override
+        public String toString() {
+            return m_name;
+        }
+
+        protected Class<T> getValueClass() {
+            return m_class;
+        }
+
+        @SuppressWarnings("unchecked")
+        protected T parseValue(String strValue) {
+            return (T) ParameterConverter.tryToMakeCompatible(m_class, strValue);
+        }
+
+        protected
+
+        final T parseAndValidate(String strValue) {
+            try {
+                T value = parseValue(strValue);
+                if (m_validator != null) {
+                    m_validator.accept(value);
+                }
+                return value;
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Invalid value for " + m_name + ": " + e.getMessage());
+            }
+        }
+    }
+}
