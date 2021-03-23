fc77f36d6f03a70e87c6718fcb172f375c8286ec
Peter Shaw <pshaw@voltdb.com>
2021-03-23 11:13:19 -0400

    Eng 20401 properties (#450)
    * ENG-20401: Add avro properties to new ddl if avro specified in apprunner options

---
+++ tests/test_apps/topicbenchmark2/topicTableAvro.sql
@@ -0,0 +1,31 @@
+-- default topic
+
+DROP TOPIC TEST_TOPIC if exists;
+DROP PROCEDURE test_topic if exists;
+DROP STREAM TEST_TOPIC if exists;
+
+CREATE STREAM TEST_TOPIC PARTITION ON COLUMN rowid (
+  rowid                     BIGINT        NOT NULL
+, type_timestamp            TIMESTAMP     DEFAULT NOW
+, type_varchar1024          VARCHAR(1024)
+);
+
+CREATE PROCEDURE test_topic PARTITION ON TABLE TEST_TOPIC COLUMN rowid AS INSERT INTO TEST_TOPIC (rowid, type_varchar1024) VALUES (?, ?);
+
+CREATE TOPIC USING STREAM TEST_TOPIC EXECUTE PROCEDURE test_topic PROFILE topicbenchmark PROPERTIES (topic.format=avro,producer.parameters.includeKey=true,consumer.keys=rowid);
+
+-- Additional topics (must have same schema)
+
+DROP TOPIC TEST_TOPIC01 if exists;
+DROP PROCEDURE test_topic01 if exists;
+DROP STREAM TEST_TOPIC01 if exists;
+
+CREATE STREAM TEST_TOPIC01 PARTITION ON COLUMN rowid (
+  rowid                     BIGINT        NOT NULL
+, type_timestamp            TIMESTAMP     DEFAULT NOW
+, type_varchar1024          VARCHAR(1024)
+);
+
+CREATE PROCEDURE test_topic01  PARTITION ON TABLE TEST_TOPIC01 COLUMN rowid AS INSERT INTO TEST_TOPIC01 (rowid, type_varchar1024) VALUES (?, ?);
+
+CREATE TOPIC USING STREAM TEST_TOPIC01 EXECUTE PROCEDURE test_topic01 PROFILE topicbenchmark PROPERTIES(topic.format=avro,producer.parameters.includeKey=true,consumer.keys=rowid);
