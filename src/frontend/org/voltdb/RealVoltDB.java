a14ffa13aeccdcbb10cdfe03fd17fc46c1f73632
jfournier2020 <74560342+jfournier2020@users.noreply.github.com>
2021-08-24 13:57:40 -0400

    Jenkins kit tools update version 150 master notest (#743)
    * Set Version 11.1
    
    * Update cataloglib from 16138cc -- jenkins-kit-tools-update-version-150 -- 2021-08-24_13-46-36
    
    * Update cataloglib from 16138cc1f -- jenkins-kit-tools-update-version-150 -- 2021-08-24_13-46-36
    
    Co-authored-by: Jenkins CI <test@voltdb.com>

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -263,9 +263,9 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, HostM
     // Cluster settings reference and supplier
     final ClusterSettingsRef m_clusterSettings = new ClusterSettingsRef();
     private String m_buildString;
-    static final String m_defaultVersionString = "11.0.beta2";
+    static final String m_defaultVersionString = "11.1";
     // by default set the version to only be compatible with itself
-    static final String m_defaultHotfixableRegexPattern = "^\\Q11.0.beta2\\E\\z";
+    static final String m_defaultHotfixableRegexPattern = "^\\Q11.1\\E\\z";
     // these next two are non-static because they can be overrriden on the CLI for test
     private String m_versionString = m_defaultVersionString;
     private String m_hotfixableRegexPattern = m_defaultHotfixableRegexPattern;
