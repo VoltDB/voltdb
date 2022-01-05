0df3a972489efff031d2cd2b3692b68fb4fb21c7
John McGrail <55410069+jmcgrail-voltdb@users.noreply.github.com>
2022-01-05 16:55:39 -0500

    ENG-21956: pick up branch from voltdb repo, not random repo (#980)
    * ENG-21956: pick up branch from voltdb repo, not randome repo
    
    while there add some more variables to describe all the repos involved
    (so in the future we can include more commit ids in kits)

--- tools/jenkins/write_kit_properties.py
+++ tools/jenkins/write_kit_properties.py
@@ -5,6 +5,33 @@ import os
 import sys
 import time
 
+repos = {}
+
+# Extract commit/branch info from GIT_URL* environment variables
+for i in range(0,10):
+    postfix = ""
+    if i != 0:
+        postfix = '_%s' %i
+    else:
+        postfix  = ''
+    if 'GIT_URL' + postfix not in os.environ:
+        break
+
+    # Extract the common name for the given repo from the url.
+    # If it's an "internal" repo, change it's common name to voltdb.
+    familiar_name = os.environ['GIT_URL' + postfix].split('/')[-1]
+    familiar_name = familiar_name[:-4]
+    if familiar_name == "internal":
+        familiar_name = "voltdb"
+
+    repos[familiar_name] = {
+        'url': os.environ['GIT_URL' + postfix],
+        'commit': os.environ['GIT_COMMIT' + postfix],
+        'branch': os.environ['GIT_BRANCH' + postfix].split('origin/')[-1],
+        # skip unusued GIT_CHECKOUT_DIR, GIT_COMMITTER_EMAIL, nor GIT_COMMITER_NAME
+    }
+
+# add jenkins variables to properties
 jenkins_vars=[
     'BUILD_NUMBER',
     'BUILD_ID',
@@ -12,17 +39,7 @@ jenkins_vars=[
     'NODE_NAME',
     'JOB_NAME',
     'BUILD_TAG',
-    'GIT_COMMIT',
-    'GIT_BRANCH',
-    'GIT_URL',
 ]
-
-repos = dict(internal='unknown',voltdb='unknown', pro='unknown')
-
-def map_repo(url, branch):
-    for r in repos.iterkeys():
-        if r in url:
-            repos[r] = branch.split('origin/')[-1]
 
 sv_prefix = 'kit_'
 outfile = 'workspace_params.properties'
@@ -30,37 +47,35 @@ outfile = 'workspace_params.properties'
 if len(sys.argv) == 2:
     outfile = sys.argv[1]
 
-#Set up the stored_vars_map (some might not be set)
-stored_vars_map = {}
+properties = {}
 for jv in jenkins_vars:
     sv = sv_prefix + jv.lower()
     if jv in os.environ:
-        stored_vars_map[sv] = os.environ[jv]
+        properties[sv] = os.environ[jv]
     else:
         print("missing env var '%s', setting to 'unknown'" % jv)
-        stored_vars_map[sv] = 'unknown'
+        properties[sv] = 'unknown'
 
-map_repo(stored_vars_map['kit_git_url'], stored_vars_map['kit_git_branch'])
+# add repo info to properties
+for r in repos:
+    values = repos[r]
+    properties['kit_git_url__' + r] = values['url']
+    properties['kit_git_commit__' + r] = values['commit']
+    properties['kit_git_branch__' + r] = values['branch']
 
-#Map all additional git repos
-for i in range(1, 10):
-    jv = 'GIT_URL_%s' % i
-    if jv not in os.environ:
-        break
-    map_repo(os.environ[jv], os.environ[jv.replace('URL', 'BRANCH')])
+# Apprunner copies kit_git_url and kit_git_branch to triage db records.
+# Until it uses the kit_git_*__ variables above, we include these.
+properties['kit_git_branch'] = 'origin/' + repos['voltdb']['branch']
+properties['kit_git_commit'] = repos['voltdb']['commit']
+properties['kit_git_url'] = repos['voltdb']['url']
 
 #BRANCH is used by downstream jobs
-branch = repos['voltdb']
-if repos['voltdb'] == "unknown":
-   branch = repos['internal']
+branch = repos['voltdb']['branch']
+if branch != repos['pro']['branch']:
+    branch += ("/" + repos['pro']['branch'])
 
-if repos['voltdb'] != repos['pro'] and repos['internal'] != repos['pro']:
-    branch += ("/" + repos['pro'])
-
-stored_vars_map['BRANCH'] = branch
+properties['BRANCH'] = branch
 
 with open (outfile,'w') as f:
-    for sv in stored_vars_map:
-        f.write( sv + '=' +  stored_vars_map[sv] + '\n')
-
-
+    for sv in properties:
+        f.write( sv + '=' +  properties[sv] + '\n')
