bda74f857d540fa1cad7cc93861017c8beac3980
Anish Khanzode <akhanzode@voltdb.com>
2022-02-18 16:56:13 -0500

    ENG-22124: Wrap jetty thread pool (#1077)
    * ENG-22124: Wrap jetty thread pool
    
    * Add debug log.

--- src/frontend/org/voltdb/utils/HTTPAdminListener.java
+++ src/frontend/org/voltdb/utils/HTTPAdminListener.java
@@ -1,5 +1,5 @@
 /* This file is part of VoltDB.
- * Copyright (C) 2008-2020 VoltDB Inc.
+ * Copyright (C) 2008-2022 VoltDB Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -177,7 +177,7 @@ public class HTTPAdminListener {
         /*
          * Don't force us to look at a huge pile of threads
          */
-        final QueuedThreadPool qtp = new QueuedThreadPool(
+        final HTTPQueuedThreadPool qtp = new HTTPQueuedThreadPool(
                 poolsize,
                 1, // minimum threads
                 timeout * 1000,
---
+++ src/frontend/org/voltdb/utils/HTTPQueuedThreadPool.java
@@ -0,0 +1,48 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2022 VoltDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with VoltDB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.voltdb.utils;
+
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.voltcore.logging.VoltLogger;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * This is a wrapper so that jetty serving exceptions are not printed, response to json apis do get back with client response
+ * so this should not affect response failures.
+ */
+public class HTTPQueuedThreadPool extends QueuedThreadPool {
+
+    private static final VoltLogger m_log = new VoltLogger("HOST");
+
+    public HTTPQueuedThreadPool(int maxThreads, int minThreads, int idleTimeout, BlockingQueue<Runnable> queue) {
+        super(maxThreads, minThreads, idleTimeout, queue);
+    }
+
+    @Override
+    protected void runJob(Runnable job) {
+        try {
+            super.runJob(job);
+        } catch (Throwable t) {
+            // Swallow it any procedure exceptions do get to response from client response.
+            if (m_log.isDebugEnabled()) {
+                m_log.debug("Failed to process http request.", t);
+            }
+        }
+    }
+}
+
