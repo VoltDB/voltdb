063da8f17a8fdd6fdda43446887bbce114d4ba0c
dave porter <57912065+dave-voltdb@users.noreply.github.com>
2022-05-16 09:23:07 -0400

    ENG-22457, unit tests for CompoundProcSummaryStatisticsTable (#1304)

---
+++ tests/frontend/org/voltdb/stats/procedure/TestAggregationOfCompoundProcStats.java
@@ -0,0 +1,234 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2022 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.voltdb.stats.procedure;
+
+import com.google.common.base.Suppliers;
+import org.junit.Test;
+import org.voltdb.TableShorthand;
+import org.voltdb.MockRow;
+import org.voltdb.VoltTable;
+import org.voltdb.assertions.VoltTablesAssertion;
+
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.entry;
+
+public class TestAggregationOfCompoundProcStats {
+
+    // The basic input to CompoundProcSummaryStatisticsTable is a
+    // VoltTableRow in the form produced by ProcedureStatsCollector.
+
+    VoltTable inputSchemaTemplate = TableShorthand.tableFromShorthand(
+            "PROCEDUREDETAIL(" +
+            "TIMESTAMP:BIGINT," +
+            "HOST_ID:INTEGER," +
+            "HOSTNAME:STRING," +
+            "SITE_ID:INTEGER," +                   // n/a
+            "PARTITION_ID:INTEGER," +              // n/a
+            "PROCEDURE:STRING," +
+            "STATEMENT:STRING," +                  // n/a
+            "INVOCATIONS:BIGINT," +
+            "TIMED_INVOCATIONS:BIGINT," +          // don't care
+            "MIN_EXECUTION_TIME:BIGINT," +
+            "MAX_EXECUTION_TIME:BIGINT," +
+            "AVG_EXECUTION_TIME:BIGINT," +
+            "MIN_RESULT_SIZE:INTEGER," +           // don't care
+            "MAX_RESULT_SIZE:INTEGER," +           // don't care
+            "AVG_RESULT_SIZE:INTEGER," +           // don't care
+            "MIN_PARAMETER_SET_SIZE:INTEGER," +    // don't care
+            "MAX_PARAMETER_SET_SIZE:INTEGER," +    // don't care
+            "AVG_PARAMETER_SET_SIZE:INTEGER," +    // don't care
+            "ABORTS:BIGINT," +
+            "FAILURES:BIGINT," +
+            "TRANSACTIONAL:TINYINT," +             // 0 for non-transactional
+            "COMPOUND:TINYINT" +                   // 1 for compound
+            ")"
+    );
+
+    // Output is only rows with COMPOUND = 1, subset of
+    // columns, no rollup of any data
+
+    VoltTable outputSchemaTemplate = TableShorthand.tableFromShorthand(
+            "PROCEDUREDETAIL(" +
+            "TIMESTAMP:BIGINT," +
+            "HOST_ID:INTEGER," +
+            "HOSTNAME:STRING," +
+            "PROCEDURE_NAME:STRING," +
+            "INVOCATIONS:BIGINT," +
+            "AVG_ELAPSED:BIGINT," +
+            "MIN_ELAPSED:BIGINT," +
+            "MAX_ELAPSED:BIGINT," +
+            "ABORTS:BIGINT," +
+            "FAILURES:BIGINT" +
+            ")"
+      );
+
+    MockRow compoundProcRow1 = MockRow.of(
+            inputSchemaTemplate,
+            11111L, // timestamp
+            1, // host id
+            "volt-node-1", // hostname
+            2,
+            34,
+            "org.voltdb.stats.procedure.CompoundProcTestCase", // procedure
+            "<ALL>",
+            2345L, // invocations
+            100L,
+            TimeUnit.MILLISECONDS.toNanos(22), // min time
+            TimeUnit.MILLISECONDS.toNanos(123), // max time
+            TimeUnit.MILLISECONDS.toNanos(42), // avg time
+            32,
+            123452,
+            433,
+            12,
+            15,
+            14,
+            3421L, // aborts
+            567L, // failures
+            (byte) 0,
+            (byte) 1 // compound
+    );
+
+    MockRow expectedOutput1 = MockRow.of(
+            outputSchemaTemplate,
+            11111L,
+            1,
+            "volt-node-1",
+            "CompoundProcTestCase",
+            2345L,
+            TimeUnit.MILLISECONDS.toNanos(42),
+            TimeUnit.MILLISECONDS.toNanos(22),
+            TimeUnit.MILLISECONDS.toNanos(123),
+            3421L,
+            567L
+     );
+
+    MockRow compoundProcRow2 = MockRow.of(
+            inputSchemaTemplate,
+            22222L, // timestamp
+            2, // host id
+            "volt-node-2", // hostname
+            2,
+            34,
+            "org.voltdb.stats.procedure.CompoundProcTestCase", // procedure
+            "<ALL>",
+            2345L, // invocations
+            100L,
+            TimeUnit.MILLISECONDS.toNanos(22), // min time
+            TimeUnit.MILLISECONDS.toNanos(123), // max time
+            TimeUnit.MILLISECONDS.toNanos(42), // avg time
+            32,
+            123452,
+            433,
+            12,
+            15,
+            14,
+            3421L, // aborts
+            567L, // failures
+            (byte) 0,
+            (byte) 1 // compound
+    );
+
+    MockRow expectedOutput2 = MockRow.of(
+            outputSchemaTemplate,
+            22222L,
+            2,
+            "volt-node-2",
+            "CompoundProcTestCase",
+            2345L,
+            TimeUnit.MILLISECONDS.toNanos(42),
+            TimeUnit.MILLISECONDS.toNanos(22),
+            TimeUnit.MILLISECONDS.toNanos(123),
+            3421L,
+            567L
+    );
+
+    MockRow transactionRow = MockRow.of(
+            inputSchemaTemplate,
+            42L, // timestamp
+            2, // host id
+            "volt-node-2", // hostname
+            2,
+            34,
+            "org.voltdb.stats.procedure.PlainOldProcedure", // procedure
+            "<ALL>",
+            2345L, // invocations
+            100L,
+            TimeUnit.MILLISECONDS.toNanos(22), // min time
+            TimeUnit.MILLISECONDS.toNanos(123), // max time
+            TimeUnit.MILLISECONDS.toNanos(42), // avg time
+            32,
+            123452,
+            433,
+            12,
+            15,
+            14,
+            3421L, // aborts
+            567L, // failures
+            (byte) 1, // transactional
+            (byte) 0
+    );
+
+
+    @Test
+    public void shouldAcceptEmptyTable() {
+        // Given
+        ProcedureDetailAggregator aggregator = new ProcedureDetailAggregator(
+                new ReadOnlyProcedureInformation(
+                        Suppliers.ofInstance(Collections.emptyMap())
+                )
+        );
+
+        // When
+        VoltTable[] voltTables = aggregator.aggregateCompoundProcByHost(new VoltTable[] { inputSchemaTemplate });
+
+        // Then
+        VoltTablesAssertion.assertThat(voltTables)
+                           .onlyResponse()
+                           .isEmpty();
+    }
+
+    @Test
+    public void shouldProcessCompoundProcsOnly() {
+        // Given
+        ProcedureDetailAggregator aggregator = new ProcedureDetailAggregator(
+                new ReadOnlyProcedureInformation(
+                        Suppliers.ofInstance(Collections.emptyMap())
+                )
+        );
+
+        VoltTable voltTable = inputSchemaTemplate.clone(0);
+        compoundProcRow1.insertInto(voltTable); // will be accepted
+        compoundProcRow2.insertInto(voltTable); // will be accepted
+        transactionRow.insertInto(voltTable); // will be rejected
+
+        // When
+        VoltTable[] voltTables = aggregator.aggregateCompoundProcByHost(new VoltTable[] { voltTable });
+
+        // Then
+        VoltTablesAssertion.assertThat(voltTables)
+                           .onlyResponse()
+                           .containsExactly(expectedOutput1, expectedOutput2);
+    }
+ }
---
+++ tests/frontend/org/voltdb/stats/procedure/TestCompoundProcSummaryStatisticsTable.java
@@ -0,0 +1,220 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2022 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.voltdb.stats.procedure;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import org.voltdb.VoltTable;
+
+public class TestCompoundProcSummaryStatisticsTable {
+
+    @Rule
+    public final TestName testname = new TestName();
+
+    // The basic input to CompoundProcSummaryStatisticsTable is a
+    // VoltTableRow in the form produced by ProcedureStatsCollector.
+    //
+    //  TIMESTAMP               (VoltType.BIGINT),
+    //  HOST_ID                 (VoltType.INTEGER),  don't care
+    //  HOSTNAME                (VoltType.STRING),   don't care
+    //  SITE_ID                 (VoltType.INTEGER),  n/a
+    //  PARTITION_ID            (VoltType.INTEGER),  n/a
+    //  PROCEDURE               (VoltType.STRING),
+    //  STATEMENT               (VoltType.STRING),   n/a
+    //  INVOCATIONS             (VoltType.BIGINT),
+    //  TIMED_INVOCATIONS       (VoltType.BIGINT),   don't care
+    //  MIN_EXECUTION_TIME      (VoltType.BIGINT),
+    //  MAX_EXECUTION_TIME      (VoltType.BIGINT),
+    //  AVG_EXECUTION_TIME      (VoltType.BIGINT),
+    //  MIN_RESULT_SIZE         (VoltType.INTEGER),  don't care
+    //  MAX_RESULT_SIZE         (VoltType.INTEGER),  don't care
+    //  AVG_RESULT_SIZE         (VoltType.INTEGER),  don't care
+    //  MIN_PARAMETER_SET_SIZE  (VoltType.INTEGER),  don't care
+    //  MAX_PARAMETER_SET_SIZE  (VoltType.INTEGER),  don't care
+    //  AVG_PARAMETER_SET_SIZE  (VoltType.INTEGER),  don't care
+    //  ABORTS                  (VoltType.BIGINT),
+    //  FAILURES                (VoltType.BIGINT),
+    //  TRANSACTIONAL           (VoltType.TINYINT),  always 0 for non-transactional
+    //  COMPOUND                (VoltType.TINYINT);  always 1 for compound
+    //
+    // n/a = not applicable to compound-proc case
+    // don't care = ignored by compound-proc summary statistics
+    //
+    // The 8 columns of interest are extracted from the VoltTableRow
+    // and passed as separate arguments to the updateTable method.
+    // For ease of coding this test, we skip dealing with VoltTables
+    // and use a private row structure.
+    //
+    // The result has essentially the same columns. We use the
+    // same structure to hold expected values. Counts are accumulated
+    // across all rows with the same procedure name; the average
+    // is suitable computed based on the average and invocation
+    // count from each such row.
+    //
+    static class Row {
+        long timestamp;
+        String procedure; // input: class; result: short name
+        long invocations;
+        long avg, min, max;
+        long aborts, failures;
+
+        Row(long timestamp, String procedure, long invocations,
+            long avg, long min, long max,
+            long aborts, long failures) {
+            this.timestamp = timestamp;
+            this.procedure = procedure;
+            this.invocations = invocations;
+            this.avg = avg;
+            this.min = min;
+            this.max = max;
+            this.aborts = aborts;
+            this.failures = failures;
+        }
+    }
+
+    // push rows from data in to the table.
+    void loadData(CompoundProcSummaryStatisticsTable dut, Row[] inputTable) {
+        for (Row in : inputTable) {
+            dut.updateTable(in.timestamp, in.procedure, in.invocations,
+                            in.min, in.max, in.avg, in.failures, in.aborts);
+        }
+    }
+
+
+    // validate contents of sorted dut vs. expectation of ResultRow[]
+    void assertEquals(CompoundProcSummaryStatisticsTable dut, Row[] data) {
+        VoltTable actual = dut.getSortedTable();
+        String test = testname.getMethodName();
+        Assert.assertEquals(test + " has wrong number of result rows in test.",
+                            actual.getRowCount(), data.length);
+        for (Row row : data) {
+            assertTrue(actual.advanceRow());
+            System.out.printf("%s: validating row %d\n", test, actual.getActiveRowIndex());
+            Assert.assertEquals(row.timestamp, actual.getLong("TIMESTAMP"));
+            Assert.assertEquals(row.procedure, actual.getString("PROCEDURE_NAME"));
+            Assert.assertEquals(row.invocations, actual.getLong("INVOCATIONS"));
+            Assert.assertEquals(row.avg, actual.getLong("AVG_ELAPSED"));
+            Assert.assertEquals(row.min, actual.getLong("MIN_ELAPSED"));
+            Assert.assertEquals(row.max, actual.getLong("MAX_ELAPSED"));
+            Assert.assertEquals(row.aborts, actual.getLong("ABORTS"));
+            Assert.assertEquals(row.failures, actual.getLong("FAILURES"));
+        }
+    }
+
+    @Test
+    public void testBaseCase() {
+        // Given
+        // validate round-trip of one row.
+        Row[] input = {
+            new Row(1371587140278L, "org.banana.A", 100L, 1L, 2L, 3L, 4L, 5L)
+        };
+        Row[] result = {
+            new Row(1371587140278L, "A", 100L, 1L, 2L, 3L, 4L, 5L)
+        };
+
+        // When
+        CompoundProcSummaryStatisticsTable dut = new CompoundProcSummaryStatisticsTable();
+        loadData(dut, input);
+
+        // Then
+        assertEquals(dut, result);
+    }
+
+    @Test
+    public void testAllZeros() {
+        // Given
+        // validate paranoia about an all zero row - just in case.
+        Row[] input = {
+            new Row(1371587140278L, "org.banana.B", 0L, 0L, 0L, 0L, 0L, 0L)
+        };
+
+        Row[] result = {
+            new Row(1371587140278L, "B", 0L, 0L, 0L, 0L, 0L, 0L)
+        };
+
+        // When
+        CompoundProcSummaryStatisticsTable dut = new CompoundProcSummaryStatisticsTable();
+        loadData(dut, input);
+
+        // Then
+        assertEquals(dut, result);
+    }
+
+    @Test
+    public void testMultipleProcs() {
+        // Given
+        // 2 procs, 2 partitions - make sure min,max,avg works
+        Row[] input = {
+            //                                      inv   avg  min  max  abo  err
+            new Row(1371587140278L, "org.banana.B", 100L, 4L,  2L,  5L,  17L, 18L),
+            new Row(1371587140279L, "org.banana.A", 1L,   10L, 20L, 30L, 0L,  18L),
+            new Row(1371587140280L, "org.banana.B", 100L, 2L,  1L,  3L,  17L, 18L)
+        };
+
+        Row[] result = {
+            //                           inv   avg  min  max  abo  err
+            new Row(1371587140278L, "B", 200L, 3L,  1L,  5L,  34L, 36L),
+            new Row(1371587140279L, "A", 1L,   10L, 20L, 30L, 0L,  18L)
+        };
+
+        // When
+        CompoundProcSummaryStatisticsTable dut = new CompoundProcSummaryStatisticsTable();
+        loadData(dut, input);
+
+        // Then
+        assertEquals(dut, result);
+    }
+
+    @Test
+    public void testNoOverflow() {
+        // Given
+        // paranoia about overflow when computing average of big numbers
+        long yuge = 2_000_000_000_000_000_000L; // Long.MAX_VALUE is 9_223_372_036_854_775_807
+        Row[] input = {
+            //                                      inv   avg     min  max  abo  err
+            new Row(1371587140278L, "org.banana.Y", yuge, yuge+1, 1L,  yuge+10,  0L, 0L),
+            new Row(1371587140279L, "org.banana.Y", yuge, yuge+2, 1L,  yuge+20,  0L, 0L),
+            new Row(1371587140280L, "org.banana.Y", yuge, yuge+1, 1L,  yuge+30,  0L, 0L),
+            new Row(1371587140281L, "org.banana.Y", yuge, yuge+4, 1L,  yuge+40,  0L, 0L),
+         };
+        Row[] result = {
+            //                           inv     avg   min  max      abo  err
+            new Row(1371587140278L, "Y", yuge*4, yuge, 1L,  yuge+40, 0L, 0L)
+            // average is reported as yuge, not yuge+2, because double-precision has
+            // only 15 to 16 decimal digits of significance. once your average latency
+            // has exceeded 2 years, though, I don't think you'll care about rounding.
+        };
+
+        // When
+        CompoundProcSummaryStatisticsTable dut = new CompoundProcSummaryStatisticsTable();
+        loadData(dut, input);
+
+        // Then
+        assertEquals(dut, result);
+    }
+}
